fc9dbd24f5a5221accfaee76b5e11df6d540a554
Wolfgang Reissnegger <w.reissnegger@gmx.net>
2007-03-08 13:59:14 -0800

    Changed termios to ktermios where needed. Converted to new code style.

--- drivers/serial/uartlite.c
+++ drivers/serial/uartlite.c
@@ -39,7 +39,7 @@
 enum XulRegister {
 	XUL_RX_FIFO = 0,
 	XUL_TX_FIFO = 4,
-	XUL_STATUS  = 8,
+	XUL_STATUS = 8,
 	XUL_CONTROL = 12,
 };
 
@@ -60,12 +60,15 @@ enum XulRegister {
 static inline u32 __xul_get_reg(struct uart_port *port, enum XulRegister reg)
 {
 	void __iomem *ioaddr = port->membase;
+
 	return ioread32(ioaddr + reg);
 }
 
-static inline void __xul_set_reg(struct uart_port *port, enum XulRegister reg, u32 val)
+static inline void __xul_set_reg(struct uart_port *port, enum XulRegister reg,
+				 u32 val)
 {
 	void __iomem *ioaddr = port->membase;
+
 	iowrite32(val, ioaddr + reg);
 }
 
@@ -102,7 +105,8 @@ static inline void __xul_set_control(struct uart_port *port, u32 val)
 
 static inline void _xul_enable_interrupt(struct uart_port *port)
 {
-	__xul_set_control(port, __xul_get_control(port) | XUL_CONTROL_ENABLE_INTR);
+	__xul_set_control(port,
+			  __xul_get_control(port) | XUL_CONTROL_ENABLE_INTR);
 }
 
 static inline int _xul_has_valid_data(struct uart_port *port)
@@ -117,19 +121,18 @@ static inline int _xul_is_tx_fifo_full(struct uart_port *port)
 
 static inline u8 _xul_getchar(struct uart_port *port)
 {
-	return (u8)__xul_get_rx_fifo(port);
+	return (u8) __xul_get_rx_fifo(port);
 }
 
 static inline void _xul_putchar(struct uart_port *port, int c)
 {
-	while (_xul_is_tx_fifo_full(port))
-		;
+	while (_xul_is_tx_fifo_full(port));
 	__xul_set_tx_fifo(port, c);
 }
 
 static irqreturn_t _xul_irq_handler(int irq, void *dev_id)
 {
-	struct uart_port *port = (struct uart_port *)dev_id;
+	struct uart_port *port = (struct uart_port *) dev_id;
 	struct uart_info *info = port->info;
 	struct tty_struct *tty = info->tty;
 
@@ -139,6 +142,7 @@ static irqreturn_t _xul_irq_handler(int irq, void *dev_id)
 	if (_xul_has_valid_data(port)) {
 		while (_xul_has_valid_data(port)) {
 			u8 c = _xul_getchar(port);
+
 			pr_debug("================> '%#x'\n", c);
 			tty_insert_flip_char(tty, c, TTY_NORMAL);
 		}
@@ -150,7 +154,8 @@ static irqreturn_t _xul_irq_handler(int irq, void *dev_id)
 
 static unsigned int xul_op_tx_empty(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 	return 0;
 }
 
@@ -161,13 +166,15 @@ static void xul_op_set_mctrl(struct uart_port *port, unsigned int mctrl)
 
 static unsigned int xul_op_get_mctrl(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 	return TIOCM_CAR;
 }
 
 static void xul_op_stop_tx(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static void xul_op_start_tx(struct uart_port *port)
@@ -175,7 +182,8 @@ static void xul_op_start_tx(struct uart_port *port)
 	struct uart_info *info = port->info;
 	struct circ_buf *circ = &info->xmit;
 
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 
 	while (uart_circ_chars_pending(circ)) {
 		_xul_putchar(port, circ->buf[circ->tail]);
@@ -186,27 +194,32 @@ static void xul_op_start_tx(struct uart_port *port)
 
 static void xul_op_send_xchar(struct uart_port *port, char ch)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static void xul_op_stop_rx(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static void xul_op_enable_ms(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static void xul_op_break_ctl(struct uart_port *port, int ctl)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static int xul_op_startup(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 
 	_xul_enable_interrupt(port);
 
@@ -215,22 +228,27 @@ static int xul_op_startup(struct uart_port *port)
 
 static void xul_op_shutdown(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
-static void xul_op_set_termios(struct uart_port *port, struct termios *new, struct termios *old)
+static void xul_op_set_termios(struct uart_port *port, struct ktermios *new,
+			       struct ktermios *old)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
-static void xul_op_pm(struct uart_port *port, unsigned int state, unsigned int oldstate)
+static void xul_op_pm(struct uart_port *port, unsigned int state,
+		      unsigned int oldstate)
 {
 	pr_debug("%s: Not Supported\n", __FUNCTION__);
 }
 
 static int xul_op_set_wake(struct uart_port *port, unsigned int state)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 	return 0;
 }
 
@@ -241,12 +259,14 @@ static const char *xul_op_type(struct uart_port *port)
 
 static void xul_op_release_port(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 }
 
 static int xul_op_request_port(struct uart_port *port)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 	return 0;
 }
 
@@ -275,28 +295,30 @@ static void xul_op_config_port(struct uart_port *port, int flags)
 	/* then map the region to virtual memory */
 	port->membase = ioremap_nocache(port->mapbase, REGION_SIZE);
 	if (!port->membase) {
-		printk(KERN_ERR "XUL: Cannot map new port at phys %#lx\n", port->mapbase);
+		printk(KERN_ERR "XUL: Cannot map new port at phys %#lx\n",
+		       port->mapbase);
 		err = -ENOMEM;
 		goto err_remap;
 	}
 
 /* FIXME */
 	/* finally, request the irq for the port */
- 	err = request_irq(port->irq, _xul_irq_handler, 0, "uartlite", port);
- 	if (err) {
- 		printk(KERN_ERR "XUL: Cannot acquire given irq (%d) for new port at phys %#lx\n", 
- 		       port->irq, port->mapbase); 
- 		err = -ENODEV; 
- 		goto err_irq; 
- 	} 
+	err = request_irq(port->irq, _xul_irq_handler, 0, "uartlite", port);
+	if (err) {
+		printk(KERN_ERR
+		       "XUL: Cannot acquire given irq (%d) for new port at phys %#lx\n",
+		       port->irq, port->mapbase);
+		err = -ENODEV;
+		goto err_irq;
+	}
 	return;
 
-err_irq:
+      err_irq:
 	iounmap(port->membase);
 	port->membase = NULL;
-err_remap:
+      err_remap:
 	release_mem_region(port->mapbase, REGION_SIZE);
-err:
+      err:
 	if (err < 0) {
 		/* FIXME: remove this port from the list */
 		pr_debug("%s: oops %d\n", __FUNCTION__, err);
@@ -306,54 +328,56 @@ err:
 
 static int xul_op_verify_port(struct uart_port *port, struct serial_struct *ser)
 {
-	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line, __FUNCTION__);
+	pr_debug("port @ %#lx, line %d: %s\n", port->mapbase, port->line,
+		 __FUNCTION__);
 	return 0;
 }
 
 static struct uart_ops xul_ops = {
-	.tx_empty	= xul_op_tx_empty,
-	.set_mctrl	= xul_op_set_mctrl,
-	.get_mctrl	= xul_op_get_mctrl,
-	.stop_tx	= xul_op_stop_tx,
-	.start_tx	= xul_op_start_tx,
-	.send_xchar	= xul_op_send_xchar,
-	.stop_rx	= xul_op_stop_rx,
-	.enable_ms	= xul_op_enable_ms,
-	.break_ctl	= xul_op_break_ctl,
-	.startup	= xul_op_startup,
-	.shutdown	= xul_op_shutdown,
-	.set_termios	= xul_op_set_termios,
-	.pm		= xul_op_pm,
-	.set_wake	= xul_op_set_wake,
-	.type		= xul_op_type,
-	.release_port	= xul_op_release_port,
-	.request_port	= xul_op_request_port,
-	.config_port	= xul_op_config_port,
-	.verify_port	= xul_op_verify_port,
-	.ioctl		= NULL,
+	.tx_empty = xul_op_tx_empty,
+	.set_mctrl = xul_op_set_mctrl,
+	.get_mctrl = xul_op_get_mctrl,
+	.stop_tx = xul_op_stop_tx,
+	.start_tx = xul_op_start_tx,
+	.send_xchar = xul_op_send_xchar,
+	.stop_rx = xul_op_stop_rx,
+	.enable_ms = xul_op_enable_ms,
+	.break_ctl = xul_op_break_ctl,
+	.startup = xul_op_startup,
+	.shutdown = xul_op_shutdown,
+	.set_termios = xul_op_set_termios,
+	.pm = xul_op_pm,
+	.set_wake = xul_op_set_wake,
+	.type = xul_op_type,
+	.release_port = xul_op_release_port,
+	.request_port = xul_op_request_port,
+	.config_port = xul_op_config_port,
+	.verify_port = xul_op_verify_port,
+	.ioctl = NULL,
 };
 
 /* just define the port for console. every other ports on uart lite
  * needs to be manually binded */
 static struct uart_port xul_port = {
-	.mapbase	= CONFIG_XILINX_UARTLITE_0_BASEADDR,
-	.irq		= CONFIG_XILINX_UARTLITE_0_IRQ,
-	.iotype		= UPIO_MEM32,
-	.flags		= UPF_BOOT_AUTOCONF,
-	.type		= PORT_UARTLITE,
-	.ops		= &xul_ops,
+	.mapbase = CONFIG_XILINX_UARTLITE_0_BASEADDR,
+	.irq = CONFIG_XILINX_UARTLITE_0_IRQ,
+	.iotype = UPIO_MEM32,
+	.flags = UPF_BOOT_AUTOCONF,
+	.type = PORT_UARTLITE,
+	.ops = &xul_ops,
 };
 
 #ifdef CONFIG_SERIAL_XILINX_UARTLITE_CONSOLE
 
-static void xul_console_write(struct console *console, const char *str, unsigned len)
+static void xul_console_write(struct console *console, const char *str,
+			      unsigned len)
 {
 	int i;
 	unsigned long flags;
 
 	local_irq_save(flags);
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		if (str[i] == '\n')
 			_xul_putchar(&xul_port, '\r');
 		_xul_putchar(&xul_port, str[i]);
@@ -381,17 +405,17 @@ static int __init xul_console_setup(struct console *console, char *options)
 
 static struct uart_driver xul_driver;
 static struct console xul_console = {
-	.name		= XUL_SERIAL_NAME,
-	.write		= xul_console_write,
-	.read		= xul_console_read,
-	.device		= uart_console_device,
-	.unblank	= xul_console_unblank,
-	.setup		= xul_console_setup,
-	.flags		= CON_PRINTBUFFER,
-	.index		= -1,
-	.cflag		= 0,
-	.data		= &xul_driver,
-	.next		= NULL,
+	.name = XUL_SERIAL_NAME,
+	.write = xul_console_write,
+	.read = xul_console_read,
+	.device = uart_console_device,
+	.unblank = xul_console_unblank,
+	.setup = xul_console_setup,
+	.flags = CON_PRINTBUFFER,
+	.index = -1,
+	.cflag = 0,
+	.data = &xul_driver,
+	.next = NULL,
 };
 
 static int __init xul_console_init(void)
@@ -401,6 +425,7 @@ static int __init xul_console_init(void)
 	register_console(&xul_console);
 	return 0;
 }
+
 console_initcall(xul_console_init);
 
 #define XUL_SERIAL_CONSOLE	&xul_console
@@ -409,13 +434,13 @@ console_initcall(xul_console_init);
 #endif /* CONFIG_XILINX_UARTLITE_CONSOLE */
 
 static struct uart_driver xul_driver = {
-	.owner		= THIS_MODULE,
-	.driver_name	= XUL_SERIAL_NAME,
-	.dev_name	= XUL_SERIAL_NAME,
-	.major		= XUL_SERIAL_MAJOR,
-	.minor		= XUL_SERIAL_MINORS,
-	.nr		= XUL_SERIAL_NR,
-	.cons		= XUL_SERIAL_CONSOLE,
+	.owner = THIS_MODULE,
+	.driver_name = XUL_SERIAL_NAME,
+	.dev_name = XUL_SERIAL_NAME,
+	.major = XUL_SERIAL_MAJOR,
+	.minor = XUL_SERIAL_MINORS,
+	.nr = XUL_SERIAL_NR,
+	.cons = XUL_SERIAL_CONSOLE,
 };
 
 static void xul_exit(void)
@@ -547,7 +572,7 @@ static int ulite_receive(struct uart_port *port, int stat)
 
 static int ulite_transmit(struct uart_port *port, int stat)
 {
-	struct circ_buf *xmit  = &port->info->xmit;
+	struct circ_buf *xmit = &port->info->xmit;
 
 	if (stat & ULITE_STATUS_TXFULL)
 		return 0;
@@ -563,7 +588,7 @@ static int ulite_transmit(struct uart_port *port, int stat)
 		return 0;
 
 	writeb(xmit->buf[xmit->tail], port->membase + ULITE_TX);
-	xmit->tail = (xmit->tail + 1) & (UART_XMIT_SIZE-1);
+	xmit->tail = (xmit->tail + 1) & (UART_XMIT_SIZE - 1);
 	port->icount.tx++;
 
 	/* wake up */
@@ -575,12 +600,13 @@ static int ulite_transmit(struct uart_port *port, int stat)
 
 static irqreturn_t ulite_isr(int irq, void *dev_id)
 {
-	struct uart_port *port = (struct uart_port *)dev_id;
+	struct uart_port *port = (struct uart_port *) dev_id;
 	int busy;
 
 	do {
 		int stat = readb(port->membase + ULITE_STATUS);
-		busy  = ulite_receive(port, stat);
+
+		busy = ulite_receive(port, stat);
 		busy |= ulite_transmit(port, stat);
 	} while (busy);
 
@@ -657,7 +683,7 @@ static int ulite_startup(struct uart_port *port)
 static void ulite_shutdown(struct uart_port *port)
 {
 	writeb(0, port->membase + ULITE_CONTROL);
-	readb(port->membase + ULITE_CONTROL); /* dummy */
+	readb(port->membase + ULITE_CONTROL);	/* dummy */
 	free_irq(port->irq, port);
 }
 
@@ -736,22 +762,22 @@ static int ulite_verify_port(struct uart_port *port, struct serial_struct *ser)
 }
 
 static struct uart_ops ulite_ops = {
-	.tx_empty	= ulite_tx_empty,
-	.set_mctrl	= ulite_set_mctrl,
-	.get_mctrl	= ulite_get_mctrl,
-	.stop_tx	= ulite_stop_tx,
-	.start_tx	= ulite_start_tx,
-	.stop_rx	= ulite_stop_rx,
-	.enable_ms	= ulite_enable_ms,
-	.break_ctl	= ulite_break_ctl,
-	.startup	= ulite_startup,
-	.shutdown	= ulite_shutdown,
-	.set_termios	= ulite_set_termios,
-	.type		= ulite_type,
-	.release_port	= ulite_release_port,
-	.request_port	= ulite_request_port,
-	.config_port	= ulite_config_port,
-	.verify_port	= ulite_verify_port
+	.tx_empty = ulite_tx_empty,
+	.set_mctrl = ulite_set_mctrl,
+	.get_mctrl = ulite_get_mctrl,
+	.stop_tx = ulite_stop_tx,
+	.start_tx = ulite_start_tx,
+	.stop_rx = ulite_stop_rx,
+	.enable_ms = ulite_enable_ms,
+	.break_ctl = ulite_break_ctl,
+	.startup = ulite_startup,
+	.shutdown = ulite_shutdown,
+	.set_termios = ulite_set_termios,
+	.type = ulite_type,
+	.release_port = ulite_release_port,
+	.request_port = ulite_request_port,
+	.config_port = ulite_config_port,
+	.verify_port = ulite_verify_port
 };
 
 #ifdef CONFIG_SERIAL_UARTLITE_CONSOLE
@@ -783,7 +809,8 @@ static void ulite_console_write(struct console *co, const char *s,
 
 	if (oops_in_progress) {
 		locked = spin_trylock_irqsave(&port->lock, flags);
-	} else
+	}
+	else
 		spin_lock_irqsave(&port->lock, flags);
 
 	/* save and disable interrupt */
@@ -828,13 +855,13 @@ static int __init ulite_console_setup(struct console *co, char *options)
 static struct uart_driver ulite_uart_driver;
 
 static struct console ulite_console = {
-	.name	= "ttyUL",
-	.write	= ulite_console_write,
-	.device	= uart_console_device,
-	.setup	= ulite_console_setup,
-	.flags	= CON_PRINTBUFFER,
-	.index	= -1, /* Specified on the cmdline (e.g. console=ttyUL0 ) */
-	.data	= &ulite_uart_driver,
+	.name = "ttyUL",
+	.write = ulite_console_write,
+	.device = uart_console_device,
+	.setup = ulite_console_setup,
+	.flags = CON_PRINTBUFFER,
+	.index = -1,		/* Specified on the cmdline (e.g. console=ttyUL0 ) */
+	.data = &ulite_uart_driver,
 };
 
 static int __init ulite_console_init(void)
@@ -848,14 +875,14 @@ console_initcall(ulite_console_init);
 #endif /* CONFIG_SERIAL_UARTLITE_CONSOLE */
 
 static struct uart_driver ulite_uart_driver = {
-	.owner		= THIS_MODULE,
-	.driver_name	= "uartlite",
-	.dev_name	= "ttyUL",
-	.major		= ULITE_MAJOR,
-	.minor		= ULITE_MINOR,
-	.nr		= ULITE_NR_UARTS,
+	.owner = THIS_MODULE,
+	.driver_name = "uartlite",
+	.dev_name = "ttyUL",
+	.major = ULITE_MAJOR,
+	.minor = ULITE_MINOR,
+	.nr = ULITE_NR_UARTS,
 #ifdef CONFIG_SERIAL_UARTLITE_CONSOLE
-	.cons		= &ulite_console,
+	.cons = &ulite_console,
 #endif
 };
 
@@ -880,18 +907,18 @@ static int __devinit ulite_probe(struct platform_device *pdev)
 
 	port = &ports[pdev->id];
 
-	port->fifosize	= 16;
-	port->regshift	= 2;
-	port->iotype	= UPIO_MEM;
-	port->iobase	= 1; /* mark port in use */
-	port->mapbase	= res->start;
-	port->membase	= 0;
-	port->ops	= &ulite_ops;
-	port->irq	= res2->start;
-	port->flags	= UPF_BOOT_AUTOCONF;
-	port->dev	= &pdev->dev;
-	port->type	= PORT_UNKNOWN;
-	port->line	= pdev->id;
+	port->fifosize = 16;
+	port->regshift = 2;
+	port->iotype = UPIO_MEM;
+	port->iobase = 1;	/* mark port in use */
+	port->mapbase = res->start;
+	port->membase = 0;
+	port->ops = &ulite_ops;
+	port->irq = res2->start;
+	port->flags = UPF_BOOT_AUTOCONF;
+	port->dev = &pdev->dev;
+	port->type = PORT_UNKNOWN;
+	port->line = pdev->id;
 
 	uart_add_one_port(&ulite_uart_driver, port);
 	platform_set_drvdata(pdev, port);
@@ -915,11 +942,11 @@ static int ulite_remove(struct platform_device *pdev)
 }
 
 static struct platform_driver ulite_platform_driver = {
-	.probe	= ulite_probe,
-	.remove	= ulite_remove,
-	.driver	= {
+	.probe = ulite_probe,
+	.remove = ulite_remove,
+	.driver = {
 		   .owner = THIS_MODULE,
-		   .name  = "uartlite",
+		   .name = "uartlite",
 		   },
 };
 
