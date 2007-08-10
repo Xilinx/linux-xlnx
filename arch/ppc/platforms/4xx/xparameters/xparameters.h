693632bf26732003ef1cc613bd9f68f322c78cc7
Wolfgang Reissnegger <w.reissnegger@gmx.net>
2007-08-09 20:50:29 -0700

    Removed references to ML5E (ML410/ML5) PPC440 emulation board.

--- arch/ppc/platforms/4xx/xparameters/xparameters.h
+++ arch/ppc/platforms/4xx/xparameters/xparameters.h
@@ -19,8 +19,6 @@
   #include "xparameters_xupv2p.h"
 #elif defined(CONFIG_XILINX_ML403)
   #include "xparameters_ml403.h"
-#elif defined(CONFIG_XILINX_ML5E)
-  #include "xparameters_ml5e.h"
 #elif defined(CONFIG_XILINX_ML507)
   #include "xparameters_ml507.h"
 #elif defined(CONFIG_XILINX_ML41x)
