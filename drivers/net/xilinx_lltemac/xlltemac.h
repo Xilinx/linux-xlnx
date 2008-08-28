38ebc4b6070233eae25ef725aeafd3ad690827fb
John Bonesio <john.bonesio@xilinx.com>
2008-08-28 13:11:07 -0700

    Updated the Copyright text to include the GPL on several files

--- drivers/net/xilinx_lltemac/xlltemac.h
+++ drivers/net/xilinx_lltemac/xlltemac.h
@@ -15,8 +15,16 @@
 *       INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
 *       FOR A PARTICULAR PURPOSE.
 *
-*       (c) Copyright 2005-2007 Xilinx Inc.
+*       (c) Copyright 2005-2008 Xilinx Inc.
 *       All rights reserved.
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 *
 ******************************************************************************/
 /*****************************************************************************/
