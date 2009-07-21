d6b954ca1a18ceaed211343171e6d580758202dd
Julie Zhu <julie.zhu@xilinx.com>
2009-07-21 13:05:05 -0600

    microblaze: Add definition for PCI_DMA_BUS_IS_PHYS in pci.h
    Add definition for PCI_DMA_BUS_IS_PHYS in pci.h, which enables support for the
    SCSI library. This is to support USB Mass Storage devices, such as thumb
    drives.
    
    Signed-off-by: Julie Zhu <julie.zhu@xilinx.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/pci.h
+++ arch/microblaze/include/asm/pci.h
@@ -1,1 +1,20 @@
+#ifndef __ASM_MICROBLAZE_PCI_H
+#define __ASM_MICROBLAZE_PCI_H
+#ifdef __KERNEL__
+
+/* PCI support is still under development. This file provides the definition
+ * for PCI_DMA_BUS_IS_PHYS, which enables SCSI support. This is to support
+ * USB Mass Storage devices, such as thumb drives.
+ */
+
 #include <linux/io.h>
+
+/* The PCI address space does equal the physical memory
+ * address space (no IOMMU).  The IDE and SCSI device layers use
+ * this boolean for bounce buffer decisions.
+ */
+#define PCI_DMA_BUS_IS_PHYS     (1)
+
+#endif	/* __KERNEL__ */
+#endif	/* __ASM_MICROBLAZE_PCI_H */
+
