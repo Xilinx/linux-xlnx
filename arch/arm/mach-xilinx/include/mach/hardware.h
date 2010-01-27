e4e99d5913f71b7aff8d494f84932ac5f8dc91c6
John Linn <john.linn@xilinx.com>
2010-01-27 14:48:00 -0700

    Xilinx: ARM: Corrected timer input clock frequency
    Moved back to old frequency as it's the old timer rather than the
    new one in the SCU. This commit helps the time in the system to
    be correct and the console to be more responsive (when running top).

--- arch/arm/mach-xilinx/include/mach/hardware.h
+++ arch/arm/mach-xilinx/include/mach/hardware.h
@@ -24,7 +24,7 @@
 /*
  * defines the clock rates
  */
-#define PERIPHERAL_CLOCK_RATE	12500000 		/* was 781250 */
+#define PERIPHERAL_CLOCK_RATE	781250
 #define CLOCK_TICK_RATE		PERIPHERAL_CLOCK_RATE / 32 /* prescaled in timer */
 
 /*
