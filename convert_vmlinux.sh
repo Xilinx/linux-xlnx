e49aef511a2c1f2e26d82a00f236cb06dba7cc2e
John Linn <john.linn@xilinx.com>
2010-07-01 10:10:00 -0600

    Xilinx: ARM: Changed Makefile to run convert script
    In order to make it easier for users, the script to convert
    the vmlinux file is now ran automatically.
    
    This will be removed when we start using the bootstrap loader
    with the kernel (uncompressor).

--- convert_vmlinux.sh
+++ convert_vmlinux.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo "Converting vmlinux for Xilinx"
+
 ${CROSS_COMPILE:-arm-none-linux-gnueabi-}objcopy -R .note.gnu.build-id -R .comment -R .ARM.attributes --change-addresses -0xC0000000 vmlinux vmlinux.elf
 ${CROSS_COMPILE:-arm-none-linux-gnueabi-}objcopy --gap-fill=0xFF -O binary vmlinux.elf vmlinux.bin
