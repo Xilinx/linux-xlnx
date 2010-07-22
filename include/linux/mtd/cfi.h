299ee23551e977ec1ced5f1c1f24ee2db7681051
John Linn <john.linn@xilinx.com>
2010-07-22 10:19:05 -0600

    Xilinx: ARM: MTD: fixing CFI for Numonyx CFI bug
    The M29EW devices seem to report the CFI information wrong when
    it's in 8 bit mode.
    
    There's an app note from Numonyx on this issue and there's a patch
    in the open source, but it doesn't seem to be in the mainline.
    
    This commit is based on that patch from Massimo Cirillo titled:
    buffer size adjustment for M29EW Numonyx devices.
    
    Signed-off-by: John Linn <john.linn@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/mtd/cfi.h
+++ include/linux/mtd/cfi.h
@@ -371,6 +371,7 @@ struct cfi_fixup {
 #define CFI_MFR_HYUNDAI		0x00AD
 #define CFI_MFR_INTEL		0x0089
 #define CFI_MFR_MACRONIX	0x00C2
+#define CFI_MFR_ISSI		0x009d
 #define CFI_MFR_NEC		0x0010
 #define CFI_MFR_PMC		0x009D
 #define CFI_MFR_SAMSUNG		0x00EC
