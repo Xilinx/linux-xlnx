00bf29b37f054cd798329af619c5b069233351e3
Naveen Mamindlapalli <naveenm@xhd-epdswlin32re1.(none)>
2011-04-12 20:37:50 +0530

    Xilinx: Disable Auto Suspend for USB hub driver
    Commented Auto suspend for USB hub devices since the USB OTG
    support depends on CONFIG_PM_RUNTIME which suspends hub until
    enabled using sysfs.
    
    Signed-off-by: Naveen Mamindlapalli <naveenm@xilinx.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1254,7 +1254,11 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	hdev = interface_to_usbdev(intf);
 
 	/* Hubs have proper suspend/resume support */
+#ifdef CONFIG_USB_XUSBPS_OTG
+	usb_disable_autosuspend(hdev);
+#else
 	usb_enable_autosuspend(hdev);
+#endif
 
 	if (hdev->level == MAX_TOPO_LEVEL) {
 		dev_err(&intf->dev,
