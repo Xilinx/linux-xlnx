4ada2f29c3f02a276338f525761d4e3bb9c73487
John Linn <john.linn@xilinx.com>
2011-06-21 15:42:00 -0600

    Xilinx: ARM: Fixing SMP for 3.0
    After moving to kernel version 3.0, the 2nd processor was not
    booting. It's now hanging so it still needs work.

--- arch/arm/mach-xilinx/smp.c
+++ arch/arm/mach-xilinx/smp.c
@@ -169,6 +169,8 @@ void __init smp_init_cpus(void)
 
 	for (i = 0; i < ncores; i++)
 		set_cpu_possible(i, true);
+
+	set_smp_cross_call(gic_raise_softirq);
 }
 
 void __init platform_smp_prepare_cpus(unsigned int max_cpus)
