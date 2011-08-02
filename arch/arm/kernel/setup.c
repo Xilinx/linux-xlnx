f3aee56019588d7125ca9365ce2cdb0594be69f9
Michal Simek <monstr@monstr.eu>
2011-08-02 16:47:43 +0200

    zynq: Use compiled-in DTB as we do it for Microblaze
    This is easier way how to include DTB to all image formats.
    Linux just take correct pointer and we don't need
    to care about atags.
    
    It will also simplify handling in our scripts.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>
    
    zynq: boot external dtb by default
    
    Boot the built-in dtb if external dtb address is not provided.
    
    DTB boot order:
     * External dtb
     * built-in dtb
    
    Signed-off-by: Jason Wu <jason.wu@petalogix.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -940,6 +940,11 @@ static int __init meminfo_cmp(const void *_a, const void *_b)
 void __init setup_arch(char **cmdline_p)
 {
 	struct machine_desc *mdesc;
+#ifdef CONFIG_ARCH_ZYNQ
+	extern u32 _fdt_start[];
+	 if (!__atags_pointer)
+		__atags_pointer = virt_to_phys(_fdt_start);
+#endif
 
 	setup_processor();
 	mdesc = setup_machine_fdt(__atags_pointer);
