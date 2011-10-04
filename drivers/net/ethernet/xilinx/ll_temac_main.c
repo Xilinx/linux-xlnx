a24d123fbb6e2c6881582ee3b144f76136d63421
Michal Simek <monstr@monstr.eu>
2011-10-04 13:58:35 +0200

    net: ll_temac: Fix mdio initialization
    Current driver required to have phy-node directly in
    the driver. After this fixed we can use standard structure.
    
    DTS fragment:
    
    phy-handle = <&phy0>;
    mdio {
    	#address-cells = <1>;
    	#size-cells = <0>;
    	phy0: phy@7 {
    		compatible = "marvell,88e1111";
    		device_type = "ethernet-phy";
    		reg = <7>;
    	} ;
    } ;
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>
    CC: Anirudha Sarangi <anirudh@xilinx.com>
    CC: John Linn <John.Linn@xilinx.com>
    CC: Grant Likely <grant.likely@secretlab.ca>
    CC: Rob Herring <rob.herring@calxeda.com>
    CC: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/xilinx/ll_temac_main.c
+++ drivers/net/ethernet/xilinx/ll_temac_main.c
@@ -1084,13 +1084,14 @@ static int temac_of_probe(struct platform_device *op)
 	}
 	temac_init_mac_address(ndev, addr);
 
-	rc = temac_mdio_setup(lp, op->dev.of_node);
-	if (rc)
-		dev_warn(&op->dev, "error registering MDIO bus\n");
-
 	lp->phy_node = of_parse_phandle(op->dev.of_node, "phy-handle", 0);
-	if (lp->phy_node)
+	if (lp->phy_node) {
 		dev_dbg(lp->dev, "using PHY node %pOF (%p)\n", np, np);
+
+		rc = temac_mdio_setup(lp, op->dev.of_node);
+		if (rc)
+			dev_warn(&op->dev, "error registering MDIO bus\n");
+	}
 
 	/* Add the device attributes */
 	rc = sysfs_create_group(&lp->dev->kobj, &temac_attr_group);
