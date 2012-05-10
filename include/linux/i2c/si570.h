9b0b671df4225bea343761c0753bff66713050cd
Pallav Joshi <pallav.joshi@xilinx.com>
2012-05-10 18:24:14 +0530

    Xilinx: ARM: I2C: SI570: driver update
    Abstracted sysfs function hooks, created new wrapper functions and exported
    them so they can be called from elsehwere in the kernel.  Used by xylon
    framebuffer driver.
    
    Signed-off-by: Davor Joja <davor.joja@logicbricks.com>

--- include/linux/i2c/si570.h
+++ include/linux/i2c/si570.h
@@ -21,4 +21,9 @@ struct si570_platform_data {
 	unsigned long initial_fout;	/* Requested initial frequency */
 };
 
+int get_frequency_si570(struct device *dev, unsigned long *freq);
+int set_frequency_si570(struct device *dev, unsigned long freq);
+void reset_si570(struct device *dev, int id);
+struct i2c_client *get_i2c_client_si570(void);
+
 #endif /* __LINUX_SI570_H */
