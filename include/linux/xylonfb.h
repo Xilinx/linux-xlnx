00ef893dd1d0932b53aba5ea71cbb4205e4a37fd
Pallav Joshi <pallav.joshi@xilinx.com>
2012-05-10 18:26:47 +0530

    Xilinx: ARM: Xylon frame driver rearchiteched
    - Added support for si570 initialization for pixel clk
    - Added support for 16bpp framebuffer.
    - Added more video modes
    - directory structure also changed.
    
    Signed-off-by: Davor Joja <davor.joja@logicbricks.com>

--- include/linux/xylonfb.h
+++ include/linux/xylonfb.h
@@ -4,14 +4,14 @@
  * Author: Xylon d.o.o.
  * e-mail: davor.joja@logicbricks.com
  *
- * 2011 (c) Xylon d.o.o.
+ * 2012 (c) Xylon d.o.o.
  *
  * This file is licensed under the terms of the GNU General Public License
  * version 2.  This program is licensed "as is" without any warranty of any
  * kind, whether express or implied.
  */
 
-#ifndef	__XYLON_FB_H__
+#ifndef __XYLON_FB_H__
 #define __XYLON_FB_H__
 
 
