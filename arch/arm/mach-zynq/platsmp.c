793f40d80dcc47e378c89be62a57a45fecafbed7
John Linn <john.linn@xilinx.com>
2012-05-22 16:03:17 -0700

    Xilinx: ARM: BSP: clean up of SMP code
    Code was incorporated into common SMP code so that
    some minor clean up to remove un-needed code and make
    it easier when code is merged upstream.

--- arch/arm/mach-zynq/platsmp.c
+++ arch/arm/mach-zynq/platsmp.c
@@ -117,14 +117,5 @@ void __init smp_init_cpus(void)
 
 void __init platform_smp_prepare_cpus(unsigned int max_cpus)
 {
-	int i;
-
-	/*
-	 * Initialise the present map, which describes the set of CPUs
-	 * actually populated at the present time.
-	 */
-	for (i = 0; i < max_cpus; i++)
-		set_cpu_present(i, true);
-
 	scu_enable(SCU_PERIPH_BASE);
 }
