d8a40df571e899f983fbde7650ebc02a9bb0174d
Michal Simek <monstr@monstr.eu>
2012-05-25 13:13:46 +0200

    Xilinx: ARM: Xylon frame driver rearchiteched
    - Added support for si570 initialization for pixel clk
    - Added support for 16bpp framebuffer.
    - Added more video modes
    - directory structure also changed.
    
    Signed-off-by: Davor Joja <davor.joja@logicbricks.com>

---
+++ drivers/video/xylon/xylonfb/xylonfb-pixclk.c
@@ -0,0 +1,164 @@
+/*
+ * Xylon logiCVC frame buffer driver pixel clock generation
+ *
+ * Author: Xylon d.o.o.
+ * e-mail: davor.joja@logicbricks.com
+ *
+ * 2012 (c) Xylon d.o.o.
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2.  This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+
+
+/*
+ * This file implements HW dependent functionality for controlling pixel clock
+ * generation on various HW platforms.
+ */
+
+
+#include <linux/fb.h>
+
+
+#define HW_PIXEL_CLOCK_CHANGE_SUPPORTED 1
+
+
+#if defined(CONFIG_FB_XYLON_TEST)
+
+#define HW_PIXEL_CLOCK_CHANGE_SUPPORTED 0
+int pixclk_set(struct fb_info *fbi)
+{
+	printk(KERN_INFO
+		"Changing of pixel clock for %s on platform TEST not supported\n",
+		fbi->fix.id);
+
+	return -EPERM;
+}
+
+#elif defined(CONFIG_FB_XYLON_ZYNQ_PS_PIXCLK)
+
+int pixclk_set(struct fb_info *fbi)
+{
+	unsigned long pllclk, sysclk, pixclk;
+	unsigned long div, delta, delta_dec, delta_inc;
+	void *slcr_regs, *clk_regs, *rst_reg;
+
+	/* all clock values are in kHz */
+	pllclk = 1000000;
+	sysclk = 100000;
+	pixclk = PICOS2KHZ(fbi->var.pixclock);
+
+	slcr_regs = ioremap_nocache(0xF8000004, 8);
+	if (!slcr_regs) {
+		printk(KERN_ERR
+			"Error mapping SLCR\n");
+		return -EBUSY;
+	}
+	clk_regs = ioremap_nocache(0xF8000170, 32);
+	if (!clk_regs) {
+		printk(KERN_ERR
+			"Error setting xylonfb pixelclock\n");
+		iounmap(slcr_regs);
+		return -EBUSY;
+	}
+	rst_reg = ioremap_nocache(0xF8000240, 4);
+	if (!rst_reg) {
+		printk(KERN_ERR
+			"Error setting xylonfb pixelclock\n");
+		iounmap(clk_regs);
+		iounmap(slcr_regs);
+		return -EBUSY;
+	}
+
+	/* unlock register access */
+	writel(0xDF0D, (slcr_regs+4));
+//	/* calculate system clock divisor */
+//	div = pllclk / sysclk;
+//	/* prepare for register writting */
+//	div = (div + 0x1000) << 8;
+//	/* set system clock */
+//	writel(div, clk_regs);
+	/* calculate video clock divisor */
+	div = pllclk / pixclk;
+	delta = (pllclk / div) - pixclk;
+	if (delta != 0) {
+		delta_inc = pixclk - (pllclk / (div+1));
+		delta_dec = (pllclk / (div-1)) - pixclk;
+		if (delta < delta_inc) {
+			if (delta > delta_dec)
+				div--;
+			//else
+			//	div = div;
+		} else {
+			if (delta > delta_dec) {
+				if (delta_inc > delta_dec)
+					div--;
+				else
+					div++;
+			} else {
+				div++;
+			}
+		}
+	}
+	/* prepare for register writting */
+	div = (div + 0x1000) << 8;
+	/* set video clock */
+	writel(div, (clk_regs+0x10));
+//	/* reset FPGA */
+//	writel(0, rst_reg);
+//	writel(0x1, rst_reg);
+//	writel(0, rst_reg);
+	/* lock register access */
+	writel(0x767B, slcr_regs);
+
+	iounmap(rst_reg);
+	iounmap(clk_regs);
+	iounmap(slcr_regs);
+
+	return 0;
+}
+
+#elif defined(CONFIG_FB_XYLON_ZC702_PIXCLK)
+
+#include <linux/i2c/si570.h>
+
+int pixclk_set(struct fb_info *fbi)
+{
+	struct i2c_client *si570_client;
+	unsigned long pixclk;
+
+	pixclk = PICOS2KHZ(fbi->var.pixclock) * 1000;
+
+	si570_client = get_i2c_client_si570();
+	if (si570_client)
+		return set_frequency_si570(&si570_client->dev, pixclk);
+	else
+		return -EPERM;
+}
+
+#else
+
+#if defined (HW_PIXEL_CLOCK_CHANGE_SUPPORTED)
+#undef HW_PIXEL_CLOCK_CHANGE_SUPPORTED
+#endif
+#define HW_PIXEL_CLOCK_CHANGE_SUPPORTED 0
+int pixclk_set(struct fb_info *fbi)
+{
+	printk(KERN_INFO "Changing of pixel clock for %s not supported\n",
+		fbi->fix.id);
+
+	return -EPERM;
+}
+
+#endif
+
+
+inline int pixclk_change(struct fb_info *fbi)
+{
+#if HW_PIXEL_CLOCK_CHANGE_SUPPORTED == 0
+	return 0;
+#elif HW_PIXEL_CLOCK_CHANGE_SUPPORTED == 1
+	return 1;
+#endif
+}
