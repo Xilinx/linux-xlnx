ca22466423b2379d1c9b798fb7643d10b355dd56
Naveen Mamindlapalli <naveenm@xilinx.com>
2012-07-09 20:31:41 +0530

    Microblaze: Added support for AXI PCIE MSI.
    Added support for AXI PCIE MSI.

--- arch/microblaze/include/asm/irq.h
+++ arch/microblaze/include/asm/irq.h
@@ -9,14 +9,21 @@
 #ifndef _ASM_MICROBLAZE_IRQ_H
 #define _ASM_MICROBLAZE_IRQ_H
 
-
 /*
  * Linux IRQ# is currently offset by one to map to the hardware
  * irq number. So hardware IRQ0 maps to Linux irq 1.
  */
 #define NO_IRQ_OFFSET	1
 #define IRQ_OFFSET	NO_IRQ_OFFSET
+/* AXI PCIe MSI support */
+#if defined(CONFIG_XILINX_AXIPCIE) && defined(CONFIG_PCI_MSI)
+#define IRQ_XILINX_MSI_0	128
+#define XILINX_NUM_MSI_IRQS	32
+#define NR_IRQS		(32 + IRQ_XILINX_MSI_0 + IRQ_OFFSET)
+#else
 #define NR_IRQS		(32 + IRQ_OFFSET)
+#endif
+
 #include <asm-generic/irq.h>
 
 /* This type is the placeholder for a hardware interrupt number. It has to
