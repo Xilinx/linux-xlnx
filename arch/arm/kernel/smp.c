2457121ef1638929778ff8af13a26e0d8c71aa65
Soren Brinkmann <soren.brinkmann@xilinx.com>
2012-08-23 00:53:29 -0700

    Xilinx: ARM: Skip delay calibration in hotplug path
    Changing the CPU frequency while one core is offline
    taints the offline CPU's loops per jiffy value.
    With this patch delay calibration is skipped when returning
    from hotplug and the correct lpj value from the master CPU
    is used.
    
    This change is the same as commit
    a279561879a795966336fb78186ee7435b6bb050 in
    https://github.com/jcrigby/linaro-ci-kernels.git.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -223,6 +223,9 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 {
 	struct mm_struct *mm = &init_mm;
 	unsigned int cpu = smp_processor_id();
+#ifdef CONFIG_ARCH_ZYNQ
+	static bool booted;
+#endif
 
 	/*
 	 * All kernel threads share the same mm context; grab a
@@ -248,7 +251,13 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 
 	notify_cpu_starting(cpu);
 
+#ifdef CONFIG_ARCH_ZYNQ
+	if (!booted)
+		calibrate_delay();
+	booted = true;
+#else
 	calibrate_delay();
+#endif
 
 	smp_store_cpu_info(cpu);
 
