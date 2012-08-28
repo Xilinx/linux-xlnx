ddd3fd22f5481a63dd614d8265fc9c2db9a26e18
John Linn <john.linn@xilinx.com>
2012-08-28 12:56:12 -0700

    Xilinx: ARM: BSP: update DC4 (XM013) SPI eeprom
    Move to slave select 2 and use a conservative speed
    to help get more consistent results.
    
    Signed-off-by: John Linn <john.linn@xilinx.com>

--- arch/arm/mach-zynq/board_zc770_xm013.c
+++ arch/arm/mach-zynq/board_zc770_xm013.c
@@ -54,9 +54,9 @@ static struct spi_eeprom at25640_0 = {
 static struct spi_board_info spi_devs[] __initdata = {
         {
                 .modalias = "at25",
-                .max_speed_hz = 10000000,
+                .max_speed_hz = 1000000,
                 .bus_num = 0,
-                .chip_select = 1,
+                .chip_select = 2,
                 .platform_data = &at25640_0,
         },
 };
