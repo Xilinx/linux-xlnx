54242f9bbfba7e82f7d93693d2cce20505db83ba
Michal Simek <monstr@monstr.eu>
2012-10-08 15:48:11 +0200

    powerpc: Remove raw format
    It is unused because there were no targets for it.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/powerpc/boot/io.h
+++ arch/powerpc/boot/io.h
@@ -99,11 +99,4 @@ static inline void barrier(void)
 	asm volatile("" : : : "memory");
 }
 
-static inline void disable_irq(void)
-{
-	int dummy;
-	asm volatile("mfmsr %0; rlwinm %0, %0, 0, ~(1<<15); mtmsr %0" :
-	             "=r" (dummy) : : "memory");
-}
-
 #endif /* _IO_H */
