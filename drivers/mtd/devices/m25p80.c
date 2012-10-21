b17660676d7ff2a3014cfed240d7ea967185c329
Michal Simek <monstr@monstr.eu>
2012-10-21 16:42:23 +0200

    mtd: m25p80: Move n25q128 to different location
    Because of unification.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -684,6 +684,8 @@ static const struct spi_device_id m25p_ids[] = {
 	/* Micron */
 	{ "n25q128",  INFO(0x20ba18, 0, 64 * 1024, 256, 0) },
 	{ "n25q256a", INFO(0x20ba19, 0, 64 * 1024, 512, SECT_4K) },
+	/* Numonyx flash n25q128 - FIXME check the name */
+	{ "n25q128",   INFO(0x20bb18, 0, 64 * 1024, 256, 0) },
 
 	/* Spansion -- single (large) sector size only, at least
 	 * for the chips listed here (without boot sectors).
@@ -730,9 +732,6 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "m25p32",  INFO(0x202016,  0,  64 * 1024,  64, 0) },
 	{ "m25p64",  INFO(0x202017,  0,  64 * 1024, 128, 0) },
 	{ "m25p128", INFO(0x202018,  0, 256 * 1024,  64, 0) },
-
-	/* Numonyx flash n25q128 */
-	{ "n25q128",   INFO(0x20bb18,  0,  64 * 1024, 256, 0) },
 
 	{ "m25p05-nonjedec",  INFO(0, 0,  32 * 1024,   2, 0) },
 	{ "m25p10-nonjedec",  INFO(0, 0,  32 * 1024,   4, 0) },
