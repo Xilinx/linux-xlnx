ca50cc2453b933c5ab7e43480923a7fda90a2b3b
Michal Simek <monstr@monstr.eu>
2012-10-22 10:42:43 +0200

    arm: zynq: Use ifdef instead of if
    Just commit fixup.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/arm/mach-zynq/platsmp.c
+++ arch/arm/mach-zynq/platsmp.c
@@ -191,7 +191,7 @@ void __init platform_smp_prepare_cpus(unsigned int max_cpus)
 	 * Remap the first three addresses at zero which are used
 	 * for 32bit long jump for SMP. Look at zynq_cpu1_start()
 	 */
-#if CONFIG_PHYS_OFFSET
+#ifdef CONFIG_PHYS_OFFSET
 	zero = ioremap(0, 12);
 	if (!zero) {
 		printk(KERN_WARNING
