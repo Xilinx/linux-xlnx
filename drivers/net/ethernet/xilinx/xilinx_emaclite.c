11de0acaa1d57e95db021383ec883d02eece5e35
Jason Wu <jason.wu@petalogix.com>
2012-11-01 20:15:57 +1000

    net: ethernet: emaclite: Fix of_irq_to_resource compilation error
    Include of_irq.h header to fix implicit declaration of function
    'of_irq_to_resource' compilation error when target machine is Zynq
    
    Signed-off-by: Jason Wu <huanyu@xilinx.com>

--- drivers/net/ethernet/xilinx/xilinx_emaclite.c
+++ drivers/net/ethernet/xilinx/xilinx_emaclite.c
@@ -22,6 +22,7 @@
 #include <linux/slab.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
+#include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <linux/of_mdio.h>
 #include <linux/of_net.h>
