37d70428c1585b59383692dd5f0ad6f03e60060c
Soren Brinkmann <soren.brinkmann@xilinx.com>
2012-11-29 09:47:23 -0800

    gpio: xilinx_gpiops: Sort #includes alphabetically
    Just sorting for easier maintenance.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

--- drivers/gpio/gpio-xilinxps.c
+++ drivers/gpio/gpio-xilinxps.c
@@ -13,20 +13,20 @@
  * Ave, Cambridge, MA 02139, USA.
  */
 
+#include <linux/clk.h>
+#include <linux/err.h>
+#include <linux/errno.h>
 #include <linux/export.h>
-#include <linux/module.h>
-#include <linux/io.h>
-#include <linux/irq.h>
 #include <linux/gpio.h>
 #include <linux/init.h>
-#include <linux/errno.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/irq.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/slab.h>
 #include <linux/pm_runtime.h>
 #include <linux/pm_wakeup.h>
-#include <linux/err.h>
-#include <linux/clk.h>
+#include <linux/slab.h>
 #include <asm/mach/irq.h>
 
 #define DRIVER_NAME "xgpiops"
