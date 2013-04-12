08370df3cf26636bfa96d93ce1445ae10f4ee2f5
Soren Brinkmann <soren.brinkmann@xilinx.com>
2013-04-12 09:25:52 -0700

    net: xemacps: Merge if-blocks
    Merging two if-blocks which checked identical conditionals.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Tested-by: Anirudha Sarangi <anirudh@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/ethernet/xilinx/xilinx_emacps.c
+++ drivers/net/ethernet/xilinx/xilinx_emacps.c
@@ -729,46 +729,36 @@ static void xemacps_adjust_link(struct net_device *ndev)
 			else
 				regval &= ~XEMACPS_NWCFG_FDEN_MASK;
 
+
+			regval &= ~(XEMACPS_NWCFG_1000_MASK |
+					XEMACPS_NWCFG_100_MASK);
 			if (phydev->speed == SPEED_1000) {
 				regval |= XEMACPS_NWCFG_1000_MASK;
 				xemacps_set_freq(lp->devclk, 125000000,
 						&lp->pdev->dev);
-			} else {
-				regval &= ~XEMACPS_NWCFG_1000_MASK;
-			}
-
-			if (phydev->speed == SPEED_100) {
+				xemacps_mdio_write(lp->mii_bus,
+					gmii2rgmii_phydev->addr,
+					XEMACPS_GMII2RGMII_REG_NUM,
+					XEMACPS_GMII2RGMII_SPEED1000_FD);
+			} else if (phydev->speed == SPEED_100) {
 				regval |= XEMACPS_NWCFG_100_MASK;
 				xemacps_set_freq(lp->devclk, 25000000,
 						&lp->pdev->dev);
-			} else {
-				regval &= ~XEMACPS_NWCFG_100_MASK;
-			}
-
-			if (phydev->speed == SPEED_10) {
+				xemacps_mdio_write(lp->mii_bus,
+						gmii2rgmii_phydev->addr,
+						XEMACPS_GMII2RGMII_REG_NUM,
+						XEMACPS_GMII2RGMII_SPEED100_FD);
+			} else if (phydev->speed == SPEED_10) {
 				xemacps_set_freq(lp->devclk, 2500000,
 						&lp->pdev->dev);
+				xemacps_mdio_write(lp->mii_bus,
+						gmii2rgmii_phydev->addr,
+						XEMACPS_GMII2RGMII_REG_NUM,
+						XEMACPS_GMII2RGMII_SPEED10_FD);
 			}
 
 			xemacps_write(lp->baseaddr, XEMACPS_NWCFG_OFFSET,
 			regval);
-
-			if (regval & XEMACPS_NWCFG_1000_MASK) {
-				xemacps_mdio_write(lp->mii_bus,
-				gmii2rgmii_phydev->addr,
-				XEMACPS_GMII2RGMII_REG_NUM,
-				XEMACPS_GMII2RGMII_SPEED1000_FD);
-			} else if (regval & XEMACPS_NWCFG_100_MASK) {
-				xemacps_mdio_write(lp->mii_bus,
-				gmii2rgmii_phydev->addr,
-				XEMACPS_GMII2RGMII_REG_NUM,
-				XEMACPS_GMII2RGMII_SPEED100_FD);
-			} else {
-				xemacps_mdio_write(lp->mii_bus,
-				gmii2rgmii_phydev->addr,
-				XEMACPS_GMII2RGMII_REG_NUM,
-				XEMACPS_GMII2RGMII_SPEED10_FD);
-			}
 
 			lp->speed = phydev->speed;
 			lp->duplex = phydev->duplex;
