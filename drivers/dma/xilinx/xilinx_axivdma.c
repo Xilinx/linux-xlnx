91fae84bc1f55583a89d02b44a1d044420db908d
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2013-05-23 19:36:26 +0200

    dma: xilinx: vdma: Remove unused struct xilinx_vdma_device irq field
    The field is unused, remove it.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Srikanth Thokala <sthokal@xilinx.com>

--- drivers/dma/xilinx/xilinx_axivdma.c
+++ drivers/dma/xilinx/xilinx_axivdma.c
@@ -193,7 +193,6 @@ struct xilinx_vdma_device {
 	struct dma_device common;
 	struct xilinx_vdma_chan *chan[XILINX_VDMA_MAX_CHANS_PER_DEVICE];
 	u32 feature;
-	int irq;
 };
 
 #define to_xilinx_chan(chan) \
@@ -614,7 +613,6 @@ static int vdma_init(struct xilinx_vdma_chan *chan)
 
 	return 0;
 }
-
 
 static irqreturn_t vdma_intr_handler(int irq, void *data)
 {
