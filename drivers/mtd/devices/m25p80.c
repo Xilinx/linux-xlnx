eb1a22e9c45c79987346c4598b2c0f31f716607c
Suneel Garapati <suneel.garapati@xilinx.com>
2013-05-31 01:23:28 +0530

    mtd: m25p80: Modified Micron 256Mb flash info structure entry
    Modified entry for micron flash memory 256Mb. Member name is
    suffix with 'a13'.
    
    Signed-off-by: Suneel Garapati <suneel.garapati@xilinx.com>
    Acked-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -778,7 +778,7 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "n25q128",   INFO(0x20bb18, 0, 64 * 1024, 256, 0) },
 	{ "n25q128a11",  INFO(0x20bb18, 0, 64 * 1024, 256, 0) },
 	{ "n25q128a13",  INFO(0x20ba18, 0, 64 * 1024, 256, 0) },
-	{ "n25q256a", INFO(0x20ba19, 0, 64 * 1024, 512, SECT_4K) },
+	{ "n25q256a13", INFO(0x20ba19,  0, 64 * 1024,  512, SECT_4K) },
 
 	/* Spansion -- single (large) sector size only, at least
 	 * for the chips listed here (without boot sectors).
