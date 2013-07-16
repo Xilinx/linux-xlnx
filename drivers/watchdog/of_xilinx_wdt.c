65773663b4b1212f667fa2cba6f5e7d2a108f59a
Michal Simek <michal.simek@xilinx.com>
2013-07-16 10:47:19 +0200

    watchdog: xilinx: Synchronize with mainline
    Based on discussion with mainline we shouldn't remove
    any compatible property from driver.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/watchdog/of_xilinx_wdt.c
+++ drivers/watchdog/of_xilinx_wdt.c
@@ -385,6 +385,7 @@ static int xwdt_remove(struct platform_device *dev)
 /* Match table for of_platform binding */
 static struct of_device_id xwdt_of_match[] = {
 	{ .compatible = "xlnx,xps-timebase-wdt-1.00.a", },
+	{ .compatible = "xlnx,xps-timebase-wdt-1.01.a", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, xwdt_of_match);
