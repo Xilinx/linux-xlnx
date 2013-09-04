81cc122c82a9fefed854f433ee38eb85b2f0fa1e
Michal Simek <michal.simek@xilinx.com>
2013-09-04 14:50:18 +0200

    dma: pl330: Simplify irq allocation
    Use devm_request_irq function.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Punnaiah Choudary Kalluri <punnaia@xilinx.com>

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -587,8 +587,6 @@ struct dma_pl330_dmac {
 
 	/* Peripheral channels connected to this DMAC */
 	struct dma_pl330_chan *peripherals; /* keep at end */
-
-	u32 dma_channels;
 };
 
 struct dma_pl330_desc {
@@ -2890,6 +2888,7 @@ pl330_probe(struct amba_device *adev, const struct amba_id *id)
 	struct resource *res;
 	int i, ret, irq;
 	int num_chan;
+	u32 dma_channels;
 
 	pdat = adev->dev.platform_data;
 
@@ -2913,20 +2912,20 @@ pl330_probe(struct amba_device *adev, const struct amba_id *id)
 	amba_set_drvdata(adev, pdmac);
 
 	of_property_read_u32(adev->dev.of_node,
-			     "#dma-channels", &pdmac->dma_channels);
+			     "#dma-channels", &dma_channels);
 
 	/* irq 0 is abort IRQ */
-	for (i = 1; i <= pdmac->dma_channels ; i++) {
+	for (i = 1; i <= dma_channels ; i++) {
 		irq = adev->irq[i];
-		ret = request_irq(irq, pl330_irq_handler, 0,
+		ret = devm_request_irq(&adev->dev, irq, pl330_irq_handler, 0,
 				dev_name(&adev->dev), pi);
 		if (ret)
-			goto probe_err1;
+			return ret;
 	}
 
 	ret = pl330_add(pi);
 	if (ret)
-		goto probe_err1;
+		return ret;
 
 	INIT_LIST_HEAD(&pdmac->desc_pool);
 	spin_lock_init(&pdmac->pool_lock);
@@ -3029,9 +3028,6 @@ probe_err3:
 	}
 probe_err2:
 	pl330_del(pi);
-probe_err1:
-	for (i = 1; i <= pdmac->dma_channels ; i++)
-		free_irq(adev->irq[i], pi);
 
 	return ret;
 }
@@ -3041,7 +3037,6 @@ static int pl330_remove(struct amba_device *adev)
 	struct dma_pl330_dmac *pdmac = amba_get_drvdata(adev);
 	struct dma_pl330_chan *pch, *_p;
 	struct pl330_info *pi;
-	int i;
 
 	if (!pdmac)
 		return 0;
@@ -3067,9 +3062,6 @@ static int pl330_remove(struct amba_device *adev)
 	pi = &pdmac->pif;
 
 	pl330_del(pi);
-
-	for (i = 1; i <= pdmac->dma_channels ; i++)
-		free_irq(adev->irq[i], pi);
 
 	return 0;
 }
