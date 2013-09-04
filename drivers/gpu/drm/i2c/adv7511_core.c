e06d0e7985dd10acda0e095fcd8a6dcfec64b777
Lars-Peter Clausen <lars@metafoo.de>
2013-09-04 17:35:05 +0200

    drm: adv7511: Fix use after free
    We need to use the free the old edid buffer not the new one.
    
    Reported-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- drivers/gpu/drm/i2c/adv7511_core.c
+++ drivers/gpu/drm/i2c/adv7511_core.c
@@ -461,14 +461,13 @@ static int adv7511_get_modes(struct drm_encoder *encoder,
 				   ADV7511_POWER_POWER_DOWN,
 				   ADV7511_POWER_POWER_DOWN);
 
+	kfree(adv7511->edid);
 	adv7511->edid = edid;
 	if (!edid)
 		return 0;
 
 	drm_mode_connector_update_edid_property(connector, edid);
 	count = drm_add_edid_modes(connector, edid);
-
-	kfree(adv7511->edid);
 
 	return count;
 }
