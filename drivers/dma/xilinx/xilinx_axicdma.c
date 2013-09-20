c770912bb3e18a5ae552c6e692287b6c5e109823
Srikanth Thokala <srikanth.thokala@xilinx.com>
2013-09-20 12:29:45 +0530

    cdma: xilinx: Already zeroed by kzalloc
    No need to initialize this variable, it is kzalloc'ed
    
    Signed-off-by: Srikanth Thokala <sthokal@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/dma/xilinx/xilinx_axicdma.c
+++ drivers/dma/xilinx/xilinx_axicdma.c
@@ -925,7 +925,6 @@ static int xilinx_cdma_probe(struct platform_device *pdev)
 	INIT_LIST_HEAD(&xdev->common.channels);
 
 	node = pdev->dev.of_node;
-	xdev->feature = 0;
 
 	/* iomap registers */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
