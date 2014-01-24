37978434a4d9fcd2c7e8a0547142b9146a6036b0
Michal Simek <michal.simek@xilinx.com>
2014-01-24 12:10:16 +0100

    gpio: zynq: Update license header
    Add 2014 to header and fix it to unified name.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/gpio/gpio-zynq.c
+++ drivers/gpio/gpio-zynq.c
@@ -1,7 +1,7 @@
 /*
  * Xilinx Zynq GPIO device driver
  *
- * 2009-2011 (c) Xilinx, Inc.
+ * Copyright (C) 2009 - 2014 Xilinx, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
