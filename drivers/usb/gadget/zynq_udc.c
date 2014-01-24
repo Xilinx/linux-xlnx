6104a2cdfec174985d9fa4a4990443a792e41d4e
Michal Simek <michal.simek@xilinx.com>
2014-01-24 12:55:15 +0100

    usb: gadget: zynq_udc: Update license header
    Add 2014 to header and fix it to unified name.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/gadget/zynq_udc.c
+++ drivers/usb/gadget/zynq_udc.c
@@ -1,7 +1,7 @@
 /*
  * Xilinx Zynq USB Device Controller Driver.
  *
- * Copyright (C) 2011 Xilinx, Inc.
+ * Copyright (C) 2011 - 2014 Xilinx, Inc.
  *
  * This file is based on fsl_udc_core.c file with few minor modifications
  * to support Xilinx Zynq USB controller.
