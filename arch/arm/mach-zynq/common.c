9d73a5958403c3a3ac1a951f1db34d0e3f7c8821
Soren Brinkmann <soren.brinkmann@xilinx.com>
2014-02-04 11:53:03 -0800

    ARM: zynq: Support cpufreq-cpu0 driver
    Add the required platform device and DT data (OPPs and clock handle) to
    support the generic cpu0 cpufreq driver.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.c
+++ arch/arm/mach-zynq/common.c
@@ -174,9 +174,12 @@ static void __init zynq_init_late(void)
  */
 static void __init zynq_init_machine(void)
 {
+	struct platform_device_info devinfo = { .name = "cpufreq-cpu0", };
+
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 
 	platform_device_register(&zynq_cpuidle_device);
+	platform_device_register_full(&devinfo);
 }
 
 static void __init zynq_timer_init(void)
