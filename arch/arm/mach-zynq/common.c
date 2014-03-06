1a85939af40acca2bf963407b497cc31c303ff3e
Punnaiah Choudary Kalluri <punnaiah.choudary.kalluri@xilinx.com>
2014-03-06 10:23:59 +0530

    ARM: zynq: Set bit 22 in the PL310 cache controller auxctlr register
    This patch is based on the below commit
    "ARM: 6395/1: VExpress: Set bit 22 in the PL310 (cache controller) AuxCtlr
    register"
    (sha1:1a8e41cd672f894bbd74874eac601e6cedf838fb)
    
    Clearing bit 22 in the PL310 Auxiliary Control register (shared
    attribute override enable) has the side effect of transforming Normal
    Shared Non-cacheable reads into Cacheable no-allocate reads.
    
    Coherent DMA buffers in Linux always have a Cacheable alias via the
    kernel linear mapping and the processor can speculatively load cache
    lines into the PL310 controller. With bit 22 cleared, Non-cacheable
    reads would unexpectedly hit such cache lines leading to buffer
    corruption.
    
    Signed-off-by: Punnaiah Choudary Kalluri <punnaia@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.c
+++ arch/arm/mach-zynq/common.c
@@ -67,13 +67,21 @@ static struct platform_device zynq_cpuidle_device = {
 #ifdef CONFIG_CACHE_L2X0
 static int __init zynq_l2c_init(void)
 {
-	/* 64KB way size, 8-way associativity, parity disabled,
-	 * prefetching option */
-#ifndef	CONFIG_XILINX_L2_PREFETCH
-	return l2x0_of_init(0x02060000, 0xF0F0FFFF);
-#else
-	return l2x0_of_init(0x72060000, 0xF0F0FFFF);
+	u32 auxctrl;
+
+	/*
+	 * 64KB way size, 8-way associativity, parity disabled,
+	 * prefetching option, shared attribute override enable
+	 */
+	auxctrl = L2X0_AUX_CTRL_SHARE_OVERRIDE_EN_MASK |
+			L2X0_AUX_CTRL_WAY_SIZE64K_MASK |
+			L2X0_AUX_CTRL_REPLACE_POLICY_RR_MASK;
+#ifdef CONFIG_XILINX_L2_PREFETCH
+	auxctrl |= L2X0_AUX_CTRL_EARLY_BRESP_EN_MASK |
+			L2X0_AUX_CTRL_INSTR_PREFETCH_EN_MASK |
+			L2X0_AUX_CTRL_DATA_PREFETCH_EN_MASK;
 #endif
+	return l2x0_of_init(auxctrl, 0xF0F0FFFF);
 }
 early_initcall(zynq_l2c_init);
 #endif
