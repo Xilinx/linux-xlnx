6c9da3f82cc67f198037d3839dd8d6220b064c7f
Davor Joja <davorjoja@logicbricks.com>
2014-03-24 19:27:19 +0100

    gpu: drm: xylon: Add a connector support
    This patch adds a Xylon DRM driver connector support.
    Functionality is based on a Xilinx DRM driver connector support.
    
    Signed-off-by: Davor Joja <davorjoja@logicbricks.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Tested-by: Christian Kohn <christian.kohn@xilinx.com>

---
+++ drivers/gpu/drm/xylon/xylon_connector.h
@@ -0,0 +1,28 @@
+/*
+ * Xylon DRM connector functions header
+ *
+ * Copyright (C) 2014 Xylon d.o.o.
+ * Author: Davor Joja <davor.joja@logicbricks.com>
+ *
+ * Reused Xilinx DRM connector header.
+ * Copyright (C) 2013 Xilinx, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _XYLON_DRM_CONNECTOR_H_
+#define _XYLON_DRM_CONNECTOR_H_
+
+struct drm_connector *
+xylon_drm_connector_create(struct drm_device *dev,
+			   struct drm_encoder *base_encoder);
+void xylon_drm_connector_destroy(struct drm_connector *base_connector);
+
+#endif /* _XYLON_DRM_CONNECTOR_H_ */
