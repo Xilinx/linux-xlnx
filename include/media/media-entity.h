4f3c57bb3164356bd5689d9b23ec676600589b20
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-03-26 04:01:46 +0100

    media: entity: Add media_entity_has_route() function
    This is a wrapper around the media entity has_route operation.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -901,6 +901,9 @@ __must_check int media_graph_walk_init(
  */
 void media_graph_walk_cleanup(struct media_graph *graph);
 
+bool media_entity_has_route(struct media_entity *entity, unsigned int sink,
+			    unsigned int source);
+
 /**
  * media_graph_walk_start - Start walking the media graph at a
  *	given entity
