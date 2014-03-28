5bb1fdce05f9c1250e75c88fa7022974629fa4c7
Michal Simek <michal.simek@xilinx.com>
2014-03-28 10:20:00 +0100

    zynq: Fix compatible string for ddrc
    Get rid of ps7 prefix.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/edac/zynq_edac.c
+++ drivers/edac/zynq_edac.c
@@ -589,7 +589,7 @@ static int zynq_edac_mc_remove(struct platform_device *pdev)
 
 /* Device tree node type and compatible tuples this driver can match on */
 static struct of_device_id zynq_edac_match[] = {
-	{ .compatible = "xlnx,zynq-ddrc-1.00.a", },
+	{ .compatible = "xlnx,zynq-ddrc-1.0", },
 	{ /* end of table */ }
 };
 
