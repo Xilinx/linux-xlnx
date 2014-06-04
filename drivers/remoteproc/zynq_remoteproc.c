4c03af9b7609d6ab23804e167d99fa010c5cedf7
Michal Simek <michal.simek@xilinx.com>
2014-06-04 07:12:29 +0200

    remoteproc: zynq: Make of_device_id array const
    Make of_device_id array const, because all OF functions handle
    it as const.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/remoteproc/zynq_remoteproc.c
+++ drivers/remoteproc/zynq_remoteproc.c
@@ -352,7 +352,7 @@ static int zynq_remoteproc_remove(struct platform_device *pdev)
 }
 
 /* Match table for OF platform binding */
-static struct of_device_id zynq_remoteproc_match[] = {
+static const struct of_device_id zynq_remoteproc_match[] = {
 	{ .compatible = "xlnx,zynq_remoteproc", },
 	{ /* end of list */ },
 };
