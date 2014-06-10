8154ebf3cd087da96a70518388db899621d84ffc
Guenter Roeck <linux@roeck-us.net>
2014-06-10 11:59:14 -0700

    of/platform: Fix microblaze build failure
    Commit bf5db2f (microblaze: Use generic device.h) removes the
    microblaze specific pdev_archdata and dma_mask.
    
    At the same time, commit 591c1ee (of: configure the platform
    device dma parameters) initializes the just removed field.
    This causes all microblaze builds to fail.
    
    Drop the unnecessary initialization.
    
    Cc: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -210,9 +210,6 @@ static struct platform_device *of_platform_device_create_pdata(
 	if (!dev)
 		return NULL;
 
-#if defined(CONFIG_MICROBLAZE)
-	dev->archdata.dma_mask = 0xffffffffUL;
-#endif
 	dev->dev.coherent_dma_mask = DMA_BIT_MASK(32);
 	if (!dev->dev.dma_mask)
 		dev->dev.dma_mask = &dev->dev.coherent_dma_mask;
