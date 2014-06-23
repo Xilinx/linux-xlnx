915c562b6ed6feccfd8e304b9eb51725b454d478
Davor Joja <davorjoja@logicbricks.com>
2014-06-23 19:58:13 +0200

    gpu: drm: xylon: Moved control and interrupt functions to hw header file
    Moved control and interrupt function declarations to hw header file.
    This keeps mentioned functionality usable only from crtc driver who is
    responsible for it.
    Updated define align spaces with tabs.
    
    Signed-off-by: Davor Joja <davorjoja@logicbricks.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- drivers/gpu/drm/xylon/xylon_logicvc_hw.h
+++ drivers/gpu/drm/xylon/xylon_logicvc_hw.h
@@ -17,18 +17,34 @@
 #ifndef _XYLON_LOGICVC_HW_H_
 #define _XYLON_LOGICVC_HW_H_
 
-#define LOGICVC_INT_L0_UPDATED    (1 << 0)
-#define LOGICVC_INT_L1_UPDATED    (1 << 1)
-#define LOGICVC_INT_L2_UPDATED    (1 << 2)
-#define LOGICVC_INT_L3_UPDATED    (1 << 3)
-#define LOGICVC_INT_L4_UPDATED    (1 << 4)
-#define LOGICVC_INT_V_SYNC        (1 << 5)
-#define LOGICVC_INT_E_VIDEO_VALID (1 << 6)
-#define LOGICVC_INT_FIFO_UNDERRUN (1 << 7)
-#define LOGICVC_INT_L0_CLUT_SW    (1 << 8)
-#define LOGICVC_INT_L1_CLUT_SW    (1 << 9)
-#define LOGICVC_INT_L2_CLUT_SW    (1 << 10)
-#define LOGICVC_INT_L3_CLUT_SW    (1 << 11)
-#define LOGICVC_INT_L4_CLUT_SW    (1 << 12)
+#define LOGICVC_INT_L0_UPDATED		(1 << 0)
+#define LOGICVC_INT_L1_UPDATED		(1 << 1)
+#define LOGICVC_INT_L2_UPDATED		(1 << 2)
+#define LOGICVC_INT_L3_UPDATED		(1 << 3)
+#define LOGICVC_INT_L4_UPDATED		(1 << 4)
+#define LOGICVC_INT_V_SYNC		(1 << 5)
+#define LOGICVC_INT_E_VIDEO_VALID	(1 << 6)
+#define LOGICVC_INT_FIFO_UNDERRUN	(1 << 7)
+#define LOGICVC_INT_L0_CLUT_SW		(1 << 8)
+#define LOGICVC_INT_L1_CLUT_SW		(1 << 9)
+#define LOGICVC_INT_L2_CLUT_SW		(1 << 10)
+#define LOGICVC_INT_L3_CLUT_SW		(1 << 11)
+#define LOGICVC_INT_L4_CLUT_SW		(1 << 12)
+
+void xylon_cvc_int_state(struct xylon_cvc *cvc, unsigned int type,
+			 bool enabled);
+u32 xylon_cvc_int_get_active(struct xylon_cvc *cvc);
+void xylon_cvc_int_clear_active(struct xylon_cvc *cvc, u32 active);
+void xylon_cvc_int_hw_enable(struct xylon_cvc *cvc);
+void xylon_cvc_int_hw_disable(struct xylon_cvc *cvc);
+int xylon_cvc_int_request(struct xylon_cvc *cvc, unsigned long flags,
+			  irq_handler_t handler, void *dev);
+void xylon_cvc_int_free(struct xylon_cvc *cvc, void *dev);
+
+void xylon_cvc_reset(struct xylon_cvc *cvc);
+void xylon_cvc_enable(struct xylon_cvc *cvc, struct videomode *vmode);
+void xylon_cvc_disable(struct xylon_cvc *cvc);
+
+struct xylon_cvc *xylon_cvc_probe(struct device *dev, struct device_node *node);
 
 #endif /* _XYLON_LOGICVC_HW_H_ */
