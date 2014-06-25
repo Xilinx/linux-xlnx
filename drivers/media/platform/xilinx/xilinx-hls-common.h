f14c72fab2a46e2688013dc42245a5dc09142f65
Laurent Pinchart <[laurent.pinchart@ideasonboard.com]>
2014-06-25 15:18:13 -0700

    media: xilinx: Add HLS core driver
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    State: pending
    [michals: Squashed with
    media: xilinx: hls: Fix warnings for NULL_RETURNS
    v5.15 update
    v6.6
    xilinx: Convert MODULE_LICENSE GPL v2 to GPL
    v4l: xilinx: hls: Fix the uapi header license
    v4l: xilinx: hls: remove from the UAPI header kernel specific __user macro
    kbuild: add xilinx-sdirxss.h/xilinx-hls.h/xilinx-v4l2-events.h to no-header-test
    v6.12
    v4l: xilinx: hls-common: Add missing SPDX license identifiers
    dt-bindings: media: xilinx: convert txt to yaml hls IP DT documentation
    s/strlcpy/strscpy/
    Fix void remove()
    bc0e8d91feec ("media: v4l: subdev: Switch to stream-aware state functions")
    ]

---
+++ drivers/media/platform/xilinx/xilinx-hls-common.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Xilinx HLS common header
+ *
+ * Copyright (C) 2013-2015 Xilinx, Inc.
+ *
+ * Contacts: Radhey Shyam Pandey <radheys@xilinx.com>
+ */
+
+#ifndef __XILINX_HLS_COMMON_H__
+#define __XILINX_HLS_COMMON_H__
+
+#include <linux/bitops.h>
+
+#define XHLS_DEF_WIDTH                          1920
+#define XHLS_DEF_HEIGHT                         1080
+
+#define XHLS_REG_CTRL_DONE                      BIT(1)
+#define XHLS_REG_CTRL_IDLE                      BIT(2)
+#define XHLS_REG_CTRL_READY                     BIT(3)
+#define XHLS_REG_CTRL_AUTO_RESTART              BIT(7)
+#define XHLS_REG_GIE                            0x04
+#define XHLS_REG_GIE_GIE                        BIT(0)
+#define XHLS_REG_IER                            0x08
+#define XHLS_REG_IER_DONE                       BIT(0)
+#define XHLS_REG_IER_READY                      BIT(1)
+#define XHLS_REG_ISR                            0x0c
+#define XHLS_REG_ISR_DONE                       BIT(0)
+#define XHLS_REG_ISR_READY                      BIT(1)
+#define XHLS_REG_ROWS                           0x10
+#define XHLS_REG_COLS                           0x18
+
+#endif /* __XILINX_HLS_COMMON_H__ */
