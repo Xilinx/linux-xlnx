fddee24ec0e271b20ed39080a9c411ee1f101927
Hyun Kwon <hyun.kwon@xilinx.com>
2014-07-02 16:11:14 -0700

    sound: soc: xilinx: Add ZynqMP DP subsystem audio drivers
    The DisplayPort subsystem of Xilinx ZynqMP SOC supports audio through,
    DPDMA - Audio buffer manager - Audio blender - DP. The DPDMA driver
    is implemented based on DMA engine API, and the audio driver is based
    on snd dmaengine pcm helpers.
    
    The DPDMA has the constraint that each buffer size should be aligned
    with 256 bytes. Enforce this limitation by adding a step size
    constraint for period bytes.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Tested-by: Damoder Mogilipaka <damoderm@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending
    [michals: Squashed with
    sound: xilinx: pcm: Set the shorter device name
    sound: soc: xilinx: Remove empty xilinx_dp_remove()
    sound: soc: xilinx: Remove empty xilinx_dp_pcm_dev_remove()
    sound: soc: xilinx: Use unique dai link per device
    sound: xilinx: pcm: Set the platform device name correctly
    sound: xilinx: pcm: Assign device name per compatible string
    v6.1 - remove non_legacy_dai_naming 36e79a44b12e ("ASoC: uniphier: Remove now redundant non_legacy_dai_naming flag")
    ]

---
+++ sound/soc/xilinx/xilinx-dp-pcm.c
@@ -0,0 +1,88 @@
+/*
+ * Xilinx DisplayPort Sound PCM support
+ *
+ *  Copyright (C) 2015 Xilinx, Inc.
+ *
+ *  Author: Hyun Woo Kwon <hyunk@xilinx.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/device.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include <sound/dmaengine_pcm.h>
+#include <sound/pcm.h>
+#include <sound/soc.h>
+
+#define DP_PCM_NAME_0 "zynqmp_dp_snd_pcm0"
+#define DP_PCM_NAME_1 "zynqmp_dp_snd_pcm1"
+
+static const struct snd_pcm_hardware xilinx_pcm_hw = {
+	.info			= SNDRV_PCM_INFO_MMAP |
+				  SNDRV_PCM_INFO_MMAP_VALID |
+				  SNDRV_PCM_INFO_INTERLEAVED |
+				  SNDRV_PCM_INFO_PAUSE |
+				  SNDRV_PCM_INFO_RESUME |
+				  SNDRV_PCM_INFO_NO_PERIOD_WAKEUP,
+	.buffer_bytes_max	= 128 * 1024,
+	.period_bytes_min	= 256,
+	.period_bytes_max	= 1024 * 1024,
+	.periods_min		= 2,
+	.periods_max		= 256,
+};
+
+static const struct snd_dmaengine_pcm_config xilinx_dmaengine_pcm_config = {
+	.pcm_hardware = &xilinx_pcm_hw,
+	.prealloc_buffer_size = 64 * 1024,
+};
+
+static int xilinx_dp_pcm_probe(struct platform_device *pdev)
+{
+	int ret;
+
+	if (of_device_is_compatible(pdev->dev.of_node, "xlnx,dp-snd-pcm0"))
+		dev_set_name(&pdev->dev, DP_PCM_NAME_0);
+	else if (of_device_is_compatible(pdev->dev.of_node, "xlnx,dp-snd-pcm1"))
+		dev_set_name(&pdev->dev, DP_PCM_NAME_1);
+	else
+		dev_set_name(&pdev->dev, pdev->dev.of_node->name);
+
+	pdev->name = dev_name(&pdev->dev);
+	ret = devm_snd_dmaengine_pcm_register(&pdev->dev,
+					      &xilinx_dmaengine_pcm_config, 0);
+	if (ret)
+		return ret;
+
+	dev_info(&pdev->dev, "Xilinx DisplayPort Sound PCM probed\n");
+
+	return 0;
+}
+
+static const struct of_device_id xilinx_dp_pcm_of_match[] = {
+	{ .compatible = "xlnx,dp-snd-pcm", },
+	{ .compatible = "xlnx,dp-snd-pcm0", },
+	{ .compatible = "xlnx,dp-snd-pcm1", },
+	{ /* end of table */ },
+};
+MODULE_DEVICE_TABLE(of, xilinx_dp_pcm_of_match);
+
+static struct platform_driver xilinx_dp_pcm_driver = {
+	.driver	= {
+		.name		= "xilinx-dp-snd-pcm",
+		.of_match_table	= xilinx_dp_pcm_of_match,
+	},
+	.probe	= xilinx_dp_pcm_probe,
+};
+module_platform_driver(xilinx_dp_pcm_driver);
+
+MODULE_DESCRIPTION("Xilinx DisplayPort Sound PCM module");
+MODULE_LICENSE("GPL v2");
