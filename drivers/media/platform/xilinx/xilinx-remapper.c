f7349c7ad275b694c166352fa9f6b2ab135c6b8c
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-09-12 23:14:08 +0300

    v4l: xilinx: remapper: Rename compatible string to xlnx,v-remapper
    This matches the compatible string values used by the DT generator.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/xilinx/xilinx-remapper.c
+++ drivers/media/platform/xilinx/xilinx-remapper.c
@@ -511,14 +511,14 @@ static int xremap_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id xremap_of_id_table[] = {
-	{ .compatible = "xlnx,axi-remapper" },
+	{ .compatible = "xlnx,v-remapper" },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, xremap_of_id_table);
 
 static struct platform_driver xremap_driver = {
 	.driver = {
-		.name = "xilinx-axi-remapper",
+		.name = "xilinx-remapper",
 		.of_match_table = xremap_of_id_table,
 	},
 	.probe = xremap_probe,
