1f3aa6be919e486a88cb684d56429be10cccac5d
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-11-02 01:53:45 +0200

    v4l: xilinx: hls: Update to the new ctrl string API
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/xilinx/xilinx-hls.c
+++ drivers/media/platform/xilinx/xilinx-hls.c
@@ -104,7 +104,7 @@ static int xhls_create_controls(struct xhls_device *xhls)
 		return xhls->ctrl_handler.error;
 	}
 
-	strlcpy(ctrl->cur.string, xhls->compatible, model.max + 1);
+	v4l2_ctrl_s_ctrl_string(ctrl, xhls->compatible);
 
 	xhls->xvip.subdev.ctrl_handler = &xhls->ctrl_handler;
 
