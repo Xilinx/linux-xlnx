83ee2206f404773ad755f516338f527d778910a9
Christian Kohn <christian.kohn@xilinx.com>
2014-11-05 13:05:12 -0800

    media: adv7604: select default input

--- drivers/media/i2c/adv7604.c
+++ drivers/media/i2c/adv7604.c
@@ -2735,7 +2735,7 @@ static int adv7604_parse_dt(struct adv7604_state *state)
 	/* Hardcode the remaining platform data fields. */
 	state->pdata.disable_pwrdnb = 0;
 	state->pdata.disable_cable_det_rst = 0;
-	state->pdata.default_input = -1;
+	state->pdata.default_input = 0;
 	state->pdata.blank_data = 1;
 	state->pdata.alt_data_sat = 1;
 	state->pdata.op_format_mode_sel = ADV7604_OP_FORMAT_MODE0;
