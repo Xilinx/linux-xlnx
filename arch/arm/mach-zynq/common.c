73a44f8ed3bb24719ed1ba8452c1edf9598355f3
Michal Simek <michal.simek@xilinx.com>
2015-01-15 14:30:43 +0100

    ARM: zynq: Clean comments in zynq_memory_init()
    Sync with mainline version.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.c
+++ arch/arm/mach-zynq/common.c
@@ -54,17 +54,10 @@ void __iomem *zynq_scu_base;
  * zynq_memory_init - Initialize special memory
  *
  * We need to stop things allocating the low memory as DMA can't work in
- * the 1st 512K of memory.  Using reserve vs remove is not totally clear yet.
+ * the 1st 512K of memory.
  */
 static void __init zynq_memory_init(void)
 {
-	/*
-	 * Reserve the 0-0x4000 addresses (before swapper page tables
-	 * and kernel) which can't be used for DMA.
-	 * 0x0 - 0x4000 - reserving below not to be used by DMA
-	 * 0x4000 - 0x8000 swapper page table
-	 * 0x8000 - 0x80000 kernel .text
-	 */
 	if (!__pa(PAGE_OFFSET))
 		memblock_reserve(__pa(PAGE_OFFSET), __pa(swapper_pg_dir));
 }
