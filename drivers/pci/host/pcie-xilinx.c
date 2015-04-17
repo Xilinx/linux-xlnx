fa2ab2e520cf3f2792b08b74863d8a6a9e28e208
Michal Simek <michal.simek@xilinx.com>
2015-04-17 12:05:05 +0200

    pcie: xilinx: Fix debug message
    Fix compilation warning:
    drivers/pci/host/pcie-xilinx.c: In function
    'xilinx_pcie_clear_err_interrupts':
    drivers/pci/host/pcie-xilinx.c:154:3: warning: format '%d' expects
    argument of type 'int', but argument 4 has type 'long unsigned int'
    [-Wformat=]
       dev_dbg(port->dev, "Requester ID %d\n",
    
    GENMASK was changed by:
    bitops: Fix shift overflow in GENMASK macros
    (sha1: 00b4d9a14125f1e51874def2b9de6092e007412d)
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/pci/host/pcie-xilinx.c
+++ drivers/pci/host/pcie-xilinx.c
@@ -151,7 +151,7 @@ static void xilinx_pcie_clear_err_interrupts(struct xilinx_pcie_port *port)
 	u32 val = pcie_read(port, XILINX_PCIE_REG_RPEFR);
 
 	if (val & XILINX_PCIE_RPEFR_ERR_VALID) {
-		dev_dbg(port->dev, "Requester ID %d\n",
+		dev_dbg(port->dev, "Requester ID %ld\n",
 			val & XILINX_PCIE_RPEFR_REQ_ID);
 		pcie_write(port, XILINX_PCIE_RPEFR_ALL_MASK,
 			   XILINX_PCIE_REG_RPEFR);
