3dff44e0b68af0c8cba5cee6b30a448dca1be34a
Suneel Garapati <suneel.garapati@xilinx.com>
2015-06-09 13:01:50 +0530

    drivers: mmc: add quirk SDHCI_QUIRK_CLOCK_DIV_ZERO_BROKEN
    adds quirk for controllers whose clock divider zero is broken,
    sdhci_set_clock function will incorporate this modification.
    
    Signed-off-by: Suneel Garapati <suneel.garapati@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -115,6 +115,8 @@ struct sdhci_host {
 #define SDHCI_QUIRK2_TUNING_WORK_AROUND			(1<<12)
 /* disable the block count for single block transactions */
 #define SDHCI_QUIRK2_SUPPORT_SINGLE			(1<<13)
+/* Broken Clock divider zero in controller */
+#define SDHCI_QUIRK2_CLOCK_DIV_ZERO_BROKEN		(1<<15)
 
 	int irq;		/* Device IRQ */
 	void __iomem *ioaddr;	/* Mapped address */
