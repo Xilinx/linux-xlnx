61c03ad115b4845ad2eb0db5aac40241128bb099
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2015-12-01 15:50:07 +0530

    media: xilinx: Use BIT() macro to set n-bit
    Prefer using in-built kernel macros.
    
    Signed-off-by: Radhey Shyam Pandey <radheys@xilinx.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Tested-by: Christian Kohn <christian.kohn@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/media/platform/xilinx/xilinx-hls-common.h
+++ drivers/media/platform/xilinx/xilinx-hls-common.h
@@ -13,22 +13,23 @@
 #ifndef __XILINX_HLS_COMMON_H__
 #define __XILINX_HLS_COMMON_H__
 
+#include <linux/bitops.h>
 
 #define XHLS_DEF_WIDTH                          1920
 #define XHLS_DEF_HEIGHT                         1080
 
-#define XHLS_REG_CTRL_DONE                      (1 << 1)
-#define XHLS_REG_CTRL_IDLE                      (1 << 2)
-#define XHLS_REG_CTRL_READY                     (1 << 3)
-#define XHLS_REG_CTRL_AUTO_RESTART              (1 << 7)
+#define XHLS_REG_CTRL_DONE                      BIT(1)
+#define XHLS_REG_CTRL_IDLE                      BIT(2)
+#define XHLS_REG_CTRL_READY                     BIT(3)
+#define XHLS_REG_CTRL_AUTO_RESTART              BIT(7)
 #define XHLS_REG_GIE                            0x04
-#define XHLS_REG_GIE_GIE                        (1 << 0)
+#define XHLS_REG_GIE_GIE                        BIT(0)
 #define XHLS_REG_IER                            0x08
-#define XHLS_REG_IER_DONE                       (1 << 0)
-#define XHLS_REG_IER_READY                      (1 << 1)
+#define XHLS_REG_IER_DONE                       BIT(0)
+#define XHLS_REG_IER_READY                      BIT(1)
 #define XHLS_REG_ISR                            0x0c
-#define XHLS_REG_ISR_DONE                       (1 << 0)
-#define XHLS_REG_ISR_READY                      (1 << 1)
+#define XHLS_REG_ISR_DONE                       BIT(0)
+#define XHLS_REG_ISR_READY                      BIT(1)
 #define XHLS_REG_ROWS                           0x10
 #define XHLS_REG_COLS                           0x18
 
