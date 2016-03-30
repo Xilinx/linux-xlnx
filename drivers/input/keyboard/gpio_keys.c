f35db7561bc07403af16888ece83f61e0cbe3da3
Nava kishore Manne <nava.manne@xilinx.com>
2016-03-30 12:49:22 +0530

    gpio_keys: Added support to read the IRQ_FLAGS from devicetree
    This patch adds the support to read the IRQ_FLAGS from the device
    instead of hard code the flags in gpio_keys_setup_key().
    
    example gpio-keys DT node:
    
    gpio-keys {
    	compatible = "gpio-keys";
    	#address-cells = <1>;
    	#size-cells = <0>;
    	autorepeat;
    		sw14 {
    			label = "sw14";
    			gpios = <&gpio0 12 1>;
    			/*
    			 * Triggering Type:
    			 *
    			 * 1 - edge rising
    			 * 2 - edge falling
    			 * 4 - level active high
    			 * 8 - level active low
    			 *
    			 */
    			linux,code = <108>; /* down */
    			gpio-key,wakeup;
    			autorepeat;
    		};
    	};
    
    Signed-off-by: Nava kishore Manne <navam@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -514,7 +514,7 @@ static int gpio_keys_setup_key(struct platform_device *pdev,
 		INIT_DELAYED_WORK(&bdata->work, gpio_keys_gpio_work_func);
 
 		isr = gpio_keys_gpio_isr;
-		irqflags = IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING;
+		irqflags = button->irq_flags;
 
 	} else {
 		if (!button->irq) {
@@ -649,11 +649,9 @@ gpio_keys_get_devtree_pdata(struct device *dev)
 
 	i = 0;
 	for_each_available_child_of_node(node, pp) {
-		enum of_gpio_flags flags;
-
 		button = &pdata->buttons[i++];
 
-		button->gpio = of_get_gpio_flags(pp, 0, &flags);
+		button->gpio = of_get_gpio_flags(pp, 0, &button->irq_flags);
 		if (button->gpio < 0) {
 			error = button->gpio;
 			if (error != -ENOENT) {
@@ -664,7 +662,8 @@ gpio_keys_get_devtree_pdata(struct device *dev)
 				return ERR_PTR(error);
 			}
 		} else {
-			button->active_low = flags & OF_GPIO_ACTIVE_LOW;
+			button->active_low = button->irq_flags
+						& OF_GPIO_ACTIVE_LOW;
 		}
 
 		button->irq = irq_of_parse_and_map(pp, 0);
