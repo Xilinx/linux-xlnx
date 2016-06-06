aa67923c4e2bbd670f99de72bcdb2d6aa1f59e73
Wendy Liang <jliang@xilinx.com>
2016-06-05 21:59:40 -0700

    remoteproc: remove memset from rp_find_vq
    As the vring memory is already memset to 0 in rproc_alloc_vring()
    through dma_alloc_coherent(). It is duplicated to memset vring
    to 0 again.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/remoteproc/remoteproc_virtio.c
+++ drivers/remoteproc/remoteproc_virtio.c
@@ -98,7 +98,6 @@ static struct virtqueue *rp_find_vq(struct virtio_device *vdev,
 
 	/* zero vring */
 	size = vring_size(len, rvring->align);
-	memset(addr, 0, size);
 
 	dev_dbg(dev, "vring%d: va %p qsz %d notifyid %d\n",
 					id, addr, len, rvring->notifyid);
