aabcb41946141e6aa42ac6507bb8feb38cb944a9
Hyun Kwon <hyun.kwon@xilinx.com>
2016-06-17 16:37:09 -0700

    phy: zynqmp: Reset the de-emphasis and swing control for DP
    When the PHY lane is initialized for DP, the de-emphasis / swing control
    should be reset in order to override the values from DP with values
    programmed in the register.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    CC: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/phy/phy-zynqmp.c
+++ drivers/phy/phy-zynqmp.c
@@ -131,6 +131,19 @@
 #define L0_TX_ANA_TM_18			0x0048
 #define TX_ANA_TM_18_OFFSET		0x4000
 
+#define L0_TX_ANA_TM_118		0x01D8
+#define TX_ANA_TM_118_OFFSET		0x4000
+#define L0_TX_ANA_TM_118_FORCE_17_0	BIT(0)
+
+#define L0_TXPMD_TM_45			0x0CB4
+#define TXPMD_TM_45_OFFSET		0x4000
+#define L0_TXPMD_TM_45_OVER_DP_MAIN	BIT(0)
+#define L0_TXPMD_TM_45_ENABLE_DP_MAIN	BIT(1)
+#define L0_TXPMD_TM_45_OVER_DP_POST1	BIT(2)
+#define L0_TXPMD_TM_45_ENABLE_DP_POST1	BIT(3)
+#define L0_TXPMD_TM_45_OVER_DP_POST2	BIT(4)
+#define L0_TXPMD_TM_45_ENABLE_DP_POST2	BIT(5)
+
 #define L0_TXPMD_TM_48			0x0CC0
 #define TXPMD_TM_48_OFFSET		0x4000
 
@@ -759,6 +772,18 @@ static int xpsgtr_phy_init(struct phy *phy)
 		ret = xpsgtr_wait_pll_lock(phy);
 		if (ret != 0)
 			goto out;
+	} else {
+		offset = gtr_phy->lane * TXPMD_TM_45_OFFSET + L0_TXPMD_TM_45;
+		reg = L0_TXPMD_TM_45_OVER_DP_MAIN |
+		      L0_TXPMD_TM_45_ENABLE_DP_MAIN |
+		      L0_TXPMD_TM_45_OVER_DP_POST1 |
+		      L0_TXPMD_TM_45_OVER_DP_POST2 |
+		      L0_TXPMD_TM_45_ENABLE_DP_POST2;
+		writel(reg, gtr_dev->serdes + offset);
+		offset = gtr_phy->lane * TX_ANA_TM_118_OFFSET +
+			 L0_TX_ANA_TM_118;
+		writel(L0_TX_ANA_TM_118_FORCE_17_0,
+		       gtr_dev->serdes + offset);
 	}
 
 	/* Do ULPI reset for usb */
