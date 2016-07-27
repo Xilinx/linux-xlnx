70c8085656fa4e61705a2e72779954915de2b994
Michal Simek <michal.simek@xilinx.com>
2016-07-27 10:06:40 +0200

    fpga: Add firmware store function
    For testing purpose.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Nava kishore Manne <navam@xilinx.com>

--- drivers/fpga/fpga-mgr.c
+++ drivers/fpga/fpga-mgr.c
@@ -351,12 +351,42 @@ static ssize_t state_show(struct device *dev,
 	return sprintf(buf, "%s\n", state_str[mgr->state]);
 }
 
+static ssize_t firmware_store(struct device *dev,
+			      struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct fpga_manager *mgr = to_fpga_manager(dev);
+	unsigned int len;
+	char image_name[NAME_MAX];
+	int ret;
+
+	/* struct with information about the FPGA image to program. */
+	struct fpga_image_info info;
+
+	/* flags indicates whether to do full or partial reconfiguration */
+	info.flags = 0;
+
+	/* lose terminating \n */
+	strcpy(image_name, buf);
+	len = strlen(image_name);
+	if (image_name[len - 1] == '\n')
+		image_name[len - 1] = 0;
+
+	ret = fpga_mgr_firmware_load(mgr, &info, image_name);
+	if (ret)
+		return ret;
+
+	return count;
+}
+
 static DEVICE_ATTR_RO(name);
 static DEVICE_ATTR_RO(state);
+static DEVICE_ATTR_WO(firmware);
 
 static struct attribute *fpga_mgr_attrs[] = {
 	&dev_attr_name.attr,
 	&dev_attr_state.attr,
+	&dev_attr_firmware.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(fpga_mgr);
