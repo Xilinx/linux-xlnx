24b3675e78fb13b1c5325cabcc9c60099faf1fe5
Michal Simek <michal.simek@xilinx.com>
2016-07-27 09:13:13 +0200

    ARM: zynq: Add support for Zynq-7000S devices
    Patch adds detection of Zynq-7000 base silicon configuration,
    namely Dual or Single CPU. Device trees attempting to enable DUAL CORE
    behavior on SINGLE CPU Zynq-7000S devices are prevented from corrupting
    system behavior.
    
    Detection of Dual or Single CPU is done via eFuses.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>
    Link: https://lkml.org/lkml/2017/8/11/375
          https://www.spinics.net/lists/arm-kernel/msg603276.html
    State: not-upstreamable

--- arch/arm/mach-zynq/platsmp.c
+++ arch/arm/mach-zynq/platsmp.c
@@ -83,6 +83,9 @@ EXPORT_SYMBOL(zynq_cpun_start);
 
 static int zynq_boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
+	if (!zynq_efuse_cpu_state(cpu))
+		return -1;
+
 	return zynq_cpun_start(__pa_symbol(secondary_startup_arm), cpu);
 }
 
