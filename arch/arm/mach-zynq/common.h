b3db0598f8ec5b90f97867f799569101dae24f1b
Michal Simek <michal.simek@xilinx.com>
2016-07-27 09:13:13 +0200

    ARM: zynq: Add support for Zynq-7000S devices
    Patch adds detection of Zynq-7000 base silicon configuration,
    namely Dual or Single CPU. Device trees attempting to enable DUAL CORE
    behavior on SINGLE CPU Zynq-7000S devices are prevented from corrupting
    system behavior.
    
    Detection of Dual or Single CPU is done via eFuses.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>

--- arch/arm/mach-zynq/common.h
+++ arch/arm/mach-zynq/common.h
@@ -26,6 +26,9 @@ extern void zynq_slcr_cpu_state_write(int cpu, bool die);
 extern u32 zynq_slcr_get_ocm_config(void);
 extern u32 zynq_slcr_get_device_id(void);
 
+extern bool zynq_efuse_cpu_state(int cpu);
+extern int zynq_early_efuse_init(void);
+
 #ifdef CONFIG_SMP
 extern char zynq_secondary_trampoline;
 extern char zynq_secondary_trampoline_jump;
