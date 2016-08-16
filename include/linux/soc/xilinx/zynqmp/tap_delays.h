bd65d21479b8861fb51c610771b90a976a3ca686
Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
2016-08-16 14:41:31 +0530

    soc: zynqmp: Add mmc tap delay programming
    This patch adds support for programming the
    tap delays required for mmc driver.
    'arasan_zynqmp_set_tap_delay' and 'zynqmp_dll_reset'
    API's are exported so that mmc driver will use these
    API's for tap delay programming.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmis@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/linux/soc/xilinx/zynqmp/tap_delays.h
@@ -0,0 +1,31 @@
+/*
+ * Xilinx Zynq MPSoC Power Management
+ *
+ *  Copyright (C) 2016 Xilinx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/soc/xilinx/zynqmp/pm.h>
+
+#ifdef CONFIG_SOC_XILINX_ZYNQMP
+/* API for programming the tap delays */
+void arasan_zynqmp_set_tap_delay(u8 deviceid, u8 timing, u8 bank);
+
+/* API to reset the DLL */
+void zynqmp_dll_reset(u8 deviceid);
+#else
+inline void arasan_zynqmp_set_tap_delay(u8 deviceid, u8 timing, u8 bank) {}
+inline void zynqmp_dll_reset(u8 deviceid) {}
+#endif
