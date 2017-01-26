f1f33599516b9c5f787fcc1e17534a7001cc635e
Wendy Liang <wendy.liang@xilinx.com>
2017-01-25 22:14:11 -0800

    remoteproc: declare rproc mem in fw_boot
    Declare rproc memory before it handles vdevs and other resources
    in booting firmware.
    
    Signed-off-by: Wendy Liang <jliang@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -885,6 +885,9 @@ static void rproc_resource_cleanup(struct rproc *rproc)
 	/* clean up remote vdev entries */
 	list_for_each_entry_safe(rvdev, rvtmp, &rproc->rvdevs, node)
 		kref_put(&rvdev->refcount, rproc_vdev_release);
+
+	/* Release DMA declared memory */
+	dma_release_declared_memory(dev->parent);
 }
 
 static int rproc_start(struct rproc *rproc, const struct firmware *fw)
@@ -994,6 +997,14 @@ static int rproc_fw_boot(struct rproc *rproc, const struct firmware *fw)
 
 	/* reset max_notifyid */
 	rproc->max_notifyid = -1;
+
+	/* look for remote processor memory and declare them. */
+	ret = rproc_handle_resources(rproc, tablesz, rproc_rproc_mem_handler);
+	if (ret) {
+		dev_err(dev, "Failed to declare rproc memory resource: %d\n",
+			ret);
+		goto clean_up_resources;
+	}
 
 	/* handle fw resources which are required to boot rproc */
 	ret = rproc_handle_resources(rproc, tablesz, rproc_loading_handlers);
