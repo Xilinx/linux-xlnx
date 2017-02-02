2a8c32259cc5b1d8fead70afde27f8faa4ab5959
Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
2017-02-03 00:31:00 +0530

    drm: mipi: Bring DT format description closer to the hardware
    Use the DSI pixel format codes as defined in the include/drm/drm_mipi_dsi.h
    to describe data type for pixel format. Based on data type the incoming
    pixel stream is converted to byte stream to match with the DSI requirements
    detailed in sec 8.8 of the MIPI Alliance Standard for DSI specification.
    This gets rid of yet another string-based format description, while
    keeping the bindings simple enough and close to the hardware.
    
    Signed-off-by: Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Hyun Kwon <hyunk@xilinx.com
    State: pending

---
+++ include/dt-bindings/drm/mipi-dsi.h
@@ -0,0 +1,11 @@
+#ifndef __DT_BINDINGS_DRM__
+#define __DT_BINDINGS_DRM__
+/*
+ * MIPI DSI pixel formats as defined in the include/drm/drm_mipi_dsi.h"
+ */
+#define MIPI_DSI_FMT_RGB888		0
+#define MIPI_DSI_FMT_RGB666		1
+#define MIPI_DSI_FMT_RGB666_PACKED	2
+#define MIPI_DSI_FMT_RGB565		3
+
+#endif /* _DT_BINDINGS_DRM__ */
