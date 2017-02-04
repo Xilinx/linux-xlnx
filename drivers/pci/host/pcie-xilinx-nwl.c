d3dfbe77277f114439972486988253b2eb65b103
Hyun Kwon <hyun.kwon@xilinx.com>
2017-02-03 18:04:07 -0800

    PCI: xilinx-nwl: Enable the clock through CCF
    Simply enable clocks. There is no remove function that's why
    this should be enough for simple operation. Suspend/resume should be
    checked.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/pci/host/pcie-xilinx-nwl.c
+++ drivers/pci/host/pcie-xilinx-nwl.c
@@ -10,6 +10,7 @@
  * (at your option) any later version.
  */
 
+#include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
@@ -171,6 +172,7 @@ struct nwl_pcie {
 	u8 root_busno;
 	struct nwl_msi msi;
 	struct irq_domain *legacy_irq_domain;
+	struct clk *clk;
 	raw_spinlock_t leg_mask_lock;
 };
 
@@ -852,6 +854,11 @@ static int nwl_pcie_probe(struct platform_device *pdev)
 		dev_err(dev, "Parsing DT failed\n");
 		return err;
 	}
+
+	pcie->clk = devm_clk_get(dev, NULL);
+	if (IS_ERR(pcie->clk))
+		return PTR_ERR(pcie->clk);
+	clk_prepare_enable(pcie->clk);
 
 	err = nwl_pcie_bridge_init(pcie);
 	if (err) {
