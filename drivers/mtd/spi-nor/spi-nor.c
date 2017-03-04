7b994e66ab4ff7ea5d7bb12caf21a3c3b1d0650b
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2017-03-04 15:00:13 +0530

    mtd: spi-nor: Fix incorrect status register update
    To check whether flash is ready or not, we use status register
    command (SR). so for QSPI dual prallel configuration, we have to
    wait for ready status of both flash devices. we are doing that,
    but due to the below & operation, we are sending incorrect status
    update to layer, causing invalid data write/read to/from the devices.
    so this patch fixes that.
    
    Signed-off-by: Naga Sureshkumar Relli <nagasure@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -102,7 +102,7 @@ static int read_sr(struct spi_nor *nor)
 			pr_err("error %d reading SR\n", (int) ret);
 			return ret;
 		}
-		val[0] &= val[1];
+		val[0] |= val[1];
 	} else {
 		ret = nor->read_reg(nor, SPINOR_OP_RDSR, &val[0], 1);
 		if (ret < 0) {
