d895c746e20e0edab4f852663f099b0811f7b050
Manish Narani <manish.narani@xilinx.com>
2017-03-23 15:56:13 +0530

    usb: xhci: Set XHCI Light Reset for DWC3_OTG mode
    This Patch corrects the XHCI reset for OTG mode. The host cannot
    have Hard Reset while in OTG mode because that will affect the
    OTG peripheral mode event buffers and it will not function.
    
    The macro for OTG is USB_DWC3_OTG not USB_DWC3_DUAL_ROLE. This patch
    corrects the same.
    
    Signed-off-by: Manish Narani <mnarani@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -174,7 +174,7 @@ int xhci_reset(struct xhci_hcd *xhci)
 
 	xhci_dbg_trace(xhci, trace_xhci_dbg_init, "// Reset the HC");
 	command = readl(&xhci->op_regs->command);
-#ifdef CONFIG_USB_DWC3_DUAL_ROLE
+#ifdef CONFIG_USB_DWC3_OTG
 	command |= CMD_LRESET;
 #else
 	command |= CMD_RESET;
@@ -192,7 +192,7 @@ int xhci_reset(struct xhci_hcd *xhci)
 		udelay(1000);
 
 	ret = xhci_handshake(&xhci->op_regs->command,
-#ifdef CONFIG_USB_DWC3_DUAL_ROLE
+#ifdef CONFIG_USB_DWC3_OTG
 			CMD_LRESET,
 #else
 			CMD_RESET,
