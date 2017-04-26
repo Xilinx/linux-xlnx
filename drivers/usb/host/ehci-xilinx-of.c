5f078eeec007704b540f71cf15c1814801eb5aba
Manish Narani <manish.narani@xilinx.com>
2017-04-26 13:38:04 +0530

    usb: host: ehci-xilinx: modified driver to resolve formatting warnings
    This patch resolves checkpatch warnings for xilinx EHCI driver.
    
    Signed-off-by: Manish Narani <mnarani@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/host/ehci-xilinx-of.c
+++ drivers/usb/host/ehci-xilinx-of.c
@@ -60,11 +60,9 @@ static int ehci_xilinx_port_handed_over(struct usb_hcd *hcd, int portnum)
 		dev_warn(hcd->self.controller,
 			"Maybe your device is not a high speed device?\n");
 		dev_warn(hcd->self.controller,
-			"The USB host controller does not support full speed "
-			"nor low speed devices\n");
+			"USB host controller doesn't support FS/LS devices\n");
 		dev_warn(hcd->self.controller,
-			"You can reconfigure the host controller to have "
-			"full speed support\n");
+			"You can reconfigure host controller to support FS\n");
 	}
 
 	return 0;
