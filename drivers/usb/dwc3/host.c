4a9fda3c58ae642c20acbb7febe314da9ef6690e
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2017-06-20 16:25:17 +0530

    usb: dwc3: Correct dwc3 host init code for making SMMU work in HOST Mode
    DWC3 driver initializes the xhci host platform device structure.
    As a part of initialization it configures xhci dev DMA attributes,
    but fails to set correct smmu dma ops (if iommus node is added in
    dts file). But SMMU driver intializes correct context block based
    on the SMMU Stream ID given by USB dts node. Due to the improper
    initalization of dma ops(SWIO instead of SMMU) no proper page table
    entries are updated in SMMU Contect Block, thus generating an abort
    when USB DMA tries to write an event in the event ring.
    
    This patch fixes the above issue by calling of_dma_configure() during
    xhci dev initialization. This of_dma_configure() checks for valid
    iommus node and assigns proper SMMU dma ops instead of SWIO dma ops.
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -16,6 +16,7 @@
  */
 
 #include <linux/platform_device.h>
+#include <linux/of_device.h>
 
 #include "core.h"
 
@@ -72,12 +73,18 @@ int dwc3_host_init(struct dwc3 *dwc)
 		return -ENOMEM;
 	}
 
-	dma_set_coherent_mask(&xhci->dev, dwc->dev->coherent_dma_mask);
-
 	xhci->dev.parent	= dwc->dev;
-	xhci->dev.dma_mask	= dwc->dev->dma_mask;
-	xhci->dev.dma_parms	= dwc->dev->dma_parms;
-	xhci->dev.archdata	= dwc->dev->archdata;
+
+	if (IS_ENABLED(CONFIG_OF)) {
+		/* Let OF configure xhci dev's DMA configuration */
+		of_dma_configure(&xhci->dev, dwc->dev->of_node);
+	} else {
+		dma_set_coherent_mask(&xhci->dev, dwc->dev->coherent_dma_mask);
+
+		xhci->dev.dma_mask	= dwc->dev->dma_mask;
+		xhci->dev.dma_parms	= dwc->dev->dma_parms;
+		xhci->dev.archdata	= dwc->dev->archdata;
+	}
 
 	dwc->xhci = xhci;
 
