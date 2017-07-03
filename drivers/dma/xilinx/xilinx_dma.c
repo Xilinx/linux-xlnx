fc9de022319c69c67ffb63773bee0d339c1ad470
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-07-03 10:56:07 +0530

    dmaengine: xilinx_dma: Fix dma_get_slave_caps gaps
    When client driver uses dma_get_slave_caps
    It checks for certain fileds of dma_device struct
    currently driver is not settings few fields resulting
    dma_get_slave_caps() returns NULL.
    
    This patch fixes this issue by populating proper values
    to the struct dma_device fields.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/dma/xilinx/xilinx_dma.c
+++ drivers/dma/xilinx/xilinx_dma.c
@@ -2426,6 +2426,7 @@ static int xilinx_dma_chan_probe(struct xilinx_dma_device *xdev,
 		chan->direction = DMA_MEM_TO_DEV;
 		chan->id = chan_id;
 		chan->tdest = chan_id;
+		xdev->common.directions = BIT(DMA_MEM_TO_DEV);
 
 		chan->ctrl_offset = XILINX_DMA_MM2S_CTRL_OFFSET;
 		if (xdev->dma_config->dmatype == XDMA_TYPE_VDMA) {
@@ -2443,6 +2444,7 @@ static int xilinx_dma_chan_probe(struct xilinx_dma_device *xdev,
 		chan->direction = DMA_DEV_TO_MEM;
 		chan->id = chan_id;
 		chan->tdest = chan_id - xdev->nr_channels;
+		xdev->common.directions |= BIT(DMA_DEV_TO_MEM);
 		chan->has_vflip = of_property_read_bool(node,
 					"xlnx,enable-vert-flip");
 		if (chan->has_vflip) {
@@ -2664,6 +2666,8 @@ static int xilinx_dma_probe(struct platform_device *pdev)
 		dma_cap_set(DMA_PRIVATE, xdev->common.cap_mask);
 	}
 
+	xdev->common.dst_addr_widths = BIT(addr_width / 8);
+	xdev->common.src_addr_widths = BIT(addr_width / 8);
 	xdev->common.device_alloc_chan_resources =
 				xilinx_dma_alloc_chan_resources;
 	xdev->common.device_free_chan_resources =
