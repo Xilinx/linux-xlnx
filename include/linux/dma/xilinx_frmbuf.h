925200ac9051af3e27f107b5754056727a007826
Jeffrey Mouroux <jeff.mouroux@xilinx.com>
2017-07-13 18:31:11 -0700

    dmaengine: xilinx_frmbuf: New driver for Video Framebuffer IP
    The Video Framebuffer IP is available in two forms: read or write.
    This driver supports either form of the IP.
    Each IP is a single channel DMA but which is video format aware
    supporting video data that arranged in memory as packed or semi-planar data.
    This driver introduces support for these IP and integrates with the
    DMA Engine framework.
    
    The 2017.3 version of the Video Framebuffer supports a separate address
    pointer for the chroma plane.  This is needed when the chroma plane
    is not contiguous with the luma plane for semi-planar formats.  This
    patch updates the client API as well.  Additionally, the IP can be
    configured for either 32-bit or 64-bit DMA address pointers.  A
    new device tree property is added which is used to indicate the address
    width and a callback is set during probe to write to memory using
    either 32-bit or 64-bit address formats depending on this dts property
    as well as the size of dma address space supported on the host.
    
    The driver used to assume that all IP supported video formats were
    legal choices for configuration.  However, the IP can be configured
    to support all or only some (or only one) of the many possible formats.
    This patch adds the needed mechanism via device-tree to communicate to
    the driver which video formats are actually supported in the IP.
    Additional changes are required to ensure that DMA client requests
    for video formats that are NOT supported by the device instance are
    rejected.
    
    The video Framebuffer DMA IP requires clients to send a fourcc code
    to indicate the memory format layout.  The IP can be configured to support
    a variety of memory formats ranging from YUYV, RGB and in either 8 bit
    or 10 bit formats.  There has been no method for clients to obtain
    this list of supported formats.  This patch adds private APIs that
    can be called from clients to retrieve this list so that user space
    applications can choose from any of the available memory formats.
    
    Some registers within the Video Framebuffer driver, such as the
    video format register, require a reset of the IP before they can
    be altered.  Because there is no software accessible reset register,
    an external GPIO is used.  This patch fixes a runtime issue wherein
    clients wish to reprogram the IP for a new memory between DMA operations.
    Without this fix, the Video Framebuffer Write IP may halt when a client
    requests a new DMA operation using a different memory format for
    writes to host memory.  In some cases, Framebuffer Read operations
    will need to be reset when the downstream video pipeline is being
    reset.
    
    Also move XILINX_FRMBUF out of XILINX_DMA_ENGINES
    
    Signed-off-by: Radhey Pandey <radheys@xilinx.com>
    Signed-off-by: John Nichols <jnichol@xilinx.com>
    Signed-off-by: Jeffrey Mouroux <jeff.mouroux@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: waiting (dmaengine)
    Link: https://www.spinics.net/lists/dmaengine/msg14761.html

---
+++ include/linux/dma/xilinx_frmbuf.h
@@ -0,0 +1,96 @@
+/*
+ * Xilinx Framebuffer DMA support header file
+ *
+ * Copyright (C) 2017 Xilinx, Inc. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef __XILINX_FRMBUF_DMA_H
+#define __XILINX_FRMBUF_DMA_H
+
+#include <linux/dmaengine.h>
+
+/**
+ * enum vid_frmwork_type - Linux video framework type
+ * @XDMA_DRM: fourcc is of type DRM
+ * @XDMA_V4L2: fourcc is of type V4L2
+ */
+enum vid_frmwork_type {
+	XDMA_DRM = 0,
+	XDMA_V4L2,
+};
+
+#if IS_ENABLED(CONFIG_XILINX_FRMBUF)
+/**
+ * xilinx_xdma_drm_config - configure video format in video aware DMA
+ * @chan: dma channel instance
+ * @drm_fourcc: DRM fourcc code describing the memory layout of video data
+ *
+ * This routine is used when utilizing "video format aware" Xilinx DMA IP
+ * (such as Video Framebuffer Read or Video Framebuffer Write).  This call
+ * must be made prior to dma_async_issue_pending() to enstablish the video
+ * data memory format within the hardware DMA.
+ */
+void xilinx_xdma_drm_config(struct dma_chan *chan, u32 drm_fourcc);
+
+/**
+ * xilinx_xdma_drm_config - configure video format in video aware DMA
+ * @chan: dma channel instance
+ * @v4l2_fourcc: V4L2 fourcc code describing the memory layout of video data
+ *
+ * This routine is used when utilizing "video format aware" Xilinx DMA IP
+ * (such as Video Framebuffer Read or Video Framebuffer Write).  This call
+ * must be made prior to dma_async_issue_pending() to enstablish the video
+ * data memory format within the hardware DMA.
+ */
+void xilinx_xdma_v4l2_config(struct dma_chan *chan, u32 v4l2_fourcc);
+
+/**
+ * xilinx_xdma_get_drm_vid_fmts - obtain list of supported DRM mem formats
+ * @chan: dma channel instance
+ * @fmt_cnt: Output param - total count of supported DRM fourcc codes
+ * @fmts: Output param - pointer to array of DRM fourcc codes (not a copy)
+ *
+ * Return: a reference to an array of DRM fourcc codes supported by this
+ * instance of the Video Framebuffer Driver
+ */
+int xilinx_xdma_get_drm_vid_fmts(struct dma_chan *chan, u32 *fmt_cnt,
+				 u32 **fmts);
+
+/**
+ * xilinx_xdma_get_v4l2_vid_fmts - obtain list of supported V4L2 mem formats
+ * @chan: dma channel instance
+ * @fmt_cnt: Output param - total count of supported V4L2 fourcc codes
+ * @fmts: Output param - pointer to array of V4L2 fourcc codes (not a copy)
+ *
+ * Return: a reference to an array of V4L2 fourcc codes supported by this
+ * instance of the Video Framebuffer Driver
+ */
+int xilinx_xdma_get_v4l2_vid_fmts(struct dma_chan *chan, u32 *fmt_cnt,
+				  u32 **fmts);
+#else
+static inline void xilinx_xdma_drm_config(struct dma_chan *chan, u32 drm_fourcc)
+{ }
+
+static inline void xilinx_xdma_v4l2_config(struct dma_chan *chan,
+					   u32 v4l2_fourcc)
+{ }
+
+static inline int xilinx_xdma_get_drm_vid_fmts(struct dma_chan *chan,
+					       u32 *fmt_cnt, u32 **fmts)
+{
+	return -ENODEV;
+}
+
+static inline int xilinx_xdma_get_v4l2_vid_fmts(struct dma_chan *chan,
+						u32 *fmt_cnt,u32 **fmts)
+{
+	return -ENODEV;
+}
+#endif
+
+#endif /*__XILINX_FRMBUF_DMA_H*/
