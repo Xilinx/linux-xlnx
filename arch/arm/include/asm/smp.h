f42fbafbc01522e4441aa13b7ddad219079b0b73
Michal Simek <michal.simek@xilinx.com>
2017-08-07 11:05:44 +0200

    arm: Add support for dynamic IPI allocation
    Inter processor interrupts are fixed in current implementation and can't
    be dynamically assign by drivers. In OpenAMP case on Zynq there is a need
    to use IPIs for communication between Linux and RTOS.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/include/asm/smp.h
+++ arch/arm/include/asm/smp.h
@@ -124,4 +124,7 @@ struct of_cpu_method {
  */
 extern void smp_set_ops(const struct smp_operations *);
 
+extern int set_ipi_handler(int ipinr, void *handler, char *desc);
+extern void clear_ipi_handler(int ipinr);
+
 #endif /* ifndef __ASM_ARM_SMP_H */
