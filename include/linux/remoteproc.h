6aba027b2ddbf211c98b1eb5dea48f55acab8d63
Sarangdhar Joshi <spjoshi@codeaurora.org>
2017-08-08 11:08:19 -0700

    remoteproc: Add RPROC_DELETED state
    Add new state RPROC_DELETED to handle synchronization
    between rproc_del() and other operations on rproc. This
    state represents the rproc device that has been "deleted".
    
    CC: Loic Pallardy <loic.pallardy@st.com>
    CC: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Sarangdhar Joshi <spjoshi@codeaurora.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Wendy Liang <jliang@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -384,6 +384,7 @@ struct rproc_ops {
  *			a message.
  * @RPROC_RUNNING:	device is up and running
  * @RPROC_CRASHED:	device has crashed; need to start recovery
+ * @RPROC_DELETED:	device is deleted
  * @RPROC_LAST:		just keep this one at the end
  *
  * Please note that the values of these states are used as indices
@@ -397,8 +398,9 @@ enum rproc_state {
 	RPROC_SUSPENDED	= 1,
 	RPROC_RUNNING	= 2,
 	RPROC_CRASHED	= 3,
-	RPROC_RUNNING_INDEPENDENT = 4,
-	RPROC_LAST	= 5,
+	RPROC_DELETED	= 4,
+	RPROC_RUNNING_INDEPENDENT = 5,
+	RPROC_LAST	= 6,
 };
 
 /**
