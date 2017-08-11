0b049fc4419587e3d88b855c74a6158b9d0d49e4
Rajnikant Bhojani <rajnikant.bhojani@xilinx.com>
2017-08-11 04:54:24 -0400

    uas: Add US_FL_NO_ATA_1X for linux tcm_usb_gadget
    tcm_usb_gadget available in linux kernel need US_FL_NO_ATA_1X quirk
    
    Signed-off-by: Rajnikant Bhojani <rajnikant.bhojani@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/storage/unusual_uas.h
+++ drivers/usb/storage/unusual_uas.h
@@ -51,6 +51,12 @@ UNUSUAL_DEV(0x0984, 0x0301, 0x0128, 0x0128,
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_IGNORE_UAS),
 
+UNUSUAL_DEV(0x0525, 0xa4a5, 0x0000, 0x9999,
+	    "Netchip",
+		"Target Product",
+		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
+		US_FL_NO_ATA_1X),
+
 /* https://bugzilla.kernel.org/show_bug.cgi?id=79511 */
 UNUSUAL_DEV(0x0bc2, 0x2312, 0x0000, 0x9999,
 		"Seagate",
