5606440edc61e3b6416908c7150878592153a866
Michal Simek <michal.simek@xilinx.com>
2017-09-11 14:52:50 +0200

    media: xilinx: Improve pipeline management code
    Introduce xvip_get_entity_sink() to improve pipeline management.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/media/platform/xilinx/xilinx-dma.c
+++ drivers/media/platform/xilinx/xilinx-dma.c
@@ -82,6 +82,35 @@ static int xvip_dma_verify_format(struct xvip_dma *dma)
  * Pipeline Stream Management
  */
 
+/* Get the sink pad internally connected to a source pad in the given entity. */
+static struct media_pad *xvip_get_entity_sink(struct media_entity *entity,
+					      struct media_pad *source)
+{
+	unsigned int i;
+
+	/* The source pad can be NULL when the entity has no source pad. Return
+	 * the first pad in that case, guaranteed to be a sink pad.
+	 */
+	if (source == NULL)
+		return &entity->pads[0];
+
+	/* Iterates through the pads to find a connected sink pad. */
+	for (i = 0; i < entity->num_pads; ++i) {
+		struct media_pad *sink = &entity->pads[i];
+
+		if (!(sink->flags & MEDIA_PAD_FL_SINK))
+			continue;
+
+		if (sink == source)
+			continue;
+
+		if (media_entity_has_route(entity, sink->index, source->index))
+			return sink;
+	}
+
+	return NULL;
+}
+
 /**
  * xvip_pipeline_start_stop - Start ot stop streaming on a pipeline
  * @pipe: The pipeline
@@ -102,8 +131,13 @@ static int xvip_pipeline_start_stop(struct xvip_pipeline *pipe, bool start)
 	int ret;
 
 	entity = &dma->video.entity;
+	pad = NULL;
+
 	while (1) {
-		pad = &entity->pads[0];
+		pad = xvip_get_entity_sink(entity, pad);
+		if (pad == NULL)
+			break;
+
 		if (!(pad->flags & MEDIA_PAD_FL_SINK))
 			break;
 
