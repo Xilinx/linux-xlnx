31edff1bee3887893c8bfb0e96253aacfe1555b7
Nava kishore Manne <nava.manne@xilinx.com>
2017-09-19 14:40:49 +0530

    soc: zynqmp: Added pm api functions for RSA and SHA
    This patch adds PM APIs to provided access to xilsecure
    library to calculate SHA3 hash on the data or
    decrypt the data by using RSA public or private
    keys respectively.
    
    Signed-off-by: Durga Challa <vnsldurg@xilinx.com>
    Signed-off-by: Nava kishore Manne <navam@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -59,8 +59,12 @@ enum pm_api_id {
 	PM_RESET_GET_STATUS,
 	PM_PM_INIT_FINALIZE = 21,
 	PM_GET_CHIPID = 24,
+	/* ID 25 is been used by U-boot to process secure boot images */
+	/* Secure library generic API functions */
+	PM_SECURE_SHA = 26,
+	PM_SECURE_RSA,
 	/* Pin control API functions */
-	PM_PINCTRL_REQUEST = 28,
+	PM_PINCTRL_REQUEST,
 	PM_PINCTRL_RELEASE,
 	PM_PINCTRL_GET_FUNCTION,
 	PM_PINCTRL_SET_FUNCTION,
@@ -431,6 +435,8 @@ struct zynqmp_eemi_ops {
 			       const u32 capabilities,
 			       const u32 qos,
 			       const enum zynqmp_pm_request_ack ack);
+	int (*sha_hash)(const u64 address, const u32 size, const u32 flags);
+	int (*rsa)(const u64 address, const u32 size, const u32 flags);
 	int (*pinctrl_request)(const u32 pin);
 	int (*pinctrl_release)(const u32 pin);
 	int (*pinctrl_get_function)(const u32 pin, u32 *id);
