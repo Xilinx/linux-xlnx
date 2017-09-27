b0835db5e30112d72d348f14c565c712d7b9d6dd
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2017-09-27 19:35:07 +0530

    usb: xhci-plat: Add remote wakeup support
    This patch adds support for enabling remote wakeup capability
    to the host controller
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -200,6 +200,9 @@ static int xhci_plat_probe(struct platform_device *pdev)
 			return ret;
 	}
 
+	/* Set the controller as wakeup capable */
+	device_set_wakeup_capable(&pdev->dev, true);
+
 	hcd = usb_create_hcd(driver, &pdev->dev, dev_name(&pdev->dev));
 	if (!hcd)
 		return -ENOMEM;
@@ -334,10 +337,12 @@ static int xhci_plat_suspend(struct device *dev)
 
 #if IS_ENABLED(CONFIG_USB_DWC3_OF_SIMPLE)
 	/* Inform dwc3 driver about the device wakeup capability */
-	if (device_may_wakeup(dev))
+	if (device_may_wakeup(&hcd->self.root_hub->dev)) {
+		enable_irq_wake(hcd->irq);
 		dwc3_host_wakeup_capable(dev, true);
-	else
+	} else {
 		dwc3_host_wakeup_capable(dev, false);
+	}
 #endif
 
 	/*
@@ -355,6 +360,9 @@ static int xhci_plat_resume(struct device *dev)
 {
 	struct usb_hcd	*hcd = dev_get_drvdata(dev);
 	struct xhci_hcd	*xhci = hcd_to_xhci(hcd);
+
+	if (device_may_wakeup(&hcd->self.root_hub->dev))
+		disable_irq_wake(hcd->irq);
 
 	return xhci_resume(xhci, 0);
 }
