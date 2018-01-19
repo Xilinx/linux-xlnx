55d6590d590285d56140712f238f459a00938100
Hyun Kwon <hyun.kwon@xilinx.com>
2018-01-18 18:05:06 -0800

    drm: xlnx: Xilinx DRM KMS driver
    Xilinx has various platforms for display, where users can create
    using multiple IPs in the programmable FPGA fabric, or where
    some hardened piepline is available on the chip. Furthermore,
    hardened pipeline can also interact with soft logics in FPGA.
    
    The Xilinx DRM KMS is to integrate multiple subdevices and to represent
    the entire pipeline as a single DRM device. The driver includes helpers
    (ex, framebuffer and gem helpers) and glue logics (ex, crtc interface).
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ drivers/gpu/drm/xlnx/xlnx_gem.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Xilinx DRM KMS GEM helper header
+ *
+ *  Copyright (C) 2015 - 2018 Xilinx, Inc.
+ *
+ *  Author: Hyun Woo Kwon <hyun.kwon@xilinx.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _XLNX_GEM_H_
+#define _XLNX_GEM_H_
+
+int xlnx_gem_cma_dumb_create(struct drm_file *file_priv,
+			     struct drm_device *drm,
+			     struct drm_mode_create_dumb *args);
+
+#endif /* _XLNX_GEM_H_ */
