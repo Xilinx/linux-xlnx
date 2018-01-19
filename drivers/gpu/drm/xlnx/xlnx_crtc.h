97e7df4b6766754ec6fc2bd06c6bca42f85665a2
Hyun Kwon <hyun.kwon@xilinx.com>
2018-01-18 18:05:06 -0800

    drm: xlnx: Xilinx DRM KMS driver
    Xilinx has various platforms for display, where users can create
    using multiple IPs in the programmable FPGA fabric, or where
    some hardened piepline is available on the chip. Furthermore,
    hardened pipeline can also interact with soft logics in FPGA.
    
    The Xilinx DRM KMS is to integrate multiple subdevices and to represent
    the entire pipeline as a single DRM device. The driver includes helpers
    (ex, framebuffer and gem helpers) and glue logics (ex, crtc interface).
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: waiting (drm)
    Link: https://lists.freedesktop.org/archives/dri-devel/2019-November/243410.html
    [michal: s/drmP.h/drm_drv.h, add missing headers, remove gem_free_object in
    xlnx_drv.c, fix drm_fb_helper_init() and remove
    drm_fb_helper_single_add_all_connectors()]
    [michals: Squashed with
    drm: xlnx: drv: add gem_free_object_unlocked callback xlnx_drm_driver
    drm: xlnx: drv: Shutdown the drm device properly
    drm: xlnx: drv: Allow multiple masters on a drm device
    drm: xlnx: xlnx_drv: Remove the dt binding
    drm: xlnx: drv: drop master only while releasing last master on a drm device
    drm: xlnx: drv: unbind all components before framebuffer cleanup
    drm: xlnx: fb: Prefer the current format depth over depth from fb helper
    drm: xlnx: fb: Guard with CONFIG_DRM_FBDEV_EMULATION
    drm: xlnx: fb: Fix OVERFLOW_BEFORE_WIDEN type warnings
    v5.15 update
    v6.1 - s/drm_gem_cma_helper.h/drm_gem_dma_helper.h/
    Use GEM DMA helpers instead of GEM CMA helpers.
    ]

---
+++ drivers/gpu/drm/xlnx/xlnx_crtc.h
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Xilinx DRM crtc header
+ *
+ *  Copyright (C) 2017 - 2018 Xilinx, Inc.
+ *
+ *  Author: Hyun Woo Kwon <hyun.kwon@xilinx.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _XLNX_CRTC_H_
+#define _XLNX_CRTC_H_
+
+/**
+ * struct xlnx_crtc - Xilinx CRTC device
+ * @crtc: DRM CRTC device
+ * @list: list node for Xilinx CRTC device list
+ * @get_align: Get the alignment requirement of CRTC device
+ * @get_dma_mask: Get the dma mask of CRTC device
+ * @get_max_width: Get the maximum supported width
+ * @get_max_height: Get the maximum supported height
+ * @get_format: Get the current format of CRTC device
+ */
+struct xlnx_crtc {
+	struct drm_crtc crtc;
+	struct list_head list;
+	unsigned int (*get_align)(struct xlnx_crtc *crtc);
+	u64 (*get_dma_mask)(struct xlnx_crtc *crtc);
+	int (*get_max_width)(struct xlnx_crtc *crtc);
+	int (*get_max_height)(struct xlnx_crtc *crtc);
+	uint32_t (*get_format)(struct xlnx_crtc *crtc);
+};
+
+/*
+ * Helper functions: used within Xlnx DRM
+ */
+
+struct xlnx_crtc_helper;
+
+unsigned int xlnx_crtc_helper_get_align(struct xlnx_crtc_helper *helper);
+u64 xlnx_crtc_helper_get_dma_mask(struct xlnx_crtc_helper *helper);
+int xlnx_crtc_helper_get_max_width(struct xlnx_crtc_helper *helper);
+int xlnx_crtc_helper_get_max_height(struct xlnx_crtc_helper *helper);
+uint32_t xlnx_crtc_helper_get_format(struct xlnx_crtc_helper *helper);
+
+struct xlnx_crtc_helper *xlnx_crtc_helper_init(struct drm_device *drm);
+void xlnx_crtc_helper_fini(struct drm_device *drm,
+			   struct xlnx_crtc_helper *helper);
+
+/*
+ * CRTC registration: used by other sub-driver modules
+ */
+
+static inline struct xlnx_crtc *to_xlnx_crtc(struct drm_crtc *crtc)
+{
+	return container_of(crtc, struct xlnx_crtc, crtc);
+}
+
+void xlnx_crtc_register(struct drm_device *drm, struct xlnx_crtc *crtc);
+void xlnx_crtc_unregister(struct drm_device *drm, struct xlnx_crtc *crtc);
+
+#endif /* _XLNX_CRTC_H_ */
