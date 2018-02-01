a6151ffbe73ac502ce80cdbff4362f412250d396
Rajan Vaja <rajan.vaja@xilinx.com>
2018-01-31 20:58:55 -0800

    soc: zynqmp: Use SPDX and update author
    Use SPDX licence identifier and update author
    list based on upstream changes.
    
    Signed-off-by: Rajan Vaja <rajanv@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/soc/xilinx/zynqmp/pm_domains.c
+++ drivers/soc/xilinx/zynqmp/pm_domains.c
@@ -1,22 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * ZynqMP Generic PM domain support
  *
- *  Copyright (C) 2015 Xilinx
+ *  Copyright (C) 2015-2018 Xilinx, Inc.
  *
  *  Davorin Mista <davorin.mista@aggios.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  Jolly Shah <jollys@xilinx.com>
+ *  Rajan Vaja <rajanv@xilinx.com>
  */
 
 #include <linux/err.h>
