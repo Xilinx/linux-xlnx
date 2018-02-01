2fdcb07b4ac5123c15587159b227d42d66b47a45
Rajan Vaja <rajan.vaja@xilinx.com>
2018-01-31 20:59:11 -0800

    clk: zynqmp: Use SPDX and update author
    Use SPDX licence identifier and update author
    list in zynqmp/clk driver based on upstream changes.
    
    Signed-off-by: Rajan Vaja <rajanv@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/clk/zynqmp/divider.c
+++ drivers/clk/zynqmp/divider.c
@@ -1,10 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Zynq UltraScale+ MPSoC Divider support
  *
- *  Copyright (C) 2016 Xilinx
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ *  Copyright (C) 2016-2018 Xilinx
  *
  * Adjustable divider clock implementation
  */
