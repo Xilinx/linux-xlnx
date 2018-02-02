f02a531dd69dce767ab676bdfb2b17176c3a754f
Saurabh Sengar <saurabh.singh@xilinx.com>
2018-02-02 13:50:31 +0530

    drm: xlnx: sdi: Adding sdi-tx driver
    Adding sdi transmitter subsystem driver
    SDI transmitter subsystem implements a SDI transmit interface in
    accordance to the serial digital interface (SDI) family of standards.
    The subsystem accepts video from AXI-4 Stream Video interface and outputs
    Native Video stream, and allows for fast selection of the top-level
    parameters and automates most of the lower level parameterization. The
    AXI4-Stream video interface allows a seamless interface to other
    AXI4-Stream-based subsystems.
    
    Signed-off-by: Saurabh Sengar <saurabh.singh@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

---
+++ drivers/gpu/drm/xlnx/xlnx_sdi_timing.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Xilinx FPGA SDI Tx timing controller driver
+ *
+ * Copyright (c) 2017 Xilinx Pvt., Ltd
+ *
+ * Contacts: Saurabh Sengar <saurabhs@xilinx.com>
+ */
+
+#ifndef _XLNX_SDI_TIMING_H_
+#define _XLNX_SDI_TIMING_H_
+
+struct videomode;
+
+void xlnx_stc_enable(void __iomem *base);
+void xlnx_stc_disable(void __iomem *base);
+void xlnx_stc_reset(void __iomem *base);
+void xlnx_stc_sig(void __iomem *base, struct videomode *vm);
+
+#endif /* _XLNX_SDI_TIMING_H_ */
