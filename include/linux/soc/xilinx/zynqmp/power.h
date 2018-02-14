2c56e23e6cf899746afa69a4c365e8ee2d0829bc
Rajan Vaja <rajan.vaja@xilinx.com>
2018-02-14 05:16:55 -0800

    drivers: soc: xilinx: Add ZynqMP PM driver
    Add ZynqMP PM driver. PM driver provides power management
    support for ZynqMP.
    
    Signed-off-by: Jolly Shah <jollys@xilinx.com>
    Signed-off-by: Rajan Vaja <rajanv@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ include/linux/soc/xilinx/zynqmp/power.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Xilinx Zynq MPSoC Power Management
+ *
+ *  Copyright (C) 2014-2018 Xilinx, Inc.
+ *
+ *  Davorin Mista <davorin.mista@aggios.com>
+ *  Jolly Shah <jollys@xilinx.com>
+ *  Rajan Vaja <rajanv@xilinx.com>
+ */
+
+#ifndef __SOC_XILINX_ZYNQMP_PM_H__
+#define __SOC_XILINX_ZYNQMP_PM_H__
+
+#define ZYNQMP_PM_MAX_LATENCY	(~0U)
+#define ZYNQMP_PM_MAX_QOS	100U
+
+#endif /* __SOC_XILINX_ZYNQMP_PM_H__ */
