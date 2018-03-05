03f6aacd9ef69c5538fd0ca633804d4fd6dadddd
Harini Katakam <harini.katakam@xilinx.com>
2018-03-05 19:24:36 +0530

    net: xilinx: axiethernet: Add USXGMII support
    This patch adds support for USXGMII IP in axiethernet driver.
    This IP has a MAC similar to 10G/25G IP and supports USXGMII phy protocol.
    USXGMII phy supports speeds from 10Mbps to 10Gbps. Only one phy speed
    can be advertised at a time and this choice is obtained from the user
    via a devicetree property. This patch was tested at 1G and 2.5G speeds.
    Since the MAC functionality is similar to 10G/25G, the same mac type is
    used in the config structure. USXGMII IP requires a GT reset and this is
    added to axienet_device_reset with a check based on mac type.
    
    Signed-off-by: Harini Katakam <harinik@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radheys@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/ethernet/xilinx/xilinx_axienet.h
+++ drivers/net/ethernet/xilinx/xilinx_axienet.h
@@ -372,13 +372,17 @@
 #endif
 
 /* XXV MAC Register Definitions */
+#define XXV_GT_RESET_OFFSET		0x00000000
 #define XXV_TC_OFFSET			0x0000000C
 #define XXV_RCW1_OFFSET			0x00000014
 #define XXV_JUM_OFFSET			0x00000018
 #define XXV_TICKREG_OFFSET		0x00000020
 #define XXV_STATRX_BLKLCK_OFFSET	0x0000040C
+#define XXV_USXGMII_AN_OFFSET		0x000000C8
+#define XXV_USXGMII_AN_STS_OFFSET	0x00000458
 
 /* XXV MAC Register Mask Definitions */
+#define XXV_GT_RESET_MASK	BIT(0)
 #define XXV_TC_TX_MASK		BIT(0)
 #define XXV_RCW1_RX_MASK	BIT(0)
 #define XXV_RCW1_FCS_MASK	BIT(1)
@@ -388,6 +392,24 @@
 #define XXV_RX_BLKLCK_MASK	BIT(0)
 #define XXV_TICKREG_STATEN_MASK BIT(0)
 #define XXV_MAC_MIN_PKT_LEN	64
+
+/* USXGMII Register Mask Definitions  */
+#define USXGMII_AN_EN		BIT(5)
+#define USXGMII_AN_RESET	BIT(6)
+#define USXGMII_AN_RESTART	BIT(7)
+#define USXGMII_EN		BIT(16)
+#define USXGMII_RATE_MASK	0x0E000700
+#define USXGMII_RATE_1G		0x04000200
+#define USXGMII_RATE_2G5	0x08000400
+#define USXGMII_RATE_10M	0x0
+#define USXGMII_RATE_100M	0x02000100
+#define USXGMII_RATE_5G		0x0A000500
+#define USXGMII_RATE_10G	0x06000300
+#define USXGMII_FD		BIT(28)
+#define USXGMII_LINK_STS	BIT(31)
+
+/* USXGMII AN STS register mask definitions */
+#define USXGMII_AN_STS_COMP_MASK	BIT(16)
 
 /* MCDMA Register Definitions */
 #define XMCDMA_CR_OFFSET	0x00
@@ -631,6 +653,7 @@ enum axienet_tsn_ioctl {
  * @chan_num: MCDMA Channel number to be operate on.
  * @chan_id:  MCMDA Channel id used in conjunction with weight parameter.
  * @weight:   MCDMA Channel weight value to be configured for.
+ * @usxgmii_rate: USXGMII PHY speed.
  */
 struct axienet_local {
 	struct net_device *ndev;
@@ -707,6 +730,8 @@ struct axienet_local {
 	/* WRR Fields */
 	u16 chan_id;
 	u16 weight;
+
+	u32 usxgmii_rate;
 };
 
 /**
