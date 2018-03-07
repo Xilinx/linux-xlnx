2e6c505da4e253026e81e19cb696eaf829e3a695
Vishal Sagar <vishal.sagar@xilinx.com>
2018-03-07 10:16:50 +0530

    dmaengine: xilinx_frmbuf: Add support for BGR8 color format in Framebuffer
    Add support for BGR8 color format (Blue in LSB, 8 bits per component)
    in Framebuffer.
    
    Signed-off-by: Vishal Sagar <vsagar@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/dma/xilinx/xilinx_frmbuf.c
+++ drivers/dma/xilinx/xilinx_frmbuf.c
@@ -90,6 +90,7 @@
 #define XILINX_FRMBUF_FMT_BGRA8			26
 #define XILINX_FRMBUF_FMT_BGRX8			27
 #define XILINX_FRMBUF_FMT_UYVY8			28
+#define XILINX_FRMBUF_FMT_BGR8				29
 
 /**
  * struct xilinx_frmbuf_desc_hw - Hardware Descriptor
@@ -212,7 +213,7 @@ static const struct xilinx_frmbuf_format_desc xilinx_frmbuf_formats[] = {
 		.ppw = 1,
 		.num_planes = 1,
 		.drm_fmt = DRM_FORMAT_XRGB8888,
-		.v4l2_fmt = V4L2_PIX_FMT_XRGB32,
+		.v4l2_fmt = V4L2_PIX_FMT_XBGR32,
 		.fmt_bitmask = BIT(2),
 	},
 	{
@@ -354,6 +355,16 @@ static const struct xilinx_frmbuf_format_desc xilinx_frmbuf_formats[] = {
 		.drm_fmt = DRM_FORMAT_Y10,
 		.v4l2_fmt = V4L2_PIX_FMT_Y10,
 		.fmt_bitmask = BIT(17),
+	},
+	{
+		.dts_name = "rgb888",
+		.id = XILINX_FRMBUF_FMT_BGR8,
+		.bpw = 24,
+		.ppw = 1,
+		.num_planes = 1,
+		.drm_fmt = DRM_FORMAT_RGB888,
+		.v4l2_fmt = V4L2_PIX_FMT_BGR24,
+		.fmt_bitmask = BIT(18),
 	},
 };
 
