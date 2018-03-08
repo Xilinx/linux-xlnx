14f1d44ce1be70ca4e2250d34bcd2963482934b0
David S. Miller <davem@davemloft.net>
2018-03-08 18:15:52 +0530

    macb: Remove bogus reference to NETIF_F_UFO.
    This driver doesn't actually support UFO explicitly yet
    it advertises this in netdev->features.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -71,7 +71,7 @@
 #define GEM_MAX_TX_LEN		((unsigned int)((1 << GEM_TX_FRMLEN_SIZE) - 1) & ~((unsigned int)(MACB_TX_LEN_ALIGN - 1)))
 
 #define GEM_MTU_MIN_SIZE	ETH_MIN_MTU
-#define MACB_NETIF_LSO		(NETIF_F_TSO | NETIF_F_UFO)
+#define MACB_NETIF_LSO		NETIF_F_TSO
 
 /* Graceful stop timeouts in us. We should allow up to
  * 1 frame time (10 Mbits/s, full-duplex, ignoring collisions)
