da50697432972479adbfaed7979102417bcec809
Hyun Kwon <hyun.kwon@xilinx.com>
2018-03-08 17:44:23 -0800

    uapi: drm: Add AVUY fourcc
    AVUY is a 32bit single plane YUV format, with 2 bit alpha,
    and in order of A:Cr:Cb:Y in little endian.
    
    Signed-off-by: Jeffrey Mouroux <jmouroux@xilinx.com>
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Reviewed-by: Satish Kumar Nagireddy <satishna@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/gpu/drm/drm_fourcc.c
+++ drivers/gpu/drm/drm_fourcc.c
@@ -175,6 +175,7 @@ const struct drm_format_info *__drm_format_info(u32 format)
 		{ .format = DRM_FORMAT_UYVY,		.depth = 0,  .num_planes = 1, .cpp = { 2, 0, 0 }, .hsub = 2, .vsub = 1 },
 		{ .format = DRM_FORMAT_VYUY,		.depth = 0,  .num_planes = 1, .cpp = { 2, 0, 0 }, .hsub = 2, .vsub = 1 },
 		{ .format = DRM_FORMAT_AYUV,		.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1, .has_alpha = true },
+		{ .format = DRM_FORMAT_AVUY,		.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1, .has_alpha = true },
 		{ .format = DRM_FORMAT_VUY888,		.depth = 0,  .num_planes = 1, .cpp = { 3, 0, 0 }, .hsub = 1, .vsub = 1 },
 		{ .format = DRM_FORMAT_XVUY8888,	.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1 },
 		{ .format = DRM_FORMAT_XVUY2101010,	.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1 },
