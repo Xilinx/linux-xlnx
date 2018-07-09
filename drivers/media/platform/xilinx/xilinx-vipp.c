c11064fb462120e5f5cdd5b16ab47e8ce31fab4a
Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
2018-07-09 00:26:55 -0700

    xilinx: v4l: dma: Multi-threading issue
    The issue occurs in case of capture pipeline has multiple
    dmas, for e.g.
    
                         |--> dma (/dev/video0)
    Camera --> switch -->
                         |--> dma2 (/dev/video1)
    
    In the function, xvip_dma_start_streaming(), driver will invoke media
    framework API media_entity_pipeline_start(). This function would assign
    a pipeline object to all entities in the whole capture pipeline. In
    current driver implementation, the pipeline object embedded in the first
    DMA will be used for this purpose.
    
    If application starts with video0 and video1 with slight time difference
    there will be two instances of dma accessing same driver and second
    application might call media_entity_pipeline_start() when the first
    application is executing the same function. In this case second
    application will get different pipleine object.
    
    Mutex variable is introduced to solve the issue.
    
    Signed-off-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/media/platform/xilinx/xilinx-vipp.c
+++ drivers/media/platform/xilinx/xilinx-vipp.c
@@ -658,6 +658,7 @@ static int xvip_composite_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	xdev->dev = &pdev->dev;
+	mutex_init(&xdev->lock);
 	INIT_LIST_HEAD(&xdev->entities);
 	INIT_LIST_HEAD(&xdev->dmas);
 
@@ -684,6 +685,7 @@ static int xvip_composite_remove(struct platform_device *pdev)
 {
 	struct xvip_composite_device *xdev = platform_get_drvdata(pdev);
 
+	mutex_destroy(&xdev->lock);
 	xvip_graph_cleanup(xdev);
 	xvip_composite_v4l2_cleanup(xdev);
 
