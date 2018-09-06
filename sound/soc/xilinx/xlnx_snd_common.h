63b70f7a516d40a86e37bb23ce3a9567c50bdf6d
Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
2018-09-06 17:43:31 +0530

    ASoC: xlnx: add PL sound card device creation
    Sound card device is a logical device for which there is no device tree
    node. This device is created if any of the source,sink node is found in
    parsing of audio formatter device tree node. These source/sink nodes will
    be used in sound card creation.
    
    Signed-off-by: Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

---
+++ sound/soc/xilinx/xlnx_snd_common.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Xilinx ASoC sound card support
+ *
+ * Copyright (C) 2018 Xilinx, Inc.
+ */
+
+#ifndef _XLNX_SND_COMMON_H
+#define _XLNX_SND_COMMON_H
+
+enum {
+	XLNX_PLAYBACK,
+	XLNX_CAPTURE,
+	XLNX_MAX_PATHS
+};
+
+#endif /* _XLNX_SND_COMMON_H */
