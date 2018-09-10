fb21bd55dd3fad91298885d3f53a120b29281a62
Rajan Vaja <rajan.vaja@xilinx.com>
2018-09-09 22:37:32 -0700

    soc: xilinx: Move structure definition to header file
    Move xvcu_device structure definition to header file
    as it will be used by other vcu files.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/soc/xilinx/xlnx_vcu.h
+++ include/soc/xilinx/xlnx_vcu.h
@@ -10,7 +10,23 @@
 #ifndef _XILINX_VCU_H_
 #define _XILINX_VCU_H_
 
-struct xvcu_device;
+/**
+ * struct xvcu_device - Xilinx VCU init device structure
+ * @dev: Platform device
+ * @pll_ref: pll ref clock source
+ * @aclk: axi clock source
+ * @logicore_reg_ba: logicore reg base address
+ * @vcu_slcr_ba: vcu_slcr Register base address
+ * @coreclk: core clock frequency
+ */
+struct xvcu_device {
+	struct device *dev;
+	struct clk *pll_ref;
+	struct clk *aclk;
+	void __iomem *logicore_reg_ba;
+	void __iomem *vcu_slcr_ba;
+	u32 coreclk;
+};
 
 u32 xvcu_get_color_depth(struct xvcu_device *xvcu);
 u32 xvcu_get_memory_depth(struct xvcu_device *xvcu);
