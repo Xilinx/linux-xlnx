11a51f1956a432ac930e8aabbb43f17de09b3c42
Dhaval Shah <dhaval.shah@xilinx.com>
2018-10-01 09:27:49 -0700

    soc: xilinx: vcu: Update vcu init/reset sequence
    Updated vcu init/reset sequence as per design changes.
    If VCU reset GPIO is available then do assert and de-assert it before
    enabling/disabling gasket isolation.
    This GPIO is added because gasket isolation will be removed during startup
    that requires access to SLCR register space. Post startup, the ownership of
    the register interface lies with logiCORE IP
    
    Signed-off-by: Dhaval Shah <dhaval.shah@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/soc/xilinx/xlnx_vcu.h
+++ include/soc/xilinx/xlnx_vcu.h
@@ -17,6 +17,7 @@
  * struct xvcu_device - Xilinx VCU init device structure
  * @dev: Platform device
  * @pll_ref: pll ref clock source
+ * @reset_gpio: vcu reset gpio
  * @aclk: axi clock source
  * @logicore_reg_ba: logicore reg base address
  * @vcu_slcr_ba: vcu_slcr Register base address
@@ -25,6 +26,7 @@ struct xvcu_device {
 	struct device *dev;
 	struct clk *pll_ref;
 	struct clk *aclk;
+	struct gpio_desc *reset_gpio;
 	void __iomem *logicore_reg_ba;
 	void __iomem *vcu_slcr_ba;
 };
