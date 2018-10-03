952fe3cf95a22472ba01a1d94c1384635718e1c9
Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
2018-10-03 18:18:45 +0530

    ASoC: xlnx: enable axi clock in audio formatter
    A phandle to axi clock is obtained and clock is enabled/disabled
    appropriately.
    
    Signed-off-by: Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- sound/soc/xilinx/xlnx_formatter_pcm.c
+++ sound/soc/xilinx/xlnx_formatter_pcm.c
@@ -6,6 +6,7 @@
  *
  */
 
+#include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of_address.h>
@@ -125,6 +126,7 @@ struct xlnx_pcm_drv_data {
 	struct snd_pcm_substream *capture_stream;
 	struct platform_device *pdev;
 	struct device_node *nodes[XLNX_MAX_PATHS];
+	struct clk *axi_clk;
 };
 
 /*
@@ -641,16 +643,31 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 	if (!aud_drv_data)
 		return -ENOMEM;
 
+	aud_drv_data->axi_clk = devm_clk_get(&pdev->dev, "s_axi_lite_aclk");
+	if (IS_ERR(aud_drv_data->axi_clk)) {
+		ret = PTR_ERR(aud_drv_data->axi_clk);
+		dev_err(&pdev->dev, "failed to get s_axi_lite_aclk(%d)\n", ret);
+		return ret;
+	}
+	ret = clk_prepare_enable(aud_drv_data->axi_clk);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"failed to enable s_axi_lite_aclk(%d)\n", ret);
+		return ret;
+	}
+
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res) {
 		dev_err(&pdev->dev,
 			"audio formatter node:addr to resource failed\n");
-		return -ENXIO;
+		ret = -ENXIO;
+		goto clk_err;
 	}
 	aud_drv_data->mmio = devm_ioremap_resource(&pdev->dev, res);
 	if (IS_ERR(aud_drv_data->mmio)) {
 		dev_err(&pdev->dev, "audio formatter ioremap failed\n");
-		return PTR_ERR(aud_drv_data->mmio);
+		ret = PTR_ERR(aud_drv_data->mmio);
+		goto clk_err;
 	}
 
 	val = readl(aud_drv_data->mmio + XLNX_AUD_CORE_CONFIG);
@@ -658,9 +675,10 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 		aud_drv_data->mm2s_presence = true;
 		aud_drv_data->mm2s_irq = platform_get_irq_byname(pdev,
 								 "irq_mm2s");
-		if (!aud_drv_data->mm2s_irq) {
+		if (aud_drv_data->mm2s_irq < 0) {
 			dev_err(&pdev->dev, "xlnx audio mm2s irq resource failed\n");
-			return aud_drv_data->mm2s_irq;
+			ret = aud_drv_data->mm2s_irq;
+			goto clk_err;
 		}
 		ret = devm_request_irq(&pdev->dev, aud_drv_data->mm2s_irq,
 				       xlnx_mm2s_irq_handler, 0,
@@ -668,7 +686,7 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 				       &pdev->dev);
 		if (ret) {
 			dev_err(&pdev->dev, "xlnx audio mm2s irq request failed\n");
-			return ret;
+			goto clk_err;
 		}
 		xlnx_formatter_pcm_reset(aud_drv_data->mmio + XLNX_MM2S_OFFSET);
 
@@ -686,9 +704,10 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 		aud_drv_data->s2mm_presence = true;
 		aud_drv_data->s2mm_irq = platform_get_irq_byname(pdev,
 								 "irq_s2mm");
-		if (!aud_drv_data->s2mm_irq) {
+		if (aud_drv_data->s2mm_irq < 0) {
 			dev_err(&pdev->dev, "xlnx audio s2mm irq resource failed\n");
-			return aud_drv_data->s2mm_irq;
+			ret = aud_drv_data->s2mm_irq;
+			goto clk_err;
 		}
 		ret = devm_request_irq(&pdev->dev, aud_drv_data->s2mm_irq,
 				       xlnx_s2mm_irq_handler, 0,
@@ -696,7 +715,7 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 				       &pdev->dev);
 		if (ret) {
 			dev_err(&pdev->dev, "xlnx audio s2mm irq request failed\n");
-			return ret;
+			goto clk_err;
 		}
 		xlnx_formatter_pcm_reset(aud_drv_data->mmio + XLNX_S2MM_OFFSET);
 
@@ -716,7 +735,7 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 	ret = devm_snd_soc_register_platform(&pdev->dev, &xlnx_asoc_platform);
 	if (ret) {
 		dev_err(&pdev->dev, "pcm platform device register failed\n");
-		return ret;
+		goto clk_err;
 	}
 
 	pdata_size = sizeof(aud_drv_data->nodes);
@@ -732,6 +751,10 @@ static int xlnx_formatter_pcm_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "sound card device creation failed\n");
 
 	dev_info(&pdev->dev, "pcm platform device registered\n");
+	return 0;
+
+clk_err:
+	clk_disable_unprepare(aud_drv_data->axi_clk);
 	return ret;
 }
 
@@ -747,6 +770,7 @@ static int xlnx_formatter_pcm_remove(struct platform_device *pdev)
 	if (adata->mm2s_presence)
 		xlnx_formatter_pcm_reset(adata->mmio + XLNX_MM2S_OFFSET);
 
+	clk_disable_unprepare(adata->axi_clk);
 	return 0;
 }
 
