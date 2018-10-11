b5a5f82611e2ddf7689e5bfe0ca840de24579dd4
Sam Bobrowicz <sam@elite-embedded.com>
2018-10-10 17:21:55 -0700

    xilinx: v4l: dma: Invoke s_power function of subdevice
    This patch enables driver to invoke s_power, which is part of subdev
    core APIs. This can put subdevice in power saving mode or normal
    operation mode.
    
    Signed-off-by: Sam Bobrowicz <sam@elite-embedded.com>
    Signed-off-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/media/platform/xilinx/xilinx-dma.c
+++ drivers/media/platform/xilinx/xilinx-dma.c
@@ -137,22 +137,46 @@ static int xvip_pipeline_start_stop(struct xvip_composite_device *xdev,
 		 * start or stop the subdev only once in case if they are
 		 * shared between sub-graphs
 		 */
-		if (start != is_streaming) {
-			ret = v4l2_subdev_call(subdev, video, s_stream,
-					       start);
-			if (start && ret < 0 && ret != -ENOIOCTLCMD) {
-				dev_err(xdev->dev, "s_stream is failed on subdev\n");
-				xvip_subdev_set_streaming(xdev, subdev, !start);
+		if (start && !is_streaming) {
+			/* power-on subdevice */
+			ret = v4l2_subdev_call(subdev, core, s_power, 1);
+			if (ret < 0 && ret != -ENOIOCTLCMD) {
+				dev_err(xdev->dev,
+					"s_power on failed on subdev\n");
+				xvip_subdev_set_streaming(xdev, subdev, 0);
 				goto error;
 			}
+
+			/* stream-on subdevice */
+			ret = v4l2_subdev_call(subdev, video, s_stream, 1);
+			if (ret < 0 && ret != -ENOIOCTLCMD) {
+				dev_err(xdev->dev,
+					"s_stream on failed on subdev\n");
+				v4l2_subdev_call(subdev, core, s_power, 0);
+				xvip_subdev_set_streaming(xdev, subdev, 0);
+			}
+		} else if (!start && is_streaming) {
+			/* stream-off subdevice */
+			ret = v4l2_subdev_call(subdev, video, s_stream, 0);
+			if (ret < 0 && ret != -ENOIOCTLCMD) {
+				dev_err(xdev->dev,
+					"s_stream off failed on subdev\n");
+				xvip_subdev_set_streaming(xdev, subdev, 1);
+			}
+
+			/* power-off subdevice */
+			ret = v4l2_subdev_call(subdev, core, s_power, 0);
+			if (ret < 0 && ret != -ENOIOCTLCMD)
+				dev_err(xdev->dev,
+					"s_power off failed on subdev\n");
+
 		}
 	}
 
 error:
 	mutex_unlock(&mdev->graph_mutex);
 	media_graph_walk_cleanup(&graph);
-
-	return 0;
+	return ret;
 }
 
 /**
