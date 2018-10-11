a2b26bd6dac8082c9355fb4619fe864b0066cdcc
Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
2018-10-11 15:54:25 +0530

    ASoC: xlnx: support multiple sampling rates for HDMI
    This adds support for HDMI playback at various supported sampling rates.
    
    Signed-off-by: Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
    Reviewed-by: Sandip Kothari <sandipk@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- sound/soc/xilinx/xlnx_pl_snd_card.c
+++ sound/soc/xilinx/xlnx_pl_snd_card.c
@@ -41,6 +41,33 @@ static struct snd_soc_card xlnx_card = {
 	.owner = THIS_MODULE,
 };
 
+static int xlnx_hdmi_card_hw_params(struct snd_pcm_substream *substream,
+				    struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct pl_card_data *prv = snd_soc_card_get_drvdata(rtd->card);
+	u32 sample_rate = params_rate(params);
+
+	switch (sample_rate) {
+	case 32000:
+	case 44100:
+	case 48000:
+	case 96000:
+	case 176400:
+	case 192000:
+		prv->mclk_ratio = 768;
+		break;
+	case 88200:
+		prv->mclk_ratio = 192;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	prv->mclk_val = prv->mclk_ratio * sample_rate;
+	return clk_set_rate(prv->mclk, prv->mclk_val);
+}
+
 static int xlnx_i2s_card_hw_params(struct snd_pcm_substream *substream,
 				   struct snd_pcm_hw_params *params)
 {
@@ -112,6 +139,10 @@ static const struct snd_soc_ops xlnx_i2s_card_ops = {
 	.hw_params = xlnx_i2s_card_hw_params,
 };
 
+static const struct snd_soc_ops xlnx_hdmi_card_ops = {
+	.hw_params = xlnx_hdmi_card_hw_params,
+};
+
 static struct snd_soc_dai_link xlnx_snd_dai[][XLNX_MAX_PATHS] = {
 	[I2S_AUDIO] = {
 		{
@@ -133,6 +164,7 @@ static struct snd_soc_dai_link xlnx_snd_dai[][XLNX_MAX_PATHS] = {
 			.codec_dai_name = "i2s-hifi",
 			.codec_name = "hdmi-audio-codec.0",
 			.cpu_dai_name = "snd-soc-dummy-dai",
+			.ops = &xlnx_hdmi_card_ops,
 		},
 		{
 			.name = "xilinx-hdmi-capture",
