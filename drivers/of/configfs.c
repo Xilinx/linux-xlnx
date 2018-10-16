926dad4380a1812d24f5aecb9671ac67cbdcbd23
Nava kishore Manne <nava.manne@xilinx.com>
2018-10-16 16:37:34 +0530

    of: configfs: Fix compilation warning
    On 64-bit:
    drivers/of/configfs.c: In function ‘cfs_overlay_item_dtbo_read’:
    include/linux/kern_levels.h:5:18: warning: format ‘%u’ expects
    argument of type ‘unsigned int’, but argument 4 has type
    ‘size_t {aka long unsigned int}’ [-Wformat=]
    
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/of/configfs.c
+++ drivers/of/configfs.c
@@ -159,7 +159,7 @@ ssize_t cfs_overlay_item_dtbo_read(struct config_item *item, void *buf,
 {
 	struct cfs_overlay_item *overlay = to_cfs_overlay_item(item);
 
-	pr_debug("%s: buf=%p max_count=%u\n", __func__,
+	pr_debug("%s: buf=%p max_count=%zu\n", __func__,
 			buf, max_count);
 
 	if (overlay->dtbo == NULL)
