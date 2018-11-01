906ef1a334e91ca7848a25987021bc0c83223c45
Vasileios Bimpikas <vasileios.bimpikas@xilinx.com>
2018-11-01 16:41:46 +0000

    staging: xroeframer: add support for multi-port Ethernet
    Support for up to 4 Ethernet ports. This wraps all the "store" & "show"
    functions of sysfs entries in a function which calculates the current
    Ethernet port (when applicable) and adds the correct offset to the
    address' register, which is ADDRESS + (0x100 * port_num). It defaults
    to 0 for the non-applicable cases so no other entries are affected.
    
    Signed-off-by: Vasileios Bimpikas <vasileios.bimpikas@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: not-upstreamable

--- drivers/staging/xroeframer/xroe_framer.h
+++ drivers/staging/xroeframer/xroe_framer.h
@@ -32,6 +32,10 @@
 #define STATS_SIZE 0x00000050
 #define RADIO_CTRL_BASE 0xA0060000
 #define RADIO_CTRL_SIZE 0x0000FFFF
+/* TODO: Remove hardcoded value of number of Ethernet ports and read the value
+ * from the device tree.
+ */
+#define MAX_NUM_ETH_PORTS 0x4
 /* TODO: to be made static as well, so that multiple instances can be used. As
  * of now, the following 3 structures are shared among the multiple
  * source files
@@ -39,6 +43,7 @@
 extern struct framer_local *lp;
 extern struct kobject *root_xroe_kobj;
 extern struct kobject *kobj_framer;
+extern struct kobject *kobj_eth_ports[MAX_NUM_ETH_PORTS];
 struct framer_local {
 	int irq;
 	unsigned long mem_start;
@@ -62,3 +67,7 @@ void xroe_sysfs_udp_exit(void);
 int utils_write32withmask(void __iomem *working_address, u32 value,
 			  u32 mask, u32 offset);
 int utils_check_address_offset(u32 offset, size_t device_size);
+void utils_sysfs_store_wrapper(u32 address, u32 offset, u32 mask, u32 value,
+			       struct kobject *kobj);
+u32 utils_sysfs_show_wrapper(u32 address, u32 offset, u32 mask,
+			     struct kobject *kobj);
