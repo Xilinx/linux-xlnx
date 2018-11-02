86131fffa560d473520dfeb4c785359831b8c8ec
Michal Simek <michal.simek@xilinx.com>
2018-11-02 09:30:42 +0100

    misc: xilinx-sdfec: Use SPDX license header
    Use SPDX license header for sdfec driver.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/misc/xilinx_sdfec.c
+++ drivers/misc/xilinx_sdfec.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Xilinx SDFEC
  *
@@ -7,19 +8,6 @@
  * This driver is developed for SDFEC16 (Soft Decision FEC 16nm)
  * IP. It exposes a char device interface in sysfs and supports file
  * operations like  open(), close() and ioctl().
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/cdev.h>
--- include/uapi/misc/xilinx_sdfec.h
+++ include/uapi/misc/xilinx_sdfec.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
 /*
  * Xilinx SDFEC
  *
@@ -6,19 +7,6 @@
  * Description:
  * This driver is developed for SDFEC16 IP. It provides a char device
  * in sysfs and supports file operations like  open(), close() and ioctl().
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __XILINX_SDFEC_H__
 #define __XILINX_SDFEC_H__
