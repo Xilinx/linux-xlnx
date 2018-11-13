4157571b5d9d4612058d58907b1a4b59bc7f2427
Colin Ian King <colin.king@canonical.com>
2018-11-13 09:43:23 +0000

    serial-uartlite: fix null pointer dereference on pointer port
    Pointer port is dereferenced on port->private_data when assigning pointer
    pdata before port is null checked, leading to a potential null pointer
    dereference.  Fix this by assigning pdata after the null pointer check on
    port.
    
    Detected by CoverityScan, CID#1475434 ("Dereference before null check")
    
    Fixes: 3b209d253e7f ("serial-uartlite: Do not use static struct uart_driver out of probe()")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/uartlite.c
+++ drivers/tty/serial/uartlite.c
@@ -654,10 +654,11 @@ static int ulite_assign(struct device *dev, int id, u32 base, int irq,
 static int ulite_release(struct device *dev)
 {
 	struct uart_port *port = dev_get_drvdata(dev);
-	struct uartlite_data *pdata = port->private_data;
 	int rc = 0;
 
 	if (port) {
+		struct uartlite_data *pdata = port->private_data;
+
 		rc = uart_remove_one_port(pdata->ulite_uart_driver, port);
 		dev_set_drvdata(dev, NULL);
 		port->mapbase = 0;
@@ -675,10 +676,12 @@ static int ulite_release(struct device *dev)
 static int __maybe_unused ulite_suspend(struct device *dev)
 {
 	struct uart_port *port = dev_get_drvdata(dev);
-	struct uartlite_data *pdata = port->private_data;
 
-	if (port)
+	if (port) {
+		struct uartlite_data *pdata = port->private_data;
+
 		uart_suspend_port(pdata->ulite_uart_driver, port);
+	}
 
 	return 0;
 }
@@ -692,10 +695,12 @@ static int __maybe_unused ulite_suspend(struct device *dev)
 static int __maybe_unused ulite_resume(struct device *dev)
 {
 	struct uart_port *port = dev_get_drvdata(dev);
-	struct uartlite_data *pdata = port->private_data;
 
-	if (port)
+	if (port) {
+		struct uartlite_data *pdata = port->private_data;
+
 		uart_resume_port(pdata->ulite_uart_driver, port);
+	}
 
 	return 0;
 }
