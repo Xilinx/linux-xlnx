4e4eec6ca5c4414f5add72d996b837f4963404ff
Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
2018-11-30 14:24:31 +0530

    ASoC: xlnx: add SPDIF sound card support
    This patch enables SPDIF sound card using audio formatter,
    SPDIF in the audio pipeline.
    
    Signed-off-by: Maruthi Srinivas Bayyavarapu <maruthi.srinivas.bayyavarapu@xilinx.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- sound/soc/xilinx/xlnx_pl_snd_card.c
+++ sound/soc/xilinx/xlnx_pl_snd_card.c
@@ -20,6 +20,7 @@ enum {
 	I2S_AUDIO = 0,
 	HDMI_AUDIO,
 	SDI_AUDIO,
+	SPDIF_AUDIO,
 	XLNX_MAX_IFACE,
 };
 
@@ -28,11 +29,14 @@ static const char *dev_compat[][XLNX_MAX_IFACE] = {
 		"xlnx,i2s-transmitter-1.0",
 		"xlnx,v-hdmi-tx-ss-3.1",
 		"xlnx,v-uhdsdi-audio-2.0",
+		"xlnx,spdif-2.0",
 	},
+
 	[XLNX_CAPTURE] = {
 		"xlnx,i2s-receiver-1.0",
 		"xlnx,v-hdmi-rx-ss-3.1",
 		"xlnx,v-uhdsdi-audio-2.0",
+		"xlnx,spdif-2.0",
 	},
 };
 
@@ -207,6 +211,19 @@ static struct snd_soc_dai_link xlnx_snd_dai[][XLNX_MAX_PATHS] = {
 		},
 
 	},
+	[SPDIF_AUDIO] = {
+		{
+			.name = "xilinx-spdif_playback",
+			.codec_dai_name = "snd-soc-dummy-dai",
+			.codec_name = "snd-soc-dummy",
+		},
+		{
+			.name = "xilinx-spdif_capture",
+			.codec_dai_name = "snd-soc-dummy-dai",
+			.codec_name = "snd-soc-dummy",
+		},
+	},
+
 };
 
 static int find_link(struct device_node *node, int direction)
@@ -281,6 +298,7 @@ static int xlnx_snd_probe(struct platform_device *pdev)
 			prv->mclk = devm_clk_get(&iface_pdev->dev, "aud_mclk");
 			if (IS_ERR(prv->mclk))
 				return PTR_ERR(prv->mclk);
+
 		}
 		of_node_put(pnode);
 
@@ -314,6 +332,16 @@ static int xlnx_snd_probe(struct platform_device *pdev)
 			dai->codec_of_node = node[i];
 			card->num_links++;
 			/* TODO: support multiple sampling rates */
+			prv->mclk_ratio = 384;
+			snd_soc_card_set_drvdata(card, prv);
+			dev_dbg(card->dev, "%s registered\n",
+				card->dai_link[i].name);
+			break;
+		case SPDIF_AUDIO:
+			*dai = xlnx_snd_dai[SPDIF_AUDIO][i];
+			dai->platform_of_node = pnode;
+			dai->cpu_of_node = node[i];
+			card->num_links++;
 			prv->mclk_ratio = 384;
 			snd_soc_card_set_drvdata(card, prv);
 			dev_dbg(card->dev, "%s registered\n",
