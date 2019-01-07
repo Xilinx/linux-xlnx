029212b6910b2c1e1b630d02f9babd835b142889
Saurabh Sengar <saurabh.singh@xilinx.com>
2019-01-07 11:50:52 +0530

    staging: xlnx_ctrl_driver: xilinx framebuffer control driver
    Initial checking xilinx framebufer control driver. This driver is puerly
    control path with no complication of drm/v4l2.
    
    Signed-off-by: Saurabh Sengar <saurabh.singh@xilinx.com>

---
+++ include/uapi/linux/xlnx_ctrl.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/*
+ * Xilinx Controls Header
+ *
+ * Copyright (C) 2019 Xilinx, Inc.
+ *
+ * Contacts: Saurabh Sengar <saurabh.singh@xilinx.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __UAPI_XLNX_CTRL_H__
+#define __UAPI_XLNX_CTRL_H__
+
+#define XSET_FB_CAPTURE		16
+#define XSET_FB_CONFIGURE	17
+#define XSET_FB_ENABLE		18
+#define XSET_FB_DISABLE		19
+#define XSET_FB_RELEASE		20
+#define XSET_FB_ENABLE_SNGL	21
+#define XSET_FB_POLL		22
+
+#endif /* __UAPI_XLNX_CTRL_H__ */
+
