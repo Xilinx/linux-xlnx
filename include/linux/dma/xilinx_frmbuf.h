9f27a32d8a1dbfd6fc65bf382ac48cab1fad6c39
Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
2019-03-05 17:54:09 -0800

    dma: xilinx: Define early callback modes
    This patch adds two early callback flags in frmbuf driver. There are
    four possible stages of buffer descriptors called pending, staged, active
    and done. EARLY_CALLBACK is to indicate the driver to release the buffer
    after staged stage. EARLY_CALLBACK_LOW_LATENCY is to indicate the driver
    to release the buffer before the actual DMA transfer start.
    
    Signed-off-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    State: pending

--- include/linux/dma/xilinx_frmbuf.h
+++ include/linux/dma/xilinx_frmbuf.h
@@ -14,6 +14,10 @@
 
 #include <linux/dmaengine.h>
 
+/* Modes to enable early callback */
+#define EARLY_CALLBACK			BIT(1) /* To avoid first frame delay */
+#define EARLY_CALLBACK_LOW_LATENCY	BIT(2) /* Low latency mode */
+
 /**
  * enum vid_frmwork_type - Linux video framework type
  * @XDMA_DRM: fourcc is of type DRM
