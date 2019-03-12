2d0c93f66ea74079127c73e3ef9636aa30736b0b
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2019-03-12 15:02:12 +0530

    staging: fclk: Add sysfs entry for set_rate
    This is a temporary hack for setting the rate.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/staging/fclk/xilinx_fclk.c
+++ drivers/staging/fclk/xilinx_fclk.c
@@ -34,17 +34,54 @@ static const struct of_device_id fclk_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, fclk_of_match);
 
+static ssize_t set_rate_show(struct device *dev,
+			     struct device_attribute *attr, char *buf)
+{
+	struct fclk_state *st = dev_get_drvdata(dev);
+
+	return scnprintf(buf, PAGE_SIZE, "%lu\n", clk_get_rate(st->pl));
+}
+
+static ssize_t set_rate_store(struct device *dev,
+			      struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	int ret = 0;
+	unsigned long rate;
+	struct fclk_state *st = dev_get_drvdata(dev);
+
+	ret = kstrtoul(buf, 0, &rate);
+	if (ret)
+		return -EINVAL;
+
+	rate = clk_round_rate(st->pl, rate);
+	ret = clk_set_rate(st->pl, rate);
+
+	return ret ? ret : count;
+}
+
+static DEVICE_ATTR_RW(set_rate);
+
+static const struct attribute *fclk_ctrl_attrs[] = {
+	&dev_attr_set_rate.attr,
+	NULL,
+};
+
+static const struct attribute_group fclk_ctrl_attr_grp = {
+	.attrs = (struct attribute **)fclk_ctrl_attrs,
+};
+
 static int fclk_probe(struct platform_device *pdev)
 {
 	struct fclk_state *st;
 	int ret;
+	struct device *dev = &pdev->dev;
 
 	st = devm_kzalloc(&pdev->dev, sizeof(*st), GFP_KERNEL);
 	if (!st)
 		return -ENOMEM;
 
-	st->dev = &pdev->dev;
-
+	st->dev = dev;
 	platform_set_drvdata(pdev, st);
 
 	st->pl = devm_clk_get(&pdev->dev, NULL);
@@ -56,6 +93,10 @@ static int fclk_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Unable to enable clock.\n");
 		return ret;
 	}
+
+	ret = sysfs_create_group(&dev->kobj, &fclk_ctrl_attr_grp);
+	if (ret)
+		return ret;
 
 	return 0;
 }
