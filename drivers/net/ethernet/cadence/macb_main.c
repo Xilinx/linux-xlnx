f5520f1ff165d561c97c397bc6bab92a6e8ce992
Harini Katakam <harini.katakam@xilinx.com>
2019-03-13 19:41:18 +0530

    macb: Sync PHY reset with mainline
    Phylib already has implementation of phy reset via gpio using the
    property "reset-gpios". Remove local code in the driver for the
    same which is redundant now.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -3431,39 +3431,6 @@ static void macb_configure_caps(struct macb *bp,
 	dev_dbg(&bp->pdev->dev, "Cadence caps 0x%08x\n", bp->caps);
 }
 
-#if defined(CONFIG_OF)
-static void macb_reset_phy(struct platform_device *pdev)
-{
-	int err, phy_reset, msec = 1;
-	bool active_low;
-	struct device_node *np = pdev->dev.of_node;
-
-	if (!np)
-		return;
-
-	of_property_read_u32(np, "phy-reset-duration", &msec);
-	active_low = of_property_read_bool(np, "phy-reset-active-low");
-
-	phy_reset = of_get_named_gpio(np, "phy-reset-gpio", 0);
-	if (!gpio_is_valid(phy_reset))
-		return;
-
-	err = devm_gpio_request_one(&pdev->dev, phy_reset,
-				    active_low ? GPIOF_OUT_INIT_LOW :
-				    GPIOF_OUT_INIT_HIGH, "phy-reset");
-	if (err) {
-		dev_err(&pdev->dev, "failed to get phy-reset-gpio: %d\n", err);
-		return;
-	}
-	msleep(msec);
-	gpio_set_value(phy_reset, active_low);
-}
-#else /* CONFIG_OF */
-static void macb_reset_phy(struct platform_device *pdev)
-{
-}
-#endif /* CONFIG_OF */
-
 static void macb_probe_queues(void __iomem *mem,
 			      bool native_io,
 			      unsigned int *queue_mask,
@@ -4313,12 +4280,6 @@ static int macb_probe(struct platform_device *pdev)
 			goto err_out_free_netdev;
 		}
 		phy_node = of_node_get(np);
-	} else {
-		int gpio = of_get_named_gpio(phy_node, "reset-gpios", 0);
-		if (gpio_is_valid(gpio)) {
-			bp->reset_gpio = gpio_to_desc(gpio);
-			gpiod_direction_output(bp->reset_gpio, 1);
-		}
 	}
 	bp->phy_node = phy_node;
 
@@ -4332,8 +4293,6 @@ static int macb_probe(struct platform_device *pdev)
 	} else {
 		bp->phy_interface = err;
 	}
-
-	macb_reset_phy(pdev);
 
 	/* IP specific init */
 	err = init(pdev);
