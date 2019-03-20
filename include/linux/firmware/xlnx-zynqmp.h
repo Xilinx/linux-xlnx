5224a0a09ac2000829ca8b1664829e05e0ace934
Michal Simek <michal.simek@xilinx.com>
2019-03-20 15:18:32 +0100

    firmware: zynqmp: Add sysfs interface
    Wire sysfs interface.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -103,6 +103,10 @@ enum pm_ioctl_id {
 	IOCTL_GET_PLL_FRAC_MODE,
 	IOCTL_SET_PLL_FRAC_DATA,
 	IOCTL_GET_PLL_FRAC_DATA,
+	IOCTL_WRITE_GGS,
+	IOCTL_READ_GGS,
+	IOCTL_WRITE_PGGS,
+	IOCTL_READ_PGGS,
 };
 
 enum pm_query_id {
@@ -392,6 +396,11 @@ enum dll_reset_type {
 	PM_DLL_RESET_ASSERT,
 	PM_DLL_RESET_RELEASE,
 	PM_DLL_RESET_PULSE,
+};
+
+enum pm_register_access_id {
+	CONFIG_REG_WRITE,
+	CONFIG_REG_READ,
 };
 
 /**
