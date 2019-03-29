3a1fa5ff1626933f7288392733bd23637efb0ab3
Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
2019-03-29 14:00:38 +0530

    reset: reset-zynqmp: License fix and copyright update
    This patch updates the copyright year and license fix for
    zynqmp reset driver.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/reset/reset-zynqmp.c
+++ drivers/reset/reset-zynqmp.c
@@ -1,5 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2016 Xilinx, Inc.
+ * Copyright (C) 2016 - 2019 Xilinx, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
