a153592a56a1d12e3ceb55dbacb5a17347633dd6
Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
2019-04-10 15:04:53 -0700

    media: v4l2-ioctl: Use correct fourcc for YUV444 10bit format
    Y_only 10bit and YUV444 10bit pixel formats are using same fourcc. This
    patch uses X410 as fourcc for YUV444 10bit format.
    
     X  -> padding
     4  -> chroma subsampling
     10 -> Bits per component
    
    Signed-off-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    State: pending

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -642,7 +642,7 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_Y210    v4l2_fourcc('Y', '2', '1', '0') /* 32  YUYV 4:2:2 */
 #define V4L2_PIX_FMT_Y212    v4l2_fourcc('Y', '2', '1', '2') /* 32  YUYV 4:2:2 */
 #define V4L2_PIX_FMT_Y216    v4l2_fourcc('Y', '2', '1', '6') /* 32  YUYV 4:2:2 */
-#define V4L2_PIX_FMT_XVUY10  v4l2_fourcc('X', 'Y', '1', '0') /* 32  XVUY 2-10-10-10 */
+#define V4L2_PIX_FMT_XVUY10  v4l2_fourcc('X', '4', '1', '0') /* 32  XVUY 2-10-10-10 */
 
 /* two planes -- one Y, one Cr + Cb interleaved  */
 #define V4L2_PIX_FMT_NV12    v4l2_fourcc('N', 'V', '1', '2') /* 12  Y/CbCr 4:2:0  */
