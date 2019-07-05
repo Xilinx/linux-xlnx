81f7424326a2e03d8d6467e60de075c5ea40471b
Harini Katakam <harini.katakam@xilinx.com>
2019-07-05 16:24:34 +0530

    include: mdio: Add private field to mdio structure
    Add a priv pointer to mdio structure to be used by mdio devices if
    required.
    
    This priv field will be used by gmii2rgmii driver. As this IP has
    no capability to read status on the MDIO bus, the driver currently
    snoops the same and needs the instance information is some private
    field. Since phy device "priv" can be used by external phy drivers,
    it is not appropriate. Hence this addition to mdio device. This is
    a temporary solution before the IP can be improved. The need for
    this priv field can be re-evaluated later based on other mdio devices.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- include/linux/mdio.h
+++ include/linux/mdio.h
@@ -42,6 +42,9 @@ struct mdio_device {
 	struct gpio_desc *reset;
 	unsigned int reset_assert_delay;
 	unsigned int reset_deassert_delay;
+
+	/* private data pointer for use by MDIO devices */
+	void *priv;
 };
 #define to_mdio_device(d) container_of(d, struct mdio_device, dev)
 
