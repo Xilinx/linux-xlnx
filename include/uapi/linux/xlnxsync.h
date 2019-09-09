4887148285231554130d029f613ddcd4b6841cb6
Vishal Sagar <vishal.sagar@xilinx.com>
2019-09-09 10:11:37 -0700

    staging: xlnxsync: Add consumer related registers
    This patch adds consumer related registers.
    
    Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Tested-by: Dylan Yip <dylan.yip@xilinx.com>

--- include/uapi/linux/xlnxsync.h
+++ include/uapi/linux/xlnxsync.h
@@ -25,10 +25,14 @@
 
 /**
  * struct xlnxsync_chan_config - Synchronizer channel configuration struct
- * @prod_luma_start_address: Start address of Luma buffer
- * @prod_chroma_start_address: Start address of Chroma buffer
- * @prod_luma_end_address: End address of Luma buffer
- * @prod_chroma_end_address: End address of Chroma buffer
+ * @prod_luma_start_address: Start producer address of Luma buffer
+ * @prod_chroma_start_address: Start producer address of Chroma buffer
+ * @cons_luma_start_address: Start consumer address of Luma buffer
+ * @cons_chroma_start_address: Start consumer address of Chroma buffer
+ * @prod_luma_end_address: End producer address of Luma buffer
+ * @prod_chroma_end_address: End producer address of Chroma buffer
+ * @cons_luma_end_address: End consumer address of Luma buffer
+ * @cons_chroma_end_address: End consumer address of Chroma buffer
  * @luma_margin: Margin for Luma buffer
  * @chroma_margin: Margin for Chroma buffer
  * @fb_id: Framebuffer index. Valid values 0/1/2/XLNXSYNC_AUTO_SEARCH
@@ -42,8 +46,12 @@
 struct xlnxsync_chan_config {
 	u64 prod_luma_start_address;
 	u64 prod_chroma_start_address;
+	u64 cons_luma_start_address;
+	u64 cons_chroma_start_address;
 	u64 prod_luma_end_address;
 	u64 prod_chroma_end_address;
+	u64 cons_luma_end_address;
+	u64 cons_chroma_end_address;
 	u32 luma_margin;
 	u32 chroma_margin;
 	u8 fb_id;
