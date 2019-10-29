fcef041ab1d3ea525365f9a7faa5250d57104511
Quanyang Wang <quanyang.wang@windriver.com>
2019-10-29 15:56:33 +0800

    fpga: zynqmp-fpga: fix compile warning in fpga_mgr_register
    This fixes the compile warning as below:
    
    drivers/fpga/fpga-mgr.c:911:2: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
    911 | struct dentry *d, *parent;
    		^~~~~~
    
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/fpga/fpga-mgr.c
+++ drivers/fpga/fpga-mgr.c
@@ -861,6 +861,9 @@ EXPORT_SYMBOL_GPL(fpga_mgr_free);
 int fpga_mgr_register(struct fpga_manager *mgr)
 {
 	int ret;
+#ifdef CONFIG_FPGA_MGR_DEBUG_FS
+	struct dentry *d, *parent;
+#endif
 
 	/*
 	 * Initialize framework state by requesting low level driver read state
@@ -874,8 +877,6 @@ int fpga_mgr_register(struct fpga_manager *mgr)
 		goto error_device;
 
 #ifdef CONFIG_FPGA_MGR_DEBUG_FS
-	struct dentry *d, *parent;
-
 	mgr->dir = debugfs_create_dir("fpga", NULL);
 	if (!mgr->dir)
 		goto error_device;
