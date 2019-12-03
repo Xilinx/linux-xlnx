815d840b085ecba2f776ae63afe80a8af4c6b6fc
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2019-12-03 01:03:53 -0800

    firmware: xilinx: sync firmware pm_ioctl_id entries with PLM pm_ioctl_id entries
    This patch adds enum IOCTL_OSPI_MUX_SELECT into pm_ioctl_id present in
    xlnx-zynqmp.h to be in sync with PLM pm_ioctl_id entries.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending
    [michal: Squashed with "firmware: xilinx: Updated OSPI mux sel API to take
    device id"]

--- drivers/firmware/xilinx/zynqmp.c
+++ drivers/firmware/xilinx/zynqmp.c
@@ -756,6 +756,12 @@ int zynqmp_pm_probe_counter_write(u32 domain, u32 reg, u32 value)
 }
 EXPORT_SYMBOL_GPL(zynqmp_pm_probe_counter_write);
 
+int zynqmp_pm_ospi_mux_select(u32 dev_id, u32 select)
+{
+	return zynqmp_pm_invoke_fn(PM_IOCTL, dev_id, IOCTL_OSPI_MUX_SELECT, select,
+				   0, NULL);
+}
+EXPORT_SYMBOL_GPL(zynqmp_pm_ospi_mux_select);
 /**
  * zynqmp_pm_set_boot_health_status() - PM API for setting healthy boot status
  * @value:	Status value to be written
