51e7e6161950ec274af39c0fef57dcef6361e011
Michal Simek <michal.simek@xilinx.com>
2019-12-31 15:11:46 +0530

    fpga: Add firmware store function
    For testing purpose.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Nava kishore Manne <navam@xilinx.com>
    State: not-upstreamable

--- include/linux/fpga/fpga-mgr.h
+++ include/linux/fpga/fpga-mgr.h
@@ -157,6 +157,7 @@ struct fpga_compat_id {
 /**
  * struct fpga_manager - fpga manager structure
  * @name: name of low level fpga manager
+ * @flags: flags determines the type of Bitstream
  * @dev: fpga manager device
  * @ref_mutex: only allows one reference to fpga manager
  * @state: state of fpga manager
@@ -166,6 +167,7 @@ struct fpga_compat_id {
  */
 struct fpga_manager {
 	const char *name;
+	unsigned long flags;
 	struct device dev;
 	struct mutex ref_mutex;
 	enum fpga_mgr_states state;
