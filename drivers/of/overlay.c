7e42c87c489b702795794e480fb5483897ff9e91
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2019-12-31 15:20:17 +0530

    OF: DT-Overlay configfs interface (v8)
    Add a runtime interface to using configfs for generic device tree overlay
    usage. With it its possible to use device tree overlays without having
    to use a per-platform overlay manager.
    
    Please see Documentation/devicetree/configfs-overlays.txt for more info.
    
    Changes since v7:
    - Xilinx alignment
    - of: configfs: Fix error handling from of_overlay_create()
    - of: configfs: lock while resolving and applying
    - of: configfs: Fix memory leak in create overlay
    - of: configfs: Fix compilation warning
    - of: configfs: Use of_overlay_fdt_apply API call
    
    Changes since v6:
    - Default groups properties API changed.
    
    Changes since v5:
    - New style configfs.
    
    Changes since v4:
    - Loading fix for multiple overlays as found out by
      Geert Uytterhoeven <geert@linux-m68k.org>
    
    Changes since v3:
    - Fixed compilation on SPARC & Xtensa
    
    Changes since v2:
    - Removed ifdef CONFIG_OF_OVERLAY (since for now it's required)
    - Created a documentation entry
    - Slight rewording in Kconfig
    
    Changes since v1:
    - of_resolve() -> of_resolve_phandles().
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: not-upstreamable
    [michals: Squashed with
    OF: overlay: Perform RCU callback barrier before releasing overlay memory resources
    ]

--- drivers/of/overlay.c
+++ drivers/of/overlay.c
@@ -1245,6 +1245,9 @@ int of_overlay_remove(int *ovcs_id)
 		if (!ret)
 			ret = ret_tmp;
 
+	/* Wait for completion of call_rcu()'s */
+	rcu_barrier();
+
 	free_overlay_changeset(ovcs);
 
 err_unlock:
