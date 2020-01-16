8b07db8715a738d953426397ae4d5f4a325c5e83
Michal Simek <michal.simek@xilinx.com>
2020-01-16 13:00:49 +0100

    microblaze: timer: Move irq code to clockevent function
    Unfortunately irq needs to be as static variable but will be tuned later.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Stefan Asserhall <stefan.asserhall@xilinx.com>

--- arch/microblaze/kernel/timer.c
+++ arch/microblaze/kernel/timer.c
@@ -25,6 +25,8 @@
 static void __iomem *timer_baseaddr;
 static void __iomem *clocksource_baseaddr;
 
+static u32 irq;
+
 static unsigned int freq_div_hz;
 static unsigned int timer_clock_freq;
 
@@ -163,13 +165,6 @@ static irqreturn_t timer_interrupt(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static struct irqaction timer_irqaction = {
-	.handler = timer_interrupt,
-	.flags = IRQF_TIMER,
-	.name = "timer",
-	.dev_id = &clockevent_xilinx_timer,
-};
-
 static __init int xilinx_clockevent_init(void)
 {
 	clockevent_xilinx_timer.mult =
@@ -189,7 +184,16 @@ static __init int xilinx_clockevent_init(void)
 
 static int microblaze_timer_starting(unsigned int cpu)
 {
+	int ret;
+
 	pr_debug("%s: cpu %d\n", __func__, cpu);
+
+	ret = request_irq(irq, timer_interrupt, IRQF_TIMER,
+				 "timer", &clockevent_xilinx_timer);
+	if (ret) {
+		pr_err("%s: request_irq failed\n", __func__);
+		return ret;
+	}
 
 	return xilinx_clockevent_init();
 }
@@ -267,7 +271,6 @@ static int __init xilinx_timer_init(struct device_node *timer)
 {
 	struct clk *clk;
 	static int initialized;
-	u32 irq;
 	u32 timer_num = 1;
 	int ret;
 	bool clocksource = true;
@@ -334,12 +337,6 @@ static int __init xilinx_timer_init(struct device_node *timer)
 	}
 
 	freq_div_hz = timer_clock_freq / HZ;
-
-	ret = setup_irq(irq, &timer_irqaction);
-	if (ret) {
-		pr_err("Failed to setup IRQ");
-		return ret;
-	}
 
 	ret = cpuhp_setup_state(CPUHP_AP_MICROBLAZE_TIMER_STARTING,
 				"clockevents/microblaze/arch_timer:starting",
