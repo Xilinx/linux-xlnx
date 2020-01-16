4345095741c9124bde625ca503b70df46f6f2df4
Wendy Liang <wendy.liang@xilinx.com>
2020-01-16 13:18:43 -0800

    remoteproc: returns message for peek remote kick function
    Update the peek remote kick API to also return the message
    from remote.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -376,7 +376,7 @@ struct rproc_ops {
 	int (*start)(struct rproc *rproc);
 	int (*stop)(struct rproc *rproc);
 	void (*kick)(struct rproc *rproc, int vqid);
-	bool (*peek_remote_kick)(struct rproc *rproc);
+	bool (*peek_remote_kick)(struct rproc *rproc, char *buf, size_t *len);
 	void (*ack_remote_kick)(struct rproc *rproc);
 	void * (*da_to_va)(struct rproc *rproc, u64 da, int len);
 	int (*parse_fw)(struct rproc *rproc, const struct firmware *fw);
