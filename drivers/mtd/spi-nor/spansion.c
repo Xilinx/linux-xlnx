0e6969f5716e0d5b47dfb8d43b0c69dfa3598200
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2020-01-19 10:58:30 -0700

    mtd: spi-nor: Update flags for serial NOR flash devices
    Updated flags for serial NOR flash devices.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    Signed-off-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>
    State: pending

--- drivers/mtd/spi-nor/spansion.c
+++ drivers/mtd/spi-nor/spansion.c
@@ -39,10 +39,10 @@ static const struct flash_info spansion_parts[] = {
 			     SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "s25fl128s0", INFO6(0x012018, 0x4d0080, 256 * 1024, 64,
 			      SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
-			      USE_CLSR) },
+			      USE_CLSR | SPI_NOR_HAS_LOCK) },
 	{ "s25fl128s1", INFO6(0x012018, 0x4d0180, 64 * 1024, 256,
 			      SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
-			      USE_CLSR) },
+			      USE_CLSR | SPI_NOR_HAS_LOCK) },
 	{ "s25fl256s0", INFO6(0x010219, 0x4d0080, 256 * 1024, 128,
 			      SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			      USE_CLSR) },
@@ -104,6 +104,8 @@ static const struct flash_info spansion_parts[] = {
 			     SPI_NOR_4B_OPCODES) },
 	{ "cy15x104q",  INFO6(0x042cc2, 0x7f7f7f, 512 * 1024, 1,
 			      SPI_NOR_NO_ERASE) },
+	{ "sst26wf016B", INFO(0xbf2651, 0, 64 * 1024, 32,
+			SECT_4K | SST_GLOBAL_PROT_UNLK) },
 };
 
 static void spansion_post_sfdp_fixups(struct spi_nor *nor)
