d0720fba4603a2eb4aad0fd4035beeb89efd972e
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2020-01-23 19:51:03 +0530

    usb: dwc3: Correct usb dma mask for making SMMU work in device mode
    By default dma_coherent_mask is set to 32 bits, because of this the
    virtual address generated by the SMMU is masked to 32 bit width.
    Due to this reason, the events generated by the USB DMA are not being
    updated to the correct physical address and undefined behaviour is
    generated.
    This patch corrects the above said problem by reading and setting
    the DMA coherent mask based on the GHWPARAMS0.MDWIDTH value.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    State: pending

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1531,9 +1531,8 @@ static int dwc3_probe(struct platform_device *pdev)
 	struct device		*dev = &pdev->dev;
 	struct resource		*res, dwc_res;
 	struct dwc3		*dwc;
-
 	int			ret;
-
+	u32			mdwidth;
 	void __iomem		*regs;
 
 	dwc = devm_kzalloc(dev, sizeof(*dwc), GFP_KERNEL);
@@ -1611,6 +1610,11 @@ static int dwc3_probe(struct platform_device *pdev)
 	dwc3_cache_hwparams(dwc);
 
 	spin_lock_init(&dwc->lock);
+
+	/* Set dma coherent mask to DMA BUS data width */
+	mdwidth = DWC3_GHWPARAMS0_MDWIDTH(dwc->hwparams.hwparams0);
+	dev_dbg(dev, "Enabling %d-bit DMA addresses.\n", mdwidth);
+	dma_set_coherent_mask(dev, DMA_BIT_MASK(mdwidth));
 
 	pm_runtime_set_active(dev);
 	pm_runtime_use_autosuspend(dev);
