e962f56d2c2721b27e80d7fd702461379683c112
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2020-01-23 19:51:07 +0530

    usb: dwc3: Check for IOC/LST bit in both event->status and TRB->ctrl fields
    The present code in dwc3_gadget_ep_reclaim_completed_trb() will check
    for IOC/LST bit in the event->status and returns if IOC/LST bit is
    set. This logic doesn't work if multiple TRBs are queued per
    request and the IOC/LST bit is set on the last TRB of that request.
    Consider an example where a queued request has multiple queued TRBs
    and IOC/LST bit is set only for the last TRB. In this case, the Core
    generates XferComplete/XferInProgress events only for the last TRB
    (since IOC/LST are set only for the last TRB). As per the logic in
    dwc3_gadget_ep_reclaim_completed_trb() event->status is checked for
    IOC/LST bit and returns on the first TRB. This makes the remaining
    TRBs left unhandled.
    To aviod this, changed the code to check for IOC/LST bits in both
    event->status & TRB->ctrl. This patch does the same.
    
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    State: pending

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2478,7 +2478,12 @@ static int dwc3_gadget_ep_reclaim_completed_trb(struct dwc3_ep *dep,
 	if (event->status & DEPEVT_STATUS_SHORT && !chain)
 		return 1;
 
-	if (event->status & DEPEVT_STATUS_IOC)
+	if ((event->status & DEPEVT_STATUS_IOC) &&
+	    (trb->ctrl & DWC3_TRB_CTRL_IOC))
+		return 1;
+
+	if ((event->status & DEPEVT_STATUS_LST) &&
+	    (trb->ctrl & DWC3_TRB_CTRL_LST))
 		return 1;
 
 	return 0;
