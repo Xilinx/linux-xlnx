f46626c877ddd07a593baf73a621d9b2e282dee2
Piyush Mehta <piyush.mehta@xilinx.com>
2020-01-23 19:51:21 +0530

    usb: dwc3: retain resource index for unforced stops
    all active transfers are stopped with force rm bit false
    while hibernating and restarted using resource index on wakeup
    
    Signed-off-by: Tanmay Upadhyay <tanmay.upadhyay@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    State: pending

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2863,6 +2863,13 @@ void dwc3_stop_active_transfer(struct dwc3_ep *dep, bool force,
 	WARN_ON_ONCE(ret);
 	dep->resource_index = 0;
 
+	/*
+	 * when transfer is stopped with force rm bit false, it can be
+	 * restarted by passing resource_index in params; don't loose it
+	 */
+	if (force)
+		dep->resource_index = 0;
+
 	if (dwc3_is_usb31(dwc) || dwc->revision < DWC3_REVISION_310A) {
 		/*
 		 * CMD COMPLETE interrupt is not getting generated for isoc
