eab294ff249d8170bf02686772dfe6f1a4eb255a
Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
2020-01-24 20:52:30 +0530

    net: axienet: Adding highmem support
    The driver issue was observed on Microblaze with Higmem enabled and 1GB
    memory.
    This patch adding highmem support for skb fragments to resolve it.
    page_address() assumes to work with a lowmem page only but fragments
    can be placed in highmem pages which has to be handled differently
    then lomem pages.
    
    This patch was inspired by:
    "net: mv643xx_eth: Fix highmem support in non-TSO egress path"
    (sha1: 9e91141)
    where the highmem problem was observed.
    
    skb_frag_dma_map() calls dma_map_page() that's why it is necessary to
    distinguish if dma_map_simple() or dma_map_page() is called.
    This is done by saving flag to every TX BD in tx_desc_mapping field.
    
    There is also reference in Documentation/DMA-API-HOWTO.txt that
    dma_map_single cannot be used with HIGMEM.
    "Using CPU pointers like this for single mappings has a
    disadvantage:
    you cannot reference HIGHMEM memory in this way.  Thus, there is a
    map/unmap interface pair akin to dma_{map,unmap}_single().  These
    interfaces deal with page/offset pairs instead of CPU pointers."
    
    Signed-off-by: Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending

--- drivers/net/ethernet/xilinx/xilinx_axienet.h
+++ drivers/net/ethernet/xilinx/xilinx_axienet.h
@@ -416,6 +416,7 @@
  * @ptp_tx_ts_tag: Tag value of 2 step timestamping if timestamping is enabled
  *		   Otherwise reserved.
  * @tx_skb:	  Transmit skb address
+ * @tx_desc_mapping: Tx Descriptor DMA mapping type.
  */
 struct axidma_bd {
 	phys_addr_t next;	/* Physical address of next buffer descriptor */
@@ -439,7 +440,11 @@ struct axidma_bd {
 	phys_addr_t ptp_tx_skb;
 	u32 ptp_tx_ts_tag;
 	phys_addr_t tx_skb;
+	u32 tx_desc_mapping;
 } __aligned(XAXIDMA_BD_MINIMUM_ALIGNMENT);
+
+#define DESC_DMA_MAP_SINGLE 0
+#define DESC_DMA_MAP_PAGE 1
 
 /**
  * struct axienet_local - axienet private per device data
