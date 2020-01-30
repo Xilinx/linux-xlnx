a2ce21556d4bb88d4ed690096be314b16b6c04c6
Kalyani Akula <kalyani.akula@xilinx.com>
2020-01-30 14:59:08 +0530

    firmware: zynqmp: Add support for secure_image
    Secure image EEMI interface.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -218,6 +218,7 @@ enum pm_api_id {
 	PM_CLOCK_GETDIVIDER = 40,
 	PM_CLOCK_SETPARENT = 43,
 	PM_CLOCK_GETPARENT = 44,
+	PM_SECURE_IMAGE = 45,
 	PM_FPGA_READ = 46,
 	PM_SECURE_AES = 47,
 	PM_EFUSE_ACCESS = 53,
@@ -655,6 +656,7 @@ int zynqmp_pm_set_requirement(const u32 node, const u32 capabilities,
 			      const enum zynqmp_pm_request_ack ack);
 int zynqmp_pm_aes_engine(const u64 address, u32 *out);
 int zynqmp_pm_efuse_access(const u64 address, u32 *out);
+int zynqmp_pm_secure_load(const u64 src_addr, u64 key_addr, u64 *dst);
 int zynqmp_pm_bbram_write_usrdata(u32 data);
 int zynqmp_pm_bbram_read_usrdata(const u64 outaddr);
 int zynqmp_pm_bbram_write_aeskey(u32 keylen, const u64 keyaddr);
@@ -1026,6 +1028,11 @@ static inline int zynqmp_pm_bbram_zeroize(void)
 }
 
 static inline int zynqmp_pm_bbram_lock_userdata(void)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_secure_load(const u64 src_addr, u64 key_addr, u64 *dst)
 {
 	return -ENODEV;
 }
