cd538ce87b57fc5dfe8f8e9db5111e7dfa6b162e
Harini Katakam <harini.katakam@xilinx.com>
2020-01-31 21:07:09 +0530

    net: macb: Use WOL via ARP
    Add wake on LAN support using ARP and remove selection of WOL support
    via ethtool using magic packet. This is because ZynqMP PM support uses
    wake source selection via sys/devices. WOL support is enabled based on
    SoC configuration rather than a devicetree property because this support
    is specific to IP version and SoC implementation. Advertise wakeup
    capability in probe. For WOL via ARP, ensure IP address is assigned and
    report error otherwise.
    
    Further, Mainline kernel uses, struct dev to set/check if a device is
    wakeup capable. However Xilinx's implementation of wake on Lan uses
    bp->dev->dev. Replace "dev" with "&bp->dev->dev" in
    macb_runtime_suspend/resume so that macb's clocks won't be disabled when
    it is selected as wake device. Also ensure a single call of napi_disable,
    netif_carrier_on etc. is handled in macb_supend with corresponding pair
    calls in resume.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Signed-off-by: Pranavi Somisetty <pranavi.somisetty@xilinx.com>
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -416,7 +416,7 @@
 #define MACB_PFR_SIZE		1
 #define MACB_PTZ_OFFSET		13 /* Enable pause time zero interrupt */
 #define MACB_PTZ_SIZE		1
-#define MACB_WOL_OFFSET		14 /* Enable wake-on-lan interrupt */
+#define MACB_WOL_OFFSET		28 /* Enable WOL received interrupt */
 #define MACB_WOL_SIZE		1
 #define MACB_DRQFR_OFFSET	18 /* PTP Delay Request Frame Received */
 #define MACB_DRQFR_SIZE		1
@@ -731,6 +731,7 @@
 #define MACB_CAPS_PCS				0x01000000
 #define MACB_CAPS_HIGH_SPEED			0x02000000
 #define MACB_CAPS_PARTIAL_STORE_FORWARD		0x00000800
+#define MACB_CAPS_WOL				0x00000200
 #define MACB_CAPS_CLK_HW_CHG			0x04000000
 #define MACB_CAPS_MACB_IS_EMAC			0x08000000
 #define MACB_CAPS_FIFO_MODE			0x10000000
