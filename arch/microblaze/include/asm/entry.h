17cbc3a2e0bebb8c0fe7d5ae060e3412fd1a0be6
Stefan Asserhall <stefan.asserhall@xilinx.com>
2020-02-05 12:59:28 +0100

    microblaze: Enable experimental SMP functionality
    This code requires:
    - MicroBlaze v8.50.a or later
    - AXI Intc v1.04.a or later
    - System cache v2.00.a or later
    - Per cpu interrupt controllers
    - One global timer followed by per cpu timers
    - Interrupt controllers with 4 software interrupts for ipi
    - Identical interrupt assignments to all interrupt controllers
    - Secondary cpu cores in sleep mode, jumping to kernel at wakeup
    
    Secondary cpus are wakeup through software interrupts which are enabled
    when interrupt controller driver is probed. All interrupts are setup as per
    cpu one because there is missing handling for distribution among cpus.
    Allocating certain irq to certain cpu is not supported now.
    
    Signed-off-by: Stefan Asserhall <stefan.asserhall@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/asm/entry.h
+++ arch/microblaze/include/asm/entry.h
@@ -10,7 +10,6 @@
 #ifndef _ASM_MICROBLAZE_ENTRY_H
 #define _ASM_MICROBLAZE_ENTRY_H
 
-#include <asm/percpu.h>
 #include <asm/ptrace.h>
 #include <linux/linkage.h>
 
@@ -21,12 +20,23 @@
 
 #define PER_CPU(var) var
 
+#ifdef CONFIG_SMP
+/* Addresses in BRAM */
+#define CURRENT_SAVE_ADDR	0x50
+#define ENTRY_SP_ADDR		0x54
+#define PT_POOL_SPACE_ADDR	0x100
+#endif /* CONFIG_SMP */
+
 # ifndef __ASSEMBLY__
+#include <asm/percpu.h>
+
+#ifndef CONFIG_SMP
 DECLARE_PER_CPU(unsigned int, KSP); /* Saved kernel stack pointer */
 DECLARE_PER_CPU(unsigned int, KM); /* Kernel/user mode */
 DECLARE_PER_CPU(unsigned int, ENTRY_SP); /* Saved SP on kernel entry */
 DECLARE_PER_CPU(unsigned int, R11_SAVE); /* Temp variable for entry */
 DECLARE_PER_CPU(unsigned int, CURRENT_SAVE); /* Saved current pointer */
+#endif /* CONFIG_SMP */
 
 extern asmlinkage void do_notify_resume(struct pt_regs *regs, int in_syscall);
 # endif /* __ASSEMBLY__ */
