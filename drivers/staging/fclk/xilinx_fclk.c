e0a8698bc48ac6f650e4a2d8920d02e4ea3b7936
Michal Simek <michal.simek@xilinx.com>
2020-02-10 09:13:13 +0100

    staging: fclk: Switch to SPDX license identifier
    Fix license header and update also year.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/staging/fclk/xilinx_fclk.c
+++ drivers/staging/fclk/xilinx_fclk.c
@@ -1,17 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2017 Xilinx, Inc.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * Xilinx fclk clock driver.
+ * Copyright (c) 2017 - 2020 Xilinx Inc.
  */
 
 #include <linux/clk.h>
