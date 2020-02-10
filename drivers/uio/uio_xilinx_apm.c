d63aa58ab3a21858e802e09b23d75fdb906745d5
Michal Simek <michal.simek@xilinx.com>
2020-02-10 09:28:35 +0100

    uio: apm: Switch to SPDX license identifier
    Fix license header and update also year.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/uio/uio_xilinx_apm.c
+++ drivers/uio/uio_xilinx_apm.c
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Xilinx AXI Performance Monitor
  *
- * Copyright (C) 2013 Xilinx, Inc. All rights reserved.
+ * Copyright (C) 2013 - 2019 Xilinx, Inc. All rights reserved.
  *
  * Description:
  * This driver is developed for AXI Performance Monitor IP,
@@ -9,18 +10,6 @@
  * of AXI bus in the system. Driver maps HW registers and parameters
  * to userspace. Userspace need not clear the interrupt of IP since
  * driver clears the interrupt.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/clk.h>
