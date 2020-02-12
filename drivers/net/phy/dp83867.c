a1c718babcc65c64ae7c63b14bedada2e862d54c
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2020-02-12 20:39:24 +0530

    net: phy: dp83867: Convert 6 wire mode to ti,sgmii-ref-clock-output-enable
    Changes have been done in mainline Linux by commit a2111c460c0c
    ("net: phy: dp83867: Add documentation for SGMII mode type") and
    by commit 507ddd5c0d47 ("net: phy: dp83867: Add SGMII mode type
    switching"). This patch align macro and variable names, use
    read-modify-write sequence for reg updates to match mainline Linux.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- drivers/net/phy/dp83867.c
+++ drivers/net/phy/dp83867.c
@@ -39,7 +39,7 @@
 #define DP83867_STRAP_STS1	0x006E
 #define DP83867_RGMIIDCTL	0x0086
 #define DP83867_IO_MUX_CFG	0x0170
-#define DP83867_SGMIITYPE	0x00D3
+#define DP83867_SGMIICTL	0x00D3
 
 #define DP83867_SW_RESET	BIT(15)
 #define DP83867_SW_RESTART	BIT(14)
@@ -107,7 +107,7 @@
 /* CFG4 bits */
 #define DP83867_CFG4_PORT_MIRROR_EN              BIT(0)
 /* SGMIICTL1 bits */
-#define DP83867_SGMIICLK_EN			BIT(14)
+#define DP83867_SGMII_TYPE		BIT(14)
 
 enum {
 	DP83867_PORT_MIRROING_KEEP,
@@ -123,7 +123,7 @@ struct dp83867_private {
 	int port_mirroring;
 	bool rxctrl_strap_quirk;
 	int clk_output_sel;
-	bool wiremode_6;
+	bool sgmii_ref_clk_en;
 };
 
 static int dp83867_ack_interrupt(struct phy_device *phydev)
@@ -207,6 +207,8 @@ static int dp83867_of_init(struct phy_device *phydev)
 
 	dp83867->rxctrl_strap_quirk = of_property_read_bool(of_node,
 					"ti,dp83867-rxctrl-strap-quirk");
+	dp83867->sgmii_ref_clk_en = of_property_read_bool(of_node,
+					"ti,sgmii-ref-clock-output-enable");
 
 	ret = of_property_read_u32(of_node, "ti,rx-internal-delay",
 				   &dp83867->rx_id_delay);
@@ -227,8 +229,6 @@ static int dp83867_of_init(struct phy_device *phydev)
 
 	if (of_property_read_bool(of_node, "enet-phy-lane-no-swap"))
 		dp83867->port_mirroring = DP83867_PORT_MIRROING_DIS;
-
-	dp83867->wiremode_6 = of_property_read_bool(of_node, "ti,6-wire-mode");
 
 	return of_property_read_u32(of_node, "ti,fifo-depth",
 				   &dp83867->fifo_depth);
@@ -300,9 +300,13 @@ static int dp83867_config_init(struct phy_device *phydev)
 
 	} else {
 		/* Set SGMIICTL1 6-wire mode */
-		if (dp83867->wiremode_6)
-			phy_write_mmd(phydev, DP83867_DEVADDR,
-				      DP83867_SGMIITYPE, DP83867_SGMIICLK_EN);
+		if (dp83867->sgmii_ref_clk_en)
+			val |= DP83867_SGMII_TYPE;
+		else
+			val &= ~DP83867_SGMII_TYPE;
+
+		phy_write_mmd(phydev, DP83867_DEVADDR,
+			      DP83867_SGMIICTL, val);
 
 		phy_write(phydev, MII_BMCR,
 			  (BMCR_ANENABLE | BMCR_FULLDPLX | BMCR_SPEED1000));
