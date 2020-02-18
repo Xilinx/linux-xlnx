b729cd2c315f8317e23c0a6ab04cb61603d6aaa5
Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>
2020-02-18 17:55:43 +0530

    Revert "PCI: XDMA PL: Use pci_parse_request_of_pci_ranges()"
    This reverts commit 3f06a3d8f2da17f94c00e89409a6d5743d7dbf99.
    PCI host resource allocation changed in v5.5 kernel.
    This patch is applicable from v5.5 kernel, as the parameters
    pci_parse_request_of_pci_ranges changed from v5.5 kernel to
    do required resource allocation.
    This patch change was a mishap due to some uncleaned file
    sources in workspace.
    
    The v5.4 kernel uses different numbers of parameters for
    pci_parse_request_of_pci_ranges; without this patch revert
    we will see a compilation error as mentioned in follwoing CR.
    
    Signed-off-by: Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>

--- drivers/pci/controller/pcie-xdma-pl.c
+++ drivers/pci/controller/pcie-xdma-pl.c
@@ -814,6 +814,8 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 	struct pci_bus *child;
 	struct pci_host_bridge *bridge;
 	int err;
+	resource_size_t iobase = 0;
+	LIST_HEAD(res);
 
 	bridge = devm_pci_alloc_host_bridge(dev, sizeof(*port));
 	if (!bridge)
@@ -837,13 +839,18 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 		return err;
 	}
 
-	err = pci_parse_request_of_pci_ranges(dev, &bridge->windows,
-					      &bridge->dma_ranges, NULL);
+	err = devm_of_pci_get_host_bridge_resources(dev, 0, 0xff, &res,
+						    &iobase);
 	if (err) {
 		dev_err(dev, "Getting bridge resources failed\n");
 		return err;
 	}
 
+	err = devm_request_pci_bus_resources(dev, &res);
+	if (err)
+		goto error;
+
+	list_splice_init(&res, &bridge->windows);
 	bridge->dev.parent = dev;
 	bridge->sysdata = port;
 	bridge->busnr = port->root_busno;
@@ -853,7 +860,7 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 
 	err = pci_scan_root_bus_bridge(bridge);
 	if (err)
-		return err;
+		goto error;
 
 	bus = bridge->bus;
 
@@ -862,6 +869,10 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 		pcie_bus_configure_settings(child);
 	pci_bus_add_devices(bus);
 	return 0;
+
+error:
+	pci_free_resource_list(&res);
+	return err;
 }
 
 static const struct of_device_id xilinx_pcie_of_match[] = {
