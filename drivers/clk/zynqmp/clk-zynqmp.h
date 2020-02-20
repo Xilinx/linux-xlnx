fdbb2e62f6a5e26d990fee0436812e48ec5632c7
Tejas Patel <tejas.patel@xilinx.com>
2020-02-20 04:40:38 -0800

    clk: zynqmp: Update CLK_FRAC support as per upstream
    This patch updates CLK_FRAC support as per upstream comments.
    It includes below changes.
    - Instead of shifting TYPE_FLAG2 into the existing type_flag, use new
      field custom_type_flag
    - To keep backward compatibility with older version of TF-A, where
      CLK_FRAC flag was part of common clk framework flags BIT(13),
      consider BIT(13) from CCF flag or BIT(4) of custom type flag.
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    State: pending

--- drivers/clk/zynqmp/clk-zynqmp.h
+++ drivers/clk/zynqmp/clk-zynqmp.h
@@ -25,11 +25,13 @@ enum topology_type {
  * @type:	Type of topology
  * @flag:	Topology flags
  * @type_flag:	Topology type specific flag
+ * @custom_type_flag: Topology type specific custome flag
  */
 struct clock_topology {
 	u32 type;
 	u32 flag;
 	u32 type_flag;
+	u8 custom_type_flag;
 };
 
 struct clk_hw *zynqmp_clk_register_pll(const char *name, u32 clk_id,
