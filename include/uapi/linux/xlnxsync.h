71bc15ef770620bf8d0c1d404afca625465b4fb2
Devarsh Thakkar <devarsh.thakkar@xilinx.com>
2020-02-24 06:36:18 -0800

    staging: xlnxsync: Optimize the driver for multi-channel use-cases
    1) Remove redundant XLNXSYNC_RESERVE_GET_CHAN_ID ioctl as application
       need not know the channel ID assigned to it (unless for debugging
       purpose where we pass it through XLNXSYNC_GET_CFG ioctl) and driver
       can maintain the assigned channel id internally.
    
    2) Channel specific ioctls like XLNXSYNC_GET_CHAN_STATUS now only
       return the status of particular channel which application
       is using.
    
    3) Wait queues for framedone and error events are made channel specific
       and are woken up only if respective event has occurred on
       particular channel being used by a particular application.
    
    4) We don't pass channel ID to IOCTL's as driver is already aware of
       unique channel ID being assigned to each application.
    
    5) Use separate lock per channel to serialize channel specific ioctl and
       polling.
    
    6) Update IOCTL header version as it's an API change.
    
    Signed-off-by: Devarsh Thakkar <devarsh.thakkar@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    State: pending

--- include/uapi/linux/xlnxsync.h
+++ include/uapi/linux/xlnxsync.h
@@ -3,7 +3,7 @@
 #ifndef __XLNXSYNC_H__
 #define __XLNXSYNC_H__
 
-#define XLNXSYNC_IOCTL_HDR_VER		0x10001
+#define XLNXSYNC_IOCTL_HDR_VER		0x10002
 
 /*
  * This is set in the fb_id of struct xlnxsync_chan_config when
@@ -35,7 +35,6 @@
  * @dma_fd: File descriptor of dma
  * @fb_id: Framebuffer index. Valid values 0/1/2/XLNXSYNC_AUTO_SEARCH
  * @ismono: Flag to indicate if buffer is Luma only.
- * @channel_id: Channel index to be configured.
  * Valid 0..3 & XLNXSYNC_AUTO_SEARCH
  *
  * This structure contains the configuration for monitoring a particular
@@ -54,13 +53,11 @@ struct xlnxsync_chan_config {
 	u32 dma_fd;
 	u8 fb_id[XLNXSYNC_IO];
 	u8 ismono[XLNXSYNC_IO];
-	u8 channel_id;
 };
 
 /**
  * struct xlnxsync_clr_err - Clear channel error
  * @hdr_ver: IOCTL header version
- * @channel_id: Channel id whose error needs to be cleared
  * @sync_err: Set this to clear sync error
  * @wdg_err: Set this to clear watchdog error
  * @ldiff_err: Set this to clear luma difference error
@@ -68,7 +65,6 @@ struct xlnxsync_chan_config {
  */
 struct xlnxsync_clr_err {
 	u64 hdr_ver;
-	u8 channel_id;
 	u8 sync_err;
 	u8 wdg_err;
 	u8 ldiff_err;
@@ -82,7 +78,7 @@ struct xlnxsync_clr_err {
  */
 struct xlnxsync_fbdone {
 	u64 hdr_ver;
-	u8 status[XLNXSYNC_MAX_ENC_CHAN][XLNXSYNC_BUF_PER_CHAN][XLNXSYNC_IO];
+	u8 status[XLNXSYNC_BUF_PER_CHAN][XLNXSYNC_IO];
 };
 
 /**
@@ -90,15 +86,20 @@ struct xlnxsync_fbdone {
  * @hdr_ver: IOCTL header version
  * @encode: true if encoder type, false for decoder type
  * @max_channels: Maximum channels this IP supports
+ * @active_channels: Number of active IP channels
+ * @reserved_id: Reserved channel ID for instance
  */
 struct xlnxsync_config {
 	u64	hdr_ver;
 	u8	encode;
 	u8	max_channels;
+	u8	active_channels;
+	u8	reserved_id;
+	u32	reserved[10];
 };
 
 /**
- * struct xlnxsync_stat - Sync IP status
+ * struct xlnxsync_stat - Sync IP channel status
  * @hdr_ver: IOCTL header version
  * @fbdone: for every pair of luma/chroma buffer for every producer/consumer
  * @enable: channel enable
@@ -109,12 +110,12 @@ struct xlnxsync_config {
  */
 struct xlnxsync_stat {
 	u64 hdr_ver;
-	u8 fbdone[XLNXSYNC_MAX_ENC_CHAN][XLNXSYNC_BUF_PER_CHAN][XLNXSYNC_IO];
-	u8 enable[XLNXSYNC_MAX_ENC_CHAN];
-	u8 sync_err[XLNXSYNC_MAX_ENC_CHAN];
-	u8 wdg_err[XLNXSYNC_MAX_ENC_CHAN];
-	u8 ldiff_err[XLNXSYNC_MAX_ENC_CHAN];
-	u8 cdiff_err[XLNXSYNC_MAX_ENC_CHAN];
+	u8 fbdone[XLNXSYNC_BUF_PER_CHAN][XLNXSYNC_IO];
+	u8 enable;
+	u8 sync_err;
+	u8 wdg_err;
+	u8 ldiff_err;
+	u8 cdiff_err;
 };
 
 #define XLNXSYNC_MAGIC			'X'
@@ -126,24 +127,21 @@ struct xlnxsync_stat {
 #define XLNXSYNC_GET_CFG		_IOR(XLNXSYNC_MAGIC, 1,\
 					     struct xlnxsync_config *)
 /* This ioctl is used to get the channel status */
-#define XLNXSYNC_GET_CHAN_STATUS	_IOR(XLNXSYNC_MAGIC, 2, u32 *)
+#define XLNXSYNC_CHAN_GET_STATUS	_IOR(XLNXSYNC_MAGIC, 2, u32 *)
 /* This is used to set the framebuffer address for a channel */
-#define XLNXSYNC_SET_CHAN_CONFIG	_IOW(XLNXSYNC_MAGIC, 3,\
+#define XLNXSYNC_CHAN_SET_CONFIG	_IOW(XLNXSYNC_MAGIC, 3,\
 					     struct xlnxsync_chan_config *)
-/* Enable a channel. The argument is channel number between 0 and 3 */
-#define XLNXSYNC_CHAN_ENABLE		_IOR(XLNXSYNC_MAGIC, 4, u8)
-/* Enable a channel. The argument is channel number between 0 and 3 */
-#define XLNXSYNC_CHAN_DISABLE		_IOR(XLNXSYNC_MAGIC, 5, u8)
+/* Enable a channel. */
+#define XLNXSYNC_CHAN_ENABLE		_IO(XLNXSYNC_MAGIC, 4)
+/* Disable a channel. */
+#define XLNXSYNC_CHAN_DISABLE		_IO(XLNXSYNC_MAGIC, 5)
 /* This is used to clear the Sync and Watchdog errors  for a channel */
-#define XLNXSYNC_CLR_CHAN_ERR		_IOW(XLNXSYNC_MAGIC, 6,\
+#define XLNXSYNC_CHAN_CLR_ERR		_IOW(XLNXSYNC_MAGIC, 6,\
 					     struct xlnxsync_clr_err *)
 /* This is used to get the framebuffer done status for a channel */
-#define XLNXSYNC_GET_CHAN_FBDONE_STAT	_IOR(XLNXSYNC_MAGIC, 7,\
+#define XLNXSYNC_CHAN_GET_FBDONE_STAT	_IOR(XLNXSYNC_MAGIC, 7,\
 					     struct xlnxsync_fbdone *)
 /* This is used to clear the framebuffer done status for a channel */
-#define XLNXSYNC_CLR_CHAN_FBDONE_STAT	_IOW(XLNXSYNC_MAGIC, 8,\
+#define XLNXSYNC_CHAN_CLR_FBDONE_STAT	_IOW(XLNXSYNC_MAGIC, 8,\
 					     struct xlnxsync_fbdone *)
-/* Reserve channel */
-#define XLNXSYNC_RESERVE_GET_CHAN_ID	_IOR(XLNXSYNC_MAGIC, 9, u8 *)
-
 #endif
