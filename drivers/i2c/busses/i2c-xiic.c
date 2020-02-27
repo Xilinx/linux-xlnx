d1920b0f0665e4879513ad527d0c8c3a38323353
Raviteja Narayanam <raviteja.narayanam@xilinx.com>
2020-02-27 20:10:31 +0530

    i2c: xiic: Fix Tx Interrupt path for grouped messages
    When a group of messages are sent from user space as a set, if
    the last message has less than Tx FIFO DEPTH number of bytes
    to transfer, Tx half empty interrupt is triggered continuously
    from the hardware. It is due to Bus not busy interrupt coming
    along with Tx half empty and tx empty.
    
    Hence, service the Tx interrupts before Bus not busy interrupt
    to update the i2c message status correctly.
    
    Signed-off-by: Raviteja Narayanam <raviteja.narayanam@xilinx.com>
    State: pending

--- drivers/i2c/busses/i2c-xiic.c
+++ drivers/i2c/busses/i2c-xiic.c
@@ -451,22 +451,6 @@ static irqreturn_t xiic_process(int irq, void *dev_id)
 			}
 		}
 	}
-	if (pend & XIIC_INTR_BNB_MASK) {
-		/* IIC bus has transitioned to not busy */
-		clr |= XIIC_INTR_BNB_MASK;
-
-		/* The bus is not busy, disable BusNotBusy interrupt */
-		xiic_irq_dis(i2c, XIIC_INTR_BNB_MASK);
-
-		if (!i2c->tx_msg)
-			goto out;
-
-		if ((i2c->nmsgs == 1) && !i2c->rx_msg &&
-			xiic_tx_space(i2c) == 0)
-			xiic_wakeup(i2c, STATE_DONE);
-		else
-			xiic_wakeup(i2c, STATE_ERROR);
-	}
 	if (pend & (XIIC_INTR_TX_EMPTY_MASK | XIIC_INTR_TX_HALF_MASK)) {
 		/* Transmit register/FIFO is empty or ½ empty */
 
@@ -503,6 +487,24 @@ static irqreturn_t xiic_process(int irq, void *dev_id)
 			 */
 			xiic_irq_dis(i2c, XIIC_INTR_TX_HALF_MASK);
 	}
+
+	if (pend & XIIC_INTR_BNB_MASK) {
+		/* IIC bus has transitioned to not busy */
+		clr |= XIIC_INTR_BNB_MASK;
+
+		/* The bus is not busy, disable BusNotBusy interrupt */
+		xiic_irq_dis(i2c, XIIC_INTR_BNB_MASK);
+
+		if (!i2c->tx_msg)
+			goto out;
+
+		if (i2c->nmsgs == 1 && !i2c->rx_msg &&
+		    xiic_tx_space(i2c) == 0)
+			xiic_wakeup(i2c, STATE_DONE);
+		else
+			xiic_wakeup(i2c, STATE_ERROR);
+	}
+
 out:
 	dev_dbg(i2c->adap.dev.parent, "%s clr: 0x%x\n", __func__, clr);
 
