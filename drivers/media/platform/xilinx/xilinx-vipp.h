239b4d6cbe37660c9e1aa475615e81b77ccfd948
Hyun Kwon <hyun.kwon@xilinx.com>
2020-03-11 14:57:56 -0700

    v4l: xilinx: vipp: move xvip_entity_start_stop() to xilinx-vipp
    This is a preparation to do the graph level stream on/off handling
    in xilinx-vipp. Rename an argument name to align: 'start' to 'on'.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Reviewed-by: Dylan Yip <dylan.yip@xilinx.com>
    State: pending

--- drivers/media/platform/xilinx/xilinx-vipp.h
+++ drivers/media/platform/xilinx/xilinx-vipp.h
@@ -41,7 +41,7 @@ struct xvip_composite_device {
 	struct mutex lock; /* lock to protect xvip pipeline instance */
 };
 
-bool xvip_subdev_set_streaming(struct xvip_composite_device *xdev,
-			       struct v4l2_subdev *subdev, bool enable);
+int xvip_entity_start_stop(struct xvip_composite_device *xdev,
+			   struct media_entity *entity, bool on);
 
 #endif /* __XILINX_VIPP_H__ */
