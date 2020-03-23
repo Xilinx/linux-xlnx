7037e2676e095dd5b43cc5ace1598e5cb828ab92
Srinivas Neeli <srinivas.neeli@xilinx.com>
2020-03-23 23:23:02 +0530

    pinctrl: zynq: use module_platform_driver to simplify the code
    module_platform_driver() makes the code simpler by eliminating
    boilerplate code.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    State: pending

--- drivers/pinctrl/pinctrl-zynq.c
+++ drivers/pinctrl/pinctrl-zynq.c
@@ -8,6 +8,7 @@
  */
 #include <linux/io.h>
 #include <linux/mfd/syscon.h>
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
@@ -1208,8 +1209,4 @@ static struct platform_driver zynq_pinctrl_driver = {
 	.probe = zynq_pinctrl_probe,
 };
 
-static int __init zynq_pinctrl_init(void)
-{
-	return platform_driver_register(&zynq_pinctrl_driver);
-}
-arch_initcall(zynq_pinctrl_init);
+module_platform_driver(zynq_pinctrl_driver);
