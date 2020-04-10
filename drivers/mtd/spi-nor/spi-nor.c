2527b868e883a537254b5faf7da6e8906a0aa7e5
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2020-04-10 10:20:26 +0530

    mtd: spi-nor: Fix page size calculation
    Do not shift the page, as we are not updating this when we start supporting
    the ubifs. there are corner cases in ubifs where the page size can be
    unaligned, in this case the page size is wrongly calculated, which is
    causing failures in ubi stress test. where as in normal mtd tests and
    flashcp we will never see unaligned page sizes.
    
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    State: pending

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -5563,7 +5563,6 @@ int spi_nor_scan(struct spi_nor *nor, const char *name,
 				nor->shift = 1;
 				info->sector_size <<= nor->shift;
 				info->page_size <<= nor->shift;
-				nor->page_size = info->page_size;
 				mtd->size <<= nor->shift;
 				nor->isparallel = 1;
 				nor->isstacked = 0;
