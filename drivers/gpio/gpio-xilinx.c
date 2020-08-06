cf1eebd8716c5260bbe068710c3a26bcdee56343
Srinivas Neeli <srinivas.neeli@xilinx.com>
2020-08-06 13:38:11 +0530

    gpio: gpio-xilinx: Check return value of of_property_read_u32
    In three different instances the return value of "of_property_read_u32"
    API was neither captured nor checked.
    Fixed it by capturing the return value and then checking for any error.
    
    Addresses-Coverity: "check_return"
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    State: pending

--- drivers/gpio/gpio-xilinx.c
+++ drivers/gpio/gpio-xilinx.c
@@ -612,18 +612,21 @@ static int xgpio_of_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	/* Update GPIO state shadow register with default value */
-	of_property_read_u32(np, "xlnx,dout-default", &chip->gpio_state);
+	if (of_property_read_u32(np, "xlnx,dout-default", &chip->gpio_state))
+		dev_dbg(&pdev->dev, "Missing xlnx,dout-default property\n");
 
 	/* By default, all pins are inputs */
 	chip->gpio_dir = 0xFFFFFFFF;
 
 	/* Update GPIO direction shadow register with default value */
-	of_property_read_u32(np, "xlnx,tri-default", &chip->gpio_dir);
+	if (of_property_read_u32(np, "xlnx,tri-default", &chip->gpio_dir))
+		dev_dbg(&pdev->dev, "Missing xlnx,tri-default property\n");
 
 	chip->no_init = of_property_read_bool(np, "xlnx,no-init");
 
 	/* Update cells with gpio-cells value */
-	of_property_read_u32(np, "#gpio-cells", &cells);
+	if (of_property_read_u32(np, "#gpio-cells", &cells))
+		dev_dbg(&pdev->dev, "Missing gpio-cells property\n");
 
 	/*
 	 * Check device node and parent device node for device width
