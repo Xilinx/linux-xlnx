57875facac1a9d4e26819b8ad22a520b29773ec9
Wendy Liang <wendy.liang@xilinx.com>
2020-09-16 16:56:20 -0700

    misc: xilinx-ai-engine: clear cached events of partition in reset/release
    Clear the cached events of an AI engine partition in partition reset
    and partition file release function. Otherwise, next time, when the
    partition is requested again or after reset, the application will still
    see the errors before the last reset/release.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>
    State: pending

--- drivers/misc/xilinx-ai-engine/ai-engine-reset.c
+++ drivers/misc/xilinx-ai-engine/ai-engine-reset.c
@@ -208,6 +208,8 @@ int aie_part_reset(struct aie_partition *apart)
 
 	aie_part_set_cols_clkbuf(apart, false);
 
+	aie_part_clear_cached_events(apart);
+
 	mutex_unlock(&apart->mlock);
 	return 0;
 }
