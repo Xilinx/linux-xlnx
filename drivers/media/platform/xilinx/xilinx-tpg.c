f7d7ec4aca02d3df52663990140822dfdd7da943
Vishal Sagar <vishal.sagar@xilinx.com>
2020-09-22 12:24:09 -0700

    v4l: xilinx: tpg: Fix warnings for NULL_RETURNS
    This patch fix the warnings for the NULL_RETURNS type.
    
    The actual warnings are:
    
    For __xtpg_get_pad_format():
    Event return_null: 	Explicitly returning null.
    
    For xtpg_get_format():
    Event returned_null: 	"__xtpg_get_pad_format" returns "NULL".
    Event dereference: 	Dereferencing "__xtpg_get_pad_format(xtpg, cfg,
    fmt->pad, fmt->which)", which is known to be "NULL".
    
    For xtpg_set_format():
    Event returned_null: 	"__xtpg_get_pad_format" returns "NULL".
    Event var_assigned: 	Assigning: "__format" = "NULL" return value from
    "__xtpg_get_pad_format".
    Event dereference: 	Dereferencing "__format", which is known to be
    "NULL".
    
    Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- drivers/media/platform/xilinx/xilinx-tpg.c
+++ drivers/media/platform/xilinx/xilinx-tpg.c
@@ -392,14 +392,22 @@ __xtpg_get_pad_format(struct xtpg_device *xtpg,
 		      struct v4l2_subdev_pad_config *cfg,
 		      unsigned int pad, u32 which)
 {
+	struct v4l2_mbus_framefmt *format;
+
 	switch (which) {
 	case V4L2_SUBDEV_FORMAT_TRY:
-		return v4l2_subdev_get_try_format(&xtpg->xvip.subdev, cfg, pad);
+		format = v4l2_subdev_get_try_format(&xtpg->xvip.subdev, cfg,
+						    pad);
+		break;
 	case V4L2_SUBDEV_FORMAT_ACTIVE:
-		return &xtpg->formats[pad];
+		format = &xtpg->formats[pad];
+		break;
 	default:
-		return NULL;
+		format = NULL;
+		break;
 	}
+
+	return format;
 }
 
 static int xtpg_get_format(struct v4l2_subdev *subdev,
@@ -407,8 +415,13 @@ static int xtpg_get_format(struct v4l2_subdev *subdev,
 			   struct v4l2_subdev_format *fmt)
 {
 	struct xtpg_device *xtpg = to_tpg(subdev);
+	struct v4l2_mbus_framefmt *format;
 
-	fmt->format = *__xtpg_get_pad_format(xtpg, cfg, fmt->pad, fmt->which);
+	format = __xtpg_get_pad_format(xtpg, cfg, fmt->pad, fmt->which);
+	if (!format)
+		return -EINVAL;
+
+	fmt->format = *format;
 
 	return 0;
 }
@@ -422,6 +435,8 @@ static int xtpg_set_format(struct v4l2_subdev *subdev,
 	u32 bayer_phase;
 
 	__format = __xtpg_get_pad_format(xtpg, cfg, fmt->pad, fmt->which);
+	if (!__format)
+		return -EINVAL;
 
 	/* In two pads mode the source pad format is always identical to the
 	 * sink pad format.
@@ -465,6 +480,8 @@ static int xtpg_set_format(struct v4l2_subdev *subdev,
 	/* Propagate the format to the source pad. */
 	if (xtpg->npads == 2) {
 		__format = __xtpg_get_pad_format(xtpg, cfg, 1, fmt->which);
+		if (!__format)
+			return -EINVAL;
 		*__format = fmt->format;
 	}
 
