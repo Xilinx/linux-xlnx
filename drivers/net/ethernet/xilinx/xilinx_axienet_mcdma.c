d42de16ed2832785de4066fac3883d46ed3f063f
Harini Katakam <harini.katakam@xilinx.com>
2020-09-24 13:16:49 +0530

    net: xilinx: XXV Ethernet HW timestamp fixes
    When mcdma error occurs, the BDs are reset. Correspondingly, the
    timestamp FIFOs should also be reset.
    
    In timestamp_mode function, there's an unnecessary read of a register
    offset that is not defined or relevant for XXV. Remove the same.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending

--- drivers/net/ethernet/xilinx/xilinx_axienet_mcdma.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_mcdma.c
@@ -723,6 +723,19 @@ void __maybe_unused axienet_mcdma_err_handler(unsigned long data)
 	if (lp->axienet_config->mactype != XAXIENET_10G_25G)
 		axienet_iow(lp, XAE_FCC_OFFSET, XAE_FCC_FCRX_MASK);
 
+#ifdef CONFIG_XILINX_AXI_EMAC_HWTSTAMP
+	if (lp->axienet_config->mactype == XAXIENET_10G_25G) {
+		axienet_rxts_iow(lp, XAXIFIFO_TXTS_RDFR,
+				 XAXIFIFO_TXTS_RESET_MASK);
+		axienet_rxts_iow(lp, XAXIFIFO_TXTS_SRR,
+				 XAXIFIFO_TXTS_RESET_MASK);
+		axienet_txts_iow(lp, XAXIFIFO_TXTS_RDFR,
+				 XAXIFIFO_TXTS_RESET_MASK);
+		axienet_txts_iow(lp, XAXIFIFO_TXTS_SRR,
+				 XAXIFIFO_TXTS_RESET_MASK);
+	}
+#endif
+
 	lp->axienet_config->setoptions(ndev, lp->options &
 				       ~(XAE_OPTION_TXEN | XAE_OPTION_RXEN));
 	axienet_set_mac_address(ndev, NULL);
