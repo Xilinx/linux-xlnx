a4a61cb598b2a87452ebefb0d77f27b2503e29cf
Shravya Kumbham <shravya.kumbham@xilinx.com>
2020-11-29 21:02:23 -0700

    dmaengine: pl330: Typecast with enum to fix the coverity warning
    Typecast the flags and flg variables with (enum dma_ctrl_flags) in
    pl330_prep_dma_cyclic, pl330_prep_dma_memcpy and pl330_prep_slave_sg
    functions to fix the coverity warning.
    
    Addresses-Coverity: Event mixed_enum_type.
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -2752,7 +2752,7 @@ static struct dma_async_tx_descriptor *pl330_prep_dma_cyclic(
 		return NULL;
 
 	pch->cyclic = true;
-	desc->txd.flags = flags;
+	desc->txd.flags = (enum dma_ctrl_flags) flags;
 
 	return &desc->txd;
 }
@@ -2804,7 +2804,7 @@ pl330_prep_dma_memcpy(struct dma_chan *chan, dma_addr_t dst,
 
 	desc->bytes_requested = len;
 
-	desc->txd.flags = flags;
+	desc->txd.flags = (enum dma_ctrl_flags) flags;
 
 	return &desc->txd;
 }
@@ -2889,7 +2889,7 @@ pl330_prep_slave_sg(struct dma_chan *chan, struct scatterlist *sgl,
 	}
 
 	/* Return the last desc in the chain */
-	desc->txd.flags = flg;
+	desc->txd.flags = (enum dma_ctrl_flags) flg;
 	return &desc->txd;
 }
 
