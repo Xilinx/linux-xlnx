5bc40514bcf1622a79dd1dbe8b8475ac1a3e3618
Ben Levinsky <ben.levinsky@xilinx.com>
2020-11-30 07:57:13 -0800

    firmware: xilinx: Add ZynqMP firmware ioctl enums for RPU configuration.
    Add ZynqMP firmware ioctl enums for RPU configuration and TCM Nodes for
    later use via request_node and release_node
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    Link: https://lore.kernel.org/r/20201130155717.26008-2-ben.levinsky@xilinx.com
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: waiting (remoteproc)

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -100,6 +100,10 @@ enum pm_ret_status {
 };
 
 enum pm_ioctl_id {
+	IOCTL_GET_RPU_OPER_MODE = 0,
+	IOCTL_SET_RPU_OPER_MODE = 1,
+	IOCTL_RPU_BOOT_ADDR_CONFIG = 2,
+	IOCTL_TCM_COMB_CONFIG = 3,
 	IOCTL_SD_DLL_RESET = 6,
 	IOCTL_SET_SD_TAPDELAY,
 	IOCTL_SET_PLL_FRAC_MODE,
@@ -123,6 +127,21 @@ enum pm_query_id {
 	PM_QID_CLOCK_GET_ATTRIBUTES,
 	PM_QID_CLOCK_GET_NUM_CLOCKS = 12,
 	PM_QID_CLOCK_GET_MAX_DIVISOR,
+};
+
+enum rpu_oper_mode {
+	PM_RPU_MODE_LOCKSTEP = 0,
+	PM_RPU_MODE_SPLIT = 1,
+};
+
+enum rpu_boot_mem {
+	PM_RPU_BOOTMEM_LOVEC = 0,
+	PM_RPU_BOOTMEM_HIVEC = 1,
+};
+
+enum rpu_tcm_comb {
+	PM_RPU_TCM_SPLIT = 0,
+	PM_RPU_TCM_COMB = 1,
 };
 
 enum zynqmp_pm_reset_action {
@@ -269,6 +288,10 @@ enum zynqmp_pm_request_ack {
 };
 
 enum pm_node_id {
+	NODE_TCM_0_A = 15,
+	NODE_TCM_0_B = 16,
+	NODE_TCM_1_A = 17,
+	NODE_TCM_1_B = 18,
 	NODE_SD_0 = 39,
 	NODE_SD_1,
 };
