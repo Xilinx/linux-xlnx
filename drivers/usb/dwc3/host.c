3cc23b58c4e6c10c32ec8639a666a8fdbc8ac9e8
Piyush Mehta <piyush.mehta@xilinx.com>
2021-02-04 11:49:33 -0700

    usb: Add quirk for enabling workaround for BULK IN streams
    This patch adds support for enabling workaround for BULK IN stream
    rings by adding "xhci-stream-quirk" flag to dts node. Enabling this
    flag activates the timer for every BULK IN stream ring at the time
    of queuing TRB's into BULK IN stream ring.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -9,6 +9,7 @@
 
 #include <linux/acpi.h>
 #include <linux/platform_device.h>
+#include <linux/of_device.h>
 
 #include "core.h"
 
@@ -44,7 +45,7 @@ out:
 
 int dwc3_host_init(struct dwc3 *dwc)
 {
-	struct property_entry	props[4];
+	struct property_entry	props[5];
 	struct platform_device	*xhci;
 	int			ret, irq;
 	struct resource		*res;
@@ -94,6 +95,10 @@ int dwc3_host_init(struct dwc3 *dwc)
 
 	if (dwc->usb2_lpm_disable)
 		props[prop_idx++] = PROPERTY_ENTRY_BOOL("usb2-lpm-disable");
+
+	if (device_property_read_bool(&dwc3_pdev->dev,
+					"snps,xhci-stream-quirk"))
+		props[prop_idx++].name = "xhci-stream-quirk";
 
 	/**
 	 * WORKAROUND: dwc3 revisions <=3.00a have a limitation
