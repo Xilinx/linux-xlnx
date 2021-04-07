4da8ce964e8fbe359a672ed3654051d5ccccec78
Jianqiang Chen <jianqiang.chen@xilinx.com>
2021-04-07 02:51:01 -0700

    drm: xlnx: Update DP subsystem with downstream version
    This patch replaces mainline DP subsystem with downstream version
    
    Signed-off-by: Jianqiang Chen <jianqiang.chen@xilinx.com>
    State: pending
    [michals: Squashed with
    gpu: drm: xlnx: Fixed DP sound card registration issue
    drm: xlnx: zynqmp_dpsub: Pass pcm auxdata when populating sound nodes
    drm: xlnx: zynqmp: Rebase downstream Display port crtc features
    drm: xlnx: zynqmp_dpsub: Fix plane ordering
    drm: xlnx: zynqmp_dpsub: Fix graphics layer blending
    drm: xlnx: zynqmp_dpsub: Add global alpha support
    drm: xlnx: Update dependencies for ZynqMP DP
    drm: xlnx: zynqmp_dpsub: Fix pixel clock issue
    drm: xlnx: zynqmp: Update planes asynchronously in the legacy entry
    drm: xlnx: zynqmp: Add XV15 and XV20 formats
    drm: xlnx: Update ZynqMP DP kconfig setting
    drm: xlnx: zynqmp: fix -Wunused-variable build warning
    drm: xlnx: zynqmp_dp: update DP encoder/connector driver with downstream version
    drm: xlnx: zynqmp_dp: fix suspend/resume issue
    drm: xlnx: zynqmp_dp: Add reset for display port phy
    drm: xlnx: zynqmp_dp: update aux drm_dev in dp bind
    drm: xlnx: zynqmp_dp: release reset for DP before accessing DP registers
    v5.15 update
    v6.1 update
    ]

--- drivers/gpu/drm/xlnx/zynqmp_dp.h
+++ drivers/gpu/drm/xlnx/zynqmp_dp.h
@@ -12,16 +12,20 @@
 #ifndef _ZYNQMP_DP_H_
 #define _ZYNQMP_DP_H_
 
-struct drm_device;
-struct platform_device;
 struct zynqmp_dp;
-struct zynqmp_dpsub;
+struct drm_display_mode;
 
+int zynqmp_dp_set_color(struct zynqmp_dp *dp, const char *color);
 void zynqmp_dp_enable_vblank(struct zynqmp_dp *dp);
 void zynqmp_dp_disable_vblank(struct zynqmp_dp *dp);
+void zynqmp_dp_encoder_mode_set_stream(struct zynqmp_dp *dp,
+				       struct drm_display_mode *mode);
+void __maybe_unused zynqmp_dp_pm_suspend(struct zynqmp_dp *dp);
+void __maybe_unused zynqmp_dp_pm_resume(struct zynqmp_dp *dp);
+int zynqmp_dp_bind(struct device *dev, struct device *master, void *data);
+void zynqmp_dp_unbind(struct device *dev, struct device *master, void *data);
 
-int zynqmp_dp_drm_init(struct zynqmp_dpsub *dpsub);
-int zynqmp_dp_probe(struct zynqmp_dpsub *dpsub, struct drm_device *drm);
-void zynqmp_dp_remove(struct zynqmp_dpsub *dpsub);
+int zynqmp_dp_probe(struct platform_device *pdev);
+int zynqmp_dp_remove(struct platform_device *pdev);
 
 #endif /* _ZYNQMP_DP_H_ */
