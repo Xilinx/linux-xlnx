91fb5a3f1808cfd790a5c64ed46097df592042c7
Miquel Raynal <miquel.raynal@bootlin.com>
2021-05-18 08:59:56 -0600

    mtd: rawnand: Use more recent ONFI specification wording
    In particular, first ONFI specifications referred to SDR modes as
    asynchronous modes, which is not the term we usually have in mind. The
    spec has then been updated, so do the same here in the NAND subsystem to
    avoid any possible confusion.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    Link: https://lore.kernel.org/linux-mtd/20210505213750.257417-1-miquel.raynal@bootlin.com/
    State: waiting (nand)

--- drivers/mtd/nand/raw/nand_onfi.c
+++ drivers/mtd/nand/raw/nand_onfi.c
@@ -315,9 +315,8 @@ int nand_onfi_detect(struct nand_chip *chip)
 	onfi->tBERS = le16_to_cpu(p->t_bers);
 	onfi->tR = le16_to_cpu(p->t_r);
 	onfi->tCCS = le16_to_cpu(p->t_ccs);
-	onfi->async_timing_mode = le16_to_cpu(p->async_timing_mode);
+	onfi->sdr_timing_modes = le16_to_cpu(p->sdr_timing_modes);
 	onfi->jedec_id = le16_to_cpu(p->jedec_id);
-	onfi->src_sync_timing_mode = le16_to_cpu(p->src_sync_timing_mode);
 	onfi->vendor_revision = le16_to_cpu(p->vendor_revision);
 	memcpy(onfi->vendor, p->vendor, sizeof(p->vendor));
 	chip->parameters.onfi = onfi;
