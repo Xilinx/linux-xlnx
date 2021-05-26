b47db6fb92c7961f9d4f1a41d874b4feff56a148
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2021-05-26 19:12:10 +0530

    irqchip: xilinx: Add support to remove Xilinx INTC irqchip driver module
    The existing irqchip implementation does not fully support use cases
    where an irqchip driver has to be used as a module. In particular there
    is no support to remove an irqchip driver module.
    The use cases where an irqchip driver has to be loaded and then removed
    as a module are really relevant in fpga world. A user can decide to
    have a irqchip as part of a removable partial fpga region. In such cases
    not only the corresponding irqchip driver has to be loaded as a module,
    but must also be removed when the removable partial region is removed.
    
    In the proposed implementation irqchip framework is updated to add a new
    function platform_irqchip_remove through irqchip.h MODULE_DEVICE_TABLE
    implementation.
    
    The platform_irqchip_remove calls the corresponding interrupt controller
    driver's remove routine. It is the responsibility of the interrupt
    controller remove routine to ensure that all irq descriptors attached to
    its irqdomain are freed up.
    Changes in this patch is made through a new irqchip kconfig option with the
    name IRQCHIP_XILINX_INTC_MODULE_SUPPORT_EXPERIMENTAL which is disabled
    by default.
    
    Signed-off-by: Anirudha Sarangi <anirudha.sarangi@xilinx.com>
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -1102,6 +1102,21 @@ struct irq_domain_chip_generic {
 	struct irq_chip_generic	*gc[];
 };
 
+#ifdef CONFIG_IRQCHIP_XILINX_INTC_MODULE_SUPPORT_EXPERIMENTAL
+/**
+ * struct irqc_init_remove_funps - Stores function pointers for irqc init
+ * and remove APIs. Used when the irqchip driver is to be used as a module.
+ * @irqchip_initp:	Function pointer for init/entry point of a irqchip driver.
+ * @irqchip_removep:Function pointer for irqchip driver remove function.
+ */
+struct irqc_init_remove_funps {
+	int (*irqchip_initp)(struct device_node *irqc,
+			     struct device_node *parent);
+	int (*irqchip_removep)(struct device_node *irqc,
+			       struct device_node *parent);
+};
+#endif
+
 /* Generic chip callback functions */
 void irq_gc_noop(struct irq_data *d);
 void irq_gc_mask_disable_reg(struct irq_data *d);
