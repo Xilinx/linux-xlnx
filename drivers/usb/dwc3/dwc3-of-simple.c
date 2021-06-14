e741389a88ca02235bf75bec889b38f83dd1e4b9
Piyush Mehta <piyush.mehta@xilinx.com>
2021-06-14 17:22:51 +0530

    usb: dwc3: Corrected wake up capability conditional check for board specific
    This patch corrects the wakeup capability support condition check
    for both boards ZynqMP and Versal USB controller.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Acked-by: Tony McDowell <tmcdowe@xilinx.com>

--- drivers/usb/dwc3/dwc3-of-simple.c
+++ drivers/usb/dwc3/dwc3-of-simple.c
@@ -188,11 +188,11 @@ void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup)
 
 	/* check for valid parent node */
 	while (node) {
-		if (!of_device_is_compatible(node, "xlnx,zynqmp-dwc3") ||
-		    !of_device_is_compatible(node, "xlnx,versal-dwc3"))
-			node = of_get_next_parent(node);
-		else
+		if (of_device_is_compatible(node, "xlnx,zynqmp-dwc3") ||
+		    of_device_is_compatible(node, "xlnx,versal-dwc3"))
 			break;
+
+		node = of_get_next_parent(node);
 	}
 
 	if (node)  {
