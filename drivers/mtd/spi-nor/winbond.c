47c02e680ae6d074c9d319fa81b5c9a764cc0daa
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2021-07-23 15:07:49 -0600

    mtd: spi-nor: Add support for w25q128jv winbond spi-nor flash
    Add support for w25q128jv winbond spi-nor flash.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -97,6 +97,8 @@ static struct flash_info winbond_parts[] = {
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024,
 			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
+	{ "w25h02jv", INFO(0xef9022, 0, 64 * 1024, 4096,
+			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
 };
 
 /**
