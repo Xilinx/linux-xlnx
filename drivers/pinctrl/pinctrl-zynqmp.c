df736fd05257ec04d813f9221d2260096a8ed735
Yang Yingliang <yangyingliang@huawei.com>
2021-07-29 15:19:05 +0800

    pinctrl: zynqmp: Drop pinctrl_unregister for devm_ registered device
    It's not necessary to unregister pin controller device registered
    with devm_pinctrl_register() and using pinctrl_unregister() leads
    to a double free.
    
    Fixes: fa99e7013827 ("pinctrl: zynqmp: some code cleanups")
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Link: https://lore.kernel.org/r/20210729071905.3235953-1-yangyingliang@huawei.com
    State: upstream ()

--- drivers/pinctrl/pinctrl-zynqmp.c
+++ drivers/pinctrl/pinctrl-zynqmp.c
@@ -927,15 +927,6 @@ static int zynqmp_pinctrl_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int zynqmp_pinctrl_remove(struct platform_device *pdev)
-{
-	struct zynqmp_pinctrl *pctrl = platform_get_drvdata(pdev);
-
-	pinctrl_unregister(pctrl->pctrl);
-
-	return 0;
-}
-
 static const struct of_device_id zynqmp_pinctrl_of_match[] = {
 	{ .compatible = "xlnx,zynqmp-pinctrl" },
 	{ }
@@ -948,7 +939,6 @@ static struct platform_driver zynqmp_pinctrl_driver = {
 		.of_match_table = zynqmp_pinctrl_of_match,
 	},
 	.probe = zynqmp_pinctrl_probe,
-	.remove = zynqmp_pinctrl_remove,
 };
 module_platform_driver(zynqmp_pinctrl_driver);
 
