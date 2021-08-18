c44b773abf240fb14c1137c011010eecbbf9eba3
Vishal Sagar <vishal.sagar@xilinx.com>
2021-08-19 01:22:17 +0530

    v4l: xilinx: vip: Add support for 3 planar YUV444 8bpc
    Add support for 3 planar YUV444 8 bits per component format by adding a
    new entry in the Xilinx supported formats table.
    
    Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Tested-by: Prasad Gutti <prasad.gutti@xilinx.com>
    State: pending

--- drivers/media/platform/xilinx/xilinx-vip.c
+++ drivers/media/platform/xilinx/xilinx-vip.c
@@ -62,6 +62,8 @@ static const struct xvip_video_format xvip_video_formats[] = {
 	  2, 16, V4L2_PIX_FMT_X216M, 2, 2, 1, 1 },
 	{ XVIP_VF_YUV_444, 8, NULL, MEDIA_BUS_FMT_VUY8_1X24,
 	  3, 24, V4L2_PIX_FMT_VUY24, 1, 1, 1, 1 },
+	{ XVIP_VF_YUV_444, 8, NULL, MEDIA_BUS_FMT_VUY8_1X24,
+	  1, 8, V4L2_PIX_FMT_YUV444M, 3, 3, 1, 1 },
 	{ XVIP_VF_YUVX, 8, NULL, MEDIA_BUS_FMT_VUY8_1X24,
 	  4, 32, V4L2_PIX_FMT_XVUY32, 1, 1, 1, 1 },
 	{ XVIP_VF_YUVX, 10, NULL, MEDIA_BUS_FMT_VUY10_1X30,
