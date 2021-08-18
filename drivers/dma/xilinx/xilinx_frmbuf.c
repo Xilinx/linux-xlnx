abdba66fdc7c95521c872c3c34adbbc9fa7972be
Vishal Sagar <vishal.sagar@xilinx.com>
2021-08-19 01:22:22 +0530

    dmaengine: xilinx: frmbuf: Update to SPDX license header
    Update the license to SPDX license header.
    
    Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
    State: pending

--- drivers/dma/xilinx/xilinx_frmbuf.c
+++ drivers/dma/xilinx/xilinx_frmbuf.c
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * DMAEngine driver for Xilinx Framebuffer IP
  *
- * Copyright (C) 2016,2017 Xilinx, Inc. All rights reserved.
+ * Copyright (C) 2016 - 2021 Xilinx, Inc.
  *
  * Authors: Radhey Shyam Pandey <radheys@xilinx.com>
  *          John Nichols <jnichol@xilinx.com>
@@ -13,11 +14,6 @@
  * The AXI Framebuffer core is a soft Xilinx IP core that
  * provides high-bandwidth direct memory access between memory
  * and AXI4-Stream.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
  */
 
 #include <linux/bitops.h>
