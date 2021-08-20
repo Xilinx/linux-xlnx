0453778142489ec8d675bc417f900ce3e44b5fac
Rajan Vaja <rajan.vaja@xilinx.com>
2021-08-20 00:19:24 -0700

    clk: zynqmp: divider: Align max_div description with mainline
    There is different description upstream added by commit 7db20bc17fcf ("clk:
    zynqmp: divider: Add missing description for 'max_div'") that's why sync
    it.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending

--- drivers/clk/zynqmp/divider.c
+++ drivers/clk/zynqmp/divider.c
@@ -35,7 +35,7 @@
  * @is_frac:	The divider is a fractional divider
  * @clk_id:	Id of clock
  * @div_type:	divisor type (TYPE_DIV1 or TYPE_DIV2)
- * @max_div:	Maximum divisor value allowed
+ * @max_div:	maximum supported divisor (fetched from firmware)
  */
 struct zynqmp_clk_divider {
 	struct clk_hw hw;
