ba1d4e4209472e7ae9c9a386cdce20f81bd066bc
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2021-11-15 12:11:15 +0530

    edac: zynqmp: Use SPDX license header
    Use SPDX license header which is also reported as checkpatch warning:
    WARNING: Missing or malformed SPDX-License-Identifier tag in line 1
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>

--- drivers/edac/zynqmp_ocm_edac.c
+++ drivers/edac/zynqmp_ocm_edac.c
@@ -1,22 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Xilinx ZynqMP OCM ECC Driver
  * This driver is based on mpc85xx_edac.c drivers
  *
- * Copyright (C) 2016 Xilinx, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details
+ * Copyright (C) 2016 - 2021 Xilinx, Inc.
  */
 
 #include <linux/edac.h>
