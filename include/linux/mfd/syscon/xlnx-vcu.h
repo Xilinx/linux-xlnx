27b1cf698a208311f8e1e01c131267ac504ddd17
Rohit Visavalia <rohit.visavalia@xilinx.com>
2021-12-12 20:41:56 -0800

    soc: xilinx: vcu: Export functions to obtain logicore settings
    In mainline linux, xlnx vcu logicore settings can only be obtained using
    the syscon framework. This is not backwards compatible with the allegro
    modules: https://gitenterprise.xilinx.com/xilinx-vcu/vcu-modules. For
    example, if the dts defines a logicore register bank instead of using
    the syscon framework, the allegro modules will not be able to obtain the
    logicore settings. These settings are required for initialization
    parameters.
    
    So this patch exports functions to obtain the xlnx vcu logicore settings
    regardless if the syscon framework or logicore register bank is used.
    
    Signed-off-by: Dylan Yip <dylan.yip@xilinx.com>
    Signed-off-by: Rohit Visavalia <rohit.visavalia@xilinx.com>
    State: pending

--- include/linux/mfd/syscon/xlnx-vcu.h
+++ include/linux/mfd/syscon/xlnx-vcu.h
@@ -36,4 +36,30 @@
 #define VCU_GASKET_INIT			0x74
 #define VCU_GASKET_VALUE		0x03
 
+/**
+ * struct xvcu_device - Xilinx VCU init device structure
+ * @dev: Platform device
+ * @pll_ref: pll ref clock source
+ * @aclk: axi clock source
+ * @logicore_reg_ba: logicore reg base address
+ * @vcu_slcr_ba: vcu_slcr Register base address
+ * @pll: handle for the VCU PLL
+ * @pll_post: handle for the VCU PLL post divider
+ * @clk_data: clocks provided by the vcu clock provider
+ */
+struct xvcu_device {
+	struct device *dev;
+	struct clk *pll_ref;
+	struct clk *aclk;
+	struct regmap *logicore_reg_ba;
+	void __iomem *vcu_slcr_ba;
+	struct clk_hw *pll;
+	struct clk_hw *pll_post;
+	struct clk_hw_onecell_data *clk_data;
+};
+
+u32 xvcu_get_color_depth(struct xvcu_device *xvcu);
+u32 xvcu_get_memory_depth(struct xvcu_device *xvcu);
+u32 xvcu_get_clock_frequency(struct xvcu_device *xvcu);
+u32 xvcu_get_num_cores(struct xvcu_device *xvcu);
 #endif /* __XLNX_VCU_H */
