b693c910aaaba6beb3a805f102ccae684d443a32
Rajan Vaja <rajan.vaja@xilinx.com>
2022-01-07 04:55:17 -0800

    firmware: xilinx: Add support for IOCTL and QUERY ID feature check
    Add support to check if IOCTL ID or QUERY ID is supported in
    firmware or not.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    Reviewed-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -34,6 +34,9 @@
 /* SMC SIP service Call Function Identifier Prefix */
 #define PM_SIP_SVC			0xC2000000
 
+/* PM API versions */
+#define PM_API_VERSION_2	2
+
 /* ATF only commands */
 #define PM_GET_TRUSTZONE_VERSION	0xa03
 #define PM_SET_SUSPEND_MODE		0xa02
@@ -736,6 +739,7 @@ int zynqmp_pm_load_pdi(const u32 src, const u64 address);
 int zynqmp_pm_register_notifier(const u32 node, const u32 event,
 				const u32 wake, const u32 enable);
 int zynqmp_pm_feature(const u32 api_id);
+int zynqmp_pm_is_function_supported(const u32 api_id, const u32 id);
 int zynqmp_pm_set_feature_config(enum pm_feature_config_id id, u32 value);
 int zynqmp_pm_get_feature_config(enum pm_feature_config_id id, u32 *payload);
 int zynqmp_pm_get_uid_info(const u64 address, const u32 size, u32 *count);
@@ -1010,6 +1014,11 @@ static inline int zynqmp_pm_pinctrl_release(const u32 pin)
 }
 
 static inline int zynqmp_pm_pinctrl_get_function(const u32 pin, u32 *id)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_is_function_supported(const u32 api_id, const u32 id)
 {
 	return -ENODEV;
 }
