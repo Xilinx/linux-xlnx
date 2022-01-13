0ae68afdbd4e858c78a7c464471db68ff287231e
Nava kishore Manne <nava.manne@xilinx.com>
2022-01-13 20:31:13 +0530

    fpga: mgr: Update the state to provide the exact error code
    Up on fpga configuration failure, the existing sysfs state interface
    is just providing the generic error message rather than providing the
    exact error code. This patch extends sysfs state interface to provide
    the exact error received from the lower layer along with the existing
    generic error message.
    
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    State: pending

--- include/linux/fpga/fpga-mgr.h
+++ include/linux/fpga/fpga-mgr.h
@@ -229,6 +229,7 @@ struct fpga_manager_ops {
  * @compat_id: FPGA manager id for compatibility check.
  * @mops: pointer to struct of fpga manager ops
  * @priv: low level driver private date
+ * @err: low level driver error code
  * @dir: debugfs image directory
  */
 struct fpga_manager {
@@ -243,6 +244,7 @@ struct fpga_manager {
 	struct fpga_compat_id *compat_id;
 	const struct fpga_manager_ops *mops;
 	void *priv;
+	int err;
 #ifdef CONFIG_FPGA_MGR_DEBUG_FS
 	struct dentry *dir;
 #endif
