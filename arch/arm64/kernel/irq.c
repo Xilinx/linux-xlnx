d69542ecf2615bb16c90ee70814de713e6bd38cc
Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
2022-01-24 12:46:41 +0530

    arm64: kernel: Avoid __init macro usage for xilinx_intc_of_init
    xilinx_intc_of_init API is not allocated into the .init.text section
    when CONFIG_IRQCHIP_XILINX_INTC_MODULE_SUPPORT_EXPERIMENTAL is
    enabled. Since xilinx_intc_of_init API calls the API set_handle_irq
    which uses __init macro to be put into .init.text section,
    this patch makes changes to ensure that set_handle_irq also does
    not stay in .init.text section.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- arch/arm64/kernel/irq.c
+++ arch/arm64/kernel/irq.c
@@ -84,7 +84,11 @@ static void default_handle_fiq(struct pt_regs *regs)
 void (*handle_arch_irq)(struct pt_regs *) __ro_after_init = default_handle_irq;
 void (*handle_arch_fiq)(struct pt_regs *) __ro_after_init = default_handle_fiq;
 
+#ifndef CONFIG_IRQCHIP_XILINX_INTC_MODULE_SUPPORT_EXPERIMENTAL
 int __init set_handle_irq(void (*handle_irq)(struct pt_regs *))
+#else
+int set_handle_irq(void (*handle_irq)(struct pt_regs *))
+#endif
 {
 	if (handle_arch_irq != default_handle_irq)
 		return -EBUSY;
