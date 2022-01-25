d39161881dc9521522ef27e73a11c95e35c333ab
Piyush Mehta <piyush.mehta@xilinx.com>
2022-01-26 01:17:33 +0530

    usb: dwc3: core: add PM_SLEEP kernel config check
    Hibernation feature is enabled when kernel config gadget, otg, and dual
    role config is enabled, for USB host-only mode this feature is disabled.
    
    When host-only mode enable then kernel compilation error occurs for power
    management calls.
    
    Fix below kernel complilation error:
    aarch64-xilinx-linux-ld.bfd: drivers/usb/dwc3/core.o: in function
    `dwc3_remove':
    undefined reference to `dwc3_gadget_exit_hibernation'
    
    To fix it, add CONFIG_PM_SLEEP kernel config check for hibernation function
    call. It also fixes below kernel crash where CONFIG_PM is disabled and USB
    is unbind.
    
    Use-case where PM is disabled then during the unbinding of USB, kernel
    crash can occur due to hibernation call in dwc3_remove API.
    
    Fix below kernel crash:
    usb usb1: USB disconnect, device number 1
    xhci-hcd xhci-hcd.1.auto: USB bus 1 deregistered
    Call trace:
     __clk_put+0x2c/0x14c
     clk_put+0x10/0x20
     clk_bulk_put+0x3c/0x60
     devm_clk_bulk_release+0x20/0x2c
     release_nodes+0x1b0/0x224
     devres_release_all+0x38/0xb0
     __device_release_driver+0x178/0x200
     device_driver_detach+0x44/0xc4
     unbind_store+0xec/0x110
     drv_attr_store+0x28/0x40
     sysfs_kf_write+0x48/0x60
     kernfs_fop_write+0xfc/0x21c
     vfs_write+0xf0/0x2b0
     ksys_write+0x6c/0x100
     __arm64_sys_write+0x20/0x30
     el0_svc_common.constprop.0+0x94/0x1c0
     do_el0_svc+0x44/0xb0
     el0_svc+0x14/0x20
     el0_sync_handler+0x1a4/0x1b0
     el0_sync+0x174/0x180
    Code: b140041f 540007a8 f90013f5 97ffe8b1 (b9403260)
    ---[ end trace c8c1f097602dab10 ]---
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    State: pending

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1817,6 +1817,7 @@ static int dwc3_remove(struct platform_device *pdev)
 
 	pm_runtime_get_sync(&pdev->dev);
 
+#ifdef CONFIG_PM_SLEEP
 	if (dwc->is_hibernated) {
 		/*
 		 * As we are about to get removed, wake the controller from
@@ -1826,6 +1827,7 @@ static int dwc3_remove(struct platform_device *pdev)
 		dwc3_gadget_exit_hibernation(dwc);
 		dwc->force_hiber_wake = false;
 	}
+#endif /* CONFIG_PM_SLEEP */
 
 	dwc3_core_exit_mode(dwc);
 	dwc3_debugfs_exit(dwc);
