ffc880ca19f9b40c11c0d1398dcad8541b6d9e4f
Michal Simek <michal.simek@xilinx.com>
2022-01-25 11:40:44 +0530

    spi: spi-cadence: Fix kernel-doc format for resume/suspend
    Kernel function name don't match with function name.
    
    Error log:
    drivers/spi/spi-cadence.c:662: warning: expecting prototype for
    cdns_spi_runtime_resume(). Prototype was for cnds_runtime_resume() instead
    drivers/spi/spi-cadence.c:691: warning: expecting prototype for
    cdns_spi_runtime_suspend(). Prototype was for cnds_runtime_suspend()
    instead
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    State: pending

--- drivers/spi/spi-cadence.c
+++ drivers/spi/spi-cadence.c
@@ -657,7 +657,7 @@ static int __maybe_unused cdns_spi_resume(struct device *dev)
 }
 
 /**
- * cdns_spi_runtime_resume - Runtime resume method for the SPI driver
+ * cdns_runtime_resume - Runtime resume method for the SPI driver
  * @dev:	Address of the platform_device structure
  *
  * This function enables the clocks
@@ -686,7 +686,7 @@ static int __maybe_unused cnds_runtime_resume(struct device *dev)
 }
 
 /**
- * cdns_spi_runtime_suspend - Runtime suspend method for the SPI driver
+ * cdns_runtime_suspend - Runtime suspend method for the SPI driver
  * @dev:	Address of the platform_device structure
  *
  * This function disables the clocks
