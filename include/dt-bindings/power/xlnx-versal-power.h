6bc6c5d95f21466238431ddd1fe18607e4bf42b6
Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>
2022-02-07 10:18:28 +0000

    iio: adc: versal-sysmon: Add support for secure access
    This patch adds support when direct axi access to sysmon is blocked.
    EEMI system calls are used to access the restricted sysmon address
    space.
    
    Signed-off-by: Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>
    State: pending

--- include/dt-bindings/power/xlnx-versal-power.h
+++ include/dt-bindings/power/xlnx-versal-power.h
@@ -48,6 +48,7 @@
 #define PM_DEV_ADMA_5				(0x1822403aU)
 #define PM_DEV_ADMA_6				(0x1822403bU)
 #define PM_DEV_ADMA_7				(0x1822403cU)
+#define PM_DEV_AMS_ROOT				(0x18224055U)
 #define PM_DEV_AI				(0x18224072U)
 
 #endif
