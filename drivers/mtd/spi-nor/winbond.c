Xilinx/linux-xlnx

commit 47c02e680ae6d074c9d319fa81b5c9a764cc0daa

mtd: spi-nor: Add support for w25q128jv winbond spi-nor flash
Add support for w25q128jv winbond spi-nor flash.

Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

drivers/mtd/spi-nor/winbond.c
@@ -97,6 +97,8 @@ static struct flash_info winbond_parts[] = {
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024,
 			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
+	{ "w25h02jv", INFO(0xef9022, 0, 64 * 1024, 4096,
+			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
 };
 
 /**

commit 79945b5d3c9361e0a8ae87aca91c010ed02b447d

mtd: spi-nor: Enable 4byte opcode for Winbond w25h02jvt flash
Winbond w25h02jvt flash supporrts 4byte opcode, So enable 4byte opcode
for Winbond w25h02jvt flash.

Fixes: 47c02e680ae6 ("mtd: spi-nor: Add support for w25q128jv winbond spi-nor flash")
Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

drivers/mtd/spi-nor/winbond.c
@@ -104,7 +104,7 @@ static struct flash_info winbond_parts[] = {
 			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ |
 			    SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
 			    SPI_NOR_TB_SR_BIT6 | SPI_NOR_4BIT_BP |
-			    SPI_NOR_BP3_SR_BIT5) },
+			    SPI_NOR_BP3_SR_BIT5 | SPI_NOR_4B_OPCODES) },
 	{ "w25q512jvq", INFO(0xef4020, 0, 64 * 1024, 1024,
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 };