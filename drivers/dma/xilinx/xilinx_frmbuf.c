fd4584bab11904fff3ceab81b493d844ccddf7fc
Rohit Visavalia <rohit.visavalia@xilinx.com>
2022-03-04 09:25:17 -0800

    dmaengine: xilinx: frmbuf: Add support for 3 planar YUV444P 8bpc
    This patch adds support for single contiguous buffer 3 planar YUV444P
    8 bpc format.
    
    Signed-off-by: Rohit Visavalia <rohit.visavalia@xilinx.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    State: pending

--- drivers/dma/xilinx/xilinx_frmbuf.c
+++ drivers/dma/xilinx/xilinx_frmbuf.c
@@ -491,6 +491,16 @@ static const struct xilinx_frmbuf_format_desc xilinx_frmbuf_formats[] = {
 		.fmt_bitmask = BIT(24),
 	},
 	{
+		.dts_name = "y_u_v8",
+		.id = XILINX_FRMBUF_FMT_Y_U_V8,
+		.bpw = 32,
+		.ppw = 4,
+		.num_planes = 3,
+		.v4l2_fmt = V4L2_PIX_FMT_YUV444P,
+		.drm_fmt = DRM_FORMAT_YUV444,
+		.fmt_bitmask = BIT(24),
+	},
+	{
 		.dts_name = "y_u_v10",
 		.id = XILINX_FRMBUF_FMT_Y_U_V10,
 		.bpw = 32,
