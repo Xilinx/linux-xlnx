2e414a2f27944cd51b5a6b5b66bcc23b92c9984c
Swati Agarwal <swati.agarwal@xilinx.com>
2022-03-11 12:38:36 +0530

    net: axienet: Add coding style related fixes
    Fix below warnings reported by checkpatch tool-
    
    1) WARNING: Possible unnecessary 'out of memory' message
    Fix this by removing unnecessary message.
    2) CHECK: Unnecessary parentheses
    Fix this by removing extra parentheses.
    3) CHECK: Alignment should match open parenthesis
    Fix this by doing proper alignment.
    4) WARNING: Prefer using '"%s...", __func__' instead of function's name,
    in a string
    Fix this by using __func__ for displacing function name.
    5) WARNING: Prefer strscpy over strlcpy
    Fix this by replacing strlcpy with strscpy.
    6) WARNING: Possible repeated word: 'The'
    Fix this by removing repeated word.
    7) CHECK: No space is necessary after a cast
    Fix this by removing extra space.
    
    Signed-off-by: Swati Agarwal <swati.agarwal@xilinx.com>
    State: pending

--- drivers/net/ethernet/xilinx/xilinx_axienet_mdio.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_mdio.c
@@ -89,8 +89,8 @@ static int axienet_mdio_read(struct mii_bus *bus, int phy_id, int reg)
 
 	rc = axienet_ior(lp, XAE_MDIO_MRD_OFFSET) & 0x0000FFFF;
 
-	dev_dbg(lp->dev, "axienet_mdio_read(phy_id=%i, reg=%x) == %x\n",
-		phy_id, reg, rc);
+	dev_dbg(lp->dev, "%s (phy_id=%i, reg=%x) == %x\n",
+		__func__, phy_id, reg, rc);
 
 	axienet_mdio_mdc_disable(lp);
 	return rc;
@@ -106,7 +106,7 @@ static int axienet_mdio_read(struct mii_bus *bus, int phy_id, int reg)
  * Return:	0 on success, -ETIMEDOUT on a timeout
  *
  * Writes the value to the requested register by first writing the value
- * into MWD register. The the MCR register is then appropriately setup
+ * into MWD register. The MCR register is then appropriately setup
  * to finish the write operation.
  */
 static int axienet_mdio_write(struct mii_bus *bus, int phy_id, int reg,
@@ -115,8 +115,8 @@ static int axienet_mdio_write(struct mii_bus *bus, int phy_id, int reg,
 	int ret;
 	struct axienet_local *lp = bus->priv;
 
-	dev_dbg(lp->dev, "axienet_mdio_write(phy_id=%i, reg=%x, val=%x)\n",
-		phy_id, reg, val);
+	dev_dbg(lp->dev, "%s (phy_id=%i, reg=%x, val=%x)\n",
+		__func__, phy_id, reg, val);
 
 	axienet_mdio_mdc_enable(lp);
 
@@ -126,7 +126,7 @@ static int axienet_mdio_write(struct mii_bus *bus, int phy_id, int reg,
 		return ret;
 	}
 
-	axienet_iow(lp, XAE_MDIO_MWD_OFFSET, (u32) val);
+	axienet_iow(lp, XAE_MDIO_MWD_OFFSET, (u32)val);
 	axienet_iow(lp, XAE_MDIO_MCR_OFFSET,
 		    (((phy_id << XAE_MDIO_MCR_PHYAD_SHIFT) &
 		      XAE_MDIO_MCR_PHYAD_MASK) |
