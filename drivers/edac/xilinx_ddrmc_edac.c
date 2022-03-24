ff817e4fb03fb240cdf494636176ac43e550609d
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2022-03-24 14:59:40 +0530

    edac: xilinx: make event manager registration configurable
    Currently the event manager does not support multiple registrations.
    The default is event manager is enabled. To move to interrupts in the
    driver xilinx-ddrmc-edac.disable_event=1 is to be passed.
    
    We can pass module parameters as arguments in the builtin driver.
    bootargs = "console=ttyAMA0  earlycon=pl011,mmio32,0xFF000000,115200n8
    	    clk_ignore_unused init_fatal_sh=1 xilinx-ddrmc-edac.disable_event=1";
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>

--- drivers/edac/xilinx_ddrmc_edac.c
+++ drivers/edac/xilinx_ddrmc_edac.c
@@ -16,6 +16,10 @@
 
 #include "edac_module.h"
 
+static bool disable_event; /* No support for notification. Default: 0 (Off) */
+module_param(disable_event, bool, 0444);
+MODULE_PARM_DESC(disable_event, "No error notification support. Default: 0 (Off)");
+
 /* Granularity of reported error in bytes */
 #define XDDR_EDAC_ERR_GRAIN			1
 
@@ -1189,20 +1193,26 @@ static int xddr_mc_probe(struct platform_device *pdev)
 
 	xddr_setup_address_map(priv);
 #endif
-
-	rc = xlnx_register_event(PM_NOTIFY_CB, XPM_NODETYPE_EVENT_ERROR_PMC_ERR1,
-				 XPM_EVENT_ERROR_MASK_DDRMC_CR | XPM_EVENT_ERROR_MASK_DDRMC_NCR,
-				 false, xddr_err_callback, mci);
-	if (rc == -ENODEV) {
+	if (disable_event) {
 		rc = xddr_setup_irq(mci, pdev);
 		if (rc)
 			goto del_edac_mc;
-	}
-	if (rc) {
-		if (rc == -EACCES)
-			rc = -EPROBE_DEFER;
+	} else {
+		rc = xlnx_register_event(PM_NOTIFY_CB, XPM_NODETYPE_EVENT_ERROR_PMC_ERR1,
+					 XPM_EVENT_ERROR_MASK_DDRMC_CR |
+					 XPM_EVENT_ERROR_MASK_DDRMC_NCR,
+					 false, xddr_err_callback, mci);
+		if (rc == -ENODEV) {
+			rc = xddr_setup_irq(mci, pdev);
+			if (rc)
+				goto del_edac_mc;
+		}
+		if (rc) {
+			if (rc == -EACCES)
+				rc = -EPROBE_DEFER;
 
-		goto del_edac_mc;
+			goto del_edac_mc;
+		}
 	}
 
 	xddr_enable_intr(priv);
@@ -1234,9 +1244,10 @@ static int xddr_mc_remove(struct platform_device *pdev)
 	edac_remove_sysfs_attributes(mci);
 #endif
 
-	xlnx_unregister_event(PM_NOTIFY_CB, XPM_NODETYPE_EVENT_ERROR_PMC_ERR1,
-			      XPM_EVENT_ERROR_MASK_DDRMC_CR | XPM_EVENT_ERROR_MASK_DDRMC_NCR,
-			      xddr_err_callback);
+	if (!disable_event)
+		xlnx_unregister_event(PM_NOTIFY_CB, XPM_NODETYPE_EVENT_ERROR_PMC_ERR1,
+				      XPM_EVENT_ERROR_MASK_DDRMC_CR |
+				      XPM_EVENT_ERROR_MASK_DDRMC_NCR, xddr_err_callback);
 	edac_mc_del_mc(&pdev->dev);
 	edac_mc_free(mci);
 
