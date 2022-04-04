8334a26b2731a0e0fe14d245ae2d4fbd5a65cfcb
Pranavi Somisetty <pranavi.somisetty@xilinx.com>
2022-04-04 12:25:49 -0600

    net: xilinx: Change irq name with details
    Add more info in irq name so that it includes device name.
    
    Signed-off-by: Pranavi Somisetty <pranavi.somisetty@xilinx.com>
    Signed-off-by: Saurabh Sengar <saurabh.singh@xilinx.com>
    Signed-off-by: Syed Syed <syed.syed@xilinx.com>
    Signed-off-by: Priyadarshini Babu <priyadarshini.babu@xilinx.com>
    Signed-off-by: Ravali Potineni <ravali.potineni@xilinx.com>
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- drivers/net/ethernet/xilinx/xilinx_tsn_shaper.c
+++ drivers/net/ethernet/xilinx/xilinx_tsn_shaper.c
@@ -214,10 +214,13 @@ int axienet_qbv_init(struct net_device *ndev)
 {
 	struct axienet_local *lp = netdev_priv(ndev);
 	int rc = 0;
+	static char irq_name[24];
 
-	if (lp->qbv_irq > 0)
-		rc = request_irq(lp->qbv_irq, axienet_qbv_irq, 0, ndev->name,
+	if (lp->qbv_irq > 0) {
+		sprintf(irq_name, "%s_qbv", ndev->name);
+		rc = request_irq(lp->qbv_irq, axienet_qbv_irq, 0, irq_name,
 				 ndev);
+	}
 	return rc;
 }
 
