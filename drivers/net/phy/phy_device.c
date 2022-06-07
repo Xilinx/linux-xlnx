361e7d019a170b006005e0132b6b305e14db8b5a
Harini Katakam <harini.katakam@xilinx.com>
2022-06-07 15:45:13 +0530

    Revert "Revert "net: phy: ensure phylib state machine is stopped after calling phy_stop""
    This reverts commit 0323818008a3c6b778586b86021cbbbfcfcc3e97.
    
    Revert xilinx board specific (TI PHY) changes in phylib directory.
    This is no longer used.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    State: not-upstreamable

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -1114,8 +1114,6 @@ void phy_disconnect(struct phy_device *phydev)
 	if (phy_interrupt_is_valid(phydev))
 		phy_free_interrupt(phydev);
 
-	phy_stop_machine(phydev);
-
 	phydev->adjust_link = NULL;
 
 	phy_detach(phydev);
