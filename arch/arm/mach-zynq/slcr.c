d379c8482478866d5722e626a222cc48ee637730
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2022-06-24 11:25:54 +0530

    ARM: zynq: Deprecate zynq ocmc driver
    Zynq OCM driver was never upstreamed and in general, it is a little bit
    smarter than the mmio-sram driver. There is code which reads OCM
    configuration (low/high) and deals with it. The truth is that almost all
    boards are using OCM at the high address where the mmio-sram driver should
    be enough. So remove the zynq-com driver and use mmio-sram if someone wants
    to map OCM.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    State: not-upstreamable

--- arch/arm/mach-zynq/slcr.c
+++ arch/arm/mach-zynq/slcr.c
@@ -22,7 +22,6 @@
 #define SLCR_PSS_IDCODE			0x530 /* PS IDCODE */
 #define SLCR_L2C_RAM			0xA1C /* L2C_RAM in AR#54190 */
 #define SLCR_LVL_SHFTR_EN_OFFSET	0x900 /* Level Shifters Enable */
-#define SLCR_OCM_CFG_OFFSET		0x910 /* OCM Address Mapping */
 
 #define SLCR_UNLOCK_MAGIC		0xDF0D
 #define SLCR_A9_CPU_CLKSTOP		0x10
@@ -117,19 +116,6 @@ static struct notifier_block zynq_slcr_restart_nb = {
 	.notifier_call	= zynq_slcr_system_restart,
 	.priority	= 192,
 };
-
-/**
- * zynq_slcr_get_ocm_config - Get SLCR OCM config
- *
- * return:	OCM config bits
- */
-u32 zynq_slcr_get_ocm_config(void)
-{
-	u32 ret;
-
-	zynq_slcr_read(&ret, SLCR_OCM_CFG_OFFSET);
-	return ret;
-}
 
 /**
  * zynq_slcr_init_preload_fpga - Disable communication from the PL to PS.
