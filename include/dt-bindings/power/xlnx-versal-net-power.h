dcf51baf1f61af21e68b314c3ac277804469ff73
Michal Simek <michal.simek@amd.com>
2022-09-05 14:14:41 +0200

    dt-bindings: xilinx: Add support for Versal NET power/reset
    Add dt-bindings headers for Versal NET power and reset.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    State: pending
    [michals: Squashed with
    dt-bindings: Add missing clock and power nodes
    dt-bindings: versal-net: Update USB_1 reset node ID
    ]

---
+++ include/dt-bindings/power/xlnx-versal-net-power.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ *  Copyright (C) 2022 Xilinx, Inc.
+ */
+
+#ifndef _DT_BINDINGS_VERSAL_NET_POWER_H
+#define _DT_BINDINGS_VERSAL_NET_POWER_H
+
+#include <dt-bindings/power/xlnx-versal-power.h>
+
+#define PM_DEV_USB_1				(0x182240D7U)
+#define PM_DEV_FPD_SWDT_0			(0x182240DBU)
+#define PM_DEV_FPD_SWDT_1			(0x182240DCU)
+#define PM_DEV_FPD_SWDT_2			(0x182240DDU)
+#define PM_DEV_FPD_SWDT_3			(0x182240DEU)
+
+/* Remove Versal specific node IDs */
+#undef PM_DEV_RPU0_0
+#undef PM_DEV_RPU0_1
+#undef PM_DEV_OCM_0
+#undef PM_DEV_OCM_1
+#undef PM_DEV_OCM_2
+#undef PM_DEV_OCM_3
+#undef PM_DEV_TCM_0_A
+#undef PM_DEV_TCM_1_A
+#undef PM_DEV_TCM_0_B
+#undef PM_DEV_TCM_1_B
+#undef PM_DEV_SWDT_FPD
+#undef PM_DEV_AI
+
+#endif
