fd64413c78dd2546eaf7bdeeeed48a2669363865
Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
2022-10-17 15:44:48 +0200

    edac: xilinx: revert making the event manager registration configurable
    Earlier the event manager registration did not support multiple
    registration. Now the support is added so revert the
    commit ff817e4fb03f ("edac: xilinx: make event manager registration
    configurable").
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>

--- drivers/edac/xilinx_ddrmc_edac.c
+++ drivers/edac/xilinx_ddrmc_edac.c
@@ -16,10 +16,6 @@
 
 #include "edac_module.h"
 
-static bool disable_event; /* No support for notification. Default: 0 (Off) */
-module_param(disable_event, bool, 0444);
-MODULE_PARM_DESC(disable_event, "No error notification support. Default: 0 (Off)");
-
 /* Granularity of reported error in bytes */
 #define XDDR_EDAC_ERR_GRAIN			1
 
@@ -1193,26 +1189,20 @@ static int xddr_mc_probe(struct platform_device *pdev)
 
 	xddr_setup_address_map(priv);
 #endif
-	if (disable_event) {
+
+	rc = xlnx_register_event(PM_NOTIFY_CB, EVENT_ERROR_PMC_ERR1,
+				 XPM_EVENT_ERROR_MASK_DDRMC_CR | XPM_EVENT_ERROR_MASK_DDRMC_NCR,
+				 false, xddr_err_callback, mci);
+	if (rc == -ENODEV) {
 		rc = xddr_setup_irq(mci, pdev);
 		if (rc)
 			goto del_edac_mc;
-	} else {
-		rc = xlnx_register_event(PM_NOTIFY_CB, EVENT_ERROR_PMC_ERR1,
-					 XPM_EVENT_ERROR_MASK_DDRMC_CR |
-					 XPM_EVENT_ERROR_MASK_DDRMC_NCR,
-					 false, xddr_err_callback, mci);
-		if (rc == -ENODEV) {
-			rc = xddr_setup_irq(mci, pdev);
-			if (rc)
-				goto del_edac_mc;
-		}
-		if (rc) {
-			if (rc == -EACCES)
-				rc = -EPROBE_DEFER;
+	}
+	if (rc) {
+		if (rc == -EACCES)
+			rc = -EPROBE_DEFER;
 
-			goto del_edac_mc;
-		}
+		goto del_edac_mc;
 	}
 
 	xddr_enable_intr(priv);
@@ -1244,10 +1234,9 @@ static int xddr_mc_remove(struct platform_device *pdev)
 	edac_remove_sysfs_attributes(mci);
 #endif
 
-	if (!disable_event)
-		xlnx_unregister_event(PM_NOTIFY_CB, EVENT_ERROR_PMC_ERR1,
-				      XPM_EVENT_ERROR_MASK_DDRMC_CR |
-				      XPM_EVENT_ERROR_MASK_DDRMC_NCR, xddr_err_callback, mci);
+	xlnx_unregister_event(PM_NOTIFY_CB, EVENT_ERROR_PMC_ERR1,
+			      XPM_EVENT_ERROR_MASK_DDRMC_CR |
+			      XPM_EVENT_ERROR_MASK_DDRMC_NCR, xddr_err_callback, mci);
 	edac_mc_del_mc(&pdev->dev);
 	edac_mc_free(mci);
 
