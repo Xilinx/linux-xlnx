f9a5394b5bc821860a48e26e599d0f4619d6efd8
Hyun Kwon <hyun.kwon@xilinx.com>
2022-10-20 14:00:20 +0200

    drm: fourcc: Add new formats needed by Xilinx IP
    This adds packed YUV and grey scale format fourccs.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>
    State: waiting (drm)
    Link: https://lists.freedesktop.org/archives/dri-devel/2018-February/165313.html
    [michals: Squashed with
    drm: drm_fourcc: Correct the DRM YUV444 10bpc
    v6.1 - remove DRM_FORMAT_XVUY8888 based on 53618649ca6d ("drm/fourcc: Add formats for packed YUV 4:4:4 AVUY and XVUY permutations")
    ]

--- drivers/gpu/drm/drm_fourcc.c
+++ drivers/gpu/drm/drm_fourcc.c
@@ -313,6 +313,10 @@ const struct drm_format_info *__drm_format_info(u32 format)
 		{ .format = DRM_FORMAT_P030,            .depth = 0,  .num_planes = 2,
 		  .char_per_block = { 4, 8, 0 }, .block_w = { 3, 3, 0 }, .block_h = { 1, 1, 0 },
 		  .hsub = 2, .vsub = 2, .is_yuv = true},
+		{ .format = DRM_FORMAT_XVUY8888,	.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1 },
+		{ .format = DRM_FORMAT_XVUY2101010,	.depth = 0,  .num_planes = 1, .cpp = { 4, 0, 0 }, .hsub = 1, .vsub = 1 },
+		{ .format = DRM_FORMAT_Y8,		.depth = 0,  .num_planes = 1, .cpp = { 1, 0, 0 }, .hsub = 1, .vsub = 1 },
+		{ .format = DRM_FORMAT_Y10,		.depth = 0,  .num_planes = 1, .pixels_per_macropixel =  { 3, 0, 0 }, .bytes_per_macropixel = { 4, 0, 0 }, .hsub = 1, .vsub = 1 },
 	};
 
 	unsigned int i;
