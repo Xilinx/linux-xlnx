b2e31d0aca818608e033aaadbfa3539808d650c1
Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>
2022-12-07 02:48:27 -0800

    drivers: clk: zynqmp: Add versal-net compatible string
    Add compatible string for versal-net.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>

--- drivers/clk/zynqmp/clkc.c
+++ drivers/clk/zynqmp/clkc.c
@@ -785,6 +785,7 @@ static int zynqmp_clock_probe(struct platform_device *pdev)
 static const struct of_device_id zynqmp_clock_of_match[] = {
 	{.compatible = "xlnx,zynqmp-clk"},
 	{.compatible = "xlnx,versal-clk"},
+	{.compatible = "xlnx,versal-net-clk"},
 	{},
 };
 MODULE_DEVICE_TABLE(of, zynqmp_clock_of_match);
