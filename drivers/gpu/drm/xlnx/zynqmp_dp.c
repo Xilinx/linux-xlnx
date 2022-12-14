e9f8388371caf1c8a06492e0f97afd11ada63a2f
Rohit Visavalia <rohit.visavalia@amd.com>
2022-12-14 03:43:33 -0800

    sound: soc: xilinx: program audio control register on audio start/stop
    Set/clear operations for TX_AUDIO_CONTROL register when starting/stopping
    audio playback.
    So the patch adds the snd_soc_dai_ops shutdown and startup callbacks.
    
    Signed-off-by: Rohit Visavalia <rohit.visavalia@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>
    State: pending

--- drivers/gpu/drm/xlnx/zynqmp_dp.c
+++ drivers/gpu/drm/xlnx/zynqmp_dp.c
@@ -1622,8 +1622,6 @@ static void zynqmp_dp_encoder_enable(struct drm_encoder *encoder)
 	dp->enabled = true;
 	zynqmp_dp_init_aux(dp);
 	zynqmp_dp_update_misc(dp);
-	if (zynqmp_disp_aud_enabled(dp->dpsub->disp))
-		zynqmp_dp_write(iomem, ZYNQMP_DP_TX_AUDIO_CONTROL, 1);
 	zynqmp_dp_write(iomem, ZYNQMP_DP_TX_PHY_POWER_DOWN, 0);
 	if (dp->status == connector_status_connected) {
 		for (i = 0; i < 3; i++) {
@@ -1667,8 +1665,6 @@ static void zynqmp_dp_encoder_disable(struct drm_encoder *encoder)
 	}
 	zynqmp_dp_write(iomem, ZYNQMP_DP_TX_PHY_POWER_DOWN,
 			ZYNQMP_DP_TX_PHY_POWER_DOWN_ALL);
-	if (zynqmp_disp_aud_enabled(dp->dpsub->disp))
-		zynqmp_dp_write(iomem, ZYNQMP_DP_TX_AUDIO_CONTROL, 0);
 	pm_runtime_put_sync(dp->dev);
 }
 
