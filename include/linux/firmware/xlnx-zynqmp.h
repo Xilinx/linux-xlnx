70e352bb0517749e72c7bc669d8553095d8ab313
Michal Simek <michal.simek@amd.com>
2023-02-01 08:52:36 +0100

    firmware: zynqmp: Mark some function as inline
    Recently added functions for case where !CONFIG_ZYNQMP_FIRMWARE are missing
    inline which ends up in compilation warnings because function is defined in
    header file but not actually used.
    
    Compilation warnings on xilinx_zynq_defconfig:
    include/linux/firmware/xlnx-zynqmp.h:1334:12: warning: 'versal_pm_sha_hash' defined but not used [-Wunused-function]
     1334 | static int versal_pm_sha_hash(const u64 src, const u64 dst, const u32 size)
          |            ^~~~~~~~~~~~~~~~~~
    include/linux/firmware/xlnx-zynqmp.h:1329:12: warning: 'zynqmp_pm_fpga_get_feature_list' defined but not used [-Wunused-function]
     1329 | static int zynqmp_pm_fpga_get_feature_list(u32 *value)
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    include/linux/firmware/xlnx-zynqmp.h:1324:12: warning: 'zynqmp_pm_fpga_get_version' defined but not used [-Wunused-function]
     1324 | static int zynqmp_pm_fpga_get_version(u32 *value)
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~
    firmware/xlnx-zynqmp.h:1254:12: warning: 'zynqmp_pm_sec_mask_write_reg' defined but not used [-Wunused-function]
     1254 | static int zynqmp_pm_sec_mask_write_reg(const u32 node_id, const u32 offset,
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    include/linux/firmware/xlnx-zynqmp.h:1249:12: warning: 'zynqmp_pm_sec_read_reg' defined but not used [-Wunused-function]
     1249 | static int zynqmp_pm_sec_read_reg(u32 node_id, u32 offset, u32 *ret_value)
     |            ^~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    State: pending

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -1246,13 +1246,15 @@ static inline int zynqmp_pm_get_uid_info(const u64 address, const u32 size,
 	return -ENODEV;
 }
 
-static int zynqmp_pm_sec_read_reg(u32 node_id, u32 offset, u32 *ret_value)
+static inline int zynqmp_pm_sec_read_reg(u32 node_id, u32 offset,
+					 u32 *ret_value)
 {
 	return -ENODEV;
 }
 
-static int zynqmp_pm_sec_mask_write_reg(const u32 node_id, const u32 offset,
-					u32 mask, u32 value)
+static inline int zynqmp_pm_sec_mask_write_reg(const u32 node_id,
+					       const u32 offset,
+					       u32 mask, u32 value)
 {
 	return -ENODEV;
 }
@@ -1321,17 +1323,18 @@ static inline int zynqmp_pm_aie_operation(u32 node, u16 start_col,
 	return -ENODEV;
 }
 
-static int zynqmp_pm_fpga_get_version(u32 *value)
+static inline int zynqmp_pm_fpga_get_version(u32 *value)
 {
 	return -ENODEV;
 }
 
-static int zynqmp_pm_fpga_get_feature_list(u32 *value)
+static inline int zynqmp_pm_fpga_get_feature_list(u32 *value)
 {
 	return -ENODEV;
 }
 
-static int versal_pm_sha_hash(const u64 src, const u64 dst, const u32 size)
+static inline int versal_pm_sha_hash(const u64 src, const u64 dst,
+				     const u32 size)
 {
 	return -ENODEV;
 }
