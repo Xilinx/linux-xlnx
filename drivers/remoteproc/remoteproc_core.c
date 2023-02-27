71a7d250619a8c522fe8d69afe989d084bb1d051
Tarak Reddy <tarak.reddy@amd.com>
2023-02-27 20:47:17 +0530

    remoteproc: zynqmp_r5: Enhance rproc_put() with support for cluster dev
    Multi-cluster remoteproc designs typically have the following DT
    declaration:
    
    	remoteproc_cluster {
    		compatible = "soc,remoteproc-cluster";
    
                    core0: core0 {
    			compatible = "soc,remoteproc-core"
                            memory-region;
                            sram;
                    };
    
                    core1: core1 {
    			compatible = "soc,remoteproc-core"
                            memory-region;
                            sram;
                    }
            };
    
    A driver exists for the cluster rather than the individual cores
    themselves so that operation mode and HW specific configurations
    applicable to the cluster can be made.
    
    Because the driver exists at the cluster level and not the individual
    core level, rproc_get_by_phandle() will return remoteproc associated
    with the phandle by looking for the cluster's driver when the driver
    for the immediate remoteproc's parent is not found.
    
    When rproc_get_by_phandle() returns remoteproc associated with phandle
    it called for by looking for the cluster driver, rproc_put() crashes while
    trying to decrement the module reference count.
    
    This patch enhances rproc_put() to support cluster remote proc devices
    as in rproc_get_by_phandle(), to find the cluster driver and decrement
    the module reference count.
    
    Signed-off-by: Tarak Reddy <tarak.reddy@amd.com>
    Reviewed-by: Ben Levinsky <ben.levinsky@amd.com>
    Tested-by: Ben Levinsky <ben.levinsky@amd.com>
    State: pending

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -2559,7 +2559,19 @@ EXPORT_SYMBOL(rproc_free);
  */
 void rproc_put(struct rproc *rproc)
 {
-	module_put(rproc->dev.parent->driver->owner);
+	struct platform_device *cluster_pdev;
+
+	if (rproc->dev.parent) {
+		if (rproc->dev.parent->driver) {
+			module_put(rproc->dev.parent->driver->owner);
+		} else {
+			cluster_pdev = of_find_device_by_node(rproc->dev.parent->of_node->parent);
+			if (cluster_pdev) {
+				module_put(cluster_pdev->dev.driver->owner);
+				put_device(&cluster_pdev->dev);
+			}
+		}
+	}
 	put_device(&rproc->dev);
 }
 EXPORT_SYMBOL(rproc_put);
