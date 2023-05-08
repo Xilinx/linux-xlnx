eab56a0f73d81e8e3cd51e3b6ee6db75dd4f5f57
Pranavi Somisetty <pranavi.somisetty@amd.com>
2023-05-07 23:40:50 -0600

    staging: xilinx-tsn: don't exit probe when EP mac address is not found
    Currently during probe, driver tries to read EP mac address from
    device-tree/EEPROM, and if it is not found, ep probe exits with
    an error. Modifying this, so that a random mac address is assigned
    to EP when it cannot find one from device tree or EEPROM.
    
    Signed-off-by: Pranavi Somisetty <pranavi.somisetty@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>

--- drivers/staging/xilinx-tsn/xilinx_tsn_ep.c
+++ drivers/staging/xilinx-tsn/xilinx_tsn_ep.c
@@ -280,7 +280,7 @@ static void tsn_ep_set_mac_address(struct net_device *ndev, const void *address)
 {
 	if (address)
 		eth_hw_addr_set(ndev, address);
-	if (!is_valid_ether_addr(ndev->dev_addr))
+	if (!address || !is_valid_ether_addr(ndev->dev_addr))
 		eth_hw_addr_random(ndev);
 }
 
@@ -554,10 +554,8 @@ static int tsn_ep_probe(struct platform_device *pdev)
 
 	/* Retrieve the MAC address */
 	ret = of_get_mac_address(pdev->dev.of_node, mac_addr);
-	if (ret) {
+	if (ret)
 		dev_err(&pdev->dev, "could not find MAC address\n");
-		goto free_netdev;
-	}
 	tsn_ep_set_mac_address(ndev, mac_addr);
 	ret = tsn_mcdma_probe(pdev, lp, ndev);
 	if (ret) {
