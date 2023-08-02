0035e8a03b8b5a74ef6566421f98d180e0eda9cc
Ronak Jain <ronak.jain@amd.com>
2023-08-02 03:30:04 -0700

    firmware: xilinx: remove clock_setrate and clock_getrate api
    As per the current code base, PM_CLOCK_SETRATE and PM_CLOCK_GETRATE
    APIs are not supported for the runtime operations. In the case of
    ZynqMP returning an error from TF-A when there is any request to
    access these APIs and for Versal also it is returning an error like
    NO_ACCESS from the firmware. So, just removing the unused code to
    avoid the confusion around these APIs.
    
    Also, there is no issue with the backward compatibility as these APIs
    were never used since implemented. Hence no need to bump up the
    version of the feature check API as well.
    
    Signed-off-by: Ronak Jain <ronak.jain@amd.com>
    State: pending

--- drivers/firmware/xilinx/zynqmp-debug.c
+++ drivers/firmware/xilinx/zynqmp-debug.c
@@ -60,8 +60,6 @@ static struct pm_api_info pm_api_list[] = {
 	PM_API(PM_CLOCK_GETSTATE),
 	PM_API(PM_CLOCK_SETDIVIDER),
 	PM_API(PM_CLOCK_GETDIVIDER),
-	PM_API(PM_CLOCK_SETRATE),
-	PM_API(PM_CLOCK_GETRATE),
 	PM_API(PM_CLOCK_SETPARENT),
 	PM_API(PM_CLOCK_GETPARENT),
 	PM_API(PM_QUERY_DATA),
@@ -166,7 +164,6 @@ static int get_pm_api_id(char *pm_api_req, u32 *pm_id)
 static int process_api_request(u32 pm_id, u64 *pm_api_arg, u32 *pm_api_ret)
 {
 	u32 pm_api_version;
-	u64 rate;
 	int ret;
 	struct zynqmp_pm_query_data qdata = {0};
 
@@ -347,14 +344,6 @@ static int process_api_request(u32 pm_id, u64 *pm_api_arg, u32 *pm_api_ret)
 		if (!ret)
 			sprintf(debugfs_buf, "Divider Value: %d\n",
 				pm_api_ret[0]);
-		break;
-	case PM_CLOCK_SETRATE:
-		ret = zynqmp_pm_clock_setrate(pm_api_arg[0], pm_api_arg[1]);
-		break;
-	case PM_CLOCK_GETRATE:
-		ret = zynqmp_pm_clock_getrate(pm_api_arg[0], &rate);
-		if (!ret)
-			sprintf(debugfs_buf, "Clock rate :%llu\n", rate);
 		break;
 	case PM_CLOCK_SETPARENT:
 		ret = zynqmp_pm_clock_setparent(pm_api_arg[0], pm_api_arg[1]);
