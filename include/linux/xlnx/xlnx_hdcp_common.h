6ec24f85bc22ea2f695d1177a96a9fe3e787bb6a
Kunal Rane <kunal.rane@amd.com>
2023-08-27 21:56:50 +0530

    staging: xilinx_hdcp: Add support for HDCP2x Rx
    Add new functions as well as make some functions global
    so they can be used by cryptography logic for HDCP2x receiver.
    These functions are mostly used for decrypting user key for
    authentication with equivalent HDCP2X Tx pair.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>
    State: pending

--- include/linux/xlnx/xlnx_hdcp_common.h
+++ include/linux/xlnx/xlnx_hdcp_common.h
@@ -18,5 +18,17 @@ size_t mp_conv_to_octets(const unsigned int a[], size_t ndigits, unsigned char *
 			 size_t nbytes);
 size_t mp_conv_from_octets(unsigned int a[], size_t ndigits, const unsigned char *c,
 			   size_t nbytes);
-
+u32 mp_subtract(u32 w[], const u32 u[], const u32 v[], size_t ndigits);
+unsigned int mp_add(unsigned int w[], const unsigned int u[], const unsigned int v[],
+		    size_t ndigits);
+unsigned int mp_shift_left(unsigned int a[], const unsigned int *b, size_t shift, size_t ndigits);
+int mp_multiply(unsigned int w[], const unsigned int u[], const unsigned int v[], size_t ndigits);
+int mp_mod_mult(u32 a[], const u32 x[], const u32 y[], u32 m[], size_t ndigits);
+int mp_modulo(u32 r[], const u32 u[], size_t udigits, u32 v[], size_t vdigits);
+int mp_mod_inv(u32 inv[], const u32 u[], const u32 v[], size_t ndigits);
+int mp_equal(const u32 a[], const u32 b[], size_t ndigits);
+int mp_get_bit(u32 a[], size_t ndigits, size_t ibit);
+int mp_short_cmp(const u32 a[], u32 d, size_t ndigits);
+int mp_divide(unsigned int q[], unsigned int r[], const unsigned int u[],
+	      size_t udigits, unsigned int v[], size_t vdigits);
 #endif
