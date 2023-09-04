1204997d6408fb1e0f0bf7420c58e4f0e0db33bb
Kunal Rane <kunal.rane@amd.com>
2023-09-04 22:40:06 +0530

    v4l2: xilinx: dprx: Add HDCP2x support
    Add HDCP2X support to the DP Rx driver.
    The HDCP protocol specifies a secure method of transmitting
    audiovisual content. Further, the audiovisual content can be
    transmitted over a DisplayPort interface. The HDCP Controller
    IP core is used for data decryption along with the DisplayPort
    Receive IP core in the DisplayPort 1.4 RX Subsystem
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.co>
    State: pending

--- include/uapi/linux/xilinx-dprxss.h
+++ include/uapi/linux/xilinx-dprxss.h
@@ -15,8 +15,18 @@ struct xdprxss_hdcp1x_keys_ioctl {
 	void const *keys;
 };
 
+struct xdprxss_hdcp2x_keys_ioctl {
+	__u32 size_lc128;
+	__u32 size_private;
+	void const *key_lc128;
+	void const *key_private;
+};
+
 /* This ioctl is used to set the hdcp1x keys into IP */
 #define XILINX_DPRXSS_HDCP_KEY_WRITE \
 	_IOW('X', BASE_VIDIOC_PRIVATE + 0, struct xdprxss_hdcp1x_keys_ioctl)
+
+#define XILINX_DPRXSS_HDCP2X_KEY_WRITE \
+	_IOW('X', BASE_VIDIOC_PRIVATE + 1, struct xdprxss_hdcp2x_keys_ioctl)
 
 #endif
