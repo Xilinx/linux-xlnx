13889a97923e478b2dc8c1fb5cf824a8fb94fa3e
Piyush Mehta <piyush.mehta@amd.com>
2023-09-29 12:18:51 +0530

    usb: chipidea: add usb2 phy interface for Zynq platform
    USB2 phy has been added to the Zynq platform data. This defines the ULPI
    phy interface for accessing the ULPI register.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    Link: https://lore.kernel.org/r/20230929064852.16642-3-piyush.mehta@amd.com
    State: waiting

--- drivers/usb/chipidea/ci_hdrc_usb2.c
+++ drivers/usb/chipidea/ci_hdrc_usb2.c
@@ -65,6 +65,14 @@ static int ci_hdrc_usb2_probe(struct platform_device *pdev)
 	if (match && match->data) {
 		/* struct copy */
 		*ci_pdata = *(struct ci_hdrc_platform_data *)match->data;
+		if (of_device_is_compatible(pdev->dev.of_node,
+					    "xlnx,zynq-usb-2.20a")) {
+			ci_pdata->usb_phy = devm_usb_get_phy_by_phandle(dev,
+									"usb-phy",
+									0);
+			if (IS_ERR(ci_pdata->usb_phy))
+				return PTR_ERR(ci_pdata->usb_phy);
+		}
 	}
 
 	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
