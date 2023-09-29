f3fb33474aeb9357fad4edc0f125b4362d0e03bc
Nikhil Agarwal <nikhil.agarwal@amd.com>
2023-09-29 11:37:17 +0530

    vfio/cdx: add interrupt support
    Support the following ioctls for CDX devices:
    - VFIO_DEVICE_GET_IRQ_INFO
    - VFIO_DEVICE_SET_IRQS
    
    This allows user to set an eventfd for cdx device interrupts and
    trigger this interrupt eventfd from userspace.
    All CDX device interrupts are MSIs. The MSIs are allocated from the
    CDX-MSI domain.
    
    Signed-off-by: Nipun Gupta <nipun.gupta@amd.com>
    Reviewed-by: Pieter Jansen van Vuuren <pieter.jansen-van-vuuren@amd.com>
    Link: https://lore.kernel.org/r/20230920133951.16953-1-nipun.gupta@amd.com/
    State: waiting

--- drivers/vfio/cdx/private.h
+++ drivers/vfio/cdx/private.h
@@ -13,6 +13,14 @@ static inline u64 vfio_cdx_index_to_offset(u32 index)
 	return ((u64)(index) << VFIO_CDX_OFFSET_SHIFT);
 }
 
+struct vfio_cdx_irq {
+	u32			flags;
+	u32			count;
+	int			irq_no;
+	struct eventfd_ctx	*trigger;
+	char			*name;
+};
+
 struct vfio_cdx_region {
 	u32			flags;
 	u32			type;
@@ -25,6 +33,16 @@ struct vfio_cdx_device {
 	struct vfio_cdx_region	*regions;
 	u32			flags;
 #define BME_SUPPORT BIT(0)
+	struct vfio_cdx_irq	*cdx_irqs;
+	u32			irq_count;
+	u32			config_msi;
 };
+
+int vfio_cdx_set_irqs_ioctl(struct vfio_cdx_device *vdev,
+			    u32 flags, unsigned int index,
+			    unsigned int start, unsigned int count,
+			    void *data);
+
+void vfio_cdx_irqs_cleanup(struct vfio_cdx_device *vdev);
 
 #endif /* VFIO_CDX_PRIVATE_H */
