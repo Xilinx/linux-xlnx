40c5bf62bb21e5b59870ba6ceb4e27bc171347c2
Michal Simek <michal.simek@amd.com>
2023-10-26 14:41:01 +0200

    Revert "i2c: mux: pca954x: write to mux channel always"
    This reverts commit d38ecaa5a9a9e086a57047190d1190faae87508a.
    
    By design multiple masters can't deal with i2c muxes without possible race
    condition. This patch was trying to improve situation on these scenarios
    but real solution is to avoid these cases completely. That's the reason why
    no Versal evaluation board is describing i2c bus structure. Only enabling
    i2c IP itself without any information about topology. On these boards
    System Controller has full i2c control. On Versal NET boards i2c muxes are
    not used and i2c devices are using i2c address translators (LTC4316) if
    there is possible i2c address colision.
    ZynqMP evaluation boards provide capability to connect multiple I2C
    controllers to single bus but by default this bus is owned only by one IP.
    
    That's why revert the patch which never really fixed the problem and
    everybody should avoid sharing i2c bus with i2c muxes on it unless both
    sides have agreement how to do it without race condition.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>

--- drivers/i2c/muxes/i2c-mux-pca954x.c
+++ drivers/i2c/muxes/i2c-mux-pca954x.c
@@ -239,9 +239,11 @@ static int pca954x_select_chan(struct i2c_mux_core *muxc, u32 chan)
 	int ret = 0;
 
 	regval = pca954x_regval(data, chan);
-
-	ret = pca954x_reg_write(muxc->parent, client, regval);
-	data->last_chan = ret < 0 ? 0 : regval;
+	/* Only select the channel if its different from the last channel */
+	if (data->last_chan != regval) {
+		ret = pca954x_reg_write(muxc->parent, client, regval);
+		data->last_chan = ret < 0 ? 0 : regval;
+	}
 
 	return ret;
 }
