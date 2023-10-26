e7ecb4fce1f3f4fb0c6ddaa7c6f759af4cee72f4
Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
2023-10-26 12:43:23 +0530

    Revert "drivers: clk: zynqmp: Add versal-net compatible string"
    This reverts commit c7302bc4d89b19c3dd51645eb4e52a918343b0fe.
    
    Based on mainline discussion, we actually don't need to update the driver
    for this. We can just simply list new dt binding and use both compatible
    string in DT like this. "xlnx,versal-net-clk", "xlnx,versal-clk"
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    Link: https://lore.kernel.org/all/20230330091309.16215-3-shubhrajyoti.datta@amd.com

--- drivers/clk/zynqmp/clkc.c
+++ drivers/clk/zynqmp/clkc.c
@@ -789,7 +789,6 @@ static int zynqmp_clock_probe(struct platform_device *pdev)
 static const struct of_device_id zynqmp_clock_of_match[] = {
 	{.compatible = "xlnx,zynqmp-clk"},
 	{.compatible = "xlnx,versal-clk"},
-	{.compatible = "xlnx,versal-net-clk"},
 	{},
 };
 MODULE_DEVICE_TABLE(of, zynqmp_clock_of_match);
