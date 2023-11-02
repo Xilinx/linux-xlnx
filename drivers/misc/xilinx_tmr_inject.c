720fe0dfa68b03eec40f4d67fea446dfc4934e76
Michal Simek <michal.simek@amd.com>
2023-11-02 13:48:36 +0100

    misc: trm-inject: Sync driver and binding with upstream version
    Removed sysfs interface and use debugfs instead. Changes are done based on
    upstream commit 895ae5bee159 ("drivers: misc: Add Support for TMR Inject
    IP") and commit 859584726a84 ("dt-bindings: misc: tmr-inject: Add
    device-tree binding for TMR Inject").
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>

--- drivers/misc/xilinx_tmr_inject.c
+++ drivers/misc/xilinx_tmr_inject.c
@@ -2,18 +2,18 @@
 /*
  * Driver for Xilinx TMR Inject IP.
  *
- * Copyright (C) 2022 Xilinx, Inc.
+ * Copyright (C) 2022 Advanced Micro Devices, Inc.
  *
  * Description:
  * This driver is developed for TMR Inject IP,The Triple Modular Redundancy(TMR)
  * Inject provides fault injection.
- * Fault injection and detection features are provided through sysfs entries
- * which allow the user to generate a fault.
  */
 
 #include <asm/xilinx_mb_manager.h>
 #include <linux/module.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/fault-inject.h>
 
 /* TMR Inject Register offsets */
 #define XTMR_INJECT_CR_OFFSET		0x0
@@ -27,92 +27,70 @@
 #define XTMR_INJECT_CR_IE_SHIFT		10
 #define XTMR_INJECT_IIR_ADDR_MASK	GENMASK(31, 16)
 
+#define XTMR_INJECT_MAGIC_MAX_VAL	255
+
 /**
  * struct xtmr_inject_dev - Driver data for TMR Inject
  * @regs: device physical base address
- * @dev: pointer to device struct
- * @cr_val: control register value
  * @magic: Magic hardware configuration value
- * @err_cnt: error statistics count
  */
 struct xtmr_inject_dev {
 	void __iomem *regs;
-	struct device *dev;
-	u32 cr_val;
 	u32 magic;
-	u32 err_cnt;
 };
 
+static DECLARE_FAULT_ATTR(inject_fault);
+static char *inject_request;
+module_param(inject_request, charp, 0);
+MODULE_PARM_DESC(inject_request, "default fault injection attributes");
+static struct dentry *dbgfs_root;
+
 /* IO accessors */
-static inline void xtmr_inject_write(struct xtmr_inject_dev *xtmr_inject, u32 addr,
-				     u32 value)
+static inline void xtmr_inject_write(struct xtmr_inject_dev *xtmr_inject,
+				     u32 addr, u32 value)
 {
 	iowrite32(value, xtmr_inject->regs + addr);
 }
 
-static inline u32 xtmr_inject_read(struct xtmr_inject_dev *xtmr_inject, u32 addr)
+static inline u32 xtmr_inject_read(struct xtmr_inject_dev *xtmr_inject,
+				   u32 addr)
 {
 	return ioread32(xtmr_inject->regs + addr);
 }
 
-static ssize_t inject_err_store(struct device *dev,
-				struct device_attribute *attr, const char *buf,
-				size_t size)
+static int xtmr_inject_set(void *data, u64 val)
 {
-	int ret;
-	long value;
-
-	ret = kstrtol(buf, 16, &value);
-	if (ret)
-		return ret;
-
-	if (value > 1)
+	if (val != 1)
 		return -EINVAL;
 
 	xmb_inject_err();
-
-	return size;
+	return 0;
 }
-static DEVICE_ATTR_WO(inject_err);
+DEFINE_DEBUGFS_ATTRIBUTE(xtmr_inject_fops, NULL, xtmr_inject_set, "%llu\n");
 
-static ssize_t inject_cpuid_store(struct device *dev,
-				  struct device_attribute *attr,
-				  const char *buf, size_t size)
+static void xtmr_init_debugfs(struct xtmr_inject_dev *xtmr_inject)
 {
-	struct xtmr_inject_dev *xtmr_inject = dev_get_drvdata(dev);
-	int ret;
-	long value;
+	struct dentry *dir;
 
-	ret = kstrtol(buf, 0, &value);
-	if (ret)
-		return ret;
-
-	if (value > 3)
-		return -EINVAL;
-
-	xtmr_inject->cr_val |= (value << XTMR_INJECT_CR_CPUID_SHIFT);
-	xtmr_inject_write(xtmr_inject, XTMR_INJECT_CR_OFFSET,
-			  xtmr_inject->cr_val);
-
-	return size;
+	dbgfs_root = debugfs_create_dir("xtmr_inject", NULL);
+	dir = fault_create_debugfs_attr("inject_fault", dbgfs_root,
+					&inject_fault);
+	debugfs_create_file("inject_fault", 0200, dir, NULL,
+			    &xtmr_inject_fops);
 }
-static DEVICE_ATTR_WO(inject_cpuid);
-
-static struct attribute *xtmr_inject_attrs[] = {
-	&dev_attr_inject_err.attr,
-	&dev_attr_inject_cpuid.attr,
-	NULL,
-};
-ATTRIBUTE_GROUPS(xtmr_inject);
 
 static void xtmr_inject_init(struct xtmr_inject_dev *xtmr_inject)
 {
+	u32 cr_val;
+
+	if (inject_request)
+		setup_fault_attr(&inject_fault, inject_request);
 	/* Allow fault injection */
-	xtmr_inject->cr_val = xtmr_inject->magic |
-				(1 << XTMR_INJECT_CR_IE_SHIFT) |
-				(1 << XTMR_INJECT_CR_CPUID_SHIFT);
+	cr_val = xtmr_inject->magic |
+		 (1 << XTMR_INJECT_CR_IE_SHIFT) |
+		 (1 << XTMR_INJECT_CR_CPUID_SHIFT);
 	xtmr_inject_write(xtmr_inject, XTMR_INJECT_CR_OFFSET,
-			  xtmr_inject->cr_val);
+			  cr_val);
 	/* Initialize the address inject and instruction inject registers */
 	xtmr_inject_write(xtmr_inject, XTMR_INJECT_AIR_OFFSET,
 			  XMB_INJECT_ERR_OFFSET);
@@ -125,24 +103,21 @@ static void xtmr_inject_init(struct xtmr_inject_dev *xtmr_inject)
  * @pdev: Pointer to the platform_device structure
  *
  * This is the driver probe routine. It does all the memory
- * allocation and creates sysfs entries for the device.
+ * allocation for the device.
  *
  * Return: 0 on success and failure value on error
  */
 static int xtmr_inject_probe(struct platform_device *pdev)
 {
 	struct xtmr_inject_dev *xtmr_inject;
-	struct resource *res;
 	int err;
 
-	xtmr_inject = devm_kzalloc(&pdev->dev, sizeof(*xtmr_inject), GFP_KERNEL);
+	xtmr_inject = devm_kzalloc(&pdev->dev, sizeof(*xtmr_inject),
+				   GFP_KERNEL);
 	if (!xtmr_inject)
 		return -ENOMEM;
 
-	xtmr_inject->dev = &pdev->dev;
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	xtmr_inject->regs = devm_ioremap_resource(xtmr_inject->dev, res);
+	xtmr_inject->regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(xtmr_inject->regs))
 		return PTR_ERR(xtmr_inject->regs);
 
@@ -153,14 +128,15 @@ static int xtmr_inject_probe(struct platform_device *pdev)
 		return err;
 	}
 
+	if (xtmr_inject->magic > XTMR_INJECT_MAGIC_MAX_VAL) {
+		dev_err(&pdev->dev, "invalid xlnx,magic property value");
+		return -EINVAL;
+	}
+
 	/* Initialize TMR Inject */
 	xtmr_inject_init(xtmr_inject);
 
-	err = sysfs_create_groups(&xtmr_inject->dev->kobj, xtmr_inject_groups);
-	if (err < 0) {
-		dev_err(&pdev->dev, "unable to create sysfs entries\n");
-		return err;
-	}
+	xtmr_init_debugfs(xtmr_inject);
 
 	platform_set_drvdata(pdev, xtmr_inject);
 
@@ -169,8 +145,8 @@ static int xtmr_inject_probe(struct platform_device *pdev)
 
 static int xtmr_inject_remove(struct platform_device *pdev)
 {
-	sysfs_remove_groups(&pdev->dev.kobj, xtmr_inject_groups);
-
+	debugfs_remove_recursive(dbgfs_root);
+	dbgfs_root = NULL;
 	return 0;
 }
 
@@ -191,6 +167,6 @@ static struct platform_driver xtmr_inject_driver = {
 	.remove = xtmr_inject_remove,
 };
 module_platform_driver(xtmr_inject_driver);
-MODULE_AUTHOR("Xilinx, Inc");
+MODULE_AUTHOR("Advanced Micro Devices, Inc");
 MODULE_DESCRIPTION("Xilinx TMR Inject Driver");
 MODULE_LICENSE("GPL");
