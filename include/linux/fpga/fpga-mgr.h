dc67651172095c04c4e6e1a64983036f4e654fd2
Nava kishore Manne <nava.manne@xilinx.com>
2023-12-08 12:14:30 +0530

    fpga: support loading from a pre-allocated buffer
    Some systems are memory constrained but they need to load very
    large Configuration files. The FPGA subsystem allows drivers to
    request this Configuration image be loaded from the filesystem,
    but this requires that the entire configuration data be loaded
    into kernel memory first before it's provided to the driver.
    This can lead to a situation where we map the configuration
    data twice, once to load the configuration data into kernel
    memory and once to copy the configuration data into the final
    resting place which is nothing but a dma-able continuous buffer.
    
    This creates needless memory pressure and delays due to multiple
    copies. Let's add a dmabuf handling support to the fpga manager
    framework that allows drivers to load the Configuration data
    directly from a pre-allocated buffer. This skips the intermediate
    step of allocating a buffer in kernel memory to hold the
    Configuration data.
    
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    Link: https://lore.kernel.org/all/20231122053035.3758124-2-nava.kishore.manne@amd.com/
    State: waiting

--- include/linux/fpga/fpga-mgr.h
+++ include/linux/fpga/fpga-mgr.h
@@ -9,6 +9,7 @@
 #define _LINUX_FPGA_MGR_H
 
 #include <linux/mutex.h>
+#include <linux/miscdevice.h>
 #include <linux/platform_device.h>
 
 #define ENCRYPTED_KEY_LEN	64 /* Bytes */
@@ -90,6 +91,7 @@ enum fpga_mgr_states {
 #define FPGA_MGR_USERKEY_ENCRYPTED_BITSTREAM	BIT(5)
 #define FPGA_MGR_DDR_MEM_AUTH_BITSTREAM		BIT(6)
 #define FPGA_MGR_SECURE_MEM_AUTH_BITSTREAM	BIT(7)
+#define FPGA_MGR_CONFIG_DMA_BUF		BIT(5)
 
 /**
  * struct fpga_image_info - information specific to an FPGA image
@@ -216,7 +218,9 @@ struct fpga_manager_ops {
  * @flags: flags determines the type of Bitstream
  * @key: key value useful for Encrypted Bitstream loading to read the userkey
  * @dev: fpga manager device
+ * @dmabuf: shared dma buffer
  * @ref_mutex: only allows one reference to fpga manager
+ * @miscdev: information about character device node
  * @state: state of fpga manager
  * @compat_id: FPGA manager id for compatibility check.
  * @mops: pointer to struct of fpga manager ops
@@ -228,7 +232,9 @@ struct fpga_manager {
 	unsigned long flags;
 	char key[ENCRYPTED_KEY_LEN + 1];
 	struct device dev;
+	struct dma_buf *dmabuf;
 	struct mutex ref_mutex;
+	struct miscdevice miscdev;
 	enum fpga_mgr_states state;
 	struct fpga_compat_id *compat_id;
 	const struct fpga_manager_ops *mops;
@@ -268,5 +274,7 @@ devm_fpga_mgr_register_full(struct device *parent, const struct fpga_manager_inf
 struct fpga_manager *
 devm_fpga_mgr_register(struct device *parent, const char *name,
 		       const struct fpga_manager_ops *mops, void *priv);
+
+#define FPGA_IOCTL_LOAD_DMA_BUFF	_IOWR('R', 1, __u32)
 
 #endif /*_LINUX_FPGA_MGR_H */
