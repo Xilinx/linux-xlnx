7ba6cc3da582e132b5454bd52402c20d45524511
Dhaval Shah <dhaval.r.shah@amd.com>
2023-12-08 15:46:29 +0530

    crypto: xilinx: AES support for the Versal platform
    For Versal platform, there are following command are supported
    XSECURE_API_AES_OP_INIT: AES operation encrypt/decrypt init based on
    details provided in AesParams
    (IV update, keysrc and keysize for encrypt/decrypt)
    XSECURE_API_AES_UPDATE_AAD: AAD and GMAC feature support related command
    XSECURE_API_AES_ENCRYPT_UPDATE: Encrypt continue packet updates
    XSECURE_API_AES_ENCRYPT_FINAL: Encrypt final packet update in dst addr
    XSECURE_API_AES_DECRYPT_UPDATE: Decrypt continue packet updates
    XSECURE_API_AES_DECRYPT_FINAL: Decrypt final packet update in dst addr
    XSECURE_API_AES_WRITE_KEY: Set the key for the provided keysrc
    
    Signed-off-by: Dhaval Shah <dhaval.r.shah@amd.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    State: pending
    [michals:
    v6.12 update
    crypto: xilinx: extra setkey check removed
    crypto: xilinx: Add support for device keysrc
    crypto: xilinx/zynqmp-aes-gcm: Fix return error code of GCM decrypt operation
    crypto: zynqmp-aes-gcm: Clear AES user key
    crypto: zynqmp-aes-gcm: Fix memory leak
    crypto: zynqmp-aes-gcm: Fix out of bound memory write
    ]

--- drivers/crypto/xilinx/zynqmp-aes-gcm.c
+++ drivers/crypto/xilinx/zynqmp-aes-gcm.c
@@ -27,6 +27,7 @@
 #define ZYNQMP_AES_BLK_SIZE		1U
 #define ZYNQMP_AES_MIN_INPUT_BLK_SIZE	4U
 #define ZYNQMP_AES_WORD_LEN		4U
+#define VERSAL_AES_QWORD_LEN		16U
 
 #define ZYNQMP_AES_GCM_TAG_MISMATCH_ERR		0x01
 #define ZYNQMP_AES_WRONG_KEY_SRC_ERR		0x13
@@ -41,6 +42,41 @@ enum zynqmp_aead_keysrc {
 	ZYNQMP_AES_KUP_KEY = 0,
 	ZYNQMP_AES_DEV_KEY,
 	ZYNQMP_AES_PUF_KEY
+};
+
+enum versal_aead_keysrc {
+	VERSAL_AES_BBRAM_KEY = 0,
+	VERSAL_AES_BBRAM_RED_KEY,
+	VERSAL_AES_BH_KEY,
+	VERSAL_AES_BH_RED_KEY,
+	VERSAL_AES_EFUSE_KEY,
+	VERSAL_AES_EFUSE_RED_KEY,
+	VERSAL_AES_EFUSE_USER_KEY_0,
+	VERSAL_AES_EFUSE_USER_KEY_1,
+	VERSAL_AES_EFUSE_USER_RED_KEY_0,
+	VERSAL_AES_EFUSE_USER_RED_KEY_1,
+	VERSAL_AES_KUP_KEY,
+	VERSAL_AES_PUF_KEY,
+	VERSAL_AES_USER_KEY_0,
+	VERSAL_AES_USER_KEY_1,
+	VERSAL_AES_USER_KEY_2,
+	VERSAL_AES_USER_KEY_3,
+	VERSAL_AES_USER_KEY_4,
+	VERSAL_AES_USER_KEY_5,
+	VERSAL_AES_USER_KEY_6,
+	VERSAL_AES_USER_KEY_7,
+	VERSAL_AES_EXPANDED_KEYS,
+	VERSAL_AES_ALL_KEYS,
+};
+
+enum versal_aead_op {
+	VERSAL_AES_ENCRYPT = 0,
+	VERSAL_AES_DECRYPT
+};
+
+enum versal_aes_keysize {
+	AES_KEY_SIZE_128 = 0,
+	AES_KEY_SIZE_256 = 2,
 };
 
 struct zynqmp_aead_tfm_ctx {
@@ -74,6 +110,19 @@ struct zynqmp_aead_hw_req {
 
 struct zynqmp_aead_req_ctx {
 	enum zynqmp_aead_op op;
+};
+
+struct versal_init_ops {
+	u64 iv;
+	u32 op;
+	u32 keysrc;
+	u32 size;
+};
+
+struct versal_in_params {
+	u64 in_data_addr;
+	u32 size;
+	u32 is_last;
 };
 
 static int zynqmp_aes_aead_cipher(struct aead_request *req)
@@ -176,6 +225,145 @@ static int zynqmp_aes_aead_cipher(struct aead_request *req)
 	return err;
 }
 
+static int versal_aes_aead_cipher(struct aead_request *req)
+{
+	struct crypto_aead *aead = crypto_aead_reqtfm(req);
+	struct zynqmp_aead_tfm_ctx *tfm_ctx = crypto_aead_ctx(aead);
+	struct zynqmp_aead_req_ctx *rq_ctx = aead_request_ctx(req);
+	dma_addr_t dma_addr_data, dma_addr_hw_req, dma_addr_in;
+	u32 total_len = req->assoclen + req->cryptlen;
+	u32 key_offset = total_len + GCM_AES_IV_SIZE;
+	struct device *dev = tfm_ctx->dev;
+	struct versal_init_ops *hwreq;
+	struct versal_in_params *in;
+	u32 gcm_offset, out_len;
+	size_t dma_size;
+	char *kbuf;
+	int ret;
+
+	if (tfm_ctx->keylen != XSECURE_AES_KEY_SIZE_128 &&
+	    tfm_ctx->keylen != XSECURE_AES_KEY_SIZE_256) {
+		ret = -EINVAL;
+		goto err;
+	}
+
+	dma_size = key_offset;
+	/* Allocate keylen for volatile user keys */
+	if (tfm_ctx->keysrc >= VERSAL_AES_USER_KEY_0 && tfm_ctx->keysrc <= VERSAL_AES_USER_KEY_7)
+		dma_size += tfm_ctx->keylen;
+
+	kbuf = dma_alloc_coherent(dev, dma_size, &dma_addr_data, GFP_KERNEL);
+	if (!kbuf) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	/*
+	 * Allocated separate memory as separate structure for init ops
+	 * Also to avoid big continuous memory allocation
+	 */
+	hwreq = dma_alloc_coherent(dev, sizeof(struct versal_init_ops),
+				   &dma_addr_hw_req, GFP_KERNEL);
+	if (!hwreq) {
+		ret = -ENOMEM;
+		goto hwreq_fail;
+	}
+
+	/*
+	 * Allocated separate memory as separate structure for in params
+	 * Also to avoid big continuous memory allocation
+	 */
+	in = dma_alloc_coherent(dev, sizeof(struct versal_in_params),
+				&dma_addr_in, GFP_KERNEL);
+	if (!in) {
+		ret = -ENOMEM;
+		goto in_fail;
+	}
+
+	scatterwalk_map_and_copy(kbuf, req->src, 0, total_len, 0);
+	memcpy(kbuf + total_len, req->iv, GCM_AES_IV_SIZE);
+	hwreq->iv = dma_addr_data + total_len;
+	hwreq->keysrc = tfm_ctx->keysrc;
+
+	if (rq_ctx->op == ZYNQMP_AES_ENCRYPT) {
+		hwreq->op = VERSAL_AES_ENCRYPT;
+		out_len = total_len + ZYNQMP_AES_AUTH_SIZE;
+		in->size = req->cryptlen;
+	} else {
+		hwreq->op = VERSAL_AES_DECRYPT;
+		out_len = total_len - ZYNQMP_AES_AUTH_SIZE;
+		in->size = req->cryptlen - ZYNQMP_AES_AUTH_SIZE;
+	}
+
+	if (tfm_ctx->keylen == XSECURE_AES_KEY_SIZE_128)
+		hwreq->size = AES_KEY_SIZE_128;
+	else if (tfm_ctx->keylen == XSECURE_AES_KEY_SIZE_256)
+		hwreq->size = AES_KEY_SIZE_256;
+
+	/* Request aes key write for volatile user keys */
+	if (hwreq->keysrc >= VERSAL_AES_USER_KEY_0 && hwreq->keysrc <= VERSAL_AES_USER_KEY_7) {
+		memcpy(kbuf + key_offset, tfm_ctx->key, tfm_ctx->keylen);
+		ret = versal_pm_aes_key_write(hwreq->size, hwreq->keysrc,
+					      dma_addr_data + key_offset);
+		if (ret)
+			goto key_fail;
+	}
+
+	ret = versal_pm_aes_op_init(dma_addr_hw_req);
+	if (ret)
+		goto clearkey;
+
+	if (req->assoclen > 0) {
+		/* Currently GMAC is OFF by default */
+		ret = versal_pm_aes_update_aad(dma_addr_data, req->assoclen);
+		if (ret)
+			goto clearkey;
+	}
+
+	in->in_data_addr = dma_addr_data + req->assoclen;
+	in->is_last = 1;
+	gcm_offset = req->assoclen + in->size;
+
+	if (rq_ctx->op == ZYNQMP_AES_ENCRYPT) {
+		ret = versal_pm_aes_enc_update(dma_addr_in,
+					       dma_addr_data + req->assoclen);
+		if (ret)
+			goto clearkey;
+
+		ret = versal_pm_aes_enc_final(dma_addr_data + gcm_offset);
+		if (ret)
+			goto clearkey;
+	} else {
+		ret = versal_pm_aes_dec_update(dma_addr_in,
+					       dma_addr_data + req->assoclen);
+		if (ret)
+			goto clearkey;
+
+		ret = versal_pm_aes_dec_final(dma_addr_data + gcm_offset);
+		if (ret) {
+			ret = -EBADMSG;
+			goto clearkey;
+		}
+	}
+
+	sg_copy_from_buffer(req->dst, sg_nents(req->dst),
+			    kbuf, out_len);
+
+clearkey:
+	if (hwreq->keysrc >= VERSAL_AES_USER_KEY_0 && hwreq->keysrc <= VERSAL_AES_USER_KEY_7)
+		versal_pm_aes_key_zero(hwreq->keysrc);
+key_fail:
+	dma_free_coherent(dev, sizeof(struct versal_in_params), in, dma_addr_in);
+in_fail:
+	memzero_explicit(hwreq, sizeof(struct versal_init_ops));
+	dma_free_coherent(dev, sizeof(struct versal_init_ops), hwreq, dma_addr_hw_req);
+hwreq_fail:
+	memzero_explicit(kbuf, dma_size);
+	dma_free_coherent(dev, dma_size, kbuf, dma_addr_data);
+err:
+	return ret;
+}
+
 static int zynqmp_fallback_check(struct zynqmp_aead_tfm_ctx *tfm_ctx,
 				 struct aead_request *req)
 {
@@ -200,6 +388,38 @@ static int zynqmp_fallback_check(struct zynqmp_aead_tfm_ctx *tfm_ctx,
 	    req->cryptlen <= ZYNQMP_AES_AUTH_SIZE) {
 		need_fallback = 1;
 	}
+	return need_fallback;
+}
+
+static int versal_fallback_check(struct zynqmp_aead_tfm_ctx *tfm_ctx,
+				 struct aead_request *req)
+{
+	int need_fallback = 0;
+	struct zynqmp_aead_req_ctx *rq_ctx = aead_request_ctx(req);
+
+	if (tfm_ctx->authsize != ZYNQMP_AES_AUTH_SIZE) {
+		need_fallback = 1;
+		goto fallback;
+	}
+
+	if (tfm_ctx->keylen != XSECURE_AES_KEY_SIZE_128 &&
+	    tfm_ctx->keylen != XSECURE_AES_KEY_SIZE_256) {
+		need_fallback = 1;
+		goto fallback;
+	}
+
+	if (req->cryptlen < ZYNQMP_AES_MIN_INPUT_BLK_SIZE ||
+	    req->cryptlen % ZYNQMP_AES_WORD_LEN ||
+	    req->assoclen % VERSAL_AES_QWORD_LEN) {
+		need_fallback = 1;
+		goto fallback;
+	}
+	if (rq_ctx->op == ZYNQMP_AES_DECRYPT &&
+	    req->cryptlen <= ZYNQMP_AES_AUTH_SIZE) {
+		need_fallback = 1;
+		goto fallback;
+	}
+fallback:
 	return need_fallback;
 }
 
@@ -264,6 +484,48 @@ static int zynqmp_aes_aead_setkey(struct crypto_aead *aead, const u8 *key,
 		if (keylen == ZYNQMP_AES_KEY_SIZE) {
 			tfm_ctx->keysrc = ZYNQMP_AES_KUP_KEY;
 			memcpy(tfm_ctx->key, key, keylen);
+		}
+	}
+
+	tfm_ctx->fbk_cipher->base.crt_flags &= ~CRYPTO_TFM_REQ_MASK;
+	tfm_ctx->fbk_cipher->base.crt_flags |= (aead->base.crt_flags &
+					CRYPTO_TFM_REQ_MASK);
+
+	return crypto_aead_setkey(tfm_ctx->fbk_cipher, key, keylen);
+}
+
+static int versal_aes_aead_setkey(struct crypto_aead *aead, const u8 *key,
+				  unsigned int keylen)
+{
+	struct crypto_tfm *tfm = crypto_aead_tfm(aead);
+	struct zynqmp_aead_tfm_ctx *tfm_ctx =
+			(struct zynqmp_aead_tfm_ctx *)crypto_tfm_ctx(tfm);
+
+	if (keylen == ZYNQMP_KEY_SRC_SEL_KEY_LEN) {
+		unsigned char keysrc = VERSAL_AES_EFUSE_USER_KEY_0;
+
+		keysrc = *key;
+		if ((keysrc >= VERSAL_AES_EFUSE_USER_KEY_0 &&
+		     keysrc  <= VERSAL_AES_USER_KEY_7) &&
+		     keysrc != VERSAL_AES_KUP_KEY) {
+			tfm_ctx->keysrc = keysrc;
+			return 0;
+		}
+		return -EINVAL;
+	} else {
+		tfm_ctx->keylen = keylen;
+		if (keylen == XSECURE_AES_KEY_SIZE_256 ||
+		    keylen == XSECURE_AES_KEY_SIZE_128) {
+			if (tfm_ctx->keysrc >= VERSAL_AES_USER_KEY_0 &&
+			    tfm_ctx->keysrc <= VERSAL_AES_USER_KEY_7) {
+				memcpy(tfm_ctx->key, key, keylen);
+			}
+		}
+
+		if (tfm_ctx->keysrc < VERSAL_AES_EFUSE_USER_KEY_0 ||
+		    tfm_ctx->keysrc > VERSAL_AES_USER_KEY_7 ||
+		    tfm_ctx->keysrc ==  VERSAL_AES_KUP_KEY) {
+			tfm_ctx->keysrc = VERSAL_AES_USER_KEY_0;
 		}
 	}
 
@@ -383,12 +645,49 @@ static struct xilinx_aead_drv_ctx zynqmp_aes_drv_ctx = {
 	},
 };
 
+static struct xilinx_aead_drv_ctx versal_aes_drv_ctx = {
+	.fallback_check		= versal_fallback_check,
+	.aes_aead_cipher	= versal_aes_aead_cipher,
+	.aead.base = {
+		.setkey		= versal_aes_aead_setkey,
+		.setauthsize	= zynqmp_aes_aead_setauthsize,
+		.encrypt	= zynqmp_aes_aead_encrypt,
+		.decrypt	= zynqmp_aes_aead_decrypt,
+		.init		= aes_aead_init,
+		.exit		= zynqmp_aes_aead_exit,
+		.ivsize		= GCM_AES_IV_SIZE,
+		.maxauthsize	= ZYNQMP_AES_AUTH_SIZE,
+		.base = {
+		.cra_name		= "gcm(aes)",
+		.cra_driver_name	= "versal-aes-gcm",
+		.cra_priority		= 300,
+		.cra_flags		= CRYPTO_ALG_TYPE_AEAD |
+					  CRYPTO_ALG_ASYNC |
+					  CRYPTO_ALG_ALLOCATES_MEMORY |
+					  CRYPTO_ALG_KERN_DRIVER_ONLY |
+					  CRYPTO_ALG_NEED_FALLBACK,
+		.cra_blocksize		= ZYNQMP_AES_BLK_SIZE,
+		.cra_ctxsize		= sizeof(struct zynqmp_aead_tfm_ctx),
+		.cra_module		= THIS_MODULE,
+		}
+	},
+	.aead.op = {
+		.do_one_request = handle_aes_req,
+	},
+};
+
 static struct xlnx_feature aes_feature_map[] = {
 	{
 		.family = ZYNQMP_FAMILY_CODE,
 		.subfamily = ALL_SUB_FAMILY_CODE,
 		.feature_id = PM_SECURE_AES,
 		.data = &zynqmp_aes_drv_ctx,
+	},
+	{
+		.family = VERSAL_FAMILY_CODE,
+		.subfamily = VERSAL_SUB_FAMILY_CODE,
+		.feature_id = XSECURE_API_AES_OP_INIT,
+		.data = &versal_aes_drv_ctx,
 	},
 	{ /* sentinel */ }
 };
