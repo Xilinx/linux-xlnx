94fac10588aae8a0e70c98d966ab644f4c5bb70a
Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
2023-12-08 00:04:48 -0700

    net: axienet: Add support for config structure
    Add config structure in the driver to differentiate different macs.
    
    Signed-off-by: Appana Durga Kedareswara Rao <appana.durga.rao@xilinx.com>
    Acked-by: Harini Katakam <harini.katakam@amd.com>
    Signed-off-by: Suraj Gupta <suraj.gupta2@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    State: pending

--- drivers/net/ethernet/xilinx/xilinx_axienet.h
+++ drivers/net/ethernet/xilinx/xilinx_axienet.h
@@ -433,6 +433,7 @@ struct axidma_bd {
  * @coalesce_count_tx:	Store the irq coalesce on TX side.
  * @coalesce_usec_tx:	IRQ coalesce delay for TX
  * @eth_hasnobuf: Ethernet is configured in Non buf mode.
+ * @axienet_config: Ethernet config structure
  */
 struct axienet_local {
 	struct net_device *ndev;
@@ -491,6 +492,22 @@ struct axienet_local {
 	u32 coalesce_count_tx;
 	u32 coalesce_usec_tx;
 	bool eth_hasnobuf;
+	const struct axienet_config *axienet_config;
+};
+
+/**
+ * enum axienet_ip_type - AXIENET IP/MAC type.
+ *
+ * @XAXIENET_1G:	 IP is 1G MAC
+ *
+ */
+enum axienet_ip_type {
+	XAXIENET_1G = 0,
+};
+
+struct axienet_config {
+	enum axienet_ip_type mactype;
+	void (*setoptions)(struct net_device *ndev, u32 options);
 };
 
 /**
