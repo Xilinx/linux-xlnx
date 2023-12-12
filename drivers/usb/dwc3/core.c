6cec17bf44fbcd337dbd6766c5a8231f76eb0ef8
Piyush Mehta <piyush.mehta@amd.com>
2023-12-12 10:08:45 +0530

    usb: dwc3: core: disable 3.0 clock when operating in 2.0 device mode
    The GUCTL1.DEV_FORCE_20_CLK_FOR_30_CLK bit enable the feature of internal
    2.0(utmi/ulpi) clock to be routed as the 3.0 (pipe) clock. This feature is
    applicable when core is operating in 2.0 device mode.
    
    When this bit is set in host mode and core is in 2.0 device mode (maximum
    speed = high-speed) then usb super speed devices not detected on host.
    
    To address the above issue added usb device mode conditional check. 
    
    Fixes: 62b20e6e0dde ("usb: dwc3: core: do not use 3.0 clock when operating in 2.0 mode")
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    Link: https://lkml.kernel.org/stable/20231005113624.8329-1-piyush.mehta@amd.com/
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    State: waiting

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1576,7 +1576,8 @@ int dwc3_core_init(struct dwc3 *dwc)
 
 		if (DWC3_VER_IS_WITHIN(DWC3, 290A, ANY)) {
 			if (dwc->maximum_speed == USB_SPEED_FULL ||
-			    dwc->maximum_speed == USB_SPEED_HIGH)
+			    dwc->maximum_speed == USB_SPEED_HIGH ||
+			    dwc->dr_mode == USB_DR_MODE_PERIPHERAL)
 				reg |= DWC3_GUCTL1_DEV_FORCE_20_CLK_FOR_30_CLK;
 			else
 				reg &= ~DWC3_GUCTL1_DEV_FORCE_20_CLK_FOR_30_CLK;
