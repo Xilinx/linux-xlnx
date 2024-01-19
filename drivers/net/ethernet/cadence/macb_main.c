4a9aa5b144a6d3d77049d5f3af58fe810a130b96
Vineeth Karumanchi <vineeth.karumanchi@amd.com>
2024-01-19 10:58:07 +0530

    net: macb: Fix missing wakeup attribute on zu+ and versal
    In versal and zynqmp macb_config "," was
    used to terminate the caps attribute and as a result WOL
    functionality is broken. To fix use "|" to extend caps to
    support MACB_CAPS_WOL in versal and zynqmp.
    
    Fixes: b7409eb64947 ("net: macb: Use WOL via ARP")
    Signed-off-by: Vineeth Karumanchi <vineeth.karumanchi@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -4854,7 +4854,7 @@ static const struct macb_config np4_config = {
 static const struct macb_config zynqmp_config = {
 	.caps = MACB_CAPS_GIGABIT_MODE_AVAILABLE |
 		MACB_CAPS_JUMBO |
-		MACB_CAPS_GEM_HAS_PTP | MACB_CAPS_BD_RD_PREFETCH,
+		MACB_CAPS_GEM_HAS_PTP | MACB_CAPS_BD_RD_PREFETCH |
 		MACB_CAPS_WOL,
 	.dma_burst_length = 16,
 	.clk_init = macb_clk_init,
@@ -4905,7 +4905,7 @@ static const struct macb_config sama7g5_emac_config = {
 
 static const struct macb_config versal_config = {
 	.caps = MACB_CAPS_GIGABIT_MODE_AVAILABLE | MACB_CAPS_JUMBO |
-		MACB_CAPS_GEM_HAS_PTP | MACB_CAPS_BD_RD_PREFETCH | MACB_CAPS_NEED_TSUCLK,
+		MACB_CAPS_GEM_HAS_PTP | MACB_CAPS_BD_RD_PREFETCH | MACB_CAPS_NEED_TSUCLK |
 		MACB_CAPS_WOL,
 	.dma_burst_length = 16,
 	.clk_init = macb_clk_init,
