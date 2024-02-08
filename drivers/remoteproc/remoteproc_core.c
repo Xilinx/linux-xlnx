b928a4334e78d3a3cbec2db0803bfc190159f19b
Mathieu Poirier <mathieu.poirier@linaro.org>
2024-02-08 04:05:30 -1200

    remoteproc: Make rproc_get_by_phandle() work for clusters
    Multi-cluster remoteproc designs typically have the following DT
    declaration:
    
            remoteproc-cluster {
                    compatible = "soc,remoteproc-cluster";
    
                    core0: core0 {
                            compatible = "soc,remoteproc-core"
                            memory-region;
                            sram;
                    };
    
                    core1: core1 {
                            compatible = "soc,remoteproc-core"
                            memory-region;
                            sram;
                    }
            };
    
    A driver exists for the cluster rather than the individual cores
    themselves so that operation mode and HW specific configurations
    applicable to the cluster can be made.
    
    Because the driver exists at the cluster level and not the individual
    core level, function rproc_get_by_phandle() fails to return the
    remoteproc associated with the phandled it is called for.
    
    This patch enhances rproc_get_by_phandle() by looking for the cluster's
    driver when the driver for the immediate remoteproc's parent is not
    found.
    
    Reported-by: Ben Levinsky <ben.levinsky@xilinx.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Co-developed-by: Tarak Reddy <tarak.reddy@amd.com>
    Signed-off-by: Tarak Reddy <tarak.reddy@amd.com>
    Co-developed-by: Tanmay Shah <tanmay.shah@amd.com>
    Signed-off-by: Tanmay Shah <tanmay.shah@amd.com>
    Link: https://lore.kernel.org/r/20240130154849.1018666-1-tanmay.shah@amd.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    State: waiting

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -2138,14 +2138,10 @@ struct rproc *rproc_get_by_phandle(phandle phandle)
 			 * look for the driver associated with the cluster.
 			 */
 			if (!driver) {
-				cluster_pdev = of_find_device_by_node(np->parent);
-				if (!cluster_pdev) {
-					dev_err(&r->dev, "can't get parent\n");
+				if (r->dev.parent->parent)
+					driver = r->dev.parent->parent->driver;
+				if (!driver)
 					break;
-				}
-
-				driver = cluster_pdev->dev.driver;
-				put_device(&cluster_pdev->dev);
 			}
 
 			if (!try_module_get(driver->owner)) {
@@ -2559,19 +2555,11 @@ EXPORT_SYMBOL(rproc_free);
  */
 void rproc_put(struct rproc *rproc)
 {
-	struct platform_device *cluster_pdev;
+	if (rproc->dev.parent->driver)
+		module_put(rproc->dev.parent->driver->owner);
+	else
+		module_put(rproc->dev.parent->parent->driver->owner);
 
-	if (rproc->dev.parent) {
-		if (rproc->dev.parent->driver) {
-			module_put(rproc->dev.parent->driver->owner);
-		} else {
-			cluster_pdev = of_find_device_by_node(rproc->dev.parent->of_node->parent);
-			if (cluster_pdev) {
-				module_put(cluster_pdev->dev.driver->owner);
-				put_device(&cluster_pdev->dev);
-			}
-		}
-	}
 	put_device(&rproc->dev);
 }
 EXPORT_SYMBOL(rproc_put);
