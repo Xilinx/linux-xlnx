a6f2a436e9d6aa96013814cface08d8602638123
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2024-06-21 17:40:41 -0700

    x86/its: Enumerate Indirect Target Selection (ITS) bug
    commit 159013a7ca18c271ff64192deb62a689b622d860 upstream.
    
    ITS bug in some pre-Alderlake Intel CPUs may allow indirect branches in the
    first half of a cache line get predicted to a target of a branch located in
    the second half of the cache line.
    
    Set X86_BUG_ITS on affected CPUs. Mitigation to follow in later commits.
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -526,4 +526,5 @@
 #define X86_BUG_RFDS			X86_BUG(1*32 + 2) /* "rfds" CPU is vulnerable to Register File Data Sampling */
 #define X86_BUG_BHI			X86_BUG(1*32 + 3) /* "bhi" CPU is affected by Branch History Injection */
 #define X86_BUG_IBPB_NO_RET	   	X86_BUG(1*32 + 4) /* "ibpb_no_ret" IBPB omits return target predictions */
+#define X86_BUG_ITS			X86_BUG(1*32 + 5) /* "its" CPU is affected by Indirect Target Selection */
 #endif /* _ASM_X86_CPUFEATURES_H */
