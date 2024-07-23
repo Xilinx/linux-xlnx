d55474432d98dda962422869dac934bbb49c5b2f
Mounik Katikala <mounik.katikala@amd.com>
2024-07-23 00:17:07 -1200

    v4l: xilinx: isppipeline: Add support for DEGAMMA
    Add support for DEGAMMA ISP configuration.
    
    Degamma also known as linearization is typically designed to linearize
    the input from a sensor (or any pre-processing IP) in order to facilitate
    ISP processing that operates on a linear domain, such as noise reduction.
    While non-linearization comes in several forms, this module is served to
    linearize only simple curves.
    
    This implementation supports Bayer Raw data of 8 and 16 bits
    per pixel with a maximum of 64 knee points PWL mapping.
    
    "select_degamma" ranges from 0 to 1 is the configurable option.
    
    Signed-off-by: Mounik Katikala <mounik.katikala@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>

---
+++ drivers/media/platform/xilinx/xilinx-isp-params.h
@@ -0,0 +1,59 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2024, Advanced Micro Devices, Inc.
+ */
+
+#ifndef __XILINX_ISP_PARAMS_H__
+#define __XILINX_ISP_PARAMS_H__
+
+#define XISP_DEGAMMA_COLOR_ID (3)
+#define XISP_DEGAMMA_KNEE_POINTS (8)
+#define XISP_DEGAMMA_PARAMS (3)
+
+/*
+ * XISP_DEGAMMA_COLOR_ID = COLOR ID (R, G, B)
+ * XISP_DEGAMMA_KNEE_POINTS = Number of knee points (we support 4 and 8)
+ * XISP_DEGAMMA_PARAMS =  Contains 3 values (max_value for range, slope and constant)
+ *
+ * The function xisp_set_degamma_entries function writes degamma values from the
+ * provided 3D array to the device's registers.
+ * The degamma values are written sequentially starting from the address specified by
+ * degamma_base.
+ * Each value is written to an offset that increments by 4 bytes (32 bits) for each
+ * subsequent value.
+ */
+static const u32 xisp_degamma_choices[2][XISP_DEGAMMA_COLOR_ID][XISP_DEGAMMA_KNEE_POINTS]
+				     [XISP_DEGAMMA_PARAMS] = {{
+	/* 8-bit */
+	{
+		{ 32, 1311, 0 },   { 64, 4915, 7 },   { 96, 9011, 23 },  { 128, 13414, 49 },
+		{ 160, 18918, 84 }, { 192, 22529, 132 }, { 224, 28672, 200 }, { 256, 32768, 256 }
+	},
+	{
+		{ 32, 1311, 0 },   { 64, 4915, 7 },   { 96, 9011, 23 },  { 128, 13414, 49 },
+		{ 160, 18918, 84 }, { 192, 22529, 132 }, { 224, 28672, 200 }, { 256, 32768, 256 }
+	},
+	{
+		{ 32, 1311, 0 },   { 64, 4915, 7 },   { 96, 9011, 23 },  { 128, 13414, 49 },
+		{ 160, 18918, 84 }, { 192, 22529, 132 }, { 224, 28672, 200 }, { 256, 32768, 256 }
+	}
+}, {
+	/* 16-bit */
+	{
+		{ 8192, 1345, 0 },   { 16384, 4853, 1749 }, { 24576, 8933, 5825 },
+		{ 32768, 13365, 12476 }, { 40960, 18023, 21782 }, { 49152, 22938, 34162 },
+		{ 57344, 28088, 49506 }, { 65536, 32768, 65536 }
+	},
+	{
+		{ 8192, 1345, 0 },   { 16384, 4853, 1749 }, { 24576, 8933, 5825 },
+		{ 32768, 13365, 12476 }, { 40960, 18023, 21782 }, { 49152, 22938, 34162 },
+		{ 57344, 28088, 49506 }, { 65536, 32768, 65536 }
+	},
+	{
+		{ 8192, 1345, 0 },   { 16384, 4853, 1749 }, { 24576, 8933, 5825 },
+		{ 32768, 13365, 12476 }, { 40960, 18023, 21782 }, { 49152, 22938, 34162 },
+		{ 57344, 28088, 49506 }, { 65536, 32768, 65536 }
+	}
+}};
+
+#endif /* __XILINX_ISP_PARAMS_H__ */
