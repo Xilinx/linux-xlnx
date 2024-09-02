64c6dddcaa127a9a3c268c5f36f39437cb336455
Harsh Jain <h.jain@amd.com>
2024-09-02 15:15:47 +0530

    crypto: xilinx: Cleanup platform device resource on module exit
    Fix warn_on trace observed on repeated driver load/unload because
    platform_device_unregister() was missing.
    
    Signed-off-by: Harsh Jain <h.jain@amd.com>
    Message-ID: <20240902094548.30851-2-h.jain@amd.com>

--- drivers/crypto/xilinx/zynqmp-rsa.c
+++ drivers/crypto/xilinx/zynqmp-rsa.c
@@ -242,19 +242,20 @@ static struct platform_driver xilinx_rsa_driver = {
 	},
 };
 
+static struct platform_device *platform_dev;
+
 static int __init rsa_driver_init(void)
 {
-	struct platform_device *pdev;
 	int ret;
 
 	ret = platform_driver_register(&xilinx_rsa_driver);
 	if (ret)
 		return ret;
 
-	pdev = platform_device_register_simple(xilinx_rsa_driver.driver.name,
+	platform_dev = platform_device_register_simple(xilinx_rsa_driver.driver.name,
 					       0, NULL, 0);
-	if (IS_ERR(pdev)) {
-		ret = PTR_ERR(pdev);
+	if (IS_ERR(platform_dev)) {
+		ret = PTR_ERR(platform_dev);
 		platform_driver_unregister(&xilinx_rsa_driver);
 	}
 
@@ -263,6 +264,7 @@ static int __init rsa_driver_init(void)
 
 static void __exit rsa_driver_exit(void)
 {
+	platform_device_unregister(platform_dev);
 	platform_driver_unregister(&xilinx_rsa_driver);
 }
 
