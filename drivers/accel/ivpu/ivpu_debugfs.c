0923a7d55a85179744926b7c11768a81679cc4d4
Jacek Lawrynowicz <jacek.lawrynowicz@linux.intel.com>
2024-09-30 21:52:55 +0200

    accel/ivpu: Refactor functions in ivpu_fw_log.c
    commit 1fc1251149a76d3b75d7f4c94d9c4e081b7df6b4 upstream.
    
    Make function names more consistent and (arguably) readable in
    fw log code. Add fw_log_print_all_in_bo() that remove duplicated code in
    ivpu_fw_log_print().
    
    Reviewed-by: Tomasz Rusinowicz <tomasz.rusinowicz@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240930195322.461209-5-jacek.lawrynowicz@linux.intel.com
    Signed-off-by: Jacek Lawrynowicz <jacek.lawrynowicz@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/accel/ivpu/ivpu_debugfs.c
+++ drivers/accel/ivpu/ivpu_debugfs.c
@@ -201,7 +201,7 @@ fw_log_fops_write(struct file *file, const char __user *user_buf, size_t size, l
 	if (!size)
 		return -EINVAL;
 
-	ivpu_fw_log_clear(vdev);
+	ivpu_fw_log_mark_read(vdev);
 	return size;
 }
 
