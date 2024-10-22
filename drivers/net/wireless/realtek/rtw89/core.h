61ad3210255b3598cec98ff413cdbffaca55900a
Zong-Zhe Yang <kevin_yang@realtek.com>
2024-10-22 16:31:05 +0800

    wifi: rtw89: tweak setting of channel and TX power for MLO
    [ Upstream commit 2305ebc1835b1ca921045b4f0941e82edde3249b ]
    
    Setting of channel and TX power depend on channel contexts, but original
    code cannot handle combination of MCC (multi-channel concurrency) and MLO
    well. So according to active interfaces, we generate a table for current
    channel contexts. And then based on entity mode, we get the corresponding
    channel context to apply during channel or TX power setting. When MLO is
    supported, there will be dual-PHY and we will apply the channel context of
    the 2nd link to the 2nd PHY.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20241022083106.149252-5-pkshih@realtek.com
    Stable-dep-of: e47f0a589854 ("wifi: rtw89: fix proceeding MCC with wrong scanning state after sequence changes")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -3424,6 +3424,8 @@ enum rtw89_roc_state {
 	RTW89_ROC_MGMT,
 };
 
+#define RTW89_ROC_BY_LINK_INDEX 0
+
 struct rtw89_roc {
 	struct ieee80211_channel chan;
 	struct delayed_work roc_work;
@@ -4619,7 +4621,7 @@ enum rtw89_chanctx_changes {
 };
 
 enum rtw89_entity_mode {
-	RTW89_ENTITY_MODE_SCC,
+	RTW89_ENTITY_MODE_SCC_OR_SMLD,
 	RTW89_ENTITY_MODE_MCC_PREPARE,
 	RTW89_ENTITY_MODE_MCC,
 
@@ -4634,6 +4636,8 @@ enum rtw89_entity_mode {
 struct rtw89_entity_mgnt {
 	struct list_head active_list;
 	struct rtw89_vif *active_roles[RTW89_MAX_INTERFACE_NUM];
+	enum rtw89_chanctx_idx chanctx_tbl[RTW89_MAX_INTERFACE_NUM]
+					  [__RTW89_MLD_MAX_LINK_NUM];
 };
 
 struct rtw89_chanctx {
@@ -6369,6 +6373,15 @@ const struct rtw89_chan_rcd *rtw89_chan_rcd_get(struct rtw89_dev *rtwdev,
 	struct rtw89_hal *hal = &rtwdev->hal;
 
 	return &hal->chanctx[idx].rcd;
+}
+
+static inline
+const struct rtw89_chan_rcd *rtw89_chan_rcd_get_by_chan(const struct rtw89_chan *chan)
+{
+	const struct rtw89_chanctx *chanctx =
+		container_of_const(chan, struct rtw89_chanctx, chan);
+
+	return &chanctx->rcd;
 }
 
 static inline
