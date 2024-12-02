681ee24cd5a2e4cafca449fecbcddba4fbdb080d
Darrick J. Wong <djwong@kernel.org>
2024-12-02 10:57:36 -0800

    xfs: don't lose solo dquot update transactions
    commit 07137e925fa951646325762bda6bd2503dfe64c6 upstream.
    
    Quota counter updates are tracked via incore objects which hang off the
    xfs_trans object.  These changes are then turned into dirty log items in
    xfs_trans_apply_dquot_deltas just prior to commiting the log items to
    the CIL.
    
    However, updating the incore deltas do not cause XFS_TRANS_DIRTY to be
    set on the transaction.  In other words, a pure quota counter update
    will be silently discarded if there are no other dirty log items
    attached to the transaction.
    
    This is currently not the case anywhere in the filesystem because quota
    updates always dirty at least one other metadata item, but a subsequent
    bug fix will add dquot log item precommits, so we actually need a dirty
    dquot log item prior to xfs_trans_run_precommits.  Also let's not leave
    a logic bomb.
    
    Cc: <stable@vger.kernel.org> # v2.6.35
    Fixes: 0924378a689ccb ("xfs: split out iclog writing from xfs_trans_commit()")
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_trans.c
+++ fs/xfs/xfs_trans.c
@@ -840,6 +840,7 @@ __xfs_trans_commit(
 	 */
 	if (tp->t_flags & XFS_TRANS_SB_DIRTY)
 		xfs_trans_apply_sb_deltas(tp);
+	xfs_trans_apply_dquot_deltas(tp);
 
 	error = xfs_trans_run_precommits(tp);
 	if (error)
@@ -868,11 +869,6 @@ __xfs_trans_commit(
 
 	ASSERT(tp->t_ticket != NULL);
 
-	/*
-	 * If we need to update the superblock, then do it now.
-	 */
-	xfs_trans_apply_dquot_deltas(tp);
-
 	xlog_cil_commit(log, tp, &commit_seq, regrant);
 
 	xfs_trans_free(tp);
@@ -898,7 +894,7 @@ out_unreserve:
 	 * the dqinfo portion to be.  All that means is that we have some
 	 * (non-persistent) quota reservations that need to be unreserved.
 	 */
-	xfs_trans_unreserve_and_mod_dquots(tp);
+	xfs_trans_unreserve_and_mod_dquots(tp, true);
 	if (tp->t_ticket) {
 		if (regrant && !xlog_is_shutdown(log))
 			xfs_log_ticket_regrant(log, tp->t_ticket);
@@ -992,7 +988,7 @@ xfs_trans_cancel(
 	}
 #endif
 	xfs_trans_unreserve_and_mod_sb(tp);
-	xfs_trans_unreserve_and_mod_dquots(tp);
+	xfs_trans_unreserve_and_mod_dquots(tp, false);
 
 	if (tp->t_ticket) {
 		xfs_log_ticket_ungrant(log, tp->t_ticket);
