59391c9129d06d5a2ba2e450f15661108982336f
Kunal Rane <kunal.rane@amd.com>
2024-12-11 16:47:22 -1200

    v4l: xilinx: hls-common: Add missing SPDX license identifiers
    Add missing SPDX license identifiers in platform data header to be
    SPDX compliant. Fixes below checkpatch warning.
    
    WARNING: Missing or malformed SPDX-License-Identifier tag in line 1
    +/**
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    Message-ID: <20241212044724.63794-6-kunal.rane@amd.com>

--- drivers/media/platform/xilinx/xilinx-hls-common.h
+++ drivers/media/platform/xilinx/xilinx-hls-common.h
@@ -1,13 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Xilinx HLS common header
  *
  * Copyright (C) 2013-2015 Xilinx, Inc.
  *
  * Contacts: Radhey Shyam Pandey <radheys@xilinx.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #ifndef __XILINX_HLS_COMMON_H__
