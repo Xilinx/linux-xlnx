5c16e0f4a609d354b1b52ecfbd2af44f549ea5ba
Sai Krishna Potthuri <sai.krishna.potthuri@amd.com>
2024-12-13 06:05:53 -1200

    spi: spi-cadence-quadspi: Add support for Stacked mode
    Add Stacked mode support for Octal SPI.
    nor->spi-mem->spi->cs_index_mask is updated with the CS information
    during runtime based on the address for read, write and erase operations.
    spi-cadence-quadspi driver configures the chipselect based on
    the 'spi->cs_index_mask'.
    
    Signed-off-by: Sai Krishna Potthuri <sai.krishna.potthuri@amd.com>
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@amd.com>
    Reviewed-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    State: pending
    Message-ID: <20241213180601.1544568-8-amit.kumar-mahapatra@amd.com>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -340,6 +340,8 @@ struct cqspi_driver_platdata {
 #define CQSPI_TX_TAP_MASTER		0x1E
 #define CQSPI_MAX_DLL_TAPS		127
 
+#define	CQSPI_SELECT_LOWER_CS		BIT(0)
+
 static int cqspi_wait_for_bit(const struct cqspi_driver_platdata *ddata,
 			      void __iomem *reg, const u32 mask, bool clr,
 			      bool busywait)
@@ -1832,6 +1834,11 @@ static int cqspi_exec_mem_op(struct spi_mem *mem, const struct spi_mem_op *op)
 	}
 
 	f_pdata = &cqspi->f_pdata[spi_get_chipselect(mem->spi, 0)];
+
+	if (mem->spi->cs_index_mask & CQSPI_SELECT_LOWER_CS)
+		f_pdata->cs = spi_get_chipselect(mem->spi, 0);
+	else
+		f_pdata->cs = spi_get_chipselect(mem->spi, 1);
 
 	if (op->cmd.dtr &&
 	    (!op->addr.nbytes || op->addr.dtr) &&
