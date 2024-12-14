26862f0223ef656cd49b2f30a71bb9de98258e6b
Min-Hua Chen <minhuadotchen@gmail.com>
2024-12-14 16:17:06 +0800

    drm/rockchip: vop2: include rockchip_drm_drv.h
    [ Upstream commit 77b1ccb2a27c7b3b118a03bf1730def92070d31b ]
    
    Move rockchip_drm_drv.h in rockchip_drm_vop2.h to fix the follow
    sparse warning:
    
    ARCH=arm64 LLVM=1 make C=1 CF='-fdiagnostic-prefix -D__CHECK_ENDIAN__'
    mrproper defconfig all  -j12
    
    drivers/gpu/drm/rockchip/rockchip_vop2_reg.c:502:24: sparse:
    warning: symbol 'vop2_platform_driver' was not declared. Should it
    be static?
    
    It is also beneficial for the upcoming support for rk3576.
    
    Fixes: 604be85547ce ("drm/rockchip: Add VOP2 driver")
    Signed-off-by: Min-Hua Chen <minhuadotchen@gmail.com>
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
    Reviewed-by: Min-Hua Chen <minhuadotchen@gmail.com>
    Tested-by: Detlev Casanova <detlev.casanova@collabora.com>
    Tested-by: Michael Riesch <michael.riesch@wolfvision.net> # on RK3568
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241214081719.3330518-8-andyshrk@163.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -33,7 +33,6 @@
 #include <uapi/linux/videodev2.h>
 #include <dt-bindings/soc/rockchip,vop2.h>
 
-#include "rockchip_drm_drv.h"
 #include "rockchip_drm_gem.h"
 #include "rockchip_drm_fb.h"
 #include "rockchip_drm_vop2.h"
