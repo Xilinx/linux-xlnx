b38f66273f89b5851fb8b667d0503f7a0eb7392d
Luca Ceresoli <luca.ceresoli@bootlin.com>
2024-12-19 10:39:46 +0100

    gpio: pca953x: log an error when failing to get the reset GPIO
    [ Upstream commit 7cef813a91c468253c80633891393478b9f2c966 ]
    
    When the dirver fails getting this GPIO, it fails silently. Log an error
    message to make debugging a lot easier by just reading dmesg.
    
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Fixes: 054ccdef8b28 ("gpio: pca953x: Add optional reset gpio control")
    Link: https://lore.kernel.org/r/20241219-pca953x-log-no-reset-gpio-v1-1-9aa7bcc45ead@bootlin.com
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -1107,7 +1107,8 @@ static int pca953x_probe(struct i2c_client *client)
 		reset_gpio = devm_gpiod_get_optional(&client->dev, "reset",
 						     GPIOD_OUT_LOW);
 		if (IS_ERR(reset_gpio))
-			return PTR_ERR(reset_gpio);
+			return dev_err_probe(dev, PTR_ERR(reset_gpio),
+					     "Failed to get reset gpio\n");
 	}
 
 	chip->client = client;
