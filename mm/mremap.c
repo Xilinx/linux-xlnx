310ac886d68de661c3a334198d8604b722d7fdf8
Ryan Roberts <ryan.roberts@arm.com>
2025-01-07 14:47:52 +0000

    mm: clear uffd-wp PTE/PMD state on mremap()
    commit 0cef0bb836e3cfe00f08f9606c72abd72fe78ca3 upstream.
    
    When mremap()ing a memory region previously registered with userfaultfd as
    write-protected but without UFFD_FEATURE_EVENT_REMAP, an inconsistency in
    flag clearing leads to a mismatch between the vma flags (which have
    uffd-wp cleared) and the pte/pmd flags (which do not have uffd-wp
    cleared).  This mismatch causes a subsequent mprotect(PROT_WRITE) to
    trigger a warning in page_table_check_pte_flags() due to setting the pte
    to writable while uffd-wp is still set.
    
    Fix this by always explicitly clearing the uffd-wp pte/pmd flags on any
    such mremap() so that the values are consistent with the existing clearing
    of VM_UFFD_WP.  Be careful to clear the logical flag regardless of its
    physical form; a PTE bit, a swap PTE bit, or a PTE marker.  Cover PTE,
    huge PMD and hugetlb paths.
    
    Link: https://lkml.kernel.org/r/20250107144755.1871363-2-ryan.roberts@arm.com
    Co-developed-by: Mikołaj Lenczewski <miko.lenczewski@arm.com>
    Signed-off-by: Mikołaj Lenczewski <miko.lenczewski@arm.com>
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Closes: https://lore.kernel.org/linux-mm/810b44a8-d2ae-4107-b665-5a42eae2d948@arm.com/
    Fixes: 63b2d4174c4a ("userfaultfd: wp: add the writeprotect API to userfaultfd ioctl")
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/mremap.c
+++ mm/mremap.c
@@ -138,6 +138,7 @@ static int move_ptes(struct vm_area_struct *vma, pmd_t *old_pmd,
 		struct vm_area_struct *new_vma, pmd_t *new_pmd,
 		unsigned long new_addr, bool need_rmap_locks)
 {
+	bool need_clear_uffd_wp = vma_has_uffd_without_event_remap(vma);
 	struct mm_struct *mm = vma->vm_mm;
 	pte_t *old_pte, *new_pte, pte;
 	spinlock_t *old_ptl, *new_ptl;
@@ -207,7 +208,18 @@ static int move_ptes(struct vm_area_struct *vma, pmd_t *old_pmd,
 			force_flush = true;
 		pte = move_pte(pte, old_addr, new_addr);
 		pte = move_soft_dirty_pte(pte);
-		set_pte_at(mm, new_addr, new_pte, pte);
+
+		if (need_clear_uffd_wp && pte_marker_uffd_wp(pte))
+			pte_clear(mm, new_addr, new_pte);
+		else {
+			if (need_clear_uffd_wp) {
+				if (pte_present(pte))
+					pte = pte_clear_uffd_wp(pte);
+				else if (is_swap_pte(pte))
+					pte = pte_swp_clear_uffd_wp(pte);
+			}
+			set_pte_at(mm, new_addr, new_pte, pte);
+		}
 	}
 
 	arch_leave_lazy_mmu_mode();
@@ -269,6 +281,15 @@ static bool move_normal_pmd(struct vm_area_struct *vma, unsigned long old_addr,
 	if (WARN_ON_ONCE(!pmd_none(*new_pmd)))
 		return false;
 
+	/* If this pmd belongs to a uffd vma with remap events disabled, we need
+	 * to ensure that the uffd-wp state is cleared from all pgtables. This
+	 * means recursing into lower page tables in move_page_tables(), and we
+	 * can reuse the existing code if we simply treat the entry as "not
+	 * moved".
+	 */
+	if (vma_has_uffd_without_event_remap(vma))
+		return false;
+
 	/*
 	 * We don't have to worry about the ordering of src and dst
 	 * ptlocks because exclusive mmap_lock prevents deadlock.
@@ -322,6 +343,15 @@ static bool move_normal_pud(struct vm_area_struct *vma, unsigned long old_addr,
 	 * should have released it.
 	 */
 	if (WARN_ON_ONCE(!pud_none(*new_pud)))
+		return false;
+
+	/* If this pud belongs to a uffd vma with remap events disabled, we need
+	 * to ensure that the uffd-wp state is cleared from all pgtables. This
+	 * means recursing into lower page tables in move_page_tables(), and we
+	 * can reuse the existing code if we simply treat the entry as "not
+	 * moved".
+	 */
+	if (vma_has_uffd_without_event_remap(vma))
 		return false;
 
 	/*
