61e931ee145eeab8196e585ff4334870b130b744
Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
2025-01-30 08:52:52 +0000

    cpufreq: amd-pstate: Remove unnecessary driver_lock in set_boost
    [ Upstream commit db1cafc77aaaf871509da06f4a864e9af6d6791f ]
    
    set_boost is a per-policy function call, hence a driver wide lock is
    unnecessary. Also this mutex_acquire can collide with the mutex_acquire
    from the mode-switch path in status_store(), which can lead to a
    deadlock. So, remove it.
    
    Signed-off-by: Dhananjay Ugwekar <dhananjay.ugwekar@amd.com>
    Acked-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/cpufreq/amd-pstate.c
+++ drivers/cpufreq/amd-pstate.c
@@ -696,7 +696,6 @@ static int amd_pstate_set_boost(struct cpufreq_policy *policy, int state)
 		pr_err("Boost mode is not supported by this processor or SBIOS\n");
 		return -EOPNOTSUPP;
 	}
-	guard(mutex)(&amd_pstate_driver_lock);
 
 	ret = amd_pstate_cpu_boost_update(policy, state);
 	WRITE_ONCE(cpudata->boost_state, !ret ? state : false);
