521e55c2b0d6028861ac0a2d06aa57bb0e3ac486
Vitaliy Shevtsov <v.shevtsov@mt-integration.ru>
2025-01-31 20:26:55 +0500

    wifi: nl80211: reject cooked mode if it is set along with other flags
    commit 49f27f29446a5bfe633dd2cc0cfebd48a1a5e77f upstream.
    
    It is possible to set both MONITOR_FLAG_COOK_FRAMES and MONITOR_FLAG_ACTIVE
    flags simultaneously on the same monitor interface from the userspace. This
    causes a sub-interface to be created with no IEEE80211_SDATA_IN_DRIVER bit
    set because the monitor interface is in the cooked state and it takes
    precedence over all other states. When the interface is then being deleted
    the kernel calls WARN_ONCE() from check_sdata_in_driver() because of missing
    that bit.
    
    Fix this by rejecting MONITOR_FLAG_COOK_FRAMES if it is set along with
    other flags.
    
    Found by Linux Verification Center (linuxtesting.org) with Syzkaller.
    
    Fixes: 66f7ac50ed7c ("nl80211: Add monitor interface configuration flags")
    Cc: stable@vger.kernel.org
    Reported-by: syzbot+2e5c1e55b9e5c28a3da7@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=2e5c1e55b9e5c28a3da7
    Signed-off-by: Vitaliy Shevtsov <v.shevtsov@mt-integration.ru>
    Link: https://patch.msgid.link/20250131152657.5606-1-v.shevtsov@mt-integration.ru
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -4218,6 +4218,11 @@ static int parse_monitor_flags(struct nlattr *nla, u32 *mntrflags)
 		if (flags[flag])
 			*mntrflags |= (1<<flag);
 
+	/* cooked monitor mode is incompatible with other modes */
+	if (*mntrflags & MONITOR_FLAG_COOK_FRAMES &&
+	    *mntrflags != MONITOR_FLAG_COOK_FRAMES)
+		return -EOPNOTSUPP;
+
 	*mntrflags |= MONITOR_FLAG_CHANGED;
 
 	return 0;
