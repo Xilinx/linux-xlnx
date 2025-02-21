fab2c75fde0c08972fd2907f1638f7838e7686ed
Rama devi Veggalam <rama.devi.veggalam@amd.com>
2025-02-21 16:39:55 +0530

    firmware: xilinx: Add support for Xilsem SSIT operations
    Add ATF EEMI call support for Xilsem scan control operations,
    read configuration, status, crc/ecc, frame details for
    Versal SSIT device.
    
    Signed-off-by: Rama devi Veggalam <rama.devi.veggalam@amd.com>
    Message-ID: <1740136197-4893-3-git-send-email-rama.devi.veggalam@amd.com>
    State: pending

--- include/linux/firmware/xlnx-zynqmp-sem.h
+++ include/linux/firmware/xlnx-zynqmp-sem.h
@@ -13,30 +13,54 @@
 #define PM_XSEM_CRAM_ERRINJ		0x304
 #define PM_XSEM_RD_CONFIG		0x309
 #define PM_XSEM_CRAM_RD_ECC		0x30B
+#define PM_XSEM_SSIT_GET_CRC		0x30C
+#define PM_XSEM_SSIT_RD_STS		0x30D
+#define PM_XSEM_SSIT_GET_FRAMES		0x30E
 
 #if IS_REACHABLE(CONFIG_ZYNQMP_FIRMWARE)
-int zynqmp_pm_xilsem_cntrl_ops(u32 cmd, u32 *const response);
-int zynqmp_pm_xilsem_cram_errinj(u32 frame, u32 qword, u32 bit, u32 row, u32 *const response);
-int zynqmp_pm_xilsem_cram_readecc(u32 frame, u32 row, u32 *const response);
-int zynqmp_pm_xilsem_read_cfg(u32 *const response);
+int zynqmp_pm_xilsem_cntrl_ops(u32 cmd, u32 slrid, u32 *const response);
+int zynqmp_pm_xilsem_cram_errinj(u32 slrid, u32 frame, u32 qword, u32 bit, u32 row,
+				 u32 *const response);
+int zynqmp_pm_xilsem_cram_readecc(u32 slrid, u32 frame, u32 row, u32 *const response);
+int zynqmp_pm_xilsem_read_cfg(u32 slrid, u32 *const response);
+int zynqmp_pm_xilsem_read_ssit_status(u32 slrid, u32 bufaddr, u32 *const response);
+int zynqmp_pm_xilsem_cram_getcrc(u32 slrid, u32 rowindex, u32 *const response);
+int zynqmp_pm_xilsem_cram_ssit_totframes(u32 slrid, u32 row, u32 framecnt,
+					 u32 *const response);
 #else
-static inline int zynqmp_pm_xilsem_cntrl_ops(u32 cmd, u32 *const response)
+static inline int zynqmp_pm_xilsem_cntrl_ops(u32 cmd, u32 slrid, u32 *const response)
 {
 	return -ENODEV;
 }
 
-static inline int zynqmp_pm_xilsem_cram_readecc(u32 frame, u32 row, u32 *const response)
+static inline int zynqmp_pm_xilsem_cram_errinj(u32 slrid, u32 frame, u32 qword, u32 bit, u32 row,
+					       u32 *const response)
 {
 	return -ENODEV;
 }
 
-static inline int zynqmp_pm_xilsem_cram_errinj(u32 frame, u32 qword, u32 bit,
-					       u32 row, u32 *const response)
+static inline int zynqmp_pm_xilsem_cram_readecc(u32 slrid, u32 frame, u32 row, u32 *const response)
 {
 	return -ENODEV;
 }
 
-static inline int zynqmp_pm_xilsem_read_cfg(u32 *const response)
+static inline int zynqmp_pm_xilsem_read_cfg(u32 slrid, u32 *const response)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_xilsem_read_ssit_status(u32 slrid, u32 bufaddr, u32 *const response)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_xilsem_cram_getcrc(u32 slrid, u32 rowindex, u32 *const response)
+{
+	return -ENODEV;
+}
+
+static inline int zynqmp_pm_xilsem_cram_ssit_totframes(u32 slrid, u32 row, u32 framecnt,
+						       u32 *const response)
 {
 	return -ENODEV;
 }
