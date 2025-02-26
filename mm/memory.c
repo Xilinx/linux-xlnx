9ed33c7bac7c800b232ce5e7e9937813b0339990
Brian Geffon <bgeffon@google.com>
2025-02-26 11:23:41 -0500

    mm: fix finish_fault() handling for large folios
    commit 34b82f33cf3f03bc39e9a205a913d790e1520ade upstream.
    
    When handling faults for anon shmem finish_fault() will attempt to install
    ptes for the entire folio.  Unfortunately if it encounters a single
    non-pte_none entry in that range it will bail, even if the pte that
    triggered the fault is still pte_none.  When this situation happens the
    fault will be retried endlessly never making forward progress.
    
    This patch fixes this behavior and if it detects that a pte in the range
    is not pte_none it will fall back to setting a single pte.
    
    [bgeffon@google.com: tweak whitespace]
      Link: https://lkml.kernel.org/r/20250227133236.1296853-1-bgeffon@google.com
    Link: https://lkml.kernel.org/r/20250226162341.915535-1-bgeffon@google.com
    Fixes: 43e027e41423 ("mm: memory: extend finish_fault() to support large folio")
    Signed-off-by: Brian Geffon <bgeffon@google.com>
    Suggested-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reported-by: Marek Maslanka <mmaslanka@google.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickens <hughd@google.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Matthew Wilcow (Oracle) <willy@infradead.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -5079,7 +5079,11 @@ vm_fault_t finish_fault(struct vm_fault *vmf)
 	bool is_cow = (vmf->flags & FAULT_FLAG_WRITE) &&
 		      !(vma->vm_flags & VM_SHARED);
 	int type, nr_pages;
-	unsigned long addr = vmf->address;
+	unsigned long addr;
+	bool needs_fallback = false;
+
+fallback:
+	addr = vmf->address;
 
 	/* Did we COW the page? */
 	if (is_cow)
@@ -5118,7 +5122,8 @@ vm_fault_t finish_fault(struct vm_fault *vmf)
 	 * approach also applies to non-anonymous-shmem faults to avoid
 	 * inflating the RSS of the process.
 	 */
-	if (!vma_is_anon_shmem(vma) || unlikely(userfaultfd_armed(vma))) {
+	if (!vma_is_anon_shmem(vma) || unlikely(userfaultfd_armed(vma)) ||
+	    unlikely(needs_fallback)) {
 		nr_pages = 1;
 	} else if (nr_pages > 1) {
 		pgoff_t idx = folio_page_idx(folio, page);
@@ -5154,9 +5159,9 @@ vm_fault_t finish_fault(struct vm_fault *vmf)
 		ret = VM_FAULT_NOPAGE;
 		goto unlock;
 	} else if (nr_pages > 1 && !pte_range_none(vmf->pte, nr_pages)) {
-		update_mmu_tlb_range(vma, addr, vmf->pte, nr_pages);
-		ret = VM_FAULT_NOPAGE;
-		goto unlock;
+		needs_fallback = true;
+		pte_unmap_unlock(vmf->pte, vmf->ptl);
+		goto fallback;
 	}
 
 	folio_ref_add(folio, nr_pages - 1);
