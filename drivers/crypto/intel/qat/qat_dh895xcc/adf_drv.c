d765505bca3cf20e5edd46c06d38912515b01712
Giovanni Cabiddu <giovanni.cabiddu@intel.com>
2025-03-26 15:59:49 +0000

    crypto: qat - add shutdown handler to qat_dh895xcc
    commit 2c4e8b228733bfbcaf49408fdf94d220f6eb78fc upstream.
    
    During a warm reset via kexec, the system bypasses the driver removal
    sequence, meaning that the remove() callback is not invoked.
    If a QAT device is not shutdown properly, the device driver will fail to
    load in a newly rebooted kernel.
    
    This might result in output like the following after the kexec reboot:
    
        QAT: AE0 is inactive!!
        QAT: failed to get device out of reset
        dh895xcc 0000:3f:00.0: qat_hal_clr_reset error
        dh895xcc 0000:3f:00.0: Failed to init the AEs
        dh895xcc 0000:3f:00.0: Failed to initialise Acceleration Engine
        dh895xcc 0000:3f:00.0: Resetting device qat_dev0
        dh895xcc 0000:3f:00.0: probe with driver dh895xcc failed with error -14
    
    Implement the shutdown() handler that hooks into the reboot notifier
    list. This brings down the QAT device and ensures it is shut down
    properly.
    
    Cc: <stable@vger.kernel.org>
    Fixes: 7afa232e76ce ("crypto: qat - Intel(R) QAT DH895xcc accelerator")
    Reviewed-by: Ahsan Atta <ahsan.atta@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/crypto/intel/qat/qat_dh895xcc/adf_drv.c
+++ drivers/crypto/intel/qat/qat_dh895xcc/adf_drv.c
@@ -19,6 +19,13 @@
 #include <adf_dbgfs.h>
 #include "adf_dh895xcc_hw_data.h"
 
+static void adf_shutdown(struct pci_dev *pdev)
+{
+	struct adf_accel_dev *accel_dev = adf_devmgr_pci_to_accel_dev(pdev);
+
+	adf_dev_down(accel_dev);
+}
+
 static const struct pci_device_id adf_pci_tbl[] = {
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_QAT_DH895XCC), },
 	{ }
@@ -33,6 +40,7 @@ static struct pci_driver adf_driver = {
 	.name = ADF_DH895XCC_DEVICE_NAME,
 	.probe = adf_probe,
 	.remove = adf_remove,
+	.shutdown = adf_shutdown,
 	.sriov_configure = adf_sriov_configure,
 	.err_handler = &adf_err_handler,
 };
