4c1d09f3ea9f7e1f1eade3fbb1662a10fa3935f4
Naman Trivedi <naman.trivedimanojbhai@amd.com>
2025-04-03 01:11:25 +0530

    firmware: xilinx: add AIE2PS IOCTL support
    Add AIE2PS IOCTL support for Versal Gen 2. Also define enum for AIE
    operation values instead of macros to accommodate more values in 32 bits.
    
    Co-developed-by: Gregory Williams <gregory.williams@amd.com>
    Signed-off-by: Gregory Williams <gregory.williams@amd.com>
    Co-developed-by: Keerthanna Mohan <keerthanna.mohan@amd.com>
    Signed-off-by: Keerthanna Mohan <keerthanna.mohan@amd.com>
    Signed-off-by: Naman Trivedi <naman.trivedimanojbhai@amd.com>
    Signed-off-by: Govindarajulu Varadarajan <govindarajulu.varadarajan@amd.com>
    Acked-by: Bill Lin <bill.lin@amd.com>
    Message-ID: <20250402194204.1402233-4-govindarajulu.varadarajan@amd.com>

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -121,16 +121,34 @@ enum pm_module_id {
 };
 
 /* AIE Operation */
-#define XILINX_AIE_OPS_COL_RST				BIT(0)
-#define XILINX_AIE_OPS_SHIM_RST				BIT(1)
-#define XILINX_AIE_OPS_ENB_COL_CLK_BUFF			BIT(2)
-#define XILINX_AIE_OPS_ZEROISATION			BIT(3)
-#define XILINX_AIE_OPS_DIS_COL_CLK_BUFF			BIT(4)
-#define XILINX_AIE_OPS_ENB_AXI_MM_ERR_EVENT		BIT(5)
-#define XILINX_AIE_OPS_SET_L2_CTRL_NPI_INTR		BIT(6)
-#define XILINX_AIE_OPS_DATA_MEM_ZEROIZATION		BIT(8U)
-#define XILINX_AIE_OPS_MEM_TILE_ZEROIZATION		BIT(9U)
-
+enum pm_aie_operations {
+	XILINX_AIE_OPS_MIN = 0U,
+	XILINX_AIE_OPS_COL_RST = 1U,
+	XILINX_AIE_OPS_SHIM_RST = 2U,
+	XILINX_AIE_OPS_UC_ZEROIZATION = 3U,
+	XILINX_AIE_OPS_ENB_COL_CLK_BUFF = 4U,
+	XILINX_AIE_OPS_HANDSHAKE = 5U,
+	XILINX_AIE_OPS_CLR_HW_ERR_STS = 6U,
+	XILINX_AIE_OPS_START_NUM_COL = 7U,
+	XILINX_AIE_OPS_ZEROISATION = 8U,
+	XILINX_AIE_OPS_AXIMM_ISOLATION = 9U,
+	XILINX_AIE_OPS_NMU_CONFIG = 10U,
+	XILINX_AIE_OPS_DIS_MEM_PRIV = 11U,
+	XILINX_AIE_OPS_DIS_MEM_INTERLEAVE = 12U,
+	XILINX_AIE_OPS_ENB_UC_DMA_PAUSE = 13U,
+	XILINX_AIE_OPS_ENB_NOC_DMA_PAUSE = 14U,
+	XILINX_AIE_OPS_SET_ECC_SCRUB_PERIOD = 15U,
+	XILINX_AIE_OPS_DIS_COL_CLK_BUFF = 16U,
+	XILINX_AIE_OPS_HW_ERR_INT = 17U,
+	XILINX_AIE_OPS_HW_ERR_MASK = 18U,
+	XILINX_AIE_OPS_ENB_MEM_PRIV = 19U,
+	XILINX_AIE_OPS_ENB_AXI_MM_ERR_EVENT = 32U,     /* Backward compatibility for AIE1/AIE2 */
+	XILINX_AIE_OPS_SET_L2_CTRL_NPI_INTR = 64U,     /* Backward compatibility for AIE1/AIE2 */
+	XILINX_AIE_OPS_PROG_MEM_ZEROIZATION = 128U,    /* Backward compatibility for AIE1/AIE2 */
+	XILINX_AIE_OPS_DATA_MEM_ZEROIZATION = 256U,    /* Backward compatibility for AIE1/AIE2 */
+	XILINX_AIE_OPS_MEM_TILE_ZEROIZATION = 512U,    /* Backward compatibility for AIE1/AIE2 */
+	XILINX_AIE_OPS_MAX = 1023U,                    /* Backward compatibility for AIE1/AIE2 */
+};
 
 enum pm_api_cb_id {
 	PM_INIT_SUSPEND_CB = 30,
@@ -239,6 +257,8 @@ enum pm_ioctl_id {
 	IOCTL_AIE_OPS = 33,
 	/* IOCTL to get default/current QoS */
 	IOCTL_GET_QOS = 34,
+	/* AIE2PS Operations */
+	IOCTL_AIE2PS_OPS = 39,
 };
 
 enum pm_query_id {
@@ -659,6 +679,7 @@ int zynqmp_pm_set_gem_config(u32 node, enum pm_gem_config_type config,
 int zynqmp_pm_get_last_reset_reason(u32 *reset_reason);
 int zynqmp_pm_aie_operation(u32 node, u16 start_col, u16 num_col, u32 operation);
 int zynqmp_pm_get_qos(u32 node, u32 *const def_qos, u32 *const qos);
+int versal2_pm_aie2ps_operation(u32 node, u32 size, u32 addr_high, u32 addr_low);
 #else
 static inline int zynqmp_pm_get_api_version(u32 *version)
 {
@@ -990,6 +1011,12 @@ static inline int zynqmp_pm_get_last_reset_reason(u32 *reset_reason)
 
 static inline int zynqmp_pm_aie_operation(u32 node, u16 start_col,
 					  u16 num_col, u32 operation)
+{
+	return -ENODEV;
+}
+
+static inline int versal2_pm_aie2ps_operation(u32 node, u32 size, u32 addr_high,
+					      u32 addr_low)
 {
 	return -ENODEV;
 }
