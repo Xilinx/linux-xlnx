62d458d8ec69403c438e65ae26efd0faf5ce23d6
Govindarajulu Varadarajan <govindarajulu.varadarajan@amd.com>
2025-04-03 01:11:35 +0530

    xilinx-ai-engine: Add clear context for aie2ps
    Add aie2ps_part_clear_context() for clear context for aie2ps. This adds
    same functionality as aie_part_clear_context() but uses new zynq pm
    opts. In addition to aie1 and aie2, this adds UC zeroization.
    
    Co-developed-by: Gregory Williams <gregory.williams@amd.com>
    Signed-off-by: Gregory Williams <gregory.williams@amd.com>
    Co-developed-by: Keerthanna Mohan <keerthanna.mohan@amd.com>
    Signed-off-by: Keerthanna Mohan <keerthanna.mohan@amd.com>
    Signed-off-by: Govindarajulu Varadarajan <govindarajulu.varadarajan@amd.com>
    Acked-by: Bill Lin <bill.lin@amd.com>
    Message-ID: <20250402194204.1402233-14-govindarajulu.varadarajan@amd.com>
    State: pending

--- drivers/misc/xilinx-ai-engine/ai-engine-internal.h
+++ drivers/misc/xilinx-ai-engine/ai-engine-internal.h
@@ -1715,6 +1715,7 @@ int aie_part_pm_ops_flush(struct aie_partition *apart);
 int aie_partition_initialize(struct device *dev, struct aie_partition_init_args *args);
 int aie2ps_part_initialize(struct aie_partition *apart, struct aie_partition_init_args *args);
 int aie2ps_part_teardown(struct aie_partition *apart);
+int aie2ps_part_clear_context(struct aie_partition *apart);
 int aie_partition_teardown(struct device *dev);
 int aie_part_maskpoll_register(struct aie_partition *apart, u32 offset, u32 data, u32 mask,
 			       u32 timeout);
