24ede35eb2ab42814ba6b564f1b565b21f7ee8a8
Hannes Reinecke <hare@kernel.org>
2025-04-03 09:19:29 +0200

    nvme: requeue namespace scan on missed AENs
    [ Upstream commit 9546ad1a9bda7362492114f5866b95b0ac4a100e ]
    
    Scanning for namespaces can take some time, so if the target is
    reconfigured while the scan is running we may miss a Attached Namespace
    Attribute Changed AEN.
    
    Check if the NVME_AER_NOTICE_NS_CHANGED bit is set once the scan has
    finished, and requeue scanning to pick up any missed change.
    
    Signed-off-by: Hannes Reinecke <hare@kernel.org>
    Reviewed-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -4273,6 +4273,10 @@ static void nvme_scan_work(struct work_struct *work)
 			nvme_scan_ns_sequential(ctrl);
 	}
 	mutex_unlock(&ctrl->scan_lock);
+
+	/* Requeue if we have missed AENs */
+	if (test_bit(NVME_AER_NOTICE_NS_CHANGED, &ctrl->events))
+		nvme_queue_scan(ctrl);
 }
 
 /*
