671dd1fb8769f97a2ae4ce4655a7aecc66634eca
Tao Chen <chen.dylane@linux.dev>
2025-04-07 11:57:51 +0800

    bpf: Check link_create.flags parameter for multi_kprobe
    [ Upstream commit 243911982aa9faf4361aa952f879331ad66933fe ]
    
    The link_create.flags are currently not used for multi-kprobes, so return
    -EINVAL if it is set, same as for other attach APIs.
    
    We allow target_fd, on the other hand, to have an arbitrary value for
    multi-kprobe, as there are existing users (libbpf) relying on this.
    
    Fixes: 0dcac2725406 ("bpf: Add multi kprobe link")
    Signed-off-by: Tao Chen <chen.dylane@linux.dev>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20250407035752.1108927-1-chen.dylane@linux.dev
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2932,6 +2932,9 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	if (sizeof(u64) != sizeof(void *))
 		return -EOPNOTSUPP;
 
+	if (attr->link_create.flags)
+		return -EINVAL;
+
 	if (!is_kprobe_multi(prog))
 		return -EINVAL;
 
