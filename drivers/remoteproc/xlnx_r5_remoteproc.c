02dabe094e61164ace4218f7cc62de6c680c7eed
Tanmay Shah <tanmay.shah@amd.com>
2025-04-09 03:35:54 +0530

    Revert "remoteproc: xilinx: Fix mbox check to look at the child"
    mailboxes are setup using mbox-names property hence should check for
    mbox-names property instead.
    This reverts commit bc78ec96ffc910f88a0eb11395f2add5a70ccf92.
    
    Signed-off-by: Tanmay Shah <tanmay.shah@amd.com>
    Reviewed-by: Ben Levinsky <ben.levinsky@amd.com>
    Message-ID: <20250408220556.42291-2-tanmay.shah@amd.com>

--- drivers/remoteproc/xlnx_r5_remoteproc.c
+++ drivers/remoteproc/xlnx_r5_remoteproc.c
@@ -1373,7 +1373,7 @@ static int zynqmp_r5_cluster_init(struct zynqmp_r5_cluster *cluster)
 		 * If mailbox nodes are disabled using "status" property then
 		 * setting up mailbox channels will fail.
 		 */
-		if (of_property_present(child, "mboxes")) {
+		if (of_property_present(dev_node, "mboxes")) {
 			ipi = zynqmp_r5_setup_mbox(&child_pdev->dev);
 			if (ipi) {
 				r5_cores[i]->ipi = ipi;
