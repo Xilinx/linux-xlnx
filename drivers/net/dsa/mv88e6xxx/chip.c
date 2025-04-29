7dd0c1b86e6b51a7c356a9a07de150205f2bbd51
Marek Behún <kabel@kernel.org>
2025-04-29 12:08:16 +0200

    net: dsa: mv88e6xxx: enable PVT for 6321 switch
    commit f85c69369854a43af2c5d3b3896da0908d713133 upstream.
    
    Commit f36456522168 ("net: dsa: mv88e6xxx: move PVT description in
    info") did not enable PVT for 6321 switch. Fix it.
    
    Fixes: f36456522168 ("net: dsa: mv88e6xxx: move PVT description in info")
    Signed-off-by: Marek Behún <kabel@kernel.org>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Link: https://patch.msgid.link/20250317173250.28780-4-kabel@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -6289,6 +6289,7 @@ static const struct mv88e6xxx_info mv88e6xxx_table[] = {
 		.g1_irqs = 8,
 		.g2_irqs = 10,
 		.atu_move_port_mask = 0xf,
+		.pvt = true,
 		.multi_chip = true,
 		.edsa_support = MV88E6XXX_EDSA_SUPPORTED,
 		.ptp_support = true,
