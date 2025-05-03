15335117c5d73a0cc0404056c94cc8b6a371e25a
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2025-05-03 09:46:31 -0700

    x86/ibt: Keep IBT disabled during alternative patching
    commit ebebe30794d38c51f71fe4951ba6af4159d9837d upstream.
    
    cfi_rewrite_callers() updates the fineIBT hash matching at the caller side,
    but except for paranoid-mode it relies on apply_retpoline() and friends for
    any ENDBR relocation. This could temporarily cause an indirect branch to
    land on a poisoned ENDBR.
    
    For instance, with para-virtualization enabled, a simple wrmsrl() could
    have an indirect branch pointing to native_write_msr() who's ENDBR has been
    relocated due to fineIBT:
    
    <wrmsrl>:
           push   %rbp
           mov    %rsp,%rbp
           mov    %esi,%eax
           mov    %rsi,%rdx
           shr    $0x20,%rdx
           mov    %edi,%edi
           mov    %rax,%rsi
           call   *0x21e65d0(%rip)        # <pv_ops+0xb8>
           ^^^^^^^^^^^^^^^^^^^^^^^
    
    Such an indirect call during the alternative patching could #CP if the
    caller is not *yet* adjusted for the new target ENDBR. To prevent a false
     #CP, keep CET-IBT disabled until all callers are patched.
    
    Patching during the module load does not need to be guarded by IBT-disable
    because the module code is not executed until the patching is complete.
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -31,6 +31,7 @@
 #include <asm/paravirt.h>
 #include <asm/asm-prototypes.h>
 #include <asm/cfi.h>
+#include <asm/ibt.h>
 
 int __read_mostly alternatives_patched;
 
@@ -1719,6 +1720,8 @@ static noinline void __init alt_reloc_selftest(void)
 
 void __init alternative_instructions(void)
 {
+	u64 ibt;
+
 	int3_selftest();
 
 	/*
@@ -1745,6 +1748,9 @@ void __init alternative_instructions(void)
 	 */
 	paravirt_set_cap();
 
+	/* Keep CET-IBT disabled until caller/callee are patched */
+	ibt = ibt_save(/*disable*/ true);
+
 	__apply_fineibt(__retpoline_sites, __retpoline_sites_end,
 			__cfi_sites, __cfi_sites_end, true);
 
@@ -1767,6 +1773,8 @@ void __init alternative_instructions(void)
 	 * Seal all functions that do not have their address taken.
 	 */
 	apply_seal_endbr(__ibt_endbr_seal, __ibt_endbr_seal_end);
+
+	ibt_restore(ibt);
 
 #ifdef CONFIG_SMP
 	/* Patch to UP if other cpus not imminent. */
