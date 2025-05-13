bb85c3abbfd89198324752a4c2c67d2b7db6ab79
Eric Biggers <ebiggers@google.com>
2025-05-12 19:58:39 -0700

    x86/its: Fix build errors when CONFIG_MODULES=n
    commit 9f35e33144ae5377d6a8de86dd3bd4d995c6ac65 upstream.
    
    Fix several build errors when CONFIG_MODULES=n, including the following:
    
    ../arch/x86/kernel/alternative.c:195:25: error: incomplete definition of type 'struct module'
      195 |         for (int i = 0; i < mod->its_num_pages; i++) {
    
    Fixes: 872df34d7c51 ("x86/its: Use dynamic thunks for indirect branches")
    Cc: stable@vger.kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Acked-by: Dave Hansen <dave.hansen@intel.com>
    Tested-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -129,7 +129,9 @@ const unsigned char * const x86_nops[ASM_NOP_MAX+1] =
 
 #ifdef CONFIG_MITIGATION_ITS
 
+#ifdef CONFIG_MODULES
 static struct module *its_mod;
+#endif
 static void *its_page;
 static unsigned int its_offset;
 
@@ -150,6 +152,7 @@ static void *its_init_thunk(void *thunk, int reg)
 	return thunk;
 }
 
+#ifdef CONFIG_MODULES
 void its_init_mod(struct module *mod)
 {
 	if (!cpu_feature_enabled(X86_FEATURE_INDIRECT_THUNK_ITS))
@@ -188,6 +191,7 @@ void its_free_mod(struct module *mod)
 	}
 	kfree(mod->its_page_array);
 }
+#endif /* CONFIG_MODULES */
 
 static void *its_alloc(void)
 {
@@ -196,6 +200,7 @@ static void *its_alloc(void)
 	if (!page)
 		return NULL;
 
+#ifdef CONFIG_MODULES
 	if (its_mod) {
 		void *tmp = krealloc(its_mod->its_page_array,
 				     (its_mod->its_num_pages+1) * sizeof(void *),
@@ -206,6 +211,7 @@ static void *its_alloc(void)
 		its_mod->its_page_array = tmp;
 		its_mod->its_page_array[its_mod->its_num_pages++] = page;
 	}
+#endif /* CONFIG_MODULES */
 
 	return no_free_ptr(page);
 }
