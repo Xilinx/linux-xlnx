141b3d4d077987c7f07ee812a841ff0879b71853
Nagadheeraj Rottela <nagadheeraj.rottela@amd.com>
2025-07-08 16:56:31 +0530

    staging: xilinx-tsn: fix TADMA stream lookup function
    All TADMA streams are tracked using a hash table, which is checked for
    duplicate stream IDs whenever a new stream is added. If multiple streams
    end up in the same hash bucket, a lookup function compares their keys to
    find a match. The lookup didn't consider the last two bytes representing
    the VLAN ID causing the streams with different VLAN IDs but the same MAC
    address to be considered as identical. As a result, only one SFM entry
    was programmed, while others were skipped. Fix the lookup function to
    include the VLAN ID, ensuring all relevant SFM entries are properly
    programmed.
    
    Signed-off-by: Nagadheeraj Rottela <nagadheeraj.rottela@amd.com>
    Reviewed-by: Harini Katakam <harini.katakam@amd.com>
    Message-ID: <20250708112633.3701568-4-nagadheeraj.rottela@amd.com>
    State: pending

--- drivers/staging/xilinx-tsn/xilinx_tsn_tadma.c
+++ drivers/staging/xilinx-tsn/xilinx_tsn_tadma.c
@@ -95,7 +95,7 @@ static inline bool mac_vlan_equal(const u8 addr1[8],
 	bool ret = 1;
 
 	if ((a[0] ^ b[0]) | (a[1] ^ b[1]) |
-		(a[2] ^ b[2]) | (a[3] ^ a[3])) {
+		(a[2] ^ b[2]) | (a[3] ^ b[3])) {
 		ret = 0;
 	}
 
