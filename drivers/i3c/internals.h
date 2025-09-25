f77c0feb2534d68a44724628b1444a2d40a20bc8
Manikanta Guntupalli <manikanta.guntupalli@amd.com>
2025-09-25 21:20:04 +0530

    i3c: master: Add endianness support for i3c_readl_fifo() and i3c_writel_fifo()
    Add endianness handling to the FIFO access helpers i3c_readl_fifo() and
    i3c_writel_fifo(). This ensures correct data transfers on platforms where
    the FIFO registers are expected to be accessed in either big-endian or
    little-endian format.
    
    Update the Synopsys, and Cadence I3C master controller drivers to
    pass the appropriate endianness argument to these helpers.
    
    Signed-off-by: Manikanta Guntupalli <manikanta.guntupalli@amd.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://lore.kernel.org/all/20250923154551.2112388-4-manikanta.guntupalli@amd.com/
    State: waiting
    Message-ID: <20250925155006.3080413-8-manikanta.guntupalli@amd.com>

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -24,21 +24,35 @@ int i3c_dev_request_ibi_locked(struct i3c_dev_desc *dev,
 			       const struct i3c_ibi_setup *req);
 void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev);
 
+enum i3c_fifo_endian {
+	I3C_FIFO_LITTLE_ENDIAN,
+	I3C_FIFO_BIG_ENDIAN,
+};
+
 /**
  * i3c_writel_fifo - Write data buffer to 32bit FIFO
  * @addr: FIFO Address to write to
  * @buf: Pointer to the data bytes to write
  * @nbytes: Number of bytes to write
+ * @endian: Endianness of FIFO write
  */
 static inline void i3c_writel_fifo(void __iomem *addr, const void *buf,
-				   int nbytes)
+				   int nbytes, enum i3c_fifo_endian endian)
 {
-	writesl(addr, buf, nbytes / 4);
+	if (endian)
+		writesl_be(addr, buf, nbytes / 4);
+	else
+		writesl(addr, buf, nbytes / 4);
+
 	if (nbytes & 3) {
 		u32 tmp = 0;
 
 		memcpy(&tmp, buf + (nbytes & ~3), nbytes & 3);
-		writel(tmp, addr);
+
+		if (endian)
+			writel_be(tmp, addr);
+		else
+			writel(tmp, addr);
 	}
 }
 
@@ -47,15 +61,24 @@ static inline void i3c_writel_fifo(void __iomem *addr, const void *buf,
  * @addr: FIFO Address to read from
  * @buf: Pointer to the buffer to store read bytes
  * @nbytes: Number of bytes to read
+ * @endian: Endianness of FIFO read
  */
 static inline void i3c_readl_fifo(const void __iomem *addr, void *buf,
-				  int nbytes)
+				  int nbytes, enum i3c_fifo_endian endian)
 {
-	readsl(addr, buf, nbytes / 4);
+	if (endian)
+		readsl_be(addr, buf, nbytes / 4);
+	else
+		readsl(addr, buf, nbytes / 4);
+
 	if (nbytes & 3) {
 		u32 tmp;
 
-		tmp = readl(addr);
+		if (endian)
+			tmp = readl_be(addr);
+		else
+			tmp = readl(addr);
+
 		memcpy(buf + (nbytes & ~3), &tmp, nbytes & 3);
 	}
 }
