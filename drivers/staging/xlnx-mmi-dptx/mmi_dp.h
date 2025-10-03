1bfd1cd3c2a9f2327edb1c6da3de1398898884e4
Rohit Visavalia <rohit.visavalia@amd.com>
2025-10-03 08:27:38 -0700

    staging: mmi-dptx: Update max link rate to HBR3
    Make HBR3 as max link rate which is supported by MMI DPTx IP.
    
    Signed-off-by: Rohit Visavalia <rohit.visavalia@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>
    Message-ID: <20251003152739.198952-3-vishal.sagar@amd.com>
    State: pending

--- drivers/staging/xlnx-mmi-dptx/mmi_dp.h
+++ drivers/staging/xlnx-mmi-dptx/mmi_dp.h
@@ -26,7 +26,7 @@
 struct dptx;
 
 /* The max rate and lanes supported by the core */
-#define DPTX_MAX_LINK_RATE		DPTX_PHYIF_CTRL_RATE_HBR
+#define DPTX_MAX_LINK_RATE		DPTX_PHYIF_CTRL_RATE_HBR3
 #define DPTX_MAX_LINK_LANES		2
 
 /* The default rate and lanes to use for link training */
