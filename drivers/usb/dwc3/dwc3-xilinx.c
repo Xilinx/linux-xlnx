b6880210705a98b225525923177cf27f5b7e1584
Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
2025-10-09 12:48:09 +0530

    usb: dwc3: xilinx: add support to program MMI USB TX deemphasis
    Introduces support for programming the 18-bit TX Deemphasis value that
    drives the pipe_TxDeemph signal, as defined in the PIPE4 specification.
    
    The configured value is recommended by Synopsys and is intended for
    standard (non-compliance) operation. These Gen2 equalization settings
    have been validated through both internal and external compliance
    testing. By applying this setting, the stability of USB 3.2 enumeration
    is improved and now SuperSpeedPlus devices are consistently recognized as
    USB 3.2 Gen 2 by the MMI USB Host controller.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@amd.com>
    Message-ID: <20251009071811.1574563-2-radhey.shyam.pandey@amd.com>

--- drivers/usb/dwc3/dwc3-xilinx.c
+++ drivers/usb/dwc3/dwc3-xilinx.c
@@ -66,6 +66,8 @@
 #define XLNX_USB_FPD_POWER_PRSNT		0x80
 #define FPD_POWER_PRSNT_OPTION			BIT(0)
 
+#define XLNX_MMI_USB_TX_DEEMPH_DEF		0x8c45
+
 enum dwc3_xlnx_core_state {
 	UNKNOWN_STATE = 0,
 	D0_STATE,
@@ -704,6 +706,10 @@ static int dwc3_set_swnode(struct device *dev)
 	if (of_dma_is_coherent(dwc3_np))
 		props[prop_idx++] = PROPERTY_ENTRY_U16("snps,gsbuscfg0-reqinfo",
 						       0xffff);
+	if (of_device_is_compatible(np, "xlnx,versal2-mmi-dwc3"))
+		props[prop_idx++] = PROPERTY_ENTRY_U32("snps,lcsr_tx_deemph",
+						       XLNX_MMI_USB_TX_DEEMPH_DEF);
+
 	of_node_put(dwc3_np);
 
 	if (prop_idx)
