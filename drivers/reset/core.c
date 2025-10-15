25d4d4604d01eb0ce5254f0516826127dacb5015
Wolfram Sang <wsa+renesas@sang-engineering.com>
2025-10-15 22:59:21 +0200

    reset: always bail out on missing RESET_GPIO driver
    Optional GPIOs mean they can be omitted. If they are described, a
    failure in acquiring them still needs to be reported. When the
    RESET_GPIO is not enabled (so the reset core cannot provide its assumed
    fallback), the user should be informed about it. So, not only bail out
    but also give a hint how to fix the situation. This means the check has
    to be moved after ensuring the GPIO is really described.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Tested-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/reset/core.c
+++ drivers/reset/core.c
@@ -1028,9 +1028,6 @@ __of_reset_control_get(struct device_node *node, const char *id, int index,
 	if (ret == -EINVAL)
 		return ERR_PTR(ret);
 	if (ret) {
-		if (!IS_ENABLED(CONFIG_RESET_GPIO))
-			return optional ? NULL : ERR_PTR(ret);
-
 		/*
 		 * There can be only one reset-gpio for regular devices, so
 		 * don't bother with the "reset-gpios" phandle index.
@@ -1039,6 +1036,11 @@ __of_reset_control_get(struct device_node *node, const char *id, int index,
 						 0, &args);
 		if (ret)
 			return optional ? NULL : ERR_PTR(ret);
+
+		if (!IS_ENABLED(CONFIG_RESET_GPIO)) {
+			pr_err("%s(): RESET_GPIO driver not enabled, cannot fall back\n", __func__);
+			return ERR_PTR(-ENOEXEC);
+		}
 
 		gpio_fallback = true;
 
