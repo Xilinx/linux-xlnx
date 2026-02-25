dc99f92ae6af04b1a04a7a09d9c9ad857f41e793
Anatoliy Klymenko <anatoliy.klymenko@amd.com>
2026-02-25 12:18:03 -0800

    drm: xlnx: mmi-dc: Factor out DRM bridge
    Factor out DRM bridge from the planector. We may want to reuse DRM bridge
    interface for the bypass MMI DC mode. In this mode up to 4 live video
    sources may be connected to the controller. This change lays a ground work
    for the upcoming MMI DC bypass mode support.
    
    Change bridges allocation policy to avoid warnings in 6.18 kernel.
    
    Signed-off-by: Anatoliy Klymenko <anatoliy.klymenko@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>
    Message-ID: <20260225201811.3292059-2-anatoliy.klymenko@amd.com>
    State: pending

---
+++ drivers/gpu/drm/xlnx/mmi_dc_bridge.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Multimedia Integrated Display Controller Bridge Driver
+ *
+ * Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
+ */
+
+#ifndef __MMI_DC_BRIDGE_H__
+#define __MMI_DC_BRIDGE_H__
+
+#include <drm/drm_bridge.h>
+#include <drm/drm_modes.h>
+
+struct mmi_dc_plane;
+
+/**
+ * struct mmi_dc_bridge - DRM bridge wrapper for the MMI Display Controller
+ * @base: DRM core bridge object embedded for registration with the bridge API
+ * @plane: Optional MMI DC plane associated with this bridge instance
+ * @connector_status: Cached connector state reported to the DRM core
+ * @display_mode: Display timings currently programmed on the controller
+ */
+struct mmi_dc_bridge {
+	struct drm_bridge		base;
+	struct mmi_dc_plane		*plane;
+	enum drm_connector_status	connector_status;
+	struct drm_display_mode		display_mode;
+};
+
+struct mmi_dc_bridge *mmi_dc_bridge_init(struct device *dev,
+					 struct mmi_dc_plane *plane);
+void mmi_dc_bridge_connect(struct mmi_dc_bridge *bridge,
+			   const struct drm_display_mode *mode);
+void mmi_dc_bridge_disconnect(struct mmi_dc_bridge *bridge);
+
+#endif /* __MMI_DC_BRIDGE_H__ */
