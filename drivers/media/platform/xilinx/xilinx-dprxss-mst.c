74aba4da7e043482815b7904128740a68798e2e0
Katta Dhanunjanrao <katta.dhanunjanrao@amd.com>
2026-05-11 19:07:51 +0530

    media: xilinx: dprx: add MST register masks, API header and Makefile
    Add the DisplayPort MST-specific register addresses and capability
    bit-masks to the shared hw header. Introduce the driver-private MST
    API header (xilinx-dprxss-mst.h under drivers/media/platform/xilinx/) that
    defines protocol constants (opcode values, sideband message sizes,
    topology limits) and the four function prototypes used by the RXSS
    driver.
    
    Create a skeleton xilinx-dprxss-mst.c that will grow into the MST sideband
    engine across subsequent patches. The skeleton establishes:
      - Three static data blobs (edid[], guid_table[], dpcd[]) used as
        virtual sink capability responses throughout the module. These
        are intentional infrastructure; callers are added in later patches.
    
    Signed-off-by: Katta Dhanunjanrao <katta.dhanunjanrao@amd.com>
    Reviewed-by: Vishal Sagar <vishal.sagar@amd.com>
    Message-ID: <20260511133805.2117330-3-katta.dhanunjanrao@amd.com>
    State: pending

---
+++ drivers/media/platform/xilinx/xilinx-dprxss-mst.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Xilinx DP RX Subsystem MST Driver
+ *
+ * Copyright (C) 2026, Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Author: Lakshmi Prasanna Eachuri <lakshmi.prasanna.eachuri@amd.com>
+ * Author: Katta Dhanunjanrao <katta.dhanunjanrao@amd.com>
+ */
+
+#include <linux/types.h>
+
+#include "xilinx-dprxss-mst.h"
